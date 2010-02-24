01c84b2b641f90091405aa4d19f4565c02cd8802
elmar <elmar@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2010-02-24 09:46:48 +0000

    update version numbers for step/lifter form
    git-svn-id: svn://develop.studip.de/studip/trunk@14661 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/wiki_steps.inc.php
+++ lib/wiki_steps.inc.php
@@ -31,8 +31,8 @@ $step_templates['step'] = array(
 <td><input size=60 name="step_zustaendig"></td></tr>
 <tr><td>Release in Version:</td>
 <td><select size=0 name="step_version">
-<option value="1.10">1.10 (Sep. 2009)</option>
-<option value="1.11">1.11 (März 2010)</option>
+<option value="1.12">1.12 (Sep. 2010)</option>
+<option value="1.13">1.13 (März 2011)</option>
 <option>langfristig</option></td></tr>
 <tr><td>Komplexität:</td>
 <td><select name="step_komplexitaet">

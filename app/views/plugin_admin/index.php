9c532189a7371e16bd54d00cebeb041edeca2448
elmar <elmar@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2010-04-12 14:59:48 +0000

    display message if no plugins are installed, fixes #1052
    git-svn-id: svn://develop.studip.de/studip/trunk@15269 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/plugin_admin/index.php
+++ app/views/plugin_admin/index.php
@@ -21,70 +21,77 @@
     <?= _('Verwaltung von Plugins')?>
 </h3>
 
-<form action="<?= $controller->url_for('plugin_admin/save') ?>" method="post">
-    <input type="hidden" name="ticket" value="<?= get_ticket() ?>">
-    <input type="hidden" name="plugin_filter" value="<?= $plugin_filter ?>">
-    <table class="default">
-        <tr>
-            <th><?= _('Aktiv') ?></th>
-            <th><?= _('Name')?></th>
-            <th><?= _('Typ') ?></th>
-            <th><?= _('Version') ?></th>
-            <th><?= _('Position') ?></th>
-            <th colspan="3"><?= _('Aktionen') ?></th>
-        </tr>
-
-        <? foreach ($plugins as $plugin): ?>
-            <? $pluginid = $plugin['id'] ?>
-            <tr class="<?= TextHelper::cycle('cycle_odd', 'cycle_even') ?>">
-                <td style="padding-left: 1ex;">
-                    <input type="checkbox" name="enabled_<?= $pluginid ?>" value="1" <?= $plugin['enabled'] ? 'checked' : '' ?>>
-                </td>
-                <td>
-                    <a href="<?= $controller->url_for('plugin_admin/manifest', $pluginid) ?>">
-                        <?= htmlspecialchars($plugin['name']) ?>
-                    </a>
-                    <? if (in_array('StandardPlugin', $plugin['type'])): ?>
-                        <a href="<?= $controller->url_for('plugin_admin/default_activation', $pluginid) ?>">
-                            <?= Assets::img('config.png', array('class' => 'middle', 'title' => _('Default-Aktivierung'))) ?>
-                        </a>
-                    <? endif ?>
-                <td <?= $plugin['enabled'] ? '' : 'class="quiet"' ?>>
-                    <?= join(', ', $plugin['type']) ?>
-                </td>
-                <td <?= $plugin['enabled'] ? '' : 'class="quiet"' ?>>
-                    <?= htmlspecialchars($update_info[$pluginid]['version']) ?>
-                </td>
-                <td>
-                    <input name="position_<?= $pluginid ?>" type="text" size="2" value="<?= $plugin['position'] ?>" <?= $plugin['enabled'] ? '' : 'disabled' ?>>
-                </td>
-                <td>
-                    <a href="<?= $controller->url_for('role_admin/assign_plugin_role', $pluginid) ?>">
-                        <?= Assets::img('edit_transparent.gif', array('title' => _('Zugriffsrechte bearbeiten'))) ?>
-                    </a>
-                </td>
-                <td>
-                    <? if (!$plugin['depends']): ?>
-                        <a href="<?= $controller->url_for('plugin_admin/download', $pluginid) ?>">
-                            <?= Assets::img('icon-disc.gif', array('title' => _('Herunterladen'))) ?>
-                        </a>
-                    <? endif ?>
-                </td>
-                <td>
-                    <? if (!$plugin['depends']): ?>
-                        <a href="<?= $controller->url_for('plugin_admin/ask_delete', $pluginid) ?>">
-                            <?= Assets::img('trash.gif', array('title' => _('Deinstallieren'))) ?>
-                        </a>
-                    <? endif ?>
-                </td>
+<? if (count($plugins) == 0): ?>
+    <?= MessageBox::info(_('Es sind noch keine Plugins in diesem Stud.IP vorhanden.'), array(
+            _('Sie können Plugins aus dem Marktplatz installieren oder manuell hochladen.'),
+            sprintf(_('Benutzen Sie dafür die Funktion "%sweitere Plugins installieren%s" in der Info-Box.'),
+                '<a href="'.$controller->url_for('plugin_admin/search').'">', '</a>'))) ?>
+<? else: ?>
+    <form action="<?= $controller->url_for('plugin_admin/save') ?>" method="post">
+        <input type="hidden" name="ticket" value="<?= get_ticket() ?>">
+        <input type="hidden" name="plugin_filter" value="<?= $plugin_filter ?>">
+        <table class="default">
+            <tr>
+                <th><?= _('Aktiv') ?></th>
+                <th><?= _('Name')?></th>
+                <th><?= _('Typ') ?></th>
+                <th><?= _('Version') ?></th>
+                <th><?= _('Position') ?></th>
+                <th colspan="3"><?= _('Aktionen') ?></th>
             </tr>
-        <? endforeach ?>
-    </table>
 
-    <div style="padding-top: 1em; text-align: center;">
-        <?= makeButton('speichern', 'input', _('Einstellungen speichern'), 'save') ?>
-    </div>
-</form>
+            <? foreach ($plugins as $plugin): ?>
+                <? $pluginid = $plugin['id'] ?>
+                <tr class="<?= TextHelper::cycle('cycle_odd', 'cycle_even') ?>">
+                    <td style="padding-left: 1ex;">
+                        <input type="checkbox" name="enabled_<?= $pluginid ?>" value="1" <?= $plugin['enabled'] ? 'checked' : '' ?>>
+                    </td>
+                    <td>
+                        <a href="<?= $controller->url_for('plugin_admin/manifest', $pluginid) ?>">
+                            <?= htmlspecialchars($plugin['name']) ?>
+                        </a>
+                        <? if (in_array('StandardPlugin', $plugin['type'])): ?>
+                            <a href="<?= $controller->url_for('plugin_admin/default_activation', $pluginid) ?>">
+                                <?= Assets::img('config.png', array('class' => 'middle', 'title' => _('Default-Aktivierung'))) ?>
+                            </a>
+                        <? endif ?>
+                    <td <?= $plugin['enabled'] ? '' : 'class="quiet"' ?>>
+                        <?= join(', ', $plugin['type']) ?>
+                    </td>
+                    <td <?= $plugin['enabled'] ? '' : 'class="quiet"' ?>>
+                        <?= htmlspecialchars($update_info[$pluginid]['version']) ?>
+                    </td>
+                    <td>
+                        <input name="position_<?= $pluginid ?>" type="text" size="2" value="<?= $plugin['position'] ?>" <?= $plugin['enabled'] ? '' : 'disabled' ?>>
+                    </td>
+                    <td>
+                        <a href="<?= $controller->url_for('role_admin/assign_plugin_role', $pluginid) ?>">
+                            <?= Assets::img('edit_transparent.gif', array('title' => _('Zugriffsrechte bearbeiten'))) ?>
+                        </a>
+                    </td>
+                    <td>
+                        <? if (!$plugin['depends']): ?>
+                            <a href="<?= $controller->url_for('plugin_admin/download', $pluginid) ?>">
+                                <?= Assets::img('icon-disc.gif', array('title' => _('Herunterladen'))) ?>
+                            </a>
+                        <? endif ?>
+                    </td>
+                    <td>
+                        <? if (!$plugin['depends']): ?>
+                            <a href="<?= $controller->url_for('plugin_admin/ask_delete', $pluginid) ?>">
+                                <?= Assets::img('trash.gif', array('title' => _('Deinstallieren'))) ?>
+                            </a>
+                        <? endif ?>
+                    </td>
+                </tr>
+            <? endforeach ?>
+        </table>
+
+        <div style="padding-top: 1em; text-align: center;">
+            <?= makeButton('speichern', 'input', _('Einstellungen speichern'), 'save') ?>
+        </div>
+    </form>
+<? endif ?>
 
 <?
 $infobox_content = array(

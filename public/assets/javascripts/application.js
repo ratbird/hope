e43ed4059f8678de33311911262bc4a12ab5b39b
mriehe <mriehe@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2010-04-15 15:31:09 +0000

    revert testcommit
    git-svn-id: svn://develop.studip.de/studip/trunk@15351 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/assets/javascripts/application.js
+++ public/assets/javascripts/application.js
@@ -1,12 +1,23 @@
 /*global window, $, $$, $A, $H, $w, Ajax, Class, Draggable, Droppables, Effect, Element, Event, Sortable */
 /*jslint browser: true, white: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, newcap: true, immed: true, indent: 2, onevar: false */
-/*
+/* ------------------------------------------------------------------------
  * application.js
+ * This file is part of Stud.IP - http://www.studip.de
  *
  * Stud.IP is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
+ *
+ * Stud.IP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Stud.IP; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
  */
 
 /* ------------------------------------------------------------------------

b9266a0e3fd78ee32f70ab26136ac1e1835274f0
mriehe <mriehe@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2010-04-22 19:13:36 +0000

    optimize layout for debug , re #985
    git-svn-id: svn://develop.studip.de/studip/trunk@15485 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/shared/debug_standard.php
+++ templates/shared/debug_standard.php
@@ -1,4 +1,4 @@
-<div style="border: 1px solid #000; padding: 5px; margin: 5px;">
+<div style="border: 1px solid #000; padding: 5px; margin: 5px; background-color: #fff; z-index: 100;">
 <? if(!empty($title)): ?><b><?= $title ?>:</b><? endif ?>
 <pre style="font-size: 10px; padding: 0px; margin: 0px;">
 <? print_r($message) ?>

dd74afe8bd9c6119839cde47c6ab6a0a13bfbebb
mlunzena <mlunzena@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2010-05-05 21:20:17 +0000

    fix MIT license, refs #1075
    git-svn-id: svn://develop.studip.de/studip/trunk@15705 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/trails/TrailsInflector.php
+++ lib/trails/TrailsInflector.php
@@ -2,6 +2,8 @@
 /**
  * TrailsInflector.php
  *
+ * Copyright (c) 2010 Marcus Lunzenauer
+ *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without

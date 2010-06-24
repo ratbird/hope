2a90ced83b989482b23d134825fd8ff9cd33a837
tgloeggl <tgloeggl@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2010-06-24 15:41:30 +0000

    convert tabs to spaces
    git-svn-id: svn://develop.studip.de/studip/trunk@16195 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/course/management/inst.php
+++ app/views/course/management/inst.php
@@ -4,7 +4,7 @@
  * * * * * * * * * * * * */
 $infobox['picture'] = 'infoboxes/administration.jpg';
 $infobox['content'][] = array(
-	'kategorie' => _("Information"),
+    'kategorie' => _("Information"),
     'eintrag'   => array(
         array(
             'text' => _("Als Mitarbeiter Ihrer Einrichtung können Sie für diese Inhalte in mehreren Kategorien bereitstellen. Inhalte in Ihrer Einrichtung können von allen Stud.IP-Nutzern abgerufen werden."),
@@ -40,41 +40,41 @@ $infobox['content'][] = array(
     <h2 class="smashbox_kategorie"><?=_("Weitere Inhaltselemente");?></h2>
 
   <div class="smashbox_stripe">
-	  <div style="margin-left: 1.5em;">
+      <div style="margin-left: 1.5em;">
 
-		  <a class="click_me" href="<?= UrlHelper::getLink('admin_news.php?section=news') ?>">
-		  <div>
-			  <span class="click_head"><?=_("Ankündigungen");?></span>
-			  <p><?=_("Erstellen Sie Ankündigungen (News) für ihre Einrichtung und bearbeiten Sie laufende Ankündigungen.");?></p>
-			  </div>
-		  </a>
+          <a class="click_me" href="<?= UrlHelper::getLink('admin_news.php?section=news') ?>">
+          <div>
+              <span class="click_head"><?=_("Ankündigungen");?></span>
+              <p><?=_("Erstellen Sie Ankündigungen (News) für ihre Einrichtung und bearbeiten Sie laufende Ankündigungen.");?></p>
+              </div>
+          </a>
 
           <? if (get_config('VOTE_ENABLE')) : ?>
-		  <a class="click_me" href="<?= UrlHelper::getLink('admin_vote.php?section=votings') ?>">
-		  <div>
-			  <span class="click_head"><?=_("Umfragen und Tests");?></span>
-			  <p><?=_("Erstellen Sie in Ihre Einrichtung einfache Umfragen und Tests.");?></p>
-			  </div>
-		  </a>
+          <a class="click_me" href="<?= UrlHelper::getLink('admin_vote.php?section=votings') ?>">
+          <div>
+              <span class="click_head"><?=_("Umfragen und Tests");?></span>
+              <p><?=_("Erstellen Sie in Ihre Einrichtung einfache Umfragen und Tests.");?></p>
+              </div>
+          </a>
 
          <a class="click_me" href="<?= UrlHelper::getLink('admin_evaluation.php?section=evaluation') ?>">
             <div>
-    			  <span class="click_head"><?=_("Evaluationen");?></span>
-	    		  <p><?=_("Richten Sie für Ihre Einrichtung eine öffentliche Umfragen ein.");?></p>
-    	    </div>
-		</a>
+                  <span class="click_head"><?=_("Evaluationen");?></span>
+                  <p><?=_("Richten Sie für Ihre Einrichtung eine öffentliche Umfragen ein.");?></p>
+            </div>
+        </a>
         <? endif ?>
 
         <? if (get_config('EXTERN_ENABLE') && $GLOBALS['perm']->have_perm('admin')) : ?>
          <a class="click_me" href="<?= UrlHelper::getLink('admin_extern.php?list=TRUE&view=extern_inst&section=extern') ?>">
             <div>
-    			  <span class="click_head"><?=_("Externe Seiten");?></span>
-	    		  <p><?=_("Verwalten Sie Ihre Einrichtungsswebseiten außerhalb von Stud.IP aktuell und fügen Sie Inhalte aus Stud.IP ein.");?></p>
-    	    </div>
-		</a>
+                  <span class="click_head"><?=_("Externe Seiten");?></span>
+                  <p><?=_("Verwalten Sie Ihre Einrichtungsswebseiten außerhalb von Stud.IP aktuell und fügen Sie Inhalte aus Stud.IP ein.");?></p>
+            </div>
+        </a>
         <? endif ?>
 
-		</div>
-		<br style="clear: both;">
-	</div>
+        </div>
+        <br style="clear: both;">
+    </div>
 </div>

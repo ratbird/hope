7e4c2e996042e35dbf215dc4ed630c14d4fe02cd
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2010-06-25 14:55:55 +0000

    refs #1251 bugfixing
    git-svn-id: svn://develop.studip.de/studip/trunk@16208 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/course/basicdata/_input.php
+++ app/views/course/basicdata/_input.php
@@ -14,7 +14,7 @@ if ($input['type'] === "select") : ?>
     <? if ($input['choices']) : foreach ($input['choices'] as $choice_value => $choice_name) : ?>
         <option value="<?= $choice_value ?>"<? 
             if ($choice_value == $input['value']) print " selected" 
-            ?>><?= $choice_name ?></option>
+            ?>><?= htmlReady($choice_name) ?></option>
     <? endforeach; endif; ?>
     </select>
 <? endif;
@@ -25,7 +25,7 @@ if ($input['type'] === "multiselect") : ?>
         <option value="<?= $choice_value ?>"<?= 
             in_array($choice_value, is_array($input['value']) ? $input['value'] : array($input['value'])) 
             ? " selected" 
-            : "" ?>><?= $choice_name ?></option>
+            : "" ?>><?= htmlReady($choice_name) ?></option>
     <? endforeach; endif; ?>
     </select>
 <? endif;

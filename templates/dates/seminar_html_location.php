df9e3d5346a34a029d203c6b75d112d0a14a43de
tgloeggl <tgloeggl@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2010-07-06 12:33:25 +0000

    fix seminar_html_location-template, refs #1269
    git-svn-id: svn://develop.studip.de/studip/trunk@16269 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/dates/seminar_html_location.php
+++ templates/dates/seminar_html_location.php
@@ -1,23 +1,36 @@
 <?
 // condense regular dates by room
 if (is_array($dates['regular']['turnus_data'])) foreach ($dates['regular']['turnus_data'] as $cycle) :
-  if (is_array($cycle['assigned_rooms'])) foreach ($cycle['assigned_rooms'] as $room_id => $count) : 
-    $resObj =& ResourceObject::Factory($room_id);
-    $output[$resObj->getFormattedLink(TRUE, TRUE, TRUE)][] = $cycle['tostring_short'] .' ('. $count .'x)';
-  endforeach;
+    if (is_array($cycle['assigned_rooms'])) foreach ($cycle['assigned_rooms'] as $room_id => $count) : 
+        $resObj =& ResourceObject::Factory($room_id);
+        $output[$resObj->getFormattedLink(TRUE, TRUE, TRUE)][] = $cycle['tostring_short'] .' ('. $count .'x)';
+    endforeach;
 
-  if (is_array($cycle['freetext_rooms'])) foreach ($cycle['freetext_rooms'] as $room => $count) : 
-    if ($room) : 
-      $output['('. $room .')'][] = $cycle['tostring_short']  .' ('. $count .'x)';
-    endif;
-  endforeach;
-
+    if (is_array($cycle['freetext_rooms'])) foreach ($cycle['freetext_rooms'] as $room => $count) : 
+        if ($room) : 
+            $output['('. $room .')'][] = $cycle['tostring_short']  .' ('. $count .'x)';
+        endif;
+    endforeach;
 endforeach;
 
 
 // condense irregular dates by room
-if (is_array($dates['irregular'])) foreach ($dates['irregular'] as $cycle) :
-  
+if (is_array($dates['irregular'])) foreach ($dates['irregular'] as $date) :
+    if ($date['resource_id']) :
+        $output_dates[$date['resource_id']][] = $date;
+    elseif ($date['raum']) :
+        $output_dates[$date['raum']][] = $date;
+    endif;
+endforeach;
+
+// now shrink the dates for each room/freetext and add them to the output
+if (is_array($output_dates)) foreach ($output_dates as $dates) :
+    if ($dates[0]['resource_id']) :
+        $resObj =& ResourceObject::Factory($dates[0]['resource_id']);
+        $output[$resObj->getFormattedLink(TRUE, TRUE, TRUE)][] = implode(', ', shrink_dates($dates));
+    elseif ($dates[0]['raum']) :
+        $output['('. $dates[0]['raum'] .')'][] = implode(', ', shrink_dates($dates));
+    endif;
 endforeach;
 ?>
 

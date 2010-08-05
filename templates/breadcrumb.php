39243ecaa978f4373e289d3b090b47feb0403b9f
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2010-08-05 13:54:42 +0000

    bugfix htmlready(), re #1299
    git-svn-id: svn://develop.studip.de/studip/trunk@16571 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/breadcrumb.php
+++ templates/breadcrumb.php
@@ -1,13 +1,13 @@
 <div class="breadcrumb print"><?= _("Navigation: ") ?>
   <? $subnavigation = Navigation::getItem('/'); $count = 0; ?>
-  <?= $subnavigation->getTitle(); ?>
+  <?= htmlready($subnavigation->getTitle()); ?>
   <? while ($subnavigation = $subnavigation->activeSubNavigation()) : ?>
     <? if ($subnavigation->isVisible() && $subnavigation->getTitle()) : ?>
       <? if ($count !== 0) print " » " ?>
       <? if ($subnavigation->getTitle() == _("Veranstaltung")) : ?>
-        <?= _("Meine Veranstaltungen") . " » " . $GLOBALS['SessSemName'][0]; ?>
+        <?= htmlready($GLOBALS['SessSemName']['header_line']); ?>
       <? else : ?>
-        <?= $subnavigation->getTitle(); ?>
+        <?= htmlready($subnavigation->getTitle()); ?>
       <? endif; ?>
     <? $count++; endif; ?>
   <? endwhile; ?>

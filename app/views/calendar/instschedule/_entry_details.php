cc116ca273ae9264d9d55834ad463fc81175c31b
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2010-08-06 12:35:43 +0000

    removing cssClassSwitcher, add i18n re #1272
    git-svn-id: svn://develop.studip.de/studip/trunk@16581 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/calendar/instschedule/_entry_details.php
+++ app/views/calendar/instschedule/_entry_details.php
@@ -2,34 +2,31 @@
 
 if (!$show_entry) return;
 
-$cssSw = new CssClassSwitcher();
-
 foreach ($show_entry['seminars'] as $sem_id) {
     $seminars[] = Seminar::getInstance($sem_id);
 }
 ?>
 <div id="edit_inst_entry" class="schedule_edit_entry">
-    <div id="edit_inst_entry_drag" class="window_heading">Liste der Veranstaltungen</div>
+    <div id="edit_inst_entry_drag" class="window_heading"><?=_("Liste der Veranstaltungen")?></div>
     <form action="<?= $controller->url_for('calendar/schedule/editseminar/'. $show_entry['id'] .'/'. $show_entry['cycle_id'] ) ?>" method="post" name="edit_entry" style="padding-left: 10px; padding-top: 10px; margin-right: 10px;">
         <table class="default">
             <thead>
                 <tr>
-                    <th>Nummer</th>
-                    <th>Name</th>
+                    <th><?=_("Nummer")?></th>
+                    <th><?=_("Name")?></th>
                 </tr>
             </thead>
             <tbody>
                 <? foreach ($seminars as $seminar) : ?>
-                    <tr <?= $cssSw->getFullClass() ?>>
-                        <td width="15%"><?= $seminar->getNumber() ?></td>
+                    <tr class="<?= TextHelper::cycle('steelgraulight', 'steel1')?>">
+                        <td width="15%"><?= htmlready($seminar->getNumber()) ?></td>
                         <td width="85%">
-                            <a target="_blank" href="<?= UrlHelper::getLink('details.php?sem_id='. $seminar->getId()) ?>">
+                            <a href="<?= UrlHelper::getLink('details.php?sem_id='. $seminar->getId()) ?>">
                                 <?= Assets::img('link_intern') ?>
-                                <?= $seminar->getName() ?>
+                                <?= htmlready($seminar->getName()) ?>
                             </a>
                         </td>
                     </tr>
-                    <? $cssSw->switchClass() ?>
                 <? endforeach ?>
             </tbody>
         </table>

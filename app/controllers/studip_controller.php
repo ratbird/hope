c8928bc5ae552407205667e9544dded995a77a7c
elmar <elmar@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2010-08-11 12:35:37 +0000

    pass result of url_for() through URLHelper, fixes #1254
    git-svn-id: svn://develop.studip.de/studip/trunk@16634 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/controllers/studip_controller.php
+++ app/controllers/studip_controller.php
@@ -12,6 +12,24 @@
 abstract class StudipController extends Trails_Controller
 {
     /**
+    * Returns a URL to a specified route to your Trails application.
+    *
+    * @param  string   a string containing a controller and optionally an action
+    * @param  strings  optional arguments
+    *
+    * @return string  a URL to this route
+    */
+    function url_for($to/*, ...*/) {
+        $args = func_get_args();
+
+        // calling parent::url_for() is non-trivial in PHP...
+        $parent = new ReflectionMethod('Trails_Controller', 'url_for');
+        $url = $parent->invokeArgs($this, $args);
+
+        return URLHelper::getURL($url);
+    }
+
+    /**
      * Exception handler called when the performance of an action raises an
      * exception.
      *

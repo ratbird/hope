405f145b90599e299b128cee76f495b56db065d8
hackl <hackl@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2010-08-20 19:13:08 +0000

    re #965 Bugfixing, Prettier Code, Default visibility for homepage elements definable by user
    git-svn-id: svn://develop.studip.de/studip/trunk@16756 c5b85792-5220-0410-ad23-ed5b6b8ad999

---
+++ db/migrations/71_step_00158_default_homepage_visibility.php
@@ -0,0 +1,25 @@
+<?php
+// include the visibility functions: we need constants here.
+require_once(realpath(dirname(__FILE__).'/../../lib/user_visible.inc.php'));
+
+class Step00158DefaultHomepageVisibility extends Migration
+{
+    
+    function description()
+    {
+        return 'users get the possibility to define a default setting for new homepage elements';
+    }
+
+    function up()
+    {
+        $db = DBManager::get();
+        $query = $db->execute("ALTER TABLE `user_visibility` ADD `default_homepage_visiblity` INT NOT NULL DEFAULT 0 AFTER `homepage` ");
+    }
+
+    function down()
+    {
+        $db = DBManager::get();
+        $query = $db->prepare("ALTER TABLE user_visibility DROP default_homepage_visibility");
+    }
+}
+?>

1f9630752537f71c4fbbcdffbe86f98150f12eca
aklassen <aklassen@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2010-08-21 19:32:22 +0000

    added comments according to ticket notes refs #1111
    git-svn-id: svn://develop.studip.de/studip/trunk@16816 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/controllers/course/management.php
+++ app/controllers/course/management.php
@@ -1,23 +1,23 @@
 <?php
 
 /*
- * Copyright (C) 2010 - Till Gloeggler <tgloeggl@uos.de>
+ * management.php - realises a redirector for administrative pages
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
+ * 
+ * @author      tgloeggl <tgloeggl@uos.de>
+ * @author      aklassen <andre.klassen@elan-ev.de>
+ * @copyright   2010 ELAN e.V.
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GPL version 2
+ * @category    Stud.IP
+ * @since       1.10
  */
 
 require_once 'app/controllers/authenticated_controller.php';
 
-/**
- * This controller realises a redirector for administrative pages
- *
- * @since 1.10
- * @author tgloeggl
- * @author aklassen
- */
 class Course_ManagementController extends AuthenticatedController
 {
 
@@ -35,6 +35,13 @@ class Course_ManagementController extends AuthenticatedController
         PageLayout::setHelpKeyword('Basis.Veranstaltungsverwaltung');
     }
 
+    /**
+     * shows index page of course management
+     *
+     * @param string section
+     *
+     * @return void
+     */
     function index_action($section = '')
     {
         Navigation::activateItem('course/admin/main');
@@ -48,6 +55,11 @@ class Course_ManagementController extends AuthenticatedController
         $this->is_admin = $GLOBALS['perm']->have_studip_perm('tutor', $_SESSION['SessSemName'][1]);
     }
 
+    /**
+     * shows index page of inst management
+     *
+     * @return void
+     */
     function inst_action()
     {
         Navigation::activateItem('course/admin/main');
@@ -55,6 +67,13 @@ class Course_ManagementController extends AuthenticatedController
 
     }
 
+    /**
+     * sets visibility of a seminar
+     *
+     * @param visible
+     * 
+     * @return void
+     */
     function visible_action($visible)
     {
         $sem = Seminar::getInstance($_SESSION['SessSemName'][1]);

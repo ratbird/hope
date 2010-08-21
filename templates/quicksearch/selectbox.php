6bb1b8c769777b7c6d78bf7ce2adec29e40b4189
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2010-08-21 19:21:37 +0000

    refs #1089
    changing the looking glass to a refresh-button
    
    git-svn-id: svn://develop.studip.de/studip/trunk@16814 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/quicksearch/selectbox.php
+++ templates/quicksearch/selectbox.php
@@ -6,7 +6,7 @@ if ($withButton) : ?>
 <div style="width: <?= $box_width ?>px; background-color: #ffffff; border: 1px #999999 solid; display:inline-block">
 <? $input_style = " style=\"width: ".($box_width-23)."px; background-color:#ffffff; border: 0px;\""; ?>
     <? if ($box_align === "left") : ?>
-    <input class="text-bottom" type="image" src="<?= Assets::image_path("icons/16/blue/search.png") ?>">
+    <input class="text-bottom" type="image" src="<?= Assets::image_path("icons/16/blue/refresh.png") ?>">
     <? endif ?>
 <? endif ?>
 <select<?= $input_style .($inputClass ? " class=\"".$inputClass."\"" : "") ?> name="<?= $name ?>">
@@ -16,7 +16,7 @@ if ($withButton) : ?>
 </select>
 <? if ($withButton) : ?>
     <? if ($box_align !== "left") : ?>
-    <input class="text-bottom" type="image" src="<?= Assets::image_path("icons/16/blue/search.png") ?>">
+    <input class="text-bottom" type="image" src="<?= Assets::image_path("icons/16/blue/refresh.png") ?>">
     <? endif ?>
     </div>
 <? endif ?>

b756108e7de7474874048f8dbcffa3ebcf3d8150
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2010-11-03 13:15:30 +0000

    refs #1430
    Jetzt wird auch der Nutzername angezeigt.
    
    git-svn-id: svn://develop.studip.de/studip/trunk@17575 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/classes/searchtypes/StandardSearch.class.php
+++ lib/classes/searchtypes/StandardSearch.class.php
@@ -116,7 +116,7 @@ class StandardSearch extends SQLSearch
     {
         switch ($this->search) {
             case "username":
-                return "SELECT DISTINCT auth_user_md5.username, CONCAT(auth_user_md5.Vorname, \" \", auth_user_md5.Nachname) " .
+                return "SELECT DISTINCT auth_user_md5.username, CONCAT(auth_user_md5.Vorname, \" \", auth_user_md5.Nachname, \" (\", auth_user_md5.username,\")\") " .
                         "FROM auth_user_md5 LEFT JOIN user_info ON (user_info.user_id = auth_user_md5.user_id) " .
                         "WHERE CONCAT(auth_user_md5.Vorname, \" \", auth_user_md5.Nachname) LIKE :input " .
                             "OR auth_user_md5.username LIKE :input " .

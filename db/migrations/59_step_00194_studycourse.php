41a08b0afb2d71548e99cc94d2faf371b406fda1
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2010-12-06 15:15:25 +0000

    add index re #1121
    git-svn-id: svn://develop.studip.de/studip/trunk@17898 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- db/migrations/59_step_00194_studycourse.php
+++ db/migrations/59_step_00194_studycourse.php
@@ -20,6 +20,7 @@ class Step00194Studycourse extends Migration
                         )");
         $db->exec("ALTER TABLE `user_studiengang` ADD `semester` TINYINT(2) DEFAULT 0");
         $db->exec("ALTER TABLE `user_studiengang` ADD `abschluss_id` CHAR(32) DEFAULT 0");
+        $db->exec("ALTER TABLE `user_studiengang` ADD INDEX ( `studiengang_id` )");
     }
 
     function down()

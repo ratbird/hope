f9fc1ef125eb83b093fc837611e67a83c43fe508
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2011-01-10 16:05:38 +0000

    fixes #1695
    git-svn-id: svn://develop.studip.de/studip/trunk@18394 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/raumzeit/MetaDate.class.php
+++ lib/raumzeit/MetaDate.class.php
@@ -189,6 +189,7 @@ class MetaDate
         }
         if(isset($data['weekday'])) $cycle->weekday = (int)$data['weekday'];
         if(isset($data['week_offset'])) $cycle->week_offset = (int)$data['week_offset'];
+        if(isset($data['cycle'])) $cycle->cycle = (int)$data['cycle'];
         if(isset($data['sws'])) $cycle->sws = $data['sws'];
 
         if (isset($data['day']) && isset($data['start_stunde']) && isset($data['start_minute']) && isset($data['end_stunde']) && isset($data['end_minute'])) {

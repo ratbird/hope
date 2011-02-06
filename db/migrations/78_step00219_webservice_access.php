edf953aed8c945b92279860917cac86f5f5f0e09
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2011-02-06 16:04:24 +0000

    re #1757
    git-svn-id: svn://develop.studip.de/studip/trunk@18874 c5b85792-5220-0410-ad23-ed5b6b8ad999

---
+++ db/migrations/78_step00219_webservice_access.php
@@ -0,0 +1,33 @@
+<?php
+
+class Step00219WebserviceAccess extends Migration
+{
+
+    function description()
+    {
+        return 'Step00219: new table webservice_access_rules';
+    }
+
+    function up()
+    {
+        $db = DBManager::get();
+        $db->exec("CREATE TABLE `webservice_access_rules` (
+                `api_key` VARCHAR( 100 ) NOT NULL DEFAULT '',
+                `method` VARCHAR( 100 ) NOT NULL DEFAULT '',
+                `ip_range` VARCHAR( 200 ) NOT NULL DEFAULT '',
+                `type` ENUM( 'allow', 'deny' ) NOT NULL DEFAULT 'allow',
+                `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+                ) ENGINE = MYISAM ;");
+        SimpleORMap::expireTableScheme();
+        if ($GLOBALS['STUDIP_API_KEY'] && $GLOBALS['WEBSERVICES_ENABLE']) {
+            $db->exec("INSERT INTO `webservice_access_rules`  (`api_key`, `method`, `ip_range`, `type`) VALUES (".$db->quote($GLOBALS['STUDIP_API_KEY']).", '%', '', 'allow')");
+        }
+    }
+
+    function down()
+    {
+        $db = DBManager::get();
+        $db->exec("DROP TABLE `webservice_access_rules` ");
+        SimpleORMap::expireTableScheme();
+    }
+}

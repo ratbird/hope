d0b6026e9dfc8a64a8942eaa25b9d438be06c9ec
tgloeggl <tgloeggl@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2011-03-15 10:46:13 +0000

    refs #1837, refs #1716, use visitdates of the seminar for participants initially
    git-svn-id: svn://develop.studip.de/studip/trunk@19467 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- db/migrations/81_step_209_teilnehmericon.php
+++ db/migrations/81_step_209_teilnehmericon.php
@@ -15,10 +15,18 @@ class Step209Teilnehmericon extends Migration
             ENUM( 'vote', 'documents', 'forum', 'literature', 'schedule', 'scm',
                 'sem', 'wiki', 'news', 'eval', 'inst', 'ilias_connect',
                 'elearning_interface', 'participants' ) NOT NULL DEFAULT 'vote'");
+
+        // copy timestamps from sem to participants to reduce red participant-icons
+        DBManager::get()->exec("INSERT INTO object_user_visits
+            (object_id, user_id, type, visitdate, last_visitdate)
+            SELECT object_id, user_id, 'participants' as type, visitdate, last_visitdate
+            FROM `object_user_visits`
+            WHERE type='sem'");
     }
 
     function down ()
     {
+        DBManager::get()->exec("DELETE FROM object_user_visits WHERE type = 'participants'");
         DBManager::get()->exec("ALTER TABLE `object_user_visits`
             CHANGE `type` `type`
             ENUM( 'vote', 'documents', 'forum', 'literature', 'schedule', 'scm',

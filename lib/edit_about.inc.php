9090f478d5e44abaaed04bb85948448539649d9e
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2011-04-01 23:19:24 +0000

    weitere nickligkeiten behoben re #1878
    git-svn-id: svn://develop.studip.de/studip/trunk@19641 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/edit_about.inc.php
+++ lib/edit_about.inc.php
@@ -315,12 +315,12 @@ function fach_abschluss_edit($fach_abschluss_delete,$new_studiengang,$new_abschl
 
         $any_change = true;
         if (is_array($fach_abschluss_delete)) {
-            $course_delete=true;
             $any_change = false;
             for ($i=0; $i < count($fach_abschluss_delete); $i++) {
                 $this->db->query("DELETE FROM user_studiengang WHERE user_id='".$this->auth_user["user_id"]."' AND studiengang_id='$fach_abschluss_delete[$i]'");
-                if (!$this->db->affected_rows())
-                    $this->msg = $this->msg."error§" . sprintf(_("Fehler beim L&ouml;schen in user_studiengang bei ID=%s"), $fach_abschluss_delete[$i]) . "§";
+                if ($this->db->affected_rows()) {
+                    $delete = true;
+                }
             }
         }
 
@@ -334,18 +334,14 @@ function fach_abschluss_edit($fach_abschluss_delete,$new_studiengang,$new_abschl
                 }
             }
 
-            if ($new_studiengang && $new_abschluss && $new_studiengang != '-- Bitte Fach auswählen --' && $new_abschluss != '-- Bitte Abschluss auswählen --') {
+            if ($new_studiengang && $new_studiengang != 'none') {
                 $this->db->query("INSERT IGNORE INTO user_studiengang (user_id,studiengang_id,abschluss_id,semester) VALUES ('".$this->auth_user["user_id"]."','$new_studiengang','$new_abschluss','$fachsem')");
-                if (!$this->db->affected_rows()) {
-                    $this->msg = $this->msg."error§" . sprintf(_("Fehler beim Einf&uuml;gen in user_studiengang bei ID=%s"), $new_studiengang) . "§";
-                }
-            } else {
-                if (!$edit_fachsem) {
-                    $this->msg = $this->msg."error§". _("Bitte Fach und Abschluss ausfüllen"). "§";
+                if ($this->db->affected_rows()) {
+                    $new = true;
                 }
             }
         }
-        if ( ($fach_abschluss_delete || $new_studiengang || $new_abschluss || $change_fachsem) && !$this->msg) {
+        if ( ($new || $delete|| $edit_fachsem) && !$this->msg) {
             $this->msg = "msg§" . _("Die Zuordnung zu Studiengängen wurde ge&auml;ndert.");
             setTempLanguage($this->auth_user["user_id"]);
             $this->priv_msg= _("Die Zuordnung zu Studiengängen wurde geändert!\n");
@@ -379,22 +375,24 @@ function fach_abschluss_edit($fach_abschluss_delete,$new_studiengang,$new_abschl
     {
         if (is_array($inst_delete)) {
             for ($i=0; $i < count($inst_delete); $i++) {
-                log_event('INST_USER_DEL', $inst_delete[$i], $this->auth_user["user_id"]);
                 $this->db->query("DELETE FROM user_inst WHERE user_id='".$this->auth_user["user_id"]."' AND Institut_id='$inst_delete[$i]'");
-                if (!$this->db->affected_rows())
-                    $this->msg = $this->msg . "error§" . sprintf(_("Fehler beim L&ouml;schen in user_inst bei ID=%s"), $inst_delete[$i]) . "§";
+                if ($this->db->affected_rows()) {
+                    $delete = true;
+                    log_event('INST_USER_DEL', $inst_delete[$i], $this->auth_user["user_id"]);
+                }
             }
         }
 
         if ($new_inst) {
-            log_event('INST_USER_ADD', $new_inst , $this->auth_user['user_id'], 'user');
-
             $this->db->query("INSERT IGNORE INTO user_inst (user_id,Institut_id,inst_perms) VALUES ('".$this->auth_user["user_id"]."','$new_inst','user')");
-            if (!$this->db->affected_rows())
-                $this->msg = $this->msg . "error§" . sprintf(_("Fehler beim Einf&uuml;gen in user_inst bei ID=%s"), $new_inst) . "§";
+            if ($this->db->affected_rows()) {
+                log_event('INST_USER_ADD', $new_inst , $this->auth_user['user_id'], 'user');
+                $new = true;
+            }
+
         }
 
-        if ( ($inst_delete || $new_inst) && !$this->msg) {
+        if ( $delete || $new ) {
             $this->msg = "msg§" . _("Die Zuordnung zu Einrichtungen wurde ge&auml;ndert.");
             setTempLanguage($this->auth_user["user_id"]);
             $this->priv_msg= _("Die Zuordnung zu Einrichtungen wurde geändert!\n");
@@ -710,7 +708,7 @@ function fach_abschluss_edit($fach_abschluss_delete,$new_studiengang,$new_abschl
     function select_inst()
     {
 
-        echo '<select name="new_inst" id="select_new_inst"><option selected="selected"> ' . _("-- Bitte Einrichtung auswählen --") . ' </option>'."\n";
+        echo '<select name="new_inst" id="select_new_inst"><option selected="selected" value=""> ' . _("-- Bitte Einrichtung auswählen --") . ' </option>'."\n";
         $this->db->query("SELECT a.Institut_id,a.Name FROM Institute AS a LEFT JOIN user_inst AS b ON (b.user_id='".$this->auth_user["user_id"]."' AND a.Institut_id=b.Institut_id) WHERE b.Institut_id IS NULL ORDER BY a.Name");
         while ($this->db->next_record()) {
             echo "<option value=\"".$this->db->f("Institut_id")."\">".htmlReady(my_substr($this->db->f("Name"),0,50))."</option>\n";

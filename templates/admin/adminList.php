b6caeafdce9ca9147efc52e44842f2679670fe7d
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2011-04-04 11:54:39 +0000

    refs #1885
    use prepared-statements
    
    git-svn-id: svn://develop.studip.de/studip/trunk@19675 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/admin/adminList.php
+++ templates/admin/adminList.php
@@ -1,5 +1,5 @@
 <form action="<? URLHelper::getLink("?", array('cid' => null)) ?>" method="get">
-    <select name="cid" onKeyDown="if (event.keyCode === 13) { jQuery(this).closest('form')[0].submit(); }" onClick="jQuery(this).closest('form')[0].submit();" size="10" style="max-width: 200px;">
+    <select aria-label="<?= _("Wählen Sie ein Seminar aus Ihrer letzten Seminarsuche aus, um dieses zu bearbeiten.") ?>" name="cid" onKeyDown="if (event.keyCode === 13) { jQuery(this).closest('form')[0].submit(); }" onClick="jQuery(this).closest('form')[0].submit();" size="10" style="max-width: 200px;">
     <? foreach ($adminList as $seminar) : ?>
         <option title="<?= htmlReady($seminar['Name']) ?>" value="<?= htmlReady($seminar['Seminar_id']) ?>"<?= ($seminar['Seminar_id'] === $course_id ? " selected" : "") ?>><?= htmlReady($seminar['Name']) ?></option>
     <? endforeach ?>

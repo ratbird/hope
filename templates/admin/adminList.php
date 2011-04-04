6fb3021bc483393c7ccc19fbbb2f8c4f4b822859
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2011-04-04 15:57:03 +0000

    refs #1885
    adding anchor
    
    git-svn-id: svn://develop.studip.de/studip/trunk@19688 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/admin/adminList.php
+++ templates/admin/adminList.php
@@ -1,4 +1,4 @@
-<form action="<? URLHelper::getLink("?admin_top_links", array('cid' => null)) ?>" method="get">
+<form action="<? URLHelper::getLink("?#admin_top_links", array('cid' => null)) ?>" method="get">
     <select aria-label="<?= _("Wählen Sie ein Seminar aus Ihrer letzten Seminarsuche aus, um dieses zu bearbeiten.") ?>" name="cid" onKeyDown="if (event.keyCode === 13) { jQuery(this).closest('form')[0].submit(); }" onClick="jQuery(this).closest('form')[0].submit();" size="10" style="max-width: 200px;">
     <? foreach ($adminList as $seminar) : ?>
         <option title="<?= htmlReady($seminar['Name']) ?>" value="<?= htmlReady($seminar['Seminar_id']) ?>"<?= ($seminar['Seminar_id'] === $course_id ? " selected" : "") ?>><?= htmlReady($seminar['Name']) ?></option>

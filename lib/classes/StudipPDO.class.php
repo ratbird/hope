97243ea88b075a6ce022c0ee0a9fffbb8929767f
elmar <elmar@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2011-04-14 14:22:53 +0000

    add StudipPDOStatement class, re #483
    git-svn-id: svn://develop.studip.de/studip/trunk@19795 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/classes/StudipPDO.class.php
+++ lib/classes/StudipPDO.class.php
@@ -18,6 +18,9 @@
  */
 class StudipPDO extends PDO
 {
+    const PARAM_ARRAY  = 100;
+    const PARAM_COLUMN = 101;
+
     /**
      * Verifies that the given SQL query only contains a single statement.
      *
@@ -47,7 +50,7 @@ class StudipPDO extends PDO
         if ($count < 1000) {
             $result = preg_replace('/"(""|\\\\.|[^\\\\"]+)*"|\'(\'\'|\\\\.|[^\\\\\']+)*\'/s', '?', $statement);
         }
-        
+
         if (!isset($result)) {
             // split string into parts at quotes and backslash
             $parts = preg_split('/([\\\\"\'])/', $statement, -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_DELIM_CAPTURE);
@@ -125,6 +128,205 @@ class StudipPDO extends PDO
     public function prepare($statement, $driver_options = array())
     {
         $this->verify($statement);
+        return new StudipPDOStatement($this, $statement, $driver_options);
+    }
+
+    /**
+     * This method is intended only for use by the StudipPDOStatement class.
+     *
+     * @param string    SQL statement
+     * @return object   PDOStatement object
+     */
+    public function prepareStatement($statement, $driver_options = array())
+    {
         return parent::prepare($statement, $driver_options);
+    }
+}
+
+/**
+ * This is a "fake" PDOStatement implementation that behaves mostly like
+ * a real statement object, but has some additional features:
+ *
+ * - Parameters passed to execute() are quoted according to their PHP type.
+ * - A PHP NULL value will result in an actual SQL NULL value in the query.
+ * - Array types are supported for all placeholders ("WHERE value IN (?)").
+ * - Positional and named parameters can be mixed in the same query.
+ */
+class StudipPDOStatement implements IteratorAggregate
+{
+    protected $db;
+    protected $query;
+    protected $options;
+    protected $columns;
+    protected $params;
+    protected $count;
+    protected $stmt;
+
+    /**
+     * Initializes a new StudipPDOStatement instance.
+     */
+    public function __construct($db, $query, $options)
+    {
+        $this->db = $db;
+        $this->query = $query;
+        $this->options = $options;
+        $this->params = array();
+    }
+
+    /**
+     * Arranges to have a particular variable bound to a given column in
+     * the result-set from a query. Each call to fetch() or fetchAll()
+     * will update all the variables that are bound to columns.
+     */
+    public function bindColumn($column, &$param/*, ...*/)
+    {
+        $args = func_get_args();
+        $args[1] = &$param;
+        $this->columns[] = $args;
+        return true;
+    }
+
+    /**
+     * Binds a PHP variable to a corresponding named or question mark place-
+     * holder in the SQL statement that was used to prepare the statement.
+     * Unlike bindValue(), the variable is bound as a reference and will
+     * only be evaluated at the time that execute() is called.
+     */
+    public function bindParam($parameter, &$variable, $data_type = PDO::PARAM_STR)
+    {
+        $this->params[$parameter] = array('value' => &$variable, 'type' => $data_type);
+        return true;
+    }
+
+    /**
+     * Binds a value to a corresponding named or question mark placeholder
+     * in the SQL statement that was used to prepare the statement.
+     */
+    public function bindValue($parameter, $value, $data_type = PDO::PARAM_STR)
+    {
+        $this->params[$parameter] = array('value' => $value, 'type' => $data_type);
+        return true;
+    }
+
+    /**
+     * Forwards all unknown methods to the actual statement object.
+     */
+    public function __call($name, array $arguments)
+    {
+        return call_user_func_array(array($this->stmt, $name), $arguments);
+    }
+
+    /**
+     * Forwards all Iterator methods to the actual statement object.
+     */
+    public function getIterator()
+    {
+        return $this->stmt;
+    }
+
+    /**
+     * Executes the prepared statement and returns a PDOStatement object.
+     */
+    public function execute($input_parameters = NULL)
+    {
+        // bind additional parameters from execute()
+        if (isset($input_parameters)) {
+            foreach ($input_parameters as $key => $value) {
+                $this->bindValue(is_int($key) ? $key + 1 : $key, $value, NULL);
+            }
+        }
+
+        // emulate prepared statement if necessary
+        foreach ($this->params as $key => $param) {
+            if ($param['type'] === StudipPDO::PARAM_ARRAY ||
+                $param['type'] === StudipPDO::PARAM_COLUMN ||
+                $param['type'] === NULL && !is_string($param['value'])) {
+                $emulate_prepare = true;
+                break;
+            }
+        }
+
+        // build the actual query string and prepared statement
+        if ($emulate_prepare) {
+            $this->count = 1;
+            $query = preg_replace_callback('/\?|:\w+/', array($this, 'replaceParam'), $this->query);
+        } else {
+            $query = $this->query;
+        }
+
+        $this->stmt = $this->db->prepareStatement($query, $this->options);
+
+        // bind query parameters on the actual statement
+        if (!$emulate_prepare) {
+            foreach ($this->params as $key => $param) {
+                $this->stmt->bindValue($key, $param['value'], $param['type']);
+            }
+        }
+
+        // set up column bindings on the actual statement
+        if (isset($this->columns)) {
+            foreach ($this->columns as $args) {
+                call_user_func_array(array($this->stmt, 'bindColumn'), $args);
+            }
+        }
+
+        return $this->stmt->execute();
+    }
+
+    /**
+     * Quotes the given value in a form appropriate for the type.
+     * If no explicit type is given, the value's PHP type is used.
+     */
+    protected function quote($value, $type = NULL)
+    {
+        if (!isset($type)) {
+            if (is_null($value)) {
+                $type = PDO::PARAM_NULL;
+            } else if (is_bool($value)) {
+                $type = PDO::PARAM_BOOL;
+            } else if (is_int($value)) {
+                $type = PDO::PARAM_INT;
+            } else if (is_array($value)) {
+                $type = StudipPDO::PARAM_ARRAY;
+            } else {
+                $type = PDO::PARAM_STR;
+            }
+        }
+
+        switch ($type) {
+            case PDO::PARAM_NULL:
+                return 'NULL';
+            case PDO::PARAM_BOOL:
+                return $value ? '1' : '0';
+            case PDO::PARAM_INT:
+                return (int) $value;
+            case StudipPDO::PARAM_ARRAY:
+                return join(',', array_map(array($this, 'quote'), $value));
+            case StudipPDO::PARAM_COLUMN:
+                return preg_replace('/\\W/', '', $value);
+            default:
+                return $this->db->quote($value);
+        }
+    }
+
+    /**
+     * Replaces a placeholder with the corresponding parameter value.
+     * Throws an exception if there is no corresponding value.
+     */
+    protected function replaceParam($matches)
+    {
+        $name = $matches[0];
+
+        if ($name == '?') {
+            $key = $this->count++;
+        } else {
+            $key = $name;
+        }
+
+        if (!isset($this->params[$key])) {
+            throw new PDOException('missing parameter in query: ' . $key);
+        }
+
+        return $this->quote($this->params[$key]['value'], $this->params[$key]['type']);
     }
 }

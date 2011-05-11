6ebb172b14cac76895ff62f369e52e727eecd3f4
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2011-05-11 08:34:06 +0000

    refs #1885: adding button to select
    git-svn-id: svn://develop.studip.de/studip/trunk@20057 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/admin/adminList.php
+++ templates/admin/adminList.php
@@ -1,7 +1,8 @@
 <form action="<? URLHelper::getLink("?#admin_top_links", array('cid' => null)) ?>" method="get">
-    <select aria-label="<?= _("Wählen Sie ein Seminar aus Ihrer letzten Seminarsuche aus, um dieses zu bearbeiten.") ?>" name="cid" onKeyDown="if (event.keyCode === 13) { jQuery(this).closest('form')[0].submit(); }" onClick="jQuery(this).closest('form')[0].submit();" size="10" style="max-width: 200px;">
+    <select class="text-top" aria-label="<?= _("Wählen Sie ein Seminar aus Ihrer letzten Seminarsuche aus, um dieses zu bearbeiten.") ?>" name="cid" onKeyDown="if (event.keyCode === 13) { jQuery(this).closest('form')[0].submit(); }" onClick="jQuery(this).closest('form')[0].submit();" size="10" style="max-width: 200px;">
     <? foreach ($adminList as $seminar) : ?>
         <option title="<?= htmlReady($seminar['Name']) ?>" value="<?= htmlReady($seminar['Seminar_id']) ?>"<?= ($seminar['Seminar_id'] === $course_id ? " selected" : "") ?>><?= htmlReady($seminar['Name']) ?></option>
     <? endforeach ?>
     </select>
+    <input class="text-top" type="image" src="<?= Assets::image_path('icons/16/green/accept.png')?>" title="<?= _("auswählen") ?>">
 </form>

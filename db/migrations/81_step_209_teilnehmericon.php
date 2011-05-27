286f72002b0018c307d93b82723fa12d2bc71c02
tgloeggl <tgloeggl@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2011-05-27 08:43:54 +0000

    fixes #1996
    git-svn-id: svn://develop.studip.de/studip/trunk@20199 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- db/migrations/81_step_209_teilnehmericon.php
+++ db/migrations/81_step_209_teilnehmericon.php
@@ -19,9 +19,10 @@ class Step209Teilnehmericon extends Migration
         // copy timestamps from sem to participants to reduce red participant-icons
         DBManager::get()->exec("INSERT INTO object_user_visits
             (object_id, user_id, type, visitdate, last_visitdate)
-            SELECT object_id, user_id, 'participants' as type, visitdate, last_visitdate
-            FROM `object_user_visits`
-            WHERE type='sem'");
+            (SELECT object_id, ouv.user_id, 'participants' as type, visitdate, last_visitdate
+            FROM object_user_visits AS ouv
+            LEFT JOIN seminar_user AS su ON (su.user_id = ouv.user_id AND su.Seminar_id = ouv.object_id)
+            WHERE type='sem' AND status IN ('tutor', 'dozent'))");
     }
 
     function down ()

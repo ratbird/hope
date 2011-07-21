9ce3317750a2eb0e251bdceb762a693b1d3cd72b
hackl <hackl@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2011-07-21 07:26:46 +0000

    re #2068 use $DEFAULT_TITLE_FOR_STATUS
    git-svn-id: svn://develop.studip.de/studip/trunk@20507 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/change_view.php
+++ templates/change_view.php
@@ -2,9 +2,9 @@
 if ($_SESSION['seminar_change_view']['cid'] && $_SESSION['seminar_change_view']['cid'] == $GLOBALS['SessSemName'][1]) {
 ?>
 <div class="messagebox messagebox_warning">
-    <?= sprintf(_('Sie sehen die Veranstaltung so wie Teilnehmer mit der '.
-        'Berechtigung "%s". Sie können die Ansicht %shier zurücksetzen%s.'), 
-        $_SESSION['seminar_change_view']['perm'], 
+    <?= sprintf(_('Die Veranstaltung wird in der Ansicht für %s angezeigt. '.
+        'Sie können die Ansicht %shier zurücksetzen%s.'), 
+        get_title_for_status($_SESSION['seminar_change_view']['perm'], 2), 
         '<a href="'.URLHelper::getLink('dispatch.php/course/change_view', 
         array('cid' => Request::option('cid'))).'">', '</a>'); ?>
 </div>

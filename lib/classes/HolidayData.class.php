e105e56801ef58e1e5f73f38a74f6235d55bbffe
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2011-07-23 20:40:28 +0000

    fixes#1493
    git-svn-id: svn://develop.studip.de/studip/trunk@20628 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/classes/HolidayData.class.php
+++ lib/classes/HolidayData.class.php
@@ -1,5 +1,4 @@
 <?php
-# Lifter010: TODO
 /**
  * HolidayData.class.php
  *
@@ -12,10 +11,16 @@
  * @license     http://www.gnu.org/licenses/gpl-2.0.html GPL version 2
  * @category    Stud.IP
  */
+require_once 'lib/classes/SemesterHoliday.class.php';
 
+/**
+ * old model class for table semester_holiday
+ * use SemesterHoliday instead
+ * @deprecated
+ *
+ */
 class HolidayData
 {
-    var $db;
 
     /**
      * get an instance of this class
@@ -38,107 +43,23 @@ class HolidayData
         }
     }
 
+
     function GetAllHolidaysArray(){
-        static $all_holiday;
-        if (is_null($all_holiday)){
-            $holiday = new HolidayData();
-            $all_holiday = $holiday->getAllHolidays();
-        }
-        return $all_holiday;
+        return SemesterHoliday::getAll();
     }
 
     function HolidayData() {
-        $this->db = new DB_Seminar;
     }
 
     function getAllHolidays() {
-        $i=0;
-        $sql = "SELECT * FROM semester_holiday order by beginn";
-        if (!$this->db->query($sql)) {
-            echo "Error! Query (getAllHolidays) not succeeded";
-            die();
-        }
-        if ($this->db->num_rows()==0) {
-            return array();
-        }
-        while ($this->db->next_record()) {
-            $holidaydata[$i] = $this->wrapHolidayData();
-            $i++;
-        }
-        return $holidaydata;
+        return SemesterHoliday::getAll();
     }
 
     function getHolidaysInPeriod($start,$end) {
-        $i=0;
-        $sql = "SELECT * FROM semester_holiday WHERE beginn >= '".$start."' AND ende <= '".$end."'";
-        if (!$this->db->query($sql)) {
-            echo "Error! Query not succeeded in getHolidayInPeriod!";
-            die();
-        }
-        if ($this->db->num_rows()==0) {
-            return array();
-        }
-        while ($this->db->next_record()) {
-            $holidaydata[$i] = $this->wrapHolidayData();
-            $i++;
-        }
-        return $holidaydata;
-    }
-
-    function deleteHoliday($holiday_id) {
-        $sql = "DELETE FROM semester_holiday WHERE holiday_id = '".$holiday_id."'";
-        if (!$this->db->query($sql)) {
-            echo "Error! Query (deleteHoliday) not succeeded";
-            die();
-        }
-        return 1;
+        return SemesterHoliday::findByTimestampRange($start, $end);
     }
 
     function getHolidayData($holiday_id) {
-        $sql = "SELECT * FROM semester_holiday WHERE holiday_id='".$holiday_id."'";
-        if (!$this->db->query($sql)) {
-            echo "Error! Query (getHolidayData) not succeeded";
-            die();
-        }
-        if ($this->db->num_rows()==0) {
-            return array();
-        }
-        $this->db->next_record();
-        return $this->wrapHolidayData();
-    }
-
-    function insertNewHoliday($holidaydata) {
-        $holiday_id = md5(uniqid("Legolas"));
-        $sql =  "INSERT INTO semester_holiday (holiday_id,semester_id,name,description,beginn,ende) ".
-                "VALUES ('".$holiday_id."','1','".$holidaydata["name"]."','".$holidaydata["description"]."','".$holidaydata["beginn"]."','".$holidaydata["ende"]."')";
-        if (!$this->db->query($sql)) {
-            echo "Error! Query (insertNewHoliday) not succeeded";
-            die();
-        }
-        return $holiday_id;
-    }
-
-    function updateExistingHoliday($holidaydata) {
-        $sql =  "UPDATE semester_holiday SET ".
-                "name = '".$holidaydata["name"]."', ".
-                "description = '".$holidaydata["description"]."', ".
-                "beginn = '".$holidaydata["beginn"]."', ".
-                "ende = '".$holidaydata["ende"]."' ".
-                "WHERE holiday_id='".$holidaydata["holiday_id"]."'";
-        if (!$this->db->query($sql)) {
-            echo "Error! Query (updateExistingHoliday) not succeeded";
-            die();
-        }
-        return 1;
-    }
-
-    function wrapHolidayData() {
-        $holidaydata = array();
-        $holidaydata["holiday_id"]  = $this->db->f("holiday_id");
-        $holidaydata["name"]        = $this->db->f("name");
-        $holidaydata["description"] = $this->db->f("description");
-        $holidaydata["beginn"]  = $this->db->f("beginn");
-        $holidaydata["ende"] = $this->db->f("ende");
-        return $holidaydata;
+        return SemesterHoliday::find($holiday_id);
     }
 }
---
+++ lib/classes/SemesterHoliday.class.php
@@ -0,0 +1,123 @@
+<?php
+/*
+ * SemesterHoliday.class.php
+ * model class for table semester_holiday
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * @author      André Noack <noack@data-quest.de>
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GPL version 2
+ * @category    Stud.IP
+*/
+require_once "lib/classes/SimpleORMap.class.php";
+
+class SemesterHoliday extends SimpleORMap
+{
+    /**
+     * cache
+     */
+    private static $holiday_cache;
+
+    /**
+     * returns SemesterHoliday object for given id or null
+     * @param string $id
+     * @return NULL|SemesterHoliday
+     */
+    static function find($id)
+    {
+        $holiday_cache = self::getAll();
+        return isset($holiday_cache[$id]) ? $holiday_cache[$id] : null;
+    }
+
+    /**
+     * returns SemesterHoliday object for given id or null
+     * @param mixed $id
+     * @return NULL|SemesterHoliday
+     */
+    static function toObject($id_or_object)
+    {
+        return SimpleORMap::findBySql(__CLASS__, $id);
+    }
+
+    /**
+     * returns array of SemesterHoliday objects
+     * @param string $where sql clause
+     * @return array
+     */
+    static function findBySql($where)
+    {
+        return SimpleORMap::findBySql(__CLASS__, $where);
+    }
+
+    /**
+     * delete SemesterHoliday in db
+     * @param string $where sql clause
+     * @return number
+     */
+    static function deleteBySQL($where)
+    {
+        return SimpleORMap::deleteBySQL(__CLASS__, $where);
+    }
+
+    /**
+     * returns all SemesterHoliday between given timestamps
+     * @param integer $timestamp_start
+     * @param integer $timestamp_end
+     * @return array of SemesterHoliday
+     */
+    static function findByTimestampRange($timestamp_start, $timestamp_end)
+    {
+        $ret = array();
+        if ($timestamp_start < $timestamp_end) {
+            foreach(self::getAll() as $holiday) {
+                if ( ($holiday->beginn >= $timestamp_start && $holiday->beginn <= $timestamp_end) 
+                    || ($holiday->beginn < $timestamp_start && $holiday->ende <= $timestamp_end) ) {
+                    $ret[] = $holiday;
+                }
+            }
+        }
+        return $ret;
+    }
+
+    /**
+     * returns all SemesterHolidays for given semester
+     * 
+     * @param mixed semester object or id as string or assoc array
+     * @return array of SemesterHoliday
+     */
+    static function findBySemester($semester)
+    {
+        $semester = self::toObject($semester);
+        return self::findByTimestampRange($semester->beginn, $semester->ende);
+    }
+
+    /**
+     * returns array of all existing SemesterHoliday objects
+     * orderd by begin
+     * @param boolean $force_reload
+     * @return array
+     */
+    static function getAll($force_reload = false)
+    {
+        if (!is_array(self::$holiday_cache) || $force_reload) {
+            self::$holiday_cache = array();
+            foreach(self::findBySql('1 ORDER BY beginn') as $holiday){
+                self::$holiday_cache[$holiday->getId()] = $holiday;
+            }
+        }
+        return self::$holiday_cache;
+    }
+
+    /**
+     *
+     * @param string $id primary key of table
+     */
+    function __construct($id = null)
+    {
+        $this->db_table = 'semester_holiday';
+        parent::__construct($id);
+    }
+}

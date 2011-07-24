36e46d2471a62eeac6134499b036a54b261910c4
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2011-07-24 09:49:54 +0000

    Metadaten nach Migration neu einlesen re #2073
    git-svn-id: svn://develop.studip.de/studip/trunk@20643 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- db/migrations/86_step_228_raumanfragen.php
+++ db/migrations/86_step_228_raumanfragen.php
@@ -12,6 +12,7 @@ class Step228Raumanfragen extends Migration
         $db = DBManager::get();
         $db->exec(
             "ALTER TABLE `resources_requests` ADD `metadate_id` VARCHAR( 32 ) NOT NULL DEFAULT '' AFTER `termin_id`");
+        SimpleORMap::expireTableScheme();
     }
 
     function down ()

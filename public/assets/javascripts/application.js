ccfb4e56778d43f3b84873f0d2d740b573952a0f
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2011-08-22 15:45:56 +0000

    refs #2073
    js-coding style
    
    git-svn-id: svn://develop.studip.de/studip/trunk@20874 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/assets/javascripts/application.js
+++ public/assets/javascripts/application.js
@@ -1830,7 +1830,7 @@ STUDIP.RoomRequestDialog = {
             jQuery.ajax({
                 url: url,
                 data: {},
-                success: function(data) {
+                success: function (data) {
                     STUDIP.RoomRequestDialog.dialog =
                         jQuery('<div id="RoomRequestDialogbox">' + data.content + '</div>').dialog({
                             show: '',
@@ -1841,47 +1841,47 @@ STUDIP.RoomRequestDialog = {
                             width: Math.min(1000, jQuery(window).width() - 64),
                             height: 'auto',
                             maxHeight: jQuery(window).height(),
-                            close: function() {
-                                    STUDIP.RoomRequestDialog.submit(jQuery('form[name=room_request]')[0], 'save');
-                                    jQuery(this).remove();
-                                    STUDIP.RoomRequestDialog.dialog = null;
-                                    }
+                            close: function () {
+                                STUDIP.RoomRequestDialog.submit(jQuery('form[name=room_request]')[0], 'save');
+                                jQuery(this).remove();
+                                STUDIP.RoomRequestDialog.dialog = null;
+                            }
                         });
                     STUDIP.RoomRequestDialog.bindevents();
                 }
             });
         }
     },
-    bindevents: function() {
-        jQuery('form[name=room_request] input[type=image]').bind('click dblclick', function() {
-                var button_clicked = this.name;
-                var form = jQuery('form[name=room_request]')[0];
-                STUDIP.RoomRequestDialog.submit(form, button_clicked);
-                return false;
+    bindevents: function () {
+        jQuery('form[name=room_request] input[type=image]').bind('click dblclick', function () {
+            var button_clicked = this.name;
+            var form = jQuery('form[name=room_request]')[0];
+            STUDIP.RoomRequestDialog.submit(form, button_clicked);
+            return false;
         });
     },
-    submit: function(form, button_clicked) {
+    submit: function (form, button_clicked) {
         if (form) {
             var form_data = jQuery(form).serializeArray();
-            form_data.push({name: button_clicked,value: 1});
+            form_data.push({name: button_clicked, value: 1});
             STUDIP.RoomRequestDialog.update(form.action, form_data);
         }
     },
-    update: function(url, data) {
+    update: function (url, data) {
         var zIndex = jQuery('#RoomRequestDialogbox').parent().zIndex();
         jQuery('#RoomRequestDialogbox').parent().zIndex(zIndex - 1);
         jQuery.ajax({
-                type: 'POST',
-                url: url,
-                data: data,
-                success: function(data) {
-                    if (STUDIP.RoomRequestDialog.dialog !== null) {
-                        STUDIP.RoomRequestDialog.dialog.dialog('option', 'title', data.title);
-                        jQuery('#RoomRequestDialogbox').html(data.content);
-                        jQuery('#RoomRequestDialogbox').parent().zIndex(zIndex);
-                        STUDIP.RoomRequestDialog.bindevents();
-                    }
+            type: 'POST',
+            url: url,
+            data: data,
+            success: function (data) {
+                if (STUDIP.RoomRequestDialog.dialog !== null) {
+                    STUDIP.RoomRequestDialog.dialog.dialog('option', 'title', data.title);
+                    jQuery('#RoomRequestDialogbox').html(data.content);
+                    jQuery('#RoomRequestDialogbox').parent().zIndex(zIndex);
+                    STUDIP.RoomRequestDialog.bindevents();
                 }
+            }
         });
     }
 };

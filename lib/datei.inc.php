421ada4e2ee4a5580cf4f592511843b2eee04be1
elmar <elmar@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2011-11-28 10:10:10 +0000

    fix fallout from [19305], fixes #2347
    git-svn-id: svn://develop.studip.de/studip/trunk@21573 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/datei.inc.php
+++ lib/datei.inc.php
@@ -2533,7 +2533,7 @@ function get_flash_player ($document_id, $filename, $type) {
         $flash_config .= '&amp;autoload=1';
     }
     // we need the absolute url if the player is delivered from a different base
-    $movie_url = $GLOBALS['ABSOLUTE_URI_STUDIP'] . str_replace($GLOBALS['CANONICAL_RELATIVE_PATH_STUDIP'], '', GetDownloadLink($document_id, $filename, $type, 'force'));
+    $movie_url = GetDownloadLink($document_id, $filename, $type, 'force');
     $flash_object  = "\n<object type=\"application/x-shockwave-flash\" id=\"FlashPlayer\" data=\"".Assets::url()."flash/player_flv.swf\" width=\"$width\" height=\"$height\">\n";
     $flash_object .= "<param name=\"movie\" value=\"".Assets::url()."flash/player_flv.swf\">\n";
     $flash_object .= "<param name=\"FlashVars\" value=\"flv=" . urlencode($movie_url) . $flash_config . "\">\n";

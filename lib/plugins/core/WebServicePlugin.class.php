286493a029f9fba30347b596edb3b34ce9082bad
mlunzena <mlunzena@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2011-12-20 22:54:35 +0000

    add WebServicePlugin interface, refs #2368
    git-svn-id: svn://develop.studip.de/studip/trunk@21695 c5b85792-5220-0410-ad23-ed5b6b8ad999

---
+++ lib/plugins/core/WebServicePlugin.class.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Copyright (c) 2011 <mlunzena@uos.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ */
+
+interface WebServicePlugin
+{
+    function getWebServices();
+}

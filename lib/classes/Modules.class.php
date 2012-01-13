7e0a4e96e1b1100e8eb9b4d08c980919c06307b6
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-01-13 08:12:57 +0000

    eliminate db_seminar and clean up according to code styles, re #483 & #651
    git-svn-id: svn://develop.studip.de/studip/trunk@21863 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/classes/Modules.class.php
+++ lib/classes/Modules.class.php
@@ -1,13 +1,13 @@
 <?php
 # Lifter002: TODO
-# Lifter007: TODO
-# Lifter003: TODO
+# Lifter007: TEST - documentation and definition array still to do 
+# Lifter003: TEST
 # Lifter010: TODO
 /**
 * Modules.class.php
-* 
+*
 * check for modules (global and local for institutes and Veranstaltungen), read and write
-* 
+*
 *
 * @author       Cornelis Kater <ckater@gwdg.de>, Suchi & Berg GmbH <info@data-quest.de>
 * @access       public
@@ -37,286 +37,219 @@
 // +---------------------------------------------------------------------------+
 
 require_once 'lib/functions.php';
-require_once ("config.inc.php");
+require_once 'config.inc.php';
 
 class Modules {
     var $registered_modules = array(
-                "forum" => array("id" => 0, "const" => "", "sem" => TRUE, "inst" => TRUE),
-                "documents" => array("id" => 1, "const" => "", "sem" => TRUE, "inst" => TRUE),
-                "schedule" => array("id" => 2, "const" => "", "sem" => TRUE, "inst" => FALSE),
-                "participants" => array("id" => 3, "const" => "", "sem" => TRUE, "inst" => FALSE),
-                "personal" => array("id" => 4, "const" => "", "sem" => FALSE, "inst" => TRUE),
-                "literature" => array("id" => 5, "const" => "LITERATURE_ENABLE", "sem" => TRUE, "inst" => TRUE),
-                "chat" => array("id" => 7, "const" => "CHAT_ENABLE", "sem" => TRUE, "inst" => TRUE),
-                "wiki" => array("id" => 8, "const" => "WIKI_ENABLE", "sem" => TRUE, "inst" => TRUE),
-                "scm" => array("id" => 12, "const" => "SCM_ENABLE", "sem" => TRUE, "inst" => TRUE),
-                "elearning_interface" => array("id" => 13, "const" => "ELEARNING_INTERFACE_ENABLE", "sem" => TRUE, "inst" => TRUE),
-                "documents_folder_permissions" => array("id" => 14, "const" => "", "sem" => TRUE, "inst" => TRUE),
-                "calendar" => array('id' => 16, 'const' => 'COURSE_CALENDAR_ENABLE', 'sem' => TRUE, 'inst' => TRUE)
-                );
-    var $db;
+        'forum' => array('id' => 0, 'const' => '', 'sem' => true, 'inst' => true),
+        'documents' => array('id' => 1, 'const' => '', 'sem' => true, 'inst' => true),
+        'schedule' => array('id' => 2, 'const' => '', 'sem' => true, 'inst' => false),
+        'participants' => array('id' => 3, 'const' => '', 'sem' => true, 'inst' => false),
+        'personal' => array('id' => 4, 'const' => '', 'sem' => false, 'inst' => true),
+        'literature' => array('id' => 5, 'const' => 'LITERATURE_ENABLE', 'sem' => true, 'inst' => true),
+        'chat' => array('id' => 7, 'const' => 'CHAT_ENABLE', 'sem' => true, 'inst' => true),
+        'wiki' => array('id' => 8, 'const' => 'WIKI_ENABLE', 'sem' => true, 'inst' => true),
+        'scm' => array('id' => 12, 'const' => 'SCM_ENABLE', 'sem' => true, 'inst' => true),
+        'elearning_interface' => array('id' => 13, 'const' => 'ELEARNING_INTERFACE_ENABLE', 'sem' => true, 'inst' => true),
+        'documents_folder_permissions' => array('id' => 14, 'const' => '', 'sem' => true, 'inst' => true),
+        'calendar' => array('id' => 16, 'const' => 'COURSE_CALENDAR_ENABLE', 'sem' => true, 'inst' => true)
+    );
     
     function Modules() {
-        $this->db = new DB_Seminar;
+        
     }
-
-    function getStatus($modul, $range_id, $range_type='') {
-        if ($this->isBit($this->getBin($range_id, $range_type),$this->registered_modules[$modul]["id"]))
-            return TRUE;
-        else
-            return FALSE;
+    
+    function getStatus($modul, $range_id, $range_type = '') {
+        $bitmask = $this->getBin($range_id, $range_type);
+        $id = $this->registered_modules[$modul]['id'];
+        return $this->isBit($bitmask, $id);
     }
 
     function getLocalModules($range_id, $range_type = '', $modules = false, $type = false) {
-        if (!$range_type)
+        if (!$range_type) {
             $range_type = get_object_type($range_id);
-
-        if ($modules === false){
-            $db = new DB_Seminar;
-            if ($range_type == "sem") {
-                $query = sprintf ("SELECT modules FROM seminare WHERE Seminar_id ='%s'", $range_id);
-            } else {
-                $query = sprintf ("SELECT modules FROM Institute WHERE Institut_id ='%s'", $range_id);
-            }
-            $db->query($query);
-            $db->next_record();
-            $modules = $db->f("modules");
         }
-        if ($modules === null){
-            $modules = $this->getDefaultBinValue($range_id, $range_type, $type);    
+
+        if ($modules === false) {
+            if ($range_type == 'sem') {
+                $query = "SELECT modules FROM seminare WHERE Seminar_id = ?";
+            } else {
+                $query = "SELECT modules FROM Institute WHERE Institut_id = ? ";
+            }
+            $statement = DBManager::get()->prepare($query);
+            $statement->execute(array($range_id));
+            $modules = $statement->fetchColumn();
+        }
+        if ($modules === null) {
+            $modules = $this->getDefaultBinValue($range_id, $range_type, $type);
         }
 
         foreach ($this->registered_modules as $key => $val) {
-            if ($this->isBit($modules,$val["id"]))
-                $modules_list[$key]= TRUE;
-            else
-                $modules_list[$key]= FALSE;
+            $modules_list[$key] = $this->isBit($modules, $val['id']);
         }
 
         return $modules_list;
     }
-    
+
     function getDefaultBinValue($range_id, $range_type = '', $type = false) {
         global $SEM_TYPE, $SEM_CLASS, $INST_MODULES;
 
-        $db = new DB_Seminar;
         $bitmask = 0;
         if (!$range_type)
             $range_type = get_object_type($range_id);
 
         if ($type === false){
             if ($range_type == "sem") {
-                $query = sprintf ("SELECT status AS type FROM seminare WHERE Seminar_id ='%s'", $range_id);
+                $query = "SELECT status FROM seminare WHERE Seminar_id = ?";
             } else {
-                $query = sprintf ("SELECT type FROM Institute WHERE Institut_id ='%s'", $range_id);
+                $query = "SELECT type FROM Institute WHERE Institut_id = ?";
             }
-
-            $db->query($query);
-            $db->next_record();
-            $type = $db->f("type");
+            $statement = DBManager::get()->prepare($query);
+            $statement->execute(array($range_id));
+            $type = $statement->fetchColumn();
         }
-        
-        if ($range_type == "sem") {
-            foreach ($this->registered_modules as $key=>$val) {
-                if (($SEM_CLASS[$SEM_TYPE[$type]["class"]][$key]) && (($GLOBALS[$val["const"]]) || (!$val["const"]))) {
-                    $this->setBit($bitmask, $val["id"]);
-                } 
+
+        foreach ($this->registered_modules as $key=>$val) {
+            if ($range_type == 'sem') {
+                $temp = $SEM_CLASS[$SEM_TYPE[$type]['class']][$key];
+            } else {
+                $temp = $INST_MODULES[$INST_MODULES[$type] ? $type : 'default'][$key];
             }
-        } else {
-            foreach ($this->registered_modules as $key=>$val) {
-                if (($INST_MODULES[($INST_MODULES[$type]) ? $type : "default"][$key]) && (($GLOBALS[$val["const"]]) || (!$val["const"])))
-                    $this->setBit($bitmask, $val["id"]);
+            if ($temp and (!$val['const'] or $GLOBALS[$val['const']])) {
+                $this->setBit($bitmask, $val['id']);
             }
         }
 
         return $bitmask;
     }
-    
-    function getBin ($range_id, $range_type='') {
-        $db = new DB_Seminar;
-        
-        if (!$range_type)
+
+    function getBin($range_id, $range_type = '') {
+        if (!$range_type) {
             $range_type = get_object_type($range_id);
-        
-        if ($range_type == "sem") {
-            $query = sprintf ("SELECT status AS type, modules FROM seminare WHERE Seminar_id ='%s'", $range_id);
-        } else {
-            $query = sprintf ("SELECT type, modules FROM Institute WHERE Institut_id ='%s' ", $range_id);
         }
 
-        $db->query($query);
-        $db->next_record();
+        if ($range_type == 'sem') {
+            $query = "SELECT modules FROM seminare WHERE Seminar_id = ?";
+        } else {
+            $query = "SELECT modules FROM Institute WHERE Institut_id = ?";
+        }
+        $statement = DBManager::get()->prepare($query);
+        $statement->execute(array($range_id));
+        $modules = $statement->fetchColumn();
 
-        if ($db->nf()) {
-            if ($db->f("modules") === null)
-                $bitmask = $this->getDefaultBinValue($range_id, $range_type);
-            else
-                $bitmask = $db->f("modules");
+        if (!$modules) {
+            $bitmask = $this->getDefaultBinValue($range_id, $range_type);
+        } else {
+            $bitmask = $modules;
         }
 
         return $bitmask;
     }
-    
-    function writeBin ($range_id, $bitmask, $range_type='') {
-        $db = new DB_Seminar;
 
-        if (!$range_type)
+    function writeBin($range_id, $bitmask, $range_type = '') {
+        if (!$range_type) {
             $range_type = get_object_type($range_id);
-        
-        if ($range_type == "sem") {
-            $query = sprintf ("UPDATE seminare SET modules = '%s' WHERE Seminar_id ='%s'", $bitmask, $range_id);
-        } else {
-            $query = sprintf ("UPDATE Institute SET modules = '%s' WHERE Institut_id ='%s'", $bitmask, $range_id);
         }
 
-        $db->query($query);
-        $db->next_record();
-
-        if ($db->affected_rows)
-            return TRUE;
-        else
-            return FALSE;
-    }
-    
-    
-    function writeDefaultStatus($range_id, $range_type='') {
-        if (!$range_type)
-            $range_type = get_object_type($range_id);
-        
-        if ($range_type == "sem") {
-            $query = sprintf ("SELECT status AS type FROM seminare WHERE Seminar_id ='%s'", $range_id);
+        if ($range_type == 'sem') {
+            $query = "UPDATE seminare SET modules = ? WHERE Seminar_id = ?";
         } else {
-            $query = sprintf ("SELECT type FROM Institute WHERE Institut_id ='%s'", $range_id);
+            $query = "UPDATE Institute SET modules = ? WHERE Institut_id = ?";
         }
-
-        $this->db->query($query);
-        $this->db->next_record();
-
-        if ($range_type == "sem") {
-            $query = sprintf ("UPDATE seminare SET modules = '%s' WHERE Seminar_id ='%s'", $this->getDefaultBinValue($range_id, $range_type), $range_id);
-            $this->db->query($query);
-            if ($this->db->affected_rows())
-                return TRUE;
-            else 
-                return FALSE;
-        } else {
-            $query = sprintf ("UPDATE Institute SET modules = '%s' WHERE Institut_id ='%s'", $this->getDefaultBinValue($range_id, $range_type), $range_id);
-            $this->db->query($query);
-            if ($this->db->affected_rows())
-                return TRUE;
-            else 
-                return FALSE;
-        }
+        $statement = DBManager::get()->prepare($query);
+        $statement->execute(array($bitmask, $range_id));
+        return (bool)$statement->rowCount();
     }
 
-    function writeStatus($modul, $range_id, $value, $range_type='') {
-        global $SEM_TYPE, $SEM_CLASS, $INST_MODULES;
-        
-        $db = new DB_Seminar;
-        
-        if (!$range_type)
+
+    function writeDefaultStatus($range_id, $range_type = '') {
+        if (!$range_type) {
             $range_type = get_object_type($range_id);
-        
-        if ($range_type == "sem") {
-            $query = sprintf ("SELECT status AS type FROM seminare WHERE Seminar_id ='%s'", $range_id);
-        } else {
-            $query = sprintf ("SELECT type FROM Institute WHERE Institut_id ='%s'", $range_id);
         }
-        
-        $db->query($query);
-        $db->next_record();
-        
+
+        if ($range_type == 'sem') {
+            $query = "UPDATE seminare SET modules = ? WHERE Seminar_id = ?";
+        } else {
+            $query = "UPDATE Institute SET modules = ? WHERE Institut_id = ?";
+        }
+        $statement = DBManager::get()->prepare($query);
+        $statement->execute(array(
+            $this->getDefaultBinValue($range_id, $range_type),
+            $range_id
+        ));
+        return (bool)$statement->rowCount();
+    }
+
+    function writeStatus($modul, $range_id, $value, $range_type = '') {
+        if (!$range_type) {
+            $range_type = get_object_type($range_id);
+        }
+
         $bitmask = $this->getBin($range_id, $range_type);
-        
-        if ($value)
-            $this->setBit($bitmask, $this->registered_modules[$modul]["id"]);
-        else
-            $this->clearBit($bitmask, $this->registered_modules[$modul]["id"]);
-            
-        if ($range_type == "sem") {
-            if ($this->checkGlobal($modul)) {
-                $query = sprintf ("UPDATE seminare SET modules = '%s' WHERE Seminar_id ='%s'", $bitmask, $range_id);
-                $db->query($query);
-                if ($db->affected_rows())
-                    return TRUE;
-                else 
-                    return FALSE;
-            } else 
-                return FALSE;
+
+        if ($value) {
+            $this->setBit($bitmask, $this->registered_modules[$modul]['id']);
         } else {
-            if ($this->checkGlobal($modul)) {
-                $query = sprintf ("UPDATE Institute SET modules = '%s' WHERE Institut_id ='%s'", $bitmask, $range_id);
-                $db->query($query);
-                if ($db->affected_rows())
-                    return TRUE;
-                else 
-                    return FALSE;
-            } else 
-                return FALSE;
+            $this->clearBit($bitmask, $this->registered_modules[$modul]['id']);
         }
+
+        if ($this->checkGlobal($modul)) {
+            return false;
+        }
+        
+        if ($range_type == 'sem') {
+            $query = "UPDATE seminare SET modules = ? WHERE Seminar_id = ?";
+        } else {
+            $query = "UPDATE Institute SET modules = ? WHERE Institut_id = ?";
+        }
+        $statement = DBManager::get()->prepare($query);
+        $statement->execute(array($bitmask, $range_id));
+        return (bool)$statement->rowCount();
     }
 
     function checkGlobal($modul) {
-        if ($this->registered_modules[$modul]["const"]) {
-            if ($GLOBALS[$this->registered_modules[$modul]["const"]])
-                return TRUE;
-            else
-                return FALSE;
-        } else
-            return TRUE;
+        $const = $this->registered_modules[$modul]['const'];
+        return !$const or $GLOBALS[$const];
     }
-    
-    function checkLocal($modul, $range_id, $range_type='') {
-        if ($this->getStatus($modul, $range_id, $range_type))
-            return TRUE;
-        else
-            return FALSE;
-    }
-    
-    function isEnableable ($modul, $range_id, $range_type='') {
 
-        if (!$range_type)
+    function checkLocal($modul, $range_id, $range_type = '') {
+        return $this->getStatus($modul, $range_id, $range_type);
+    }
+
+    function isEnableable($modul, $range_id, $range_type = '') {
+        if (!$range_type) {
             $range_type = get_object_type($range_id);
-        
-        if ($range_type == "sem") {
-            if (($this->checkGlobal($modul)) && ($this->registered_modules[$modul]["sem"] == TRUE))
-                return TRUE;
-            else
-                return FALSE;
-        } else {
-            if (($this->checkGlobal($modul))  && ($this->registered_modules[$modul]["inst"] == TRUE))
-                return TRUE;
-            else
-                return FALSE;
         }
-    }
-    
-    function setBit(&$bitField,$n) { 
-        // Ueberprueft, ob der Wert zwischen 0-31 liegt 
-        // $n ist hier der Wert der aktivierten Checkbox, z.B. 15 
-        // Somit waere hier die 15. Checkbox aktiviert 
-        if(($n < 0) or ($n > 31)) return false;  
-       
-       
-        // Bit Shifting 
-        // Hier wird nun der Binaerwert fuer die aktuelle Checkbox gesetzt. 
-        // In unserem Beispiel wird hier nun die 15. Stelle von rechts auf 1 gesetzt 
-        // 100000000000000 <-- Dieses entspricht der Zahl 16384 
-        // | ist nicht das logische ODER sondern das BIT-oder 
-        $bitField |= (0x01 << ($n)); 
-        return true; 
+        $type = ($range_type == 'sem' ? 'sem' : 'inst');
+        return $this->checkGlobal($modul) and $this->registered_modules[$modul][$type];
     }
 
-    function clearBit(&$bitField,$n) { 
-        // Loescht ein Bit oder ein Bitfeld 
-        // & ist nicht das logische UND sondern das BIT-and 
-        $bitField &= ~(0x01 << ($n)); 
-        return true; 
+    function setBit(&$bitField,$n) {
+        // Ueberprueft, ob der Wert zwischen 0-31 liegt
+        // $n ist hier der Wert der aktivierten Checkbox, z.B. 15
+        // Somit waere hier die 15. Checkbox aktiviert
+        if ($n < 0 or $n > 31) {
+            return false;
+        }
+
+        // Bit Shifting
+        // Hier wird nun der Binaerwert fuer die aktuelle Checkbox gesetzt.
+        // In unserem Beispiel wird hier nun die 15. Stelle von rechts auf 1 gesetzt
+        // 100000000000000 <-- Dieses entspricht der Zahl 16384
+        // | ist nicht das logische ODER sondern das BIT-oder
+        $bitField |= (0x01 << $n);
+        return true;
     }
 
-    function isBit($bitField,$n) { 
-        // Ist die x-te Stelle eine 1? 
-        return (($bitField & (0x01 << ($n)))); 
+    function clearBit(&$bitField, $n) {
+        // Loescht ein Bit oder ein Bitfeld
+        // & ist nicht das logische UND sondern das BIT-and
+        $bitField &= ~(0x01 << ($n));
+        return true;
     }
-    
+
+    function isBit($bitField, $n) {
+        // Ist die x-te Stelle eine 1?
+        return $bitField & (0x01 << $n);
+    }
 }

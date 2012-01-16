d043bad1c5ee3a031aca311720d3d6bbb35b0ab6
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-01-16 20:05:57 +0000

    enhanced exception handling's output in development mode, re #2387
    git-svn-id: svn://develop.studip.de/studip/trunk@21899 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/unhandled_exception.php
+++ templates/unhandled_exception.php
@@ -6,12 +6,33 @@ require_once 'lib/classes/MessageBox.class.php';
 include 'lib/include/html_head.inc.php';
 
 $current_page = _('Fehler');
+
+$title = _('Fehler! Bitte wenden Sie sich an Ihren Systemadministrator.');
+$details = array(htmlentities($exception->getMessage()));
+
+if (Studip\ENV == 'development') {
+    $title = "Houston, we've got a problem.";
+    $details = array();
+
+    do {
+        $details[] = 'Type: ' . get_class($exception);
+        $details[] = 'Message: '.htmlentities($exception->getMessage());
+        $details[] = 'Code: ' . $exception->getCode();
+
+        $trace = sprintf("#$ %s(%u)\n", $exception->getFile(), $exception->getLine())
+               . $exception->getTraceAsString();
+        $trace = str_replace($GLOBALS['STUDIP_BASE_PATH'] . '/', '', $trace);
+        $details[] = 'Stack trace:<br>'
+                   . nl2br(htmlentities($trace))
+                   . '<br><br>'; // Separator from potential previous exception
+    } while ($exception = $exception->getPrevious());
+}
 ?>
 
 <?= $this->render_partial('header', compact('current_page')) ?>
 
 <div style="background-color: white; width: 70%; padding: 1em; margin: auto;">
-    <?= MessageBox::exception(_('Fehler! Bitte wenden Sie sich an Ihren Systemadministrator.'), array(htmlentities($exception->getMessage()))) ?>
+    <?= MessageBox::exception($title, $details) ?>
     <p>
       <?= _('Zurück zur') ?> <a href="<?= URLHelper::getLink('index.php') ?>"><?= _('Startseite') ?></a>
     </p>

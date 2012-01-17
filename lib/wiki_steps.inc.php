a3c05af87bf2396396fca03b2fafe153b6f1539c
nadine <nadine@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-01-17 11:23:35 +0000

    make Button entfernt
    git-svn-id: svn://develop.studip.de/studip/trunk@21909 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/wiki_steps.inc.php
+++ lib/wiki_steps.inc.php
@@ -45,7 +45,7 @@ $step_templates['step'] = array(
 <tr><td>Foren-Thema erzeugen:</td>
 <td><input type="checkbox" name="step_create_topic" value="1" checked></td></tr>
 
-<tr><td>&nbsp;</td><td><input type="image" '.makeButton('eintragen','src') . ' border=0></td></tr>
+<tr><td>&nbsp;</td><td> '.Button::create(_('eintragen')). '</td></tr>
 </table>',
     // template is evaluated alter to form default text
     // important: make sure that variables evaluate at the right time

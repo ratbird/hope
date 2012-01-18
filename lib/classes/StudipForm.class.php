a87f60789a5a83eb0d9a1956fb6b69e592cd9869
sewert <sewert@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-01-18 16:47:31 +0000

    namespace for buttons added, refs #2357
    git-svn-id: svn://develop.studip.de/studip/trunk@21953 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/classes/StudipForm.class.php
+++ lib/classes/StudipForm.class.php
@@ -24,6 +24,7 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // +---------------------------------------------------------------------------+
 
+use Studip\Button, Studip\LinkButton; 
 
 require_once('lib/visual.inc.php');
 require_once 'lib/functions.php';

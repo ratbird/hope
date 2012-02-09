e467f2108c278cbca478b951e2989dba45ad64bc
mlunzena <mlunzena@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-02-09 07:35:12 +0000

    fix "signature" button's markup, refs #2422
    git-svn-id: svn://develop.studip.de/studip/trunk@22162 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/assets/javascripts/application.js
+++ public/assets/javascripts/application.js
@@ -450,14 +450,14 @@ STUDIP.study_area_selection = {
  * ------------------------------------------------------------------------ */
 STUDIP.Markup = {
     buttonSet: [
-        {"name": "bold",          "label": "<strong>B</strong>", open: "**",     close: "**"},
-        {"name": "italic",        "label": "<em>i</em>",         open: "%%",     close: "%%"},
-        {"name": "underline",     "label": "<u>u</u>",           open: "__",     close: "__"},
-        {"name": "strikethrough", "label": "<del>u</del>",       open: "{-",     close: "-}"},
-        {"name": "code",          "label": "code",               open: "[code]", close: "[/code]"},
-        {"name": "larger",        "label": "A+",                 open: "++",     close: "++"},
-        {"name": "smaller",       "label": "A-",                 open: "--",     close: "--"},
-        {"name": "signature",     "label": "signature",          open: "--~~~",  close: ""}
+        {"name": "bold",          "label": "<strong>B</strong>", open: "**",         close: "**"},
+        {"name": "italic",        "label": "<em>i</em>",         open: "%%",         close: "%%"},
+        {"name": "underline",     "label": "<u>u</u>",           open: "__",         close: "__"},
+        {"name": "strikethrough", "label": "<del>u</del>",       open: "{-",         close: "-}"},
+        {"name": "code",          "label": "code",               open: "[code]",     close: "[/code]"},
+        {"name": "larger",        "label": "A+",                 open: "++",         close: "++"},
+        {"name": "smaller",       "label": "A-",                 open: "--",         close: "--"},
+        {"name": "signature",     "label": "signature",          open: "\u2013~~~",  close: ""}
     ]
 };
 

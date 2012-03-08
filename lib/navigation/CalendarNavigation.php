fe26c716e1551f673b1d6a50efeb8a5fcfc90213
pthiene <pthiene@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-03-08 13:52:36 +0000

    Kalender: aktuelle Zeit wird in Navigation übergeben, fixes #2495
    git-svn-id: svn://develop.studip.de/studip/trunk@22385 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/navigation/CalendarNavigation.php
+++ lib/navigation/CalendarNavigation.php
@@ -37,20 +37,21 @@ class CalendarNavigation extends Navigation
      */
     public function initSubNavigation()
     {
-        global $perm;
+        global $perm, $atime;
 
         parent::initSubNavigation();
 
         // calendar
+        $atime = $atime ? intval($atime) : Request::int($atime);
         if (get_config('CALENDAR_ENABLE')) {
             $navigation = new Navigation(_('Terminkalender'), 'calendar.php', array('caluser' => 'self'));
-            $navigation->addSubNavigation('day', new Navigation(_('Tag'), 'calendar.php', array('cmd' => 'showday')));
-            $navigation->addSubNavigation('week', new Navigation(_('Woche'), 'calendar.php', array('cmd' => 'showweek')));
-            $navigation->addSubNavigation('month', new Navigation(_('Monat'), 'calendar.php', array('cmd' => 'showmonth')));
-            $navigation->addSubNavigation('year', new Navigation(_('Jahr'), 'calendar.php', array('cmd' => 'showyear')));
-            $navigation->addSubNavigation('edit', new Navigation(_('Termin anlegen/bearbeiten'), 'calendar.php', array('cmd' => 'edit')));
-            $navigation->addSubNavigation('course', new Navigation(_('Veranstaltungstermine'), 'calendar.php', array('cmd' => 'bind')));
-            $navigation->addSubNavigation('export', new Navigation(_('Export/Sync'), 'calendar.php', array('cmd' => 'export')));
+            $navigation->addSubNavigation('day', new Navigation(_('Tag'), 'calendar.php', array('cmd' => 'showday', 'atime' => $atime)));
+            $navigation->addSubNavigation('week', new Navigation(_('Woche'), 'calendar.php', array('cmd' => 'showweek', 'atime' => $atime)));
+            $navigation->addSubNavigation('month', new Navigation(_('Monat'), 'calendar.php', array('cmd' => 'showmonth', 'atime' => $atime)));
+            $navigation->addSubNavigation('year', new Navigation(_('Jahr'), 'calendar.php', array('cmd' => 'showyear', 'atime' => $atime)));
+            $navigation->addSubNavigation('edit', new Navigation(_('Termin anlegen/bearbeiten'), 'calendar.php', array('cmd' => 'edit', 'atime' => $atime)));
+            $navigation->addSubNavigation('course', new Navigation(_('Veranstaltungstermine'), 'calendar.php', array('cmd' => 'bind', 'atime' => $atime)));
+            $navigation->addSubNavigation('export', new Navigation(_('Export/Sync'), 'calendar.php', array('cmd' => 'export', 'atime' => $atime)));
             if (get_config('CALENDAR_GROUP_ENABLE')) {
                 $navigation->addSubNavigation('admin_groups', new Navigation(_('Kalendergruppen'), 'contact_statusgruppen.php', array('nav' => 'calendar')));
             }

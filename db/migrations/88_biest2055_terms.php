2c582469da9632961e50f97cda70824cb70fc16d
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-03-19 14:13:33 +0000

    fixes #2505
    git-svn-id: svn://develop.studip.de/studip/trunk@22469 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- db/migrations/88_biest2055_terms.php
+++ db/migrations/88_biest2055_terms.php
@@ -11,7 +11,7 @@ class Biest2055Terms extends Migration
     {
         $sql = "INSERT INTO `user_config` ".
             "(`userconfig_id`, `parent_id`, `user_id`, `field`, `value`, `mkdate`, `chdate`, `comment`) ".
-            "VALUES " . join(",", $this->getValues());
+            "SELECT MD5(CONCAT(user_id,'TERMS_ACCEPTED')), NULL, user_id, 'TERMS_ACCEPTED', 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(),'' FROM auth_user_md5";
         DBManager::get()->exec($sql);
     }
 
@@ -19,25 +19,5 @@ class Biest2055Terms extends Migration
     {
         $sql = 'DELETE FROM `user_config` WHERE `field` = "TERMS_ACCEPTED"';
         DBManager::get()->exec($sql);
-    }
-
-    function getValues()
-    {
-        return array_map(array($this, 'getValue'), $this->getUserIds());
-    }
-
-    function getValue($id)
-    {
-        $now = time();
-        $field = 'TERMS_ACCEPTED';
-        return sprintf("('%s', NULL, '%s', '%s', '1', %d, %d, '')",
-                       md5($id . "TERMS_ACCEPTED"),
-                       $id, $field, $now, $now);
-
-    }
-
-    function getUserIds()
-    {
-        return DBManager::get()->query("SELECT user_id FROM auth_user_md5")->fetchAll(PDO::FETCH_COLUMN);
     }
 }

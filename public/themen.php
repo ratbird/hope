d84dbf48941469de278ccd0e290010130c89ef2d
nadine <nadine@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-03-30 11:27:44 +0000

    unregister_globals re#1922
    git-svn-id: svn://develop.studip.de/studip/trunk@22617 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/themen.php
+++ public/themen.php
@@ -26,13 +26,14 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 require '../lib/bootstrap.php';
 
+unregister_globals();
 page_open(array("sess" => "Seminar_Session", "auth" => "Seminar_Auth", "perm" => "Seminar_Perm", "user" => "Seminar_User"));
 $perm->check("tutor");
 
 include ("lib/seminar_open.php"); // initialise Stud.IP-Session
 require_once('lib/raumzeit/raumzeit_functions.inc.php');
 unQuoteAll();
-$sess->register('viewModeFilter');
+//$sess->register('viewModeFilter');
 
 // search for a valid id
 $id = Request::option('cid');
@@ -50,17 +51,17 @@ if (!$id) {
 }
 
 
-if (!$viewModeFilter) {
-    $viewModeFilter = 'simple';
+if (!$_SESSION['viewModeFilter']) {
+    $_SESSION['viewModeFilter'] = 'simple';
 }
 
-if ($cmd == 'changeViewMode') {
-    $viewModeFilter = $_REQUEST['newFilter'];
+if (Request::option('cmd') == 'changeViewMode') {
+    $_SESSION['viewModeFilter'] = $_REQUEST['newFilter'];
 }
 
 // expert view enabled ?
 if(!$GLOBALS["RESOURCES_ENABLE_EXPERT_SCHEDULE_VIEW"]){
-    $viewModeFilter = 'simple';
+    $_SESSION['viewModeFilter'] = 'simple';
 }
 
 PageLayout::setTitle(_("Verwaltung der Themen des Ablaufplans"));
@@ -76,9 +77,9 @@ $header_line = getHeaderLine($id);
 if ($header_line)
     PageLayout::setTitle($header_line." - ".PageLayout::getTitle());
 
-switch ($viewModeFilter) {
+switch ($_SESSION['viewModeFilter']) {
     case 'expert':
-        PageLayout::setHelpKeyword("Basis.VeranstaltungenVerwaltenAblaufplanExpertenansicht");        
+        PageLayout::setHelpKeyword("Basis.VeranstaltungenVerwaltenAblaufplanExpertenansicht");
         include('lib/raumzeit/themen_expert.php');
         break;
 
@@ -86,5 +87,5 @@ switch ($viewModeFilter) {
         PageLayout::setHelpKeyword("Basis.VeranstaltungenVerwaltenAblaufplan");
         include('lib/raumzeit/themen_ablaufplan.php');
         break;
-        
+
 }

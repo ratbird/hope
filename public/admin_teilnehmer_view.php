8d1d6566e3d790ed514331928427c54c06f962b3
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-04-04 08:51:03 +0000

    extracted template, re #82
    git-svn-id: svn://develop.studip.de/studip/trunk@22653 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/admin_teilnehmer_view.php
+++ public/admin_teilnehmer_view.php
@@ -1,6 +1,6 @@
 <?php
 # Lifter001: TEST
-# Lifter002: TODO
+# Lifter002: TEST
 # Lifter007: TODO
 # Lifter003: TODO
 # Lifter010: TODO
@@ -23,111 +23,51 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-use Studip\Button, Studip\LinkButton;
-
 require '../lib/bootstrap.php';
 
 page_open(array("sess" => "Seminar_Session", "auth" => "Seminar_Auth", "perm" => "Seminar_Perm", "user" => "Seminar_User"));
 
-include "lib/seminar_open.php"; //hier werden die sessions initialisiert
-
-require_once ("lib/msg.inc.php");
-require_once ("lib/visual.inc.php");
-require_once ("lib/functions.php");
-require_once ("lib/admission.inc.php"); //Funktionen der Teilnehmerbegrenzung
-require_once ("lib/statusgruppe.inc.php");  //Funktionen der Statusgruppen
-require_once ("lib/messaging.inc.php"); //Funktionen des Nachrichtensystems
-require_once ("config/config.inc.php");     //We need the config for some parameters of the class of the Veranstaltung
-require_once ("lib/classes/Table.class.php");
-require_once ("lib/classes/ZebraTable.class.php");
+include 'lib/seminar_open.php'; //hier werden die sessions initialisiert
 
 PageLayout::setTitle(_("Teilnehmeransicht konfigurieren"));
 Navigation::activateItem('/admin/config/member_view');
 
-// Start  of Output
-include ("lib/include/html_head.inc.php"); // Output of html head
-include ("lib/include/header.php");   //hier wird der "Kopf" nachgeladen
+$GLOBALS['perm']->check('root');
 
-$cssSw=new cssClassSwitcher;
+if (Request::submitted('assign')) {
+    $fields = Request::getArray('fields');
 
-// Aenderungen nur in dem Seminar, in dem ich gerade bin...
+    $update = DBManager::get()->prepare("REPLACE INTO teilnehmer_view (datafield_id, seminar_id) VALUES (?, ?)");
+    $delete = DBManager::get()->prepare("DELETE FROM teilnehmer_view WHERE datafield_id = ? AND seminar_id = ?");
 
-$id = "";
-if (!($auth->auth["perm"] == "root")) die;
-
-if ($cmd == "change") {
-
-    foreach ($_REQUEST as $key => $val) {
-        if ($key[0] == "#") {
-            $zw = substr($key, 1, strlen($key));
-            $zw2 = explode("##", $zw);
-            
-            if ($val == 1) {
-                $query = "REPLACE INTO teilnehmer_view (datafield_id, seminar_id) VALUES (?, ?)";
+    foreach ($fields as $key => $data) {
+        foreach ($data as $field => $value) {
+            if ($value == 1) {
+                $update->execute(array($field, $key));
             } else {
-                $query = "DELETE FROM teilnehmer_view WHERE datafield_id = ? AND seminar_id = ?";
+                $delete->execute(array($field, $key));
             }
-            $statement = DBManager::get()->prepare($query);
-            $statement->execute(array($zw2[0], $zw2[1]));
         }
     }
+
+    PageLayout::postMessage(MessageBox::success(_('Die Konfiguration wurde gespeichert.')));
+    header('Location: ' . URLHelper::getURL());
+    page_close();
+    die;
 }
 
-$tbl_blank = array("class" => "blank", "colspan" => "2");
-
-$table = new ContainerTable(array("cellspacing" => 0, "border" => "0", "width" => "100%", "cellpadding" => "0"));
-$tbl2 = new ZebraTable(array("width" => "99%", "align" => "center"));
-
-// Titelleiste und Leere Zeile
-echo $table->headerRow("&nbsp;<b>". _("Teilnehmeransicht konfigurieren")."</b>", array("colspan" => "3"));
-echo $table->openRow();
-//echo $table->cell("&nbsp;", array("class" => "blank"));
-echo $table->openCell();
-
-// Daten
-echo $tbl2->open();
-
+// Read all active views for datafields from db
 $active = array();
 
-$query = "SELECT * FROM teilnehmer_view WHERE seminar_id IN (?)";
+$query = "SELECT seminar_id, datafield_id FROM teilnehmer_view WHERE seminar_id IN (?)";
 $statement = DBManager::get()->prepare($query);
 $statement->execute(array(array_keys($SEM_CLASS)));
 while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
     $active[ $row['seminar_id'] ][ $row['datafield_id'] ] = true;
 }
 
-echo "<form action=\"". URLHelper::getLink() ."\" method=\"post\">";
-echo CSRFProtection::tokenTag();
-foreach ($SEM_CLASS as $key => $val) {
-    echo $tbl2->headerRow(array("&nbsp;<b>". $val["name"]."</b>", "<b>Status</b>", "<b>Anzeige</b>"));
-    echo $tbl2->closeRow();
-    foreach ($TEILNEHMER_VIEW as $data) {
-        echo $tbl2->openRow();
-        echo $tbl2->cell($data["name"], array("width" => "50%"));
-        echo $tbl2->cell(($active[$key][$data["field"]] ? "<font color=\"green\">". _("Anzeigen erlaubt"). "</font>" : "<font color=\"red\">". _("Anzeigen nicht erlaubt"). "</font>"), array("width" => "25%"));
-        echo $tbl2->cell(sprintf("<input type=\"radio\" name=\"#".$data["field"]."##".$key."\" value=\"1\" %s>". _("erlaubt")."<input type=\"radio\" name=\"#".$data["field"]."##".$key."\" value=\"0\" %s>". _("nicht erlaubt"), ($active[$key][$data["field"]]) ? "checked" : "",($active[$key][$data["field"]]) ? "" : "checked"));
-        echo $tbl2->closeRow();
-    }
-    echo "<input type=\"hidden\" name=\"cmd\" value=\"change\">";
-    echo $table->openRow();
-    echo $table->cell("&nbsp;",array("colspan" => "2"));
-    echo $table->cell(Button::create(_('Zuweisen')));
-    echo $table->closeRow();
-    echo $table->blankRow(array("colspan" => "3"));
-}
-echo "</form>";
-echo $tbl2->close();
-
-// Abschluss für unten (Leere Zeile)
-echo $table->closeCell();
-echo $table->cell("&nbsp;", array("class" => "blank"));
-echo $table->closeRow();
-echo $table->blankCell($tbl_blank);
-// Alles schließen
-echo $table->close();
-
-// Save data back to database.
-page_close();
-?>
-</body>
-</html>
+// Prepare and display template
+$template = $GLOBALS['template_factory']->open('admin/teilnehmer_view.php');
+$template->set_layout('layouts/base_without_infobox');
+$template->active = $active;
+echo $template->render();
---
+++ templates/admin/teilnehmer_view.php
@@ -0,0 +1,54 @@
+<h1><?= _('Teilnehmeransicht konfigurieren') ?></h1>
+
+<form action="<?= URLHelper::getURL() ?>" method="post">
+    <?= CSRFProtection::tokenTag() ?>
+
+<? foreach ($GLOBALS['SEM_CLASS'] as $key => $val): ?>
+    <table class="default">
+        <colgroup>
+            <col width="50%">
+            <col width="25%">
+            <col width="25%">
+        </colgroup>
+        <thead>
+            <tr>
+                <th><?= htmlReady($val['name']) ?></th>
+                <th><?= _('Status') ?></th>
+                <th><?= _('Anzeige') ?></th>
+            </tr>
+        </thead>
+        <tbody>
+        <? foreach ($GLOBALS['TEILNEHMER_VIEW'] as $data): ?>
+            <tr class="<?= TextHelper::cycle('cycle_even', 'cycle_odd') ?>">
+                <td><?= htmlReady($data['name']) ?></td>
+                <td>
+                <? if ($active[$key][ $data['field'] ]): ?>
+                    <?= Assets::img('icons/16/blue/checkbox-checked', array('class' => 'text-top')) ?>
+                    <span style="color: green;"><?= _('Anzeigen erlaubt') ?></span>
+                <? else: ?>
+                    <?= Assets::img('icons/16/blue/checkbox-unchecked', array('class' => 'text-top')) ?>
+                    <span style="color: red;"><?= _('Anzeigen nicht erlaubt') ?></span>
+                <? endif; ?>
+                </td>
+                <td>
+                    <input type="hidden" name="fields[<?= $key ?>][<?= htmlReady($data['field']) ?>]" value="0">
+                    <label>
+                        <input type="checkbox" name="fields[<?= $key ?>][<?= htmlReady($data['field']) ?>]" value="1"
+                               <? if ($active[$key][ $data['field'] ]) echo 'checked'; ?>>
+                        <?= _('erlauben') ?>
+                    </label>
+                </td>
+            </tr>
+        <? endforeach; ?>
+        </tbody>
+        <tfoot>
+            <tr>
+                <td colspan="2">&nbsp;</td>
+                <td><?= Studip\Button::create(_('Zuweisen'), 'assign') ?></td>
+            </tr>
+        </tfoot>
+    </table>
+
+    <br>
+<? endforeach; ?>
+</form>

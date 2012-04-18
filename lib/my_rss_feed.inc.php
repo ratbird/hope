e1b14334858103d3e824eb81a5edeb72e40a6e39
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-04-18 08:12:10 +0000

    eliminate DB_Seminar
    git-svn-id: svn://develop.studip.de/studip/trunk@22726 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/my_rss_feed.inc.php
+++ lib/my_rss_feed.inc.php
@@ -1,7 +1,7 @@
 <?
 # Lifter002: TODO
 # Lifter007: TODO
-# Lifter003: TODO
+# Lifter003: TEST
 # Lifter010: TODO
 /**
 * my_rss_feed.inc.php
@@ -39,12 +39,19 @@ function print_rss($username) {
 
     global $view,$PHP_SELF,$auth;
 
-    $db=new DB_Seminar;
     $cssSw=new cssClassSwitcher;
 
     $cssSw->switchClass();
 
-    $db->query(sprintf("SELECT * FROM auth_user_md5 a, rss_feeds r WHERE a.username='%s' AND a.user_id=r.user_id ORDER BY r.priority",$username));
+    $query = "SELECT feed_id, name, fetch_title, url, hidden
+              FROM auth_user_md5
+              JOIN rss_feeds USING (user_id)
+              WHERE username = ?
+              ORDER BY priority";
+    $statement = DBManager::get()->prepare($query);
+    $statement->execute(array($username));
+    $feeds = $statement->fetchAll(PDO::FETCH_ASSOC);
+    
     echo "<tr><td id=\"main_content\" align=\"left\" valign=\"top\" class=\"blank\"><p class=\"info\"><br>";
     echo _("Hier können Sie beliebige eigene RSS-Feeds einbinden. Diese RSS-Feeds erscheinen auf Ihrer pers&ouml;nlichen Startseite. Mit den Pfeilsymbolen k&ouml;nnen Sie die Reihenfolge, in der die RSS-Feeds angezeigt werden, ver&auml;ndern.");
     echo "<br>\n";
@@ -54,41 +61,40 @@ function print_rss($username) {
     echo "\n<br></p></td></tr>\n<tr><td class=blank><table width=100% class=blank border=0 cellpadding=0 cellspacing=0>";
     echo "<form action=\"$PHP_SELF?rss=update_rss&username=$username&view=$view&show_rss_bsp=$show_rss_bsp\" method=\"POST\" name=\"edit_rss\">";
     echo CSRFProtection::tokenTag();
-    if (!$db->num_rows())
+    if (empty($feeds)) {
         echo "<tr><td class=\"".$cssSw->getClass()."\"><b><p class=\"info\">" . _("Es existieren zur Zeit keine eigenen RSS-Feeds.") . "</p></b></td></tr>\n";
+    }
     echo "<tr><td class=\"".$cssSw->getClass()."\"><p class=\"info\">" . _("RSS-Feed") . "&nbsp; " . LinkButton::create(_("Neuanlegen"), 
                 URLHelper::getURL('', array('rss' => 'create_rss', 'view' => $view, 'username' => $username, 'show_rss_bsp' => $show_rss_bsp))) . "</a></p></td></tr>";
-    $count = 0;
-    while ($db->next_record() ){
 
-            $cssSw->switchClass();
-            $id = $db->f("feed_id");
-            echo "<tr><td class=\"".$cssSw->getClass()."\">";
-            if ($count)
-                echo "<br>";
-            echo "<input type=\"hidden\" name=\"rss_id[]\" value=\"".$db->f("feed_id")."\">\n";
-            echo "<div style=\"padding: 10px; margin: 0px;\"><label>"._("Name:")."<BR><input type=\"text\" name=\"rss_name[]\" id=\"rss_name_$count\" style=\"width: 50%\" value='".htmlReady($db->f("name"))."' size=40></label>";
-            echo "&nbsp; &nbsp; &nbsp; <label><input type=checkbox name=\"rss_fetch_title[$count]\" value=\"1\"";
-            IF ($db->f("fetch_title")=='1') echo " checked";
-            echo ">" . _("Name des Feeds holen") . "</label>&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;";
-            if ($count){
-                echo "\n<a href=\"$PHP_SELF?rss=order_rss&direction=up&username=$username&view=$view&cat_id=" . $db->f("feed_id")
-                . "&show_rss_bsp=$show_rss_bsp\">" . Assets::img('icons/16/yellow/arr_2up.png', array('class' => 'text-top', 'title' =>_('RSS-Feed nach oben verschieben')))
-                . "</a>";
-            }
-            if ($count != ($db->num_rows()-1) ){
-                echo "\n<a href=\"$PHP_SELF?rss=order_rss&direction=down&username=$username&view=$view&cat_id=" . $db->f("feed_id")
-                . "&show_rss_bsp=$show_rss_bsp\">" .  Assets::img('icons/16/yellow/arr_2down.png', array('class' => 'text-top', 'title' =>_('RSS-Feed nach unten verschieben')))
-                . "</a>";
-              }
-            echo "<br>&nbsp;</div></td></tr>";
-            echo "<tr><td class=\"".$cssSw->getClass()."\"><div style=\"padding: 10px; margin: 0px;\"><label>"._("URL:")."<BR><input type='text' name='rss_url[]' style=\"width: 50%\" value='".htmlReady($db->f("url"))."' size=40></label>";
-            echo "&nbsp; &nbsp; &nbsp; <label><input type=checkbox name='rss_secret[$count]' value='1'";
-            IF ($db->f("hidden")=='1') echo " checked";
-            echo ">" . _("unsichtbar") . "</label>&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;";
-            echo "<br><br>" . Button::create(_("Übernehmen"), array('title' => _("verändern")));
-            echo LinkButton::create(_("Löschen"), URLHelper::getURL('', array('rss' => 'delete_rss', 'rss_id' => $id, 'view' => $view, 'username' => $username, 'show_rss_bsp' => $show_rss_bsp))) . "<br>&nbsp; </div></td></tr>";
-            $count++;
+    foreach ($feeds as $index => $feed) {
+        $cssSw->switchClass();
+        echo "<tr><td class=\"".$cssSw->getClass()."\">";
+        if ($index) {
+            echo "<br>";
+        }
+        echo "<input type=\"hidden\" name=\"rss_id[]\" value=\"".$feed['feed_id']."\">\n";
+        echo "<div style=\"padding: 10px; margin: 0px;\"><label>"._("Name:")."<BR><input type=\"text\" name=\"rss_name[]\" id=\"rss_name_$index\" style=\"width: 50%\" value='".htmlReady($feed['name'])."' size=40></label>";
+        echo "&nbsp; &nbsp; &nbsp; <label><input type=checkbox name=\"rss_fetch_title[$index]\" value=\"1\"";
+        if ($feed['fetch_title']=='1') echo " checked";
+        echo ">" . _("Name des Feeds holen") . "</label>&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;";
+        if ($index){
+            echo "\n<a href=\"$PHP_SELF?rss=order_rss&direction=up&username=$username&view=$view&cat_id=" . $feed['feed_id']
+            . "&show_rss_bsp=$show_rss_bsp\">" . Assets::img('icons/16/yellow/arr_2up.png', array('class' => 'text-top', 'title' =>_('RSS-Feed nach oben verschieben')))
+            . "</a>";
+        }
+        if ($index != count($feeds) - 1) {
+            echo "\n<a href=\"$PHP_SELF?rss=order_rss&direction=down&username=$username&view=$view&cat_id=" . $feed['feed_id']
+            . "&show_rss_bsp=$show_rss_bsp\">" .  Assets::img('icons/16/yellow/arr_2down.png', array('class' => 'text-top', 'title' =>_('RSS-Feed nach unten verschieben')))
+            . "</a>";
+          }
+        echo "<br>&nbsp;</div></td></tr>";
+        echo "<tr><td class=\"".$cssSw->getClass()."\"><div style=\"padding: 10px; margin: 0px;\"><label>"._("URL:")."<BR><input type='text' name='rss_url[]' style=\"width: 50%\" value='".htmlReady($feed['url'])."' size=40></label>";
+        echo "&nbsp; &nbsp; &nbsp; <label><input type=checkbox name='rss_secret[$index]' value='1'";
+        if ($feed['hidden']=='1') echo " checked";
+        echo ">" . _("unsichtbar") . "</label>&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;";
+        echo "<br><br>" . Button::create(_("Übernehmen"), array('title' => _("verändern")));
+        echo LinkButton::create(_("Löschen"), URLHelper::getURL('', array('rss' => 'delete_rss', 'rss_id' => $feed['feed_id'], 'view' => $view, 'username' => $username, 'show_rss_bsp' => $show_rss_bsp))) . "<br>&nbsp; </div></td></tr>";
     }
     echo "</form></td></tr></table></td></tr>";
 }
@@ -96,15 +102,19 @@ function print_rss($username) {
 function create_rss() {
     global $username;
 
-    $db=new DB_Seminar;
-    $now = time();
-    $feed_id=md5(uniqid("blablubburegds4"));
-    $db->query ("SELECT user_id FROM auth_user_md5 WHERE username = '$username'");
-    $db->next_record();
-    $user_id = $db->f("user_id");
-    $db->query("UPDATE rss_feeds SET priority=priority+1 WHERE user_id='$user_id'");
-    $db->query("INSERT INTO rss_feeds (feed_id,name, url, mkdate, chdate, user_id,priority,fetch_title,hidden) VALUES ('$feed_id','" . _("neuer Feed") . "','" . _("URL") . "','$now','$now','$user_id',0,1,1)");
-    if ($db->affected_rows() == 0) {
+    $user    = User::findByUsername($username);
+    $feed_id = md5(uniqid('blablubburegds4', true));
+
+    $query = "UPDATE rss_feeds SET priority = priority + 1 WHERE user_id = ?";
+    $statement = DBManager::get()->prepare($query);
+    $statement->execute(array($user->id));
+
+    $query = "INSERT INTO rss_feeds (feed_id, name, url, user_id, priority, fetch_title, hidden, mkdate, chdate)
+              VALUES (?, ?, ?, ?, 0, 1, 1, UNIX_TIMESTAMP(), UNIX_TIMESTAMP())";
+    $statement = DBManager::get()->prepare($query);
+    $statement->execute(array($feed_id, _('neuer Feed'), _('URL'), $user->user_id));
+
+    if ($statement->rowCount() == 0) {
         parse_msg ("info§" . _("Anlegen fehlgeschlagen"));
         die;
     }
@@ -113,16 +123,25 @@ function create_rss() {
 function delete_rss($rss_id) {
     global $username;
 
-    $db=new DB_Seminar;
-    $db->query ("SELECT * FROM rss_feeds LEFT JOIN auth_user_md5 USING (user_id) WHERE username = '$username' and feed_id='$rss_id'");
-    if (!$db->next_record()) { //hier wollte jemand schummeln
+    $query = "SELECT 1
+              FROM rss_feeds
+              LEFT JOIN auth_user_md5 USING (user_id)
+              WHERE username = ? AND feed_id = ?";
+    $statement = DBManager::get()->prepare($query);
+    $statement->execute(array($username, $rss_id));
+    $check = $statement->fetchColumn();
+
+    if (!$check) { //hier wollte jemand schummeln
         parse_msg ("info§" . _("Sie haben leider nicht die notwendige Berechtigung für diese Aktion."));
         die;
-    } else {
-        $db->query("DELETE FROM rss_feeds WHERE feed_id='$rss_id'");
-        if ($db->affected_rows() == 1) {
-            parse_msg ("msg§" . _("RSS-Feed gel&ouml;scht!"));
-        }
+    }
+
+    $query = "DELETE FROM rss_feeds WHERE feed_id = ?";
+    $statement = DBManager::get()->prepare($query);
+    $statement->execute(array($rss_id));
+
+    if ($statement->rowCount() == 1) {
+        parse_msg ("msg§" . _("RSS-Feed gel&ouml;scht!"));
     }
 }
 
@@ -130,16 +149,21 @@ function update_rss() {
     global $rss_id,$rss_name,$rss_url, $rss_secret, $rss_fetch_title;
     check_rss();
     $max = sizeof($rss_id);
+
+    $query = "UPDATE rss_feeds
+              SET name = ?, url = ?, hidden = ?, fetch_title = ?, chdate = UNIX_TIMESTAMP()
+              WHERE feed_id = ?";
+    $update = DBManager::get()->prepare($query);
+
     FOR ($i=0;$i<$max;$i++) {
         if (trim($rss_name[$i])!="" && trim($rss_url[$i])) {
-            $now = time();
-            $db=new DB_Seminar;
             $name = $rss_name[$i];
             $url = $rss_url[$i];
             $secret=$rss_secret[$i];
             $id = $rss_id[$i];
             $fetch_title = $rss_fetch_title[$i];
-            $db->query("UPDATE rss_feeds SET name='$name', url='$url', hidden='$secret',fetch_title='$fetch_title', chdate='$now' WHERE feed_id='$id'");
+            
+            $update->execute(array($name, $url, $secret, $fetch_title, $id));
         }
     }
     $msg[] = array('msg', _("RSS-Feeds ge&auml;ndert!"));
@@ -167,12 +191,13 @@ function check_rss() {
 }
 
 function order_rss($cat_id,$direction,$username){
-    $items_to_order = array();
-    $user_id = get_userid($username);
-    $db = new DB_Seminar("SELECT feed_id FROM rss_feeds WHERE user_id='$user_id' ORDER BY priority");
-    while($db->next_record()) {
-        $items_to_order[] = $db->f("feed_id");
-    }
+    $user = User::findByUsername($username);
+    
+    $query = "SELECT feed_id FROM rss_feeds WHERE user_id = ? ORDER BY priority";
+    $statement = DBManager::get()->prepare($query);
+    $statement->execute(array($user->user_id));
+    $items_to_order = $statement->fetchAll(PDO::FETCH_COLUMN);
+
     for ($i = 0; $i < count($items_to_order); ++$i) {
         if ($cat_id == $items_to_order[$i])
             break;
@@ -184,8 +209,12 @@ function order_rss($cat_id,$direction,$username){
         $items_to_order[$i] = $items_to_order[$i+1];
         $items_to_order[$i+1] = $cat_id;
     }
+
+    $query = "UPDATE rss_feeds SET priority = ? WHERE feed_id = ?";
+    $update = DBManager::get()->prepare($query);
+
     for ($i = 0; $i < count($items_to_order); ++$i) {
-        $db->query("UPDATE rss_feeds SET priority=$i WHERE feed_id='$items_to_order[$i]'");
+        $update->execute(array($i, $items_to_order[$i]));
     }
     $msg[] = array('msg', _("RSS-Feeds wurden neu geordnet"));
     parse_msg_array ($msg,'blank',2,0,1);

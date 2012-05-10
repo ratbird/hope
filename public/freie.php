fa37790b9ac3f01957da921238386c9cf51ab48a
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-05-10 15:07:58 +0000

    eliminate DB_Seminar, re #483
    git-svn-id: svn://develop.studip.de/studip/trunk@23000 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/freie.php
+++ public/freie.php
@@ -1,7 +1,7 @@
 <?php
 # Lifter002: TODO
 # Lifter007: TODO
-# Lifter003: TODO
+# Lifter003: TEST
 # Lifter010: TODO
 /**
 * freie.php
@@ -44,80 +44,107 @@ page_open(array("sess" => "Seminar_Session", "auth" => "Seminar_Default_Auth", "
 
 
 function get_my_sem_values(&$my_sem) {
-    $db2 = new DB_seminar;
-    $my_semids="('".implode("','",array_keys($my_sem))."')";
+    $my_semids = array_keys($my_sem);
 // Postings
-    $db2->query ("SELECT Seminar_id,count(*) as count FROM px_topics WHERE Seminar_id IN ".$my_semids." GROUP BY Seminar_id");
-    while($db2->next_record()) {
-        if ($db2->f('count')) {
-            $nav = new Navigation('forum', 'forum.php?view=reset');
-            $nav->setImage('icons/16/grey/forum.png', array('title' => sprintf(_('%s Postings'), $db2->f('count'))));
-            $my_sem[$db2->f('Seminar_id')]['forum'] = $nav;
-        }
+    $query = "SELECT Seminar_id, COUNT(*) AS count
+              FROM px_topics
+              WHERE Seminar_id IN (?)
+              GROUP BY Seminar_id";
+    $statement = DBManager::get()->prepare($query);
+    $statement->execute(array($my_semids));
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        $nav = new Navigation('forum', 'forum.php?view=reset');
+        $nav->setImage('icons/16/grey/forum.png', array('title' => sprintf(_('%s Postings'), $row['count'])));
+        $my_sem[$row['Seminar_id']]['forum'] = $nav;
     }
 //dokumente
-    $db2->query ("SELECT seminar_id, count(*) as count FROM dokumente WHERE seminar_id IN ".$my_semids." GROUP BY seminar_id");
-    while($db2->next_record()) {
-        if ($db2->f('count')) {
-            $nav = new Navigation('files', 'folder.php?cmd=tree');
-            $nav->setImage('icons/16/grey/files.png', array('title' => sprintf(_('%s Dokumente'), $db2->f('count'))));
-            $my_sem[$db2->f('seminar_id')]['files'] = $nav;
-        }
+    $query = "SELECT seminar_id, COUNT(*) AS count
+              FROM dokumente
+              WHERE seminar_id IN (?)
+              GROUP BY seminar_id";
+    $statement = DBManager::get()->prepare($query);
+    $statement->execute(array($my_semids));
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        $nav = new Navigation('files', 'folder.php?cmd=tree');
+        $nav->setImage('icons/16/grey/files.png', array('title' => sprintf(_('%s Dokumente'), $row['count'])));
+        $my_sem[$row['seminar_id']]['files'] = $nav;
     }
 //Ankündigungen
-    $db2->query ("SELECT range_id,count(*) as count FROM news_range LEFT JOIN news USING(news_id) WHERE range_id IN ".$my_semids." GROUP BY range_id");
-    while($db2->next_record()) {
-        if ($db2->f('count')) {
-            $nav = new Navigation('news', '');
-            $nav->setImage('icons/16/grey/news.png', array('title' => sprintf(_('%s Ankündigungen'), $db2->f('count'))));
-            $my_sem[$db2->f('range_id')]['news'] = $nav;
-        }
+    $query = "SELECT range_id, COUNT(*) AS count
+              FROM news_range
+              LEFT JOIN news USING (news_id)
+              WHERE range_id IN (?)
+              GROUP BY range_id
+              HAVING count > 0";
+    $statement = DBManager::get()->prepare($query);
+    $statement->execute(array($my_semids));
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        $nav = new Navigation('news', '');
+        $nav->setImage('icons/16/grey/news.png', array('title' => sprintf(_('%s Ankündigungen'), $row['count'])));
+        $my_sem[$row['range_id']]['news'] = $nav;
     }
-
-    $db2->query ("SELECT range_id,count(*) as count FROM scm WHERE range_id IN ".$my_semids." GROUP BY range_id");
-    while($db2->next_record()) {
-        if ($db2->f('count')) {
-            $nav = new Navigation('scm', 'scm.php');
-            $nav->setImage('icons/16/grey/infopage.png', array('title' => sprintf(_('%s Einträge'), $db2->f('count'))));
-            $my_sem[$db2->f('range_id')]['scm'] = $nav;
-        }
+// Freie Informationsseite
+    $query = "SELECT range_id, COUNT(*) AS count
+              FROM scm
+              WHERE range_id IN (?)
+              GROUP BY range_id";
+    $statement = DBManager::get()->prepare($query);
+    $statement->execute(array($my_semids));
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        $nav = new Navigation('scm', 'scm.php');
+        $nav->setImage('icons/16/grey/infopage.png', array('title' => sprintf(_('%s Einträge'), $row['count'])));
+        $my_sem[$row['range_id']]['scm'] = $nav;
     }
 // Literatur?
-    $db2->query("SELECT range_id,count(list_id) as count FROM lit_list WHERE range_id IN $my_semids AND visibility=1 GROUP BY range_id");
-    while($db2->next_record()) {
-        if ($db2->f('count')) {
-            $nav = new Navigation('literature', 'literatur.php');
-            $nav->setImage('icons/16/grey/literature.png', array('title' => sprintf(_('%s Literaturlisten'), $db2->f('count'))));
-            $my_sem[$db2->f('range_id')]['literature'] = $nav;
-        }
+    $query = "SELECT range_id, COUNT(list_id) AS count
+              FROM lit_list
+              WHERE range_id IN (?) AND visibility = 1
+              GROUP BY range_id";
+    $statement = DBManager::get()->prepare($query);
+    $statement->execute(array($my_semids));
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        $nav = new Navigation('literature', 'literatur.php');
+        $nav->setImage('icons/16/grey/literature.png', array('title' => sprintf(_('%s Literaturlisten'), $row['count'])));
+        $my_sem[$row['range_id']]['literature'] = $nav;
     }
 //termine
-    $db2->query ("SELECT range_id,count(*) as count FROM termine WHERE range_id IN ".$my_semids." GROUP BY range_id");
-    while($db2->next_record()) {
-        if ($db2->f('count')) {
-            $nav = new Navigation('schedule', 'dates.php');
-            $nav->setImage('icons/16/grey/schedule.png', array('title' => sprintf(_('%s Termine'), $db2->f('count'))));
-            $my_sem[$db2->f('range_id')]['schedule'] = $nav;
-        }
+    $query = "SELECT range_id, COUNT(*) AS count
+              FROM termine
+              WHERE range_id IN (?)
+              GROUP BY range_id";
+    $statement = DBManager::get()->prepare($query);
+    $statement->execute(array($my_semids));
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        $nav = new Navigation('schedule', 'dates.php');
+        $nav->setImage('icons/16/grey/schedule.png', array('title' => sprintf(_('%s Termine'), $row['count'])));
+        $my_sem[$row['range_id']]['schedule'] = $nav;
     }
+// Wiki
     if (get_config('WIKI_ENABLE')) {
-        $db2->query("SELECT range_id, COUNT(DISTINCT keyword) as count FROM wiki  WHERE range_id IN ".$my_semids." GROUP BY range_id");
-        while($db2->next_record()) {
-            if ($db2->f('count')) {
-                $nav = new Navigation('wiki', 'wiki.php');
-                $nav->setImage('icons/16/grey/wiki.png', array('title' => sprintf(_('%s WikiSeiten'), $db2->f('count'))));
-                $my_sem[$db2->f('range_id')]['wiki'] = $nav;
-            }
+        $query = "SELECT range_id, COUNT(DISTINCT keyword) AS count
+                  FROM wiki
+                  WHERE range_id IN (?)
+                  GROUP BY range_id";
+        $statement = DBManager::get()->prepare($query);
+        $statement->execute(array($my_semids));
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+            $nav = new Navigation('wiki', 'wiki.php');
+            $nav->setImage('icons/16/grey/wiki.png', array('title' => sprintf(_('%s WikiSeiten'), $row['count'])));
+            $my_sem[$row['range_id']]['wiki'] = $nav;
         }
     }
+// Votes
     if (get_config('VOTE_ENABLE')) {
-        $db2->query("SELECT range_id,count(vote_id) as count FROM vote  WHERE state IN('active','stopvis') AND range_id IN ".$my_semids." GROUP BY range_id");
-        while($db2->next_record()) {
-            if ($db2->f('count')) {
-                $nav = new Navigation('vote', '#vote');
-                $nav->setImage('icons/16/grey/vote.png', array('title' => sprintf(_('%s Umfrage(n)'), $db2->f('count'))));
-                $my_sem[$db2->f('range_id')]['vote'] = $nav;
-            }
+        $query = "SELECT range_id, COUNT(vote_id) AS count
+                  FROM vote
+                  WHERE state IN ('active','stopvis') AND range_id IN (?)
+                  GROUP BY range_id";
+        $statement = DBManager::get()->prepare($query);
+        $statement->execute(array($my_semids));
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+            $nav = new Navigation('vote', '#vote');
+            $nav->setImage('icons/16/grey/vote.png', array('title' => sprintf(_('%s Umfrage(n)'), $row['count'])));
+            $my_sem[$row['range_id']]['vote'] = $nav;
         }
     }
 
@@ -173,22 +200,23 @@ require_once('lib/visual.inc.php');
 // we are definitely not in an lexture or institute
 closeObject();
 
-
-$db=new DB_Seminar;
-$db2=new DB_Seminar;
-$num_my_sem = false;
+$my_sem = array();
 if(get_config('ENABLE_FREE_ACCESS')){
-    $sortby = Request::option('sortby', 'Name');
-    $db->query("SELECT seminare.*, Institute.Name AS Institut, Institute.Institut_id AS id FROM seminare LEFT JOIN Institute USING (institut_id) WHERE Lesezugriff='0' AND seminare.visible='1' ORDER BY $sortby");
-    $num_my_sem = $db->num_rows();
-}
-
-if ($num_my_sem) {
-    while ($db->next_record()) {
-        $my_sem[$db->f("Seminar_id")]=array("name"=>$db->f("Name"),"status"=>$db->f("status"),"Institut"=>$db->f("Institut"),"id"=>$db->f("id"),"Schreibzugriff"=>$db->f("Schreibzugriff"));
+    $query = "SELECT Seminar_id, seminare.Name AS name, seminare.status, seminare.Schreibzugriff,
+                     Institute.Name AS Institut, Institut_id AS id
+              FROM seminare
+              LEFT JOIN Institute USING (Institut_id)
+              WHERE Lesezugriff = '0' AND seminare.visible = '1'
+              ORDER BY :order";
+    $statement = DBManager::get()->prepare($query);
+    $statement->bindParam(':order', Request::option('sortby', 'Name'), StudipPDO::PARAM_COLUMN);
+    $statement->execute();
+    
+    $my_sem = $statement->fetchGrouped(PDO::FETCH_ASSOC);
+    
+    if (!empty($my_sem)) {
+        get_my_sem_values($my_sem);
     }
-
-    get_my_sem_values($my_sem);
 }
 
 ?>
@@ -207,7 +235,7 @@ if ($num_my_sem) {
     print( _("In der rechten Spalte k&ouml;nnen Sie sehen, was in den einzelnen Veranstaltungen an Inhalten vorhanden ist."));
     print("</blockquote>");
 
-    if (!$num_my_sem) {
+    if (empty($my_sem)) {
         echo MessageBox::info('Es gibt keine Veranstaltungen, die einen freien Zugriff erlauben!');
     }
 ?>
@@ -215,7 +243,7 @@ if ($num_my_sem) {
     <td class="blank"  width="1%" align="right" valign="top"><?=Assets::img('infobox/board1.jpg') ?></td>
 </tr>
 
-<? if ($num_my_sem): ?>
+<? if (!empty($my_sem)): ?>
     <tr><td colspan="2">
     <table class="default">
     <tr>

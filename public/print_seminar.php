0d356089f19358045c0db68c98f90a89fc2e9d86
nadine <nadine@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-05-15 13:06:30 +0000

    re #1922
    git-svn-id: svn://develop.studip.de/studip/trunk@23041 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/print_seminar.php
+++ public/print_seminar.php
@@ -25,6 +25,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 require '../lib/bootstrap.php';
 
+unregister_globals();
 page_open(array("sess" => "Seminar_Session", "auth" => "Seminar_Auth", "perm" => "Seminar_Perm", "user" => "Seminar_User"));
 
 include ('lib/seminar_open.php'); // initialise Stud.IP-Session

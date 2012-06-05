190edd5ab6bbfe21a2ec445cc72033e8b4c043cd
nadine <nadine@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-06-05 12:15:06 +0000

    Variablen angepasst re #1922
    git-svn-id: svn://develop.studip.de/studip/trunk@23209 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/admin_range_tree.php
+++ public/admin_range_tree.php
@@ -38,6 +38,8 @@
 
 require '../lib/bootstrap.php';
 
+unregister_globals();
+
 page_open(array("sess" => "Seminar_Session", "auth" => "Seminar_Auth", "perm" => "Seminar_Perm", "user" => "Seminar_User"));
 $perm->check($RANGE_TREE_ADMIN_PERM ? $RANGE_TREE_ADMIN_PERM  : 'admin');
         

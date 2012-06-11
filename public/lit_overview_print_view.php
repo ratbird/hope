b9638d1dd960f0e37f172e8253d9c8370deb50ed
nadine <nadine@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-06-11 10:47:01 +0000

    Variablen angepasst re #1922
    git-svn-id: svn://develop.studip.de/studip/trunk@23267 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/lit_overview_print_view.php
+++ public/lit_overview_print_view.php
@@ -24,54 +24,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 require '../lib/bootstrap.php';
 unregister_globals();
-function my_session_open($id = false){
-    if (!$id){
-        $id = md5(basename($GLOBALS['PHP_SELF']));
-    }
-    if (!$GLOBALS['sess']->is_registered($id)){
-        $GLOBALS['sess']->register($id);
-    }
-    if (isset($GLOBALS[$id])){
-        $GLOBALS[$id] = unserialize($GLOBALS[$id]);
-    }
-}
-
-function my_session_close($id = false){
-    if (!$id){
-        $id = md5(basename($GLOBALS['PHP_SELF']));
-    }
-    if (isset($GLOBALS[$id])){
-        $GLOBALS[$id] = serialize($GLOBALS[$id]);
-    }
-}
-
-function my_session_var($var, $id = false){
-    if (!$id){
-        $id = md5(basename($GLOBALS['PHP_SELF']));
-    }
-    if (is_array($var)){
-        foreach ($var as $name){
-            if (isset($_REQUEST[$name])){
-                $GLOBALS[$id][$name] = $_REQUEST[$name];
-            } else {
-                $_REQUEST[$name] = $GLOBALS[$id][$name];
-            }
-            $GLOBALS[$name] =& $GLOBALS[$id][$name];
-        }
-    } else {
-        if (isset($_REQUEST[$var])){
-            $GLOBALS[$id][$var] = $_REQUEST[$var];
-        } else {
-            $_REQUEST[$var] = $GLOBALS[$id][$var];
-        }
-        $GLOBALS[$var] =& $GLOBALS[$id][$var];
-    }
-}
 
 page_open(array("sess" => "Seminar_Session", "auth" => "Seminar_Auth", "perm" => "Seminar_Perm", "user" => "Seminar_User"));
-my_session_open(md5('admin_literatur_overview.php'));
 $perm->check('admin');
-
 require_once('lib/classes/StudipLitCatElement.class.php');
 
 include ('lib/seminar_open.php'); // initialise Stud.IP-Session
@@ -82,16 +37,19 @@ PageLayout::addStylesheet('print.css'); // use special stylesheet for printing
 include ('lib/include/html_head.inc.php'); // Output of html head
 
 $element = new StudipLitCatElement();
+if(Request::quotedArray('_lit_data'))
+     $_SESSION['_lit_data'] = Request::quotedArray('_lit_data');
 
-my_session_var(array('_semester_id','_inst_id','_lit_data','_lit_data_id'),md5('admin_literatur_overview.php'));
+
+
 $header = get_object_name($_inst_id, 'inst');
 ?>
 <h1>
 <?=htmlReady(sprintf(_("Literaturliste %s"), $header['type'] . ": " . $header['name']))?>
 </h1>
 <?
-if (is_array($_lit_data)){
-    foreach ($_lit_data as $cid => $data){
+if (is_array($_SESSION['_lit_data'])){
+    foreach ($_SESSION['_lit_data'] as $cid => $data){
         $element->setValues($data);
         if ($element->getValue('catalog_id')){
             $titel = htmlReady($element->getShortName());
@@ -108,7 +66,7 @@ if (is_array($_lit_data)){
             $content .= "<b>" . _("Identifikation:") ."</b>&nbsp;&nbsp;" . htmlReady($element->getValue("dc_identifier")) . "<br>";
             
             $content .= "<b>" . _("Veranstaltungen:") . "</b>&nbsp;&nbsp;";
-            foreach ($_lit_data[$cid]['sem_data'] as $sem_data){
+            foreach ($_SESSION['_lit_data'][$cid]['sem_data'] as $sem_data){
                 $content .= htmlReady(my_substr($sem_data["Name"],0,50)) .', ';
                 $estimated_p += $sem_data['admission_turnout'];
                 $participants += $sem_data['participants'];
@@ -116,7 +74,7 @@ if (is_array($_lit_data)){
             $content = substr($content,0,-2);
             $content .= "<br>";
             $content .= "<b>" . _("Dozenten:") . "</b>&nbsp;&nbsp;";
-            foreach ($_lit_data[$cid]['doz_data'] as $doz_data){
+            foreach ($_SESSION['_lit_data'][$cid]['doz_data'] as $doz_data){
                 $content .= htmlReady($doz_data["Nachname"]) . ", ";
             }
             $content = substr($content,0,-2);
@@ -125,9 +83,9 @@ if (is_array($_lit_data)){
             $content .= ($estimated_p ? $estimated_p : _("unbekannt"));
             $content .= ' / ' . (int)$participants;
             $content .= "<br>";
-            if (is_array($_lit_data[$cid]['check_accession'])){
+            if (is_array($_SESSION['_lit_data'][$cid]['check_accession'])){
                 $content .= "<div style=\"margin-top: 10px;border: 1px solid black;padding: 5px; width:96%;\"<b>" ._("Verf&uuml;gbarkeit in externen Katalogen:") . "</b><br>";
-                foreach ( $_lit_data[$cid]['check_accession'] as $plugin_name => $ret){
+                foreach ( $_SESSION['_lit_data'][$cid]['check_accession'] as $plugin_name => $ret){
                     $content .= "<b>&nbsp;{$plugin_name}&nbsp;</b>";
                     if ($ret['found']){
                         $content .= _("gefunden") . "&nbsp;";
@@ -149,6 +107,5 @@ if (is_array($_lit_data)){
 
 include ('lib/include/html_end.inc.php');
 // Save data back to database.
-my_session_close(md5('admin_literatur_overview.php'));
 page_close();
 ?>

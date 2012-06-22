ba2d53483b9893c532b4c80fe6f75957837d8188
nadine <nadine@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-06-22 10:16:06 +0000

    re #1922
    git-svn-id: svn://develop.studip.de/studip/trunk@23432 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- tools/resources_extern/sem_plan_view.inc.php
+++ tools/resources_extern/sem_plan_view.inc.php
@@ -1,4 +1,4 @@
-<a href="<?=$PHP_SELF?>?semester_id=<?=$_semester_id?>&timespan=<?=$_timespan?>" style="font-size:90%">
+<a href="<?=URLHelper::getLink('?semester_id='.$_semester_id.'&timespan='.$_timespan)?>" style="font-size:90%">
 <img src="pictures/pfeil.gif" border="0" align="absmiddle">
 zur&uuml;ck zur Startseite
 </a>
@@ -7,7 +7,7 @@ show_sem_chooser($_semester_id, $_timespan);
 show_sem_plan($_REQUEST['rid'], $_semester_id, $_timespan);
 ?>
 <br>
-<a href="<?=$PHP_SELF?>?semester_id=<?=$_semester_id?>&timespan=<?=$_timespan?>" style="font-size:90%">
+<a href="<?=URLHelper::getLink('?semester_id='.$_semester_id.'&timespan='.$_timespan)?>" style="font-size:90%">
 <img src="pictures/pfeil.gif" border="0" align="absmiddle">
 zur&uuml;ck zur Startseite
 </a>

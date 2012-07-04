895d681a41f513edf44d392bc91e485a2a48d503
nadine <nadine@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-07-04 11:01:02 +0000

    re #1922
    git-svn-id: svn://develop.studip.de/studip/trunk@23569 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/rss.php
+++ public/rss.php
@@ -24,6 +24,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 require '../lib/bootstrap.php';
 
+unregister_globals();
 ob_end_clean();
 ob_start();
 require_once('lib/showNews.inc.php');

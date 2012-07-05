d48ea387b11e40c58d32d07d52aa44100232a492
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-07-05 06:53:13 +0000

    refine [23558], re #2780
    git-svn-id: svn://develop.studip.de/studip/trunk@23570 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/assets/javascripts/audio.js
+++ public/assets/javascripts/audio.js
@@ -4,7 +4,7 @@
         loaded      = false,
         queue       = [],
         load_audioplayer = function () {
-            AudioPlayer.setup(STUDIP.URLHelper.getURL('assets/flash/player.swf'), {
+            AudioPlayer.setup(STUDIP.ASSETS_URL + 'flash/player.swf', {
                 animation        : 'no',
                 transparentpagebg: 'yes',
                 width            : 300
@@ -20,7 +20,7 @@
         initialise = function () {
             if (!initialised) {
                 var script = document.createElement('script');
-                script.src    = STUDIP.URLHelper.getURL('assets/javascripts/audio-player.js');
+                script.src    = STUDIP.ASSETS_URL + 'javascripts/audio-player.js';
                 script.onload = load_audioplayer;
                 document.getElementsByTagName('head')[0].appendChild(script);
                 initialised = true;

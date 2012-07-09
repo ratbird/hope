7be30ed456bc7249ab7714d14d47ab5ed6ef89c7
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-07-09 11:57:07 +0000

    refs #2758 add partial
    git-svn-id: svn://develop.studip.de/studip/trunk@23621 c5b85792-5220-0410-ad23-ed5b6b8ad999

---
+++ app/views/admin/sem_classes/_sem_type.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ *  Copyright (c) 2012  Rasmus Fuhse <fuhse@data-quest.de>
+ * 
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation; either version 2 of
+ *  the License, or (at your option) any later version.
+ */
+$number_of_seminars = $sem_type->countSeminars();
+$id = $sem_type['id'];
+?>
+<li id="sem_type_<?= htmlReady($id) ?>">
+    <span class="name_container">
+        <span class="name_html">
+            <?= htmlReady($sem_type['name']) ?>
+        </span>
+        <span class="name_input" style="display: none;">
+            <input type="text" value="<?= htmlReady($sem_type['name']) ?>">
+        </span>
+    </span>
+    (<?= sprintf(_("%s Veranstaltungen"), $number_of_seminars ? $number_of_seminars : _("keine")) ?>)
+    <a href="#" class="sem_type_edit" onClick="jQuery(this).closest('li').find('.name_container').children().toggle(); return false;" title="<?= _("Seminartyp umbenennen") ?>"><?= Assets::img("icons/16/blue/edit.png") ?></a>
+    <? if ($number_of_seminars == 0) : ?>
+        <a href="#" class="sem_type_delete" onClick="return false;" title="<?= _("Seminartyp löschen") ?>"><?= Assets::img("icons/16/blue/trash.png") ?></a>
+    <? endif ?>
+</li>

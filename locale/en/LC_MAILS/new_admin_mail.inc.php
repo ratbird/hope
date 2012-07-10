ed969cce786dee7d51c2faa8bfc4239b6ef18153
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-07-10 16:12:26 +0000

    forgot adjusted mail, refines [23639], re #483
    git-svn-id: svn://develop.studip.de/studip/trunk@23641 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- locale/en/LC_MAILS/new_admin_mail.inc.php
+++ locale/en/LC_MAILS/new_admin_mail.inc.php
@@ -1,7 +1,8 @@
 <?
-        $subject = "New administrator in your institution";
-        
-        $mailbody = sprintf("Dear %s %s,\n\n"
-        ."%s %s has been registered as an administrator in the institution '%s' and will support you from now on in handling with StudIP.",$db->f('Vorname'),$db->f('Nachname'),$vorname,$nachname,$instname);
-        
-?>
+    $subject = 'New administrator in your institution';    
+    $mailbody = sprintf("Dear %s %s,\n\n"
+                       ."%s %s has been registered as an administrator in the institution '%s' "
+                       ."and will support you from now on in handling with StudIP.",
+                       $row['Vorname'], $row['Nachname'],
+                       $vorname, $nachname,
+                       $instname);

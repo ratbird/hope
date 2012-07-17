05a0b7cc225aaecf5fad2a15f351ba5d0a03303d
strehl <strehl@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-07-17 12:52:37 +0000

    fix dozent-edit re #2762
    git-svn-id: svn://develop.studip.de/studip/trunk@23719 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/controllers/course/basicdata.php
+++ app/controllers/course/basicdata.php
@@ -392,7 +392,7 @@ class Course_BasicdataController extends AuthenticatedController
             }
 		         //check if required datafield was not filled out
 		         $sem_data = $sem->getData();
-		    $dataFieldStructures = DataFieldStructure::getDataFieldStructures('sem',$sem_data['form'], true);
+		    $dataFieldStructures = DataFieldStructure::getDataFieldStructures('sem',$sem_data['status'], true);
 		    foreach ((array)$dataFieldStructures as $id=>$struct) {
 		        if ($struct->accessAllowed($perm) && $perm->have_perm($struct->getEditPerms()) && $struct->getIsRequired() ) {
 		           if (! trim($_REQUEST['datafields'][$id])&& !in_array($struct->getName(), $invalid_datafields))

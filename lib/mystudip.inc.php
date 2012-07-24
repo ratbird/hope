e9ad3dceba80c55afb9cd47bbc2548593b08c5ed
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-07-24 11:40:35 +0000

    open wikipedia in new window, fixes #2126
    git-svn-id: svn://develop.studip.de/studip/trunk@23786 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/mystudip.inc.php
+++ lib/mystudip.inc.php
@@ -157,7 +157,7 @@ function change_general_view() {
                         <? print _("Mit dieser Einstellung k&ouml;nnen Sie f&uuml;r die meisten in der Kopfzeile erreichbaren Hauptfunktionen eine Bedienung "
                         . "&uuml;ber Tastenkombinationen aktivieren. <br>Die Tastenkombination wird im Tooltip des jeweiligen Icons angezeigt. "
                         . "Diese kann für jeden Browser und jedes Betriebssystem unterschiedlich sein (siehe <a href='"
-                        . URLHelper::getLink('http://en.wikipedia.org/wiki/Accesskey', NULL)."'>Wikipedia</a>)"); ?>
+                        . URLHelper::getLink('http://en.wikipedia.org/wiki/Accesskey', NULL)."' target='_blank'>Wikipedia</a>)"); ?>
                         </div>
                     </td>
                     <td <?=$cssSw->getFullClass()?>>

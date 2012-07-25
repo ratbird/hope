54263f6fd753ad8e8e764567adac746c782ff5ec
nadine <nadine@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-07-25 11:44:22 +0000

    re #1922
    git-svn-id: svn://develop.studip.de/studip/trunk@23865 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/controllers/admin/role.php
+++ app/controllers/admin/role.php
@@ -38,7 +38,7 @@ class Admin_RoleController extends AuthenticatedController
 
     /**
      * Validate ticket (passed via request environment).
-     * This method always checks $_REQUEST['ticket'].
+     * This method always checks Request::quoted('ticket').
      *
      * @throws InvalidArgumentException  if ticket is not valid
      */

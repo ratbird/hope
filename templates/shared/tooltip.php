0aefae45992fc727049df80fdd7d292bf59ce6cc
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-07-30 11:26:42 +0000

    enable tooltip to be marked as important (used by the resources administration for example), re #2563
    git-svn-id: svn://develop.studip.de/studip/trunk@23916 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/shared/tooltip.php
+++ templates/shared/tooltip.php
@@ -1,8 +1,8 @@
 <a class="tooltip">
 <? if ($important): ?>
-    <?= Assets::img('icons/16/grey/info-circle.png', array('class' => 'text-top')) ?>
-<? else: ?>
     <?= Assets::img('icons/16/red/info-circle.png', array('class' => 'text-top')) ?>
+<? else: ?>
+    <?= Assets::img('icons/16/grey/info-circle.png', array('class' => 'text-top')) ?>
 <? endif; ?>
     <span><?= $text ?></span>
 </a>

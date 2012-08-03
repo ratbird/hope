2ce642ec00ec8b6189ce9f4ef0d9b3ddab9aac2a
nadine <nadine@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-08-03 09:37:25 +0000

    re #483
    git-svn-id: svn://develop.studip.de/studip/trunk@23983 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/extern/modules/ExternModuleDownload.class.php
+++ lib/extern/modules/ExternModuleDownload.class.php
@@ -105,7 +105,7 @@ class ExternModuleDownload extends ExternModule {
     }
 
     function toString ($args = NULL) {
-        $db = new DB_Seminar();
+        
         $error_message = "";
 
         // check for valid range_id
@@ -116,10 +116,15 @@ class ExternModuleDownload extends ExternModule {
         if ($args['seminar_id']) {
             $seminar_id = $args['seminar_id'];
             $query = "SELECT Lesezugriff FROM seminare s LEFT JOIN seminar_inst si ";
-            $query .= "USING(seminar_id) WHERE s.seminar_id='$seminar_id' ";
-            $query .= "AND si.institut_id='{$this->config->range_id}'";
-            $db->query($query);
-            if (!($db->next_record() && $db->f('Lesezugriff') == 0))
+            $query .= "USING(seminar_id) WHERE s.seminar_id = ? ";
+            $query .= "AND si.institut_id = ?";
+
+            $parameters = array($seminar_id, $this->config->range_id);
+            $statement = DBManager::get()->prepare($query);
+            $statement->execute($parameters);
+            $row = $statement->fetch(PDO::FETCH_ASSOC);
+            
+            if ($row === false && $row['Lesezugriff'] == 0)
                 $error_message = $GLOBALS["EXTERN_ERROR_MESSAGE"];
         } else {
             $seminar_id = $this->config->range_id;
@@ -146,12 +151,15 @@ class ExternModuleDownload extends ExternModule {
         $query .= $GLOBALS["_fullname_sql"][$nameformat];
         $query .= "AS fullname, username, aum.user_id, author_name FROM dokumente d LEFT JOIN user_info USING (user_id) ";
         $query .= "LEFT JOIN auth_user_md5 aum USING (user_id) WHERE ";
-        $query .= "seminar_id='$seminar_id' AND range_id IN ('";
+        $query .= "seminar_id = ? AND range_id IN ('";
         $query .= implode("','", $allowed_folders) . "')$query_order";
 
-        $db->query($query);
+        $parameters = array($seminar_id);
+        $statement = DBManager::get()->prepare($query);
+        $statement->execute($parameters);
+        $row = $statement->fetch(PDO::FETCH_ASSOC);
 
-        if (!$db->num_rows()) {
+        if ($row === false) {
             $error_message = $this->config->getValue("Main", "nodatatext");
         }
 
@@ -165,9 +173,8 @@ class ExternModuleDownload extends ExternModule {
             $out = $this->elements['TableRow']->toString(array('content' => array('' => $error_message)));
         } else {
             $table_row_data["data_fields"] = $this->data_fields;
-            while ($db->next_record()) {
-
-                preg_match("/^.+\.([a-z1-9_-]+)$/i", $db->f("filename"), $file_suffix);
+            do{
+                preg_match("/^.+\.([a-z1-9_-]+)$/i", $row['filename'], $file_suffix);
 
                 $icon = "";
                 switch ($file_suffix[1]) {
@@ -214,7 +221,7 @@ class ExternModuleDownload extends ExternModule {
                     $picture_file = $GLOBALS['ASSETS_URL']."images/$icon";
                 }
                 
-                $download_link = GetDownloadLink($db->f('dokument_id'), $db->f('filename'));
+                $download_link = GetDownloadLink($row['dokument_id'], $row['filename']);
                 
                 // Aufbereiten der Daten
                 $table_row_data["content"] = array(
@@ -222,28 +229,28 @@ class ExternModuleDownload extends ExternModule {
                                                         , $download_link, $picture_file),
                                                                              
                     "filename"    => $this->elements["Link"]->toString(array("content" =>
-                                                        htmlReady($db->f("filename")), "link" => $download_link)),
+                                                        htmlReady($row['filename']), "link" => $download_link)),
                                                          
-                    "description" => htmlReady(mila_extern($db->f("description"),
+                    "description" => htmlReady(mila_extern($row['description'],
                                                      $this->config->getValue("Main", "lengthdesc"))),
                     
-                    "mkdate"      => strftime($this->config->getValue("Main", "dateformat"), $db->f("mkdate")),
+                    "mkdate"      => strftime($this->config->getValue("Main", "dateformat"), $row['mkdate']),
                     
-                    "filesize"    => $db->f("filesize") > 1048576 ? round($db->f("filesize") / 1048576, 1) . " MB"
-                                                        : round($db->f("filesize") / 1024, 1) . " kB",
+                    "filesize"    => $row['filesize'] > 1048576 ? round($row['filesize'] / 1048576, 1) . " MB"
+                                                        : round($row['filesize'] / 1024, 1) . " kB",
                                                             
                 );
                 // if user is member of a group then link name to details page
-                if (GetRoleNames(GetAllStatusgruppen($this->config->range_id, $db->f('user_id')))) {
+                if (GetRoleNames(GetAllStatusgruppen($this->config->range_id, $row['user_id']))) {
                     $table_row_data['content']['fullname'] = 
                             $this->elements['LinkIntern']->toString(array('content' =>
-                            htmlReady($db->f('fullname')), 'module' => 'Persondetails',
-                            'link_args' => 'username=' . $db->f('username')));
+                            htmlReady($row['fullname']), 'module' => 'Persondetails',
+                            'link_args' => 'username=' . $row['username']));
                 } else {
-                    $table_row_data['content']['fullname'] = htmlReady($db->f('fullname') ? $db->f('fullname') : $db->f('author_name'));
+                    $table_row_data['content']['fullname'] = htmlReady($row['username'] ? $row['username'] : $row['author_name']);
                 }
                 $out .= $this->elements["TableRow"]->toString($table_row_data);
-            }
+            }while($row = $statement->fetch(PDO::FETCH_ASSOC));
         }
 
         return $this->elements["TableHeader"]->toString(array("content" => $out));

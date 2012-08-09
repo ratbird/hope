bbf2041f1151a2b7d63c2981e2dbf16996602f02
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-08-09 09:49:25 +0000

    refs #2819: fix too early binding of js-event
    git-svn-id: svn://develop.studip.de/studip/trunk@24038 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/assets/javascripts/sem_classes.js
+++ public/assets/javascripts/sem_classes.js
@@ -158,13 +158,15 @@ STUDIP.admin_sem_class = {
         });
     }
 };
-jQuery(".sem_type_delete").live("click", STUDIP.admin_sem_class.delete_sem_type_question);
-jQuery(".name_input > input").live("blur", STUDIP.admin_sem_class.rename_sem_type);
-jQuery(STUDIP.admin_sem_class.make_sortable);
-jQuery("div[container] > div.droparea > div.plugin select[name=sticky]").change(function () {
-    if (this.value === "sticky") {
-        jQuery(this).closest("div.plugin").addClass("sticky");
-    } else {
-        jQuery(this).closest("div.plugin").removeClass("sticky");
-    }
+jQuery(function () {
+    jQuery(".sem_type_delete").live("click", STUDIP.admin_sem_class.delete_sem_type_question);
+    jQuery(".name_input > input").live("blur", STUDIP.admin_sem_class.rename_sem_type);
+    jQuery(STUDIP.admin_sem_class.make_sortable);
+    jQuery("div[container] > div.droparea > div.plugin select[name=sticky]").change(function () {
+        if (this.value === "sticky") {
+            jQuery(this).closest("div.plugin").addClass("sticky");
+        } else {
+            jQuery(this).closest("div.plugin").removeClass("sticky");
+        }
+    });
 });

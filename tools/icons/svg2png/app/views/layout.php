60f694e0c88baeda7b51e11a38cc32850c46b537
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-08-26 08:35:02 +0000

    rearrange assets, re #2881
    git-svn-id: svn://develop.studip.de/studip/trunk@24184 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- tools/icons/svg2png/app/views/layout.php
+++ tools/icons/svg2png/app/views/layout.php
@@ -3,7 +3,7 @@
 <head>
     <meta http-equiv="content-type" content="text/html;charset=UTF-8">
     <title>Stud.IP: Icon SVG to PNG</title>
-    <link href="assets.php?stylesheets/style.css" rel="stylesheet" type="text/css">
+    <link href="<?= URLHelper::getLink('assets.php') ?>?stylesheets/style.css" rel="stylesheet" type="text/css">
     <link href="<?= URLHelper::getLink('assets/style.css') ?>" rel="stylesheet" type="text/css">
     <link href="<?= URLHelper::getLink('assets/jquery.miniColors.css') ?>" rel="stylesheet" type="text/css">
 </head>
@@ -16,7 +16,7 @@
         </div>
     </div>
 
-    <script type="text/javascript" src="assets.php?javascripts/jquery/jquery-1.7.js"></script>
+    <script type="text/javascript" src="<?= URLHelper::getLink('assets.php') ?>?javascripts/jquery/jquery-1.7.js"></script>
     <script type="text/javascript" src="<?= URLHelper::getLink('assets/jquery.miniColors.min.js') ?>"></script>
     <script type="text/javascript" src="<?= URLHelper::getLink('assets/script.js') ?>"></script>
 </body>

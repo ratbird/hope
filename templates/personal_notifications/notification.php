8da349db05b339bb445ad1de403b7a087d870ac2
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-09-03 12:47:23 +0000

    refs #2878: x instead of eye
    git-svn-id: svn://develop.studip.de/studip/trunk@24254 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/personal_notifications/notification.php
+++ templates/personal_notifications/notification.php
@@ -1,6 +1,6 @@
 <li class="notification" data-id="<?= $notification['personal_notification_id'] ?>">
     <a class="mark_as_read" href="#">
-        <?= Assets::img("icons/16/blue/visibility-visible", array('title' => _("Als gelesen markieren"))) ?>
+        <?= Assets::img("icons/16/blue/decline", array('title' => _("Als gelesen markieren"))) ?>
     </a>
     <a href="<?= URLHelper::getLink('dispatch.php/jsupdater/mark_notification_read/' . $notification['personal_notification_id']) ?>">
     <? if ($notification['avatar']): ?>

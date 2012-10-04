bf3be6d106656732666011e56f08307651b4effc
nadine <nadine@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-10-04 10:42:58 +0000

    clean up user session re#1922
    git-svn-id: svn://develop.studip.de/studip/trunk@24452 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/messaging.inc.php
+++ lib/messaging.inc.php
@@ -316,6 +316,43 @@ class messaging
     {
         global $user, $my_messaging_settings;
 
+        if(empty ($my_messaging_settings)){
+             $my_messaging_settings = json_decode( UserConfig::get($user->id)->__get('my_messaging_settings'), true);
+             if (!$my_messaging_settings['show_only_buddys'])
+                  $my_messaging_settings['show_only_buddys'] = FALSE;
+             if (!$my_messaging_settings['delete_messages_after_logout'])
+                  $my_messaging_settings['delete_messages_after_logout'] = FALSE;
+             if (!$my_messaging_settings['start_messenger_at_startup'])
+                  $my_messaging_settings['start_messenger_at_startup'] = FALSE;
+             if (!$my_messaging_settings['default_setted'])
+                  $my_messaging_settings['default_setted'] = time();
+             if (!$my_messaging_settings['last_login'])
+                  $my_messaging_settings['last_login'] = FALSE;
+             if (!$my_messaging_settings['timefilter'])
+                  $my_messaging_settings['timefilter'] = "30d";
+             if (!$my_messaging_settings['opennew'])
+                  $my_messaging_settings['opennew'] = 1;
+             if (!$my_messaging_settings['logout_markreaded'])
+                  $my_messaging_settings['logout_markreaded'] = FALSE;
+             if (!$my_messaging_settings['openall'])
+                  $my_messaging_settings['openall'] = FALSE;
+             if (!$my_messaging_settings['addsignature'])
+                  $my_messaging_settings['addsignature'] = FALSE;
+             if (!$my_messaging_settings['save_snd'])
+                  $my_messaging_settings['save_snd'] = 1;
+             if (!$my_messaging_settings['sms_sig'])
+                  $my_messaging_settings['sms_sig'] = FALSE;
+             if (!$my_messaging_settings['send_view'])
+                  $my_messaging_settings['send_view'] = FALSE;
+             if (!$my_messaging_settings['last_box_visit'])
+                  $my_messaging_settings['last_box_visit'] = 1;
+             if (!$my_messaging_settings['folder']['in'])
+                  $my_messaging_settings['folder']['in'][0] = "dummy";
+             if (!$my_messaging_settings['folder']['out'])
+                  $my_messaging_settings['folder']['out'][0] = "dummy";
+             if (!$my_messaging_settings['confirm_reading'])
+                  $my_messaging_settings['confirm_reading'] = 3;
+       }
         if (basename($_SERVER['PHP_SELF']) == 'sms_send.php'){
             $sms_data = $_SESSION['sms_data'];
         } else {

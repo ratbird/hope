1fd0e7be5b5f4a4b8ed61f3c49850f643787edaa
tgloeggl <tgloeggl@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-10-04 11:21:54 +0000

    fixes #2813
    git-svn-id: svn://develop.studip.de/studip/trunk@24464 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/forum/search.php
+++ templates/forum/search.php
@@ -1,4 +1,4 @@
-<? use Studip\Button, Studip\LinkButton; ?>
+<? use Studip\Button; ?>
 <table border="0" width="100%" cellspacing="0" cellpadding="0" align="center">
     <tr>
         <td class="blank">
@@ -7,7 +7,7 @@
                 <tr>
                     <td class="blank" width="302" align="center">
 
-                        <form name="search" method="post" action="<?= URLHelper::getLink('') ?>">
+                        <form name="search" method="post" action="<?= URLHelper::getLink() ?>">
                             <?= CSRFProtection::tokenTag() ?>
 
                             <table cellpadding="2" cellspacing="0" border="0">
@@ -16,7 +16,7 @@
                                         <label for="suchbegriff"><?= _('Suchbegriff:') ?></label>
                                     </td>
                                     <td class="table_row_even" style="text-align: right;">
-                                        <input type="text" name="suchbegriff" id="suchbegriff" value="<?= _(Request::get('suchbegriff')) ?>">
+                                        <input type="text" name="suchbegriff" id="suchbegriff" value="<?= htmlReady(Request::get('suchbegriff')) ?>">
                                     </td>
                                 </tr>
                                 <tr class="table_row_even">
@@ -24,11 +24,11 @@
                                         <label for="author"><?= _('Von:') ?></label>
                                     </td>
                                     <td>
-                                        <input type="text" name="author" id="author" value="<?= _(Request::get('author')) ?>">
+                                        <input type="text" name="author" id="author" value="<?= htmlReady(Request::get('author')) ?>">
                                     </td>
                                 </tr>
                                 <tr>
-                                    <td class="steelgraulight" align="center" colspan="2">
+                                    <td class="table_row_odd" align="center" colspan="2">
                                         <input type="hidden" name="view" value="search">
                                         <br>
                                         <?= Button::create(_('Suchen')) ?>

0778494bc5621db00e20428974a93ec055c810c7
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-10-05 15:32:02 +0000

    fixes #2932
    git-svn-id: svn://develop.studip.de/studip/trunk@24473 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/edit_about.php
+++ public/edit_about.php
@@ -596,7 +596,6 @@ switch($view) {
 // Start of Output
 include ('lib/include/html_head.inc.php'); // Output of html head
 
-if ($auth->auth["jscript"]) { // nur wenn JS aktiv
 if ($view == 'Daten') {
     $validator=new email_validation_class;
 ?>
@@ -720,7 +719,6 @@ function update_pw_fields() {
 
 <?
 } // end if view == Daten
-} // Ende nur wenn JS aktiv
 
 include ('lib/include/header.php');   // Output of Stud.IP head
 

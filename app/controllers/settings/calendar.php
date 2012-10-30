fa57407e3b50fa98cb94e94c2fc6d138cd553e6a
nadine <nadine@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-10-30 11:15:42 +0000

    re #2931
    git-svn-id: svn://develop.studip.de/studip/trunk@24708 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/controllers/settings/calendar.php
+++ app/controllers/settings/calendar.php
@@ -12,16 +12,14 @@
  * @license     http://www.gnu.org/licenses/gpl-2.0.html GPL version 2
  * @category    Stud.IP
  * @since       2.4
- */
+*/
 
 require_once 'settings.php';
 
 /**
  */
-class Settings_CalendarController extends Settings_SettingsController
-{
-    public function before_filter(&$action, &$args)
-    {
+class Settings_CalendarController extends Settings_SettingsController {
+    public function before_filter(&$action, &$args) {
         if (!get_config('CALENDAR_ENABLE')) {
             throw new AccessDeniedException(_('Der Kalender ist nicht aktiviert.'));
         }
@@ -38,33 +36,32 @@ class Settings_CalendarController extends Settings_SettingsController
     /**
      *
      */
-    public function index_action()
-    {
+    public function index_action() {
         foreach ($GLOBALS['calendar_user_control_data'] as $key => $value) {
             $this->$key = $value;
         }
     }
 
-    public function store_action()
-    {
+    public function store_action() {
         $this->check_ticket();
 
         $GLOBALS['calendar_user_control_data'] = array(
-            'view'            => Request::option('cal_view'),
-            'start'           => Request::option('cal_start'),
-            'end'             => Request::option('cal_end'),
-            'step_day'        => Request::option('cal_step_day'),
-            'step_week'       => Request::option('cal_step_week'),
-            'type_week'       => Request::option('cal_type_week'),
-            'holidays'        => Request::option('cal_holidays'),
-            'sem_data'        => Request::option('cal_sem_data'),
-            'delete'          => Request::option('cal_delete'),
-            'step_week_group' => Request::option('cal_step_week_group'),
-            'step_day_group'  => Request::option('cal_step_day_group')
+                'view'            => Request::option('cal_view'),
+                'start'           => Request::option('cal_start'),
+                'end'             => Request::option('cal_end'),
+                'step_day'        => Request::option('cal_step_day'),
+                'step_week'       => Request::option('cal_step_week'),
+                'type_week'       => Request::option('cal_type_week'),
+                'holidays'        => Request::option('cal_holidays'),
+                'sem_data'        => Request::option('cal_sem_data'),
+                'delete'          => Request::option('cal_delete'),
+                'step_week_group' => Request::option('cal_step_week_group'),
+                'step_day_group'  => Request::option('cal_step_day_group')
         );
 
         $this->reportSuccess(_('Ihre Einstellungen wurden gespeichert'));
         $this->redirect('settings/calendar');
+
+        UserConfig::get($user->id)->store("calendar_user_control_data", json_encode($calendar_user_control_data));
     }
-    UserConfig::get($user->id)->store("calendar_user_control_data", json_encode($calendar_user_control_data));
 }

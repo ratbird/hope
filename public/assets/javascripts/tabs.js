f87ae12ef5accf04ad062b14645f88c3f5f3254e
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-10-30 15:45:20 +0000

    refs #1617
    JSLint: still some errors to fix.
    
    git-svn-id: svn://develop.studip.de/studip/trunk@24718 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/assets/javascripts/tabs.js
+++ public/assets/javascripts/tabs.js
@@ -1,92 +1,102 @@
 /*jslint browser: true, white: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, newcap: true, immed: true, indent: 4, onevar: false */
 /*global window, $, jQuery, _ */
 
-(function() {
+(function () {
 
-  if (window.STUDIP == null) window.STUDIP = {};
-
-  STUDIP.Tabs = (function() {
-    /*
-      STUDIP.Tabs
-      -----------
-      Create a new STUDIP.Tabs object from an element selector
-      whose childrens´ texts are compressed dynamically to fit the
-      tabs into a single row.
-    */
-    var findCompressable, needsCompression, truncate;
-
-    function Tabs(element, childSelector) {
-      _.bindAll(this, 'compress', 'uncompress');
-      if ((this.list = $(element)).length) {
-        if (childSelector == null) childSelector = 'li a span';
-        this.items = $(childSelector, this.list);
-        _.each(this.items.text(function() {
-          return $.trim($(this).text());
-        }), function(item) {
-          return $(item).data('orig-text', $(item).text());
-        });
-        $(window).resize(this.compress);
-      }
+    if (!window.STUDIP) {
+        window.STUDIP = {};
     }
 
-    /*
-      Calling this will re-truncate the children of the Tabs object
-      trying to fit all the children into a single line.
-      This is called automatically as soon as the browser window is resized.
-    */
+    STUDIP.Tabs = (function () {
+        /*
+            STUDIP.Tabs
+            -----------
+            Create a new STUDIP.Tabs object from an element selector
+            whose childrens´ texts are compressed dynamically to fit the
+            tabs into a single row.
+        */
+        var findCompressable, needsCompression, truncate;
 
-    Tabs.prototype.compress = function() {
-      var item, newWidth;
-      newWidth = $(window).width();
-      if (this.oldWidth) if (newWidth > this.oldWidth) this.uncompress();
-      this.oldWidth = newWidth;
-      while (needsCompression(this.list) && (item = findCompressable(this.items))) {
-        truncate(item);
-      }
-      return this;
-    };
+        function Tabs(element, childSelector) {
+            _.bindAll(this, 'compress', 'uncompress');
+            if ((this.list = $(element)).length) {
+                if (!childSelector) {
+                    childSelector = 'li a span';
+                }
+                this.items = $(childSelector, this.list);
+                _.each(this.items.text(function () {
+                    return $.trim($(this).text());
+                }), function (item) {
+                    return $(item).data('orig-text', $(item).text());
+                });
+                $(window).resize(this.compress);
+            }
+        }
 
-    /*
-      This method  undoes the compression of the childrens´ texts and
-      reset them to their original values.
-    */
+        /*
+            Calling this will re-truncate the children of the Tabs object
+            trying to fit all the children into a single line.
+            This is called automatically as soon as the browser window is resized.
+        */
 
-    Tabs.prototype.uncompress = function() {
-      this.items.text(function() {
-        return $(this).data('orig-text');
-      });
-      return this;
-    };
+        Tabs.prototype.compress = function () {
+            var item, newWidth;
+            newWidth = $(window).width();
+            if (this.oldWidth) {
+                if (newWidth > this.oldWidth) {
+                    this.uncompress();
+                }
+            }
+            this.oldWidth = newWidth;
+            while (needsCompression(this.list) && (item = findCompressable(this.items))) {
+                truncate(item);
+            }
+            return this;
+        };
 
-    needsCompression = function(list) {
-      return list.length > 0 && list.height() > _.max(_.map(list.children(), function(kid) {
-        return $(kid).outerHeight(true);
-      }));
-    };
+        /*
+            This method    undoes the compression of the childrens´ texts and
+            reset them to their original values.
+        */
 
-    findCompressable = function(items) {
-      var largest;
-      largest = _.max(items, function(item) {
-        return $(item).text().length;
-      });
-      if (largest && $(largest).text().length > 5) return largest;
-    };
+        Tabs.prototype.uncompress = function () {
+            this.items.text(function () {
+                return $(this).data('orig-text');
+            });
+            return this;
+        };
 
-    truncate = function(item) {
-      var length, text;
-      text = $(item).text().replace("\u2026", "");
-      length = Math.max(text.length - 3, 4);
-      return $(item).text(text.slice(0, length) + "\u2026");
-    };
+        needsCompression = function (list) {
+            return list.length > 0 && list.height() > _.max(_.map(list.children(), function (kid) {
+                return $(kid).outerHeight(true);
+            }));
+        };
 
-    return Tabs;
+        findCompressable = function (items) {
+            var largest;
+            largest = _.max(items, function (item) {
+                return $(item).text().length;
+            });
+            if (largest && $(largest).text().length > 5) {
+                return largest;
+            }
+        };
 
-  })();
+        truncate = function (item) {
+            var length, text;
+            text = $(item).text().replace("\u2026", "");
+            length = Math.max(text.length - 3, 4);
+            return $(item).text(text.slice(0, length) + "\u2026");
+        };
 
-  $(function() {
-    var tabs;
-    tabs = new STUDIP.Tabs($("#tabs"));
-    return tabs.compress();
-  });
+        return Tabs;
+
+    })();
+
+    $(function () {
+        var tabs;
+        tabs = new STUDIP.Tabs($("#tabs"));
+        return tabs.compress();
+    });
 
 }).call(this);

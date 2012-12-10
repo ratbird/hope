bf4c4ac30e60182f9f1026407f4b804ed12b62a5
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-12-10 10:43:11 +0000

    fixes #3150
    git-svn-id: svn://develop.studip.de/studip/trunk@25095 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/datei.inc.php
+++ lib/datei.inc.php
@@ -1171,6 +1171,11 @@ function getUploadMetadata($range_id, $refresh = FALSE) {
 
 
 function JS_for_upload() {
+    if ($GLOBALS['i_page'] == "sms_send.php") {
+        $active_upload_type = "attachments";
+    } else {
+        $active_upload_type = $GLOBALS['SessSemName']["art_num"];
+    }
     //displays the templates for upload windows now
     //for upload code see application.js : STUDIP.OldUpload
     ?>
@@ -1189,10 +1194,10 @@ function JS_for_upload() {
     ?></div>
     <div id="upload_file_types" style="display: none;"><?=
         json_encode(
-            $GLOBALS['UPLOAD_TYPES'][$GLOBALS['SessSemName']["art_num"]]
+            $GLOBALS['UPLOAD_TYPES'][$active_upload_type]
             ? array(
-                'allow' => $GLOBALS['UPLOAD_TYPES'][$GLOBALS['SessSemName']["art_num"]]["type"] === "allow" ? 1 : 0,
-                'types' => $GLOBALS['UPLOAD_TYPES'][$GLOBALS['SessSemName']["art_num"]]["file_types"]
+                'allow' => $GLOBALS['UPLOAD_TYPES'][$active_upload_type]["type"] === "allow" ? 1 : 0,
+                'types' => $GLOBALS['UPLOAD_TYPES'][$active_upload_type]["file_types"]
             )
             : array(
                 'allow' => $GLOBALS['UPLOAD_TYPES']["default"]["type"] === "allow" ? 1 : 0,

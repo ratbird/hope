6267b3685518abc211ce7d5f3e093ee5f92baade
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2012-12-12 11:49:23 +0000

    refs #3159 remove wrong include
    git-svn-id: svn://develop.studip.de/studip/trunk@25125 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/controllers/jsupdater.php
+++ app/controllers/jsupdater.php
@@ -10,7 +10,6 @@
 
 require_once 'app/controllers/authenticated_controller.php';
 require_once 'lib/classes/UpdateInformation.class.php';
-require_once 'lib/classes/PersonalNotifications.class.php';
 
 /**
  * Controller called by the main periodical ajax-request. It collects data,

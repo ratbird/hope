1d6c908d0c552175af069b2351a7764867a678c0
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2013-01-08 15:23:23 +0000

    renew pagination, fixes #3207
    git-svn-id: svn://develop.studip.de/studip/trunk@25229 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/shared/pagechooser.php
+++ templates/shared/pagechooser.php
@@ -1,91 +1,51 @@
 <?
-# Lifter010: TODO
-
-$pages = ceil($num_postings / $perPage);
-if ($pages <= 1) return;
-
-if ($page) $cur_page = $page; else $cur_page = 1;
-
-$run = true;
-$add_dots = false;
-
-// show additional text over thread-postings
-// $ret .= "$num_postings "._("Einträge") .", ".  _("Seite") . " $cur_page von $pages &bull; ";
-
-// previous page
-if ($cur_page > 1) {
-    $ret .= '<a href="'. URLHelper::getLink(sprintf($pagelink, $cur_page-1) ) .'">'. _("zurück") .'</a>&nbsp;|&nbsp;';
-}
-
-// show pages
-for ($i = 1; $i <= $pages; $i++) {
-
-    if ($pages >= 6) {
-        $add_dots = false;
-        // show the two first and the two last pages
-        if ($cur_page == -1) {
-            if (($pages - 2) >= $i && (2 < $i)) {
-                $run = false;
-            } else {
-                $run = true;
-            }
-
-            if ($i == 3) {
-                $add_dots = true;
-            }
-        }
-
-        // show the first and the last page, as well as the two pages before and after
-        else {
-            $run= false;
-
-            if ($cur_page < 3) {
-                $start = 1;
-                $end = 5;
-            } else if ($cur_page > ($pages - 3)) {
-                $start = $pages - 4;
-                $end = $pages;
-            } else {
-                $start = $cur_page -2;
-                $end = $cur_page + 2;
-            }
-
-            if ($start != 1 && $i == 1) {
-                $run = true;
-            }
-
-            if ($start > 2 && $i == 2) $add_dots = true;
-
-            if ($end != $pages && $i == $pages) {
-                $run = true;
-                if ($end < $pages - 1) $add_dots = true;
-            }
-
-            if ($i >= $start && $i <= $end) {
-                $run = true;
-            }
-        }
+    $num_pages = ceil($num_postings / $perPage);
+    if ($num_pages <= 1) {
+        return;
     }
 
-    if ($add_dots) {
-        $ret .= '&nbsp;|&nbsp;&hellip;';
-    }
+    $cur_page = $page ?: 1;
 
-    // only show pages to choose if they are meant to be shown
-    if ($run) {
+    $items = array_unique(array(1, $cur_page - 2, $cur_page - 1, $cur_page, $cur_page + 1, $cur_page + 2, $num_pages));
+    $items = array_filter($items, function ($item) use ($num_pages) { return $item >= 1 && $item <= $num_pages; });
+    sort($items);
 
-        if ($i > 1) $ret .= '&nbsp;|&nbsp;';
-        if ($cur_page == $i) {
-            $ret .= '<b>'. $i.'</b>';
-        } else {
-            $ret .= '<a href="'. URLHelper::getLink(sprintf($pagelink, $i) ) .'">'. $i .'</a>';
-        }
-    }
-}
-
-// next page
-if ($cur_page < ($i-1)) {
-    $ret .= '&nbsp;|&nbsp;<a href="'. URLHelper::getLink(sprintf($pagelink, $cur_page+1) ) .'">'. _("weiter") .'</a>';
-}
-
-echo $ret;
+    $last_page = reset($items) - 1;
+    $random_id = substr(md5(uniqid('pagination', true)), -8);
+?>
+<p id="pagination-label-<?= $random_id ?>" class="audible">
+    <?= _('Blättern') ?>
+</p>
+<ul class="pagination" role="navigation"
+    aria-labelledby="pagination-label-<?= $random_id ?>">
+<? if ($cur_page > 1): ?>
+    <li class="prev">
+        <a href="<?= URLHelper::getLink(sprintf($pagelink, $cur_page - 1)) ?>" rel="prev">
+            <span class="audible"><?= _('Eine Seite') ?></span>
+            <?= _('zurück') ?>
+        </a>
+    </li>
+<? endif; ?>
+<? foreach ($items as $item): ?>
+<? if ($item != $last_page + 1): ?>
+    <li class="divider" data-skipped="<?= $last_page + 1 ?>-<?= $item - 1 ?>">&hellip;</li>
+<? endif; ?>
+    <li <? if ($item == $cur_page) echo 'class="current"'; ?>>
+        <a href="<?= URLHelper::getLink(sprintf($pagelink, $item)) ?>">
+            <span class="audible"><?= _('Seite') ?></span>
+            <?= $item ?>
+        </a>
+    </li>
+<? 
+    $last_page = $item;
+    endforeach;
+?>
+<? if ($cur_page < $num_pages): ?>
+    <li class="next">
+        <a href="<?= URLHelper::getLink(sprintf($pagelink, $cur_page + 1)) ?>" rel="next">
+            <span class="audible"><?= _('Eine Seite') ?></span>
+            <?= _('weiter') ?>
+        </a>
+    </li>
+<? endif; ?>
+</ul>

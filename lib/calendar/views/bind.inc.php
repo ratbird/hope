434fffd74f0ddb37a351152321fccff3f6c51323
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2013-01-09 06:41:56 +0000

    extracted template and removed cssClassSwitcher,, re #82, re #2609
    git-svn-id: svn://develop.studip.de/studip/trunk@25234 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/calendar/views/bind.inc.php
+++ lib/calendar/views/bind.inc.php
@@ -18,12 +18,6 @@
  * @category    Stud.IP
  * @package     calendar
  */
-//TODO: templates
-
-use Studip\Button, Studip\LinkButton;
-
-include('lib/include/html_head.inc.php');
-include('lib/include/header.php');
 
 // add skip link
 SkipLinks::addIndex(_("Veranstaltungstermine"), 'main_content', 100);
@@ -51,90 +45,22 @@ $statement->execute(array(
 ));
 $result = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-echo "<table width=\"100%\" border=\"0\" cellpadding=\"5\" cellspacing=\"0\">\n";
-echo "<tr><td class=\"blank\">\n";
-echo "<table border=\"0\" width=\"100%\" cellspacing=\"0\" cellpadding=\"1\" class=\"blank\" id=\"main_content\">\n";
+$template = $GLOBALS['template_factory']->open('calendar/bind');
+$template->set_layout('layouts/base');
+$template->calendar_sess_control_data = $calendar_sess_control_data;
+$template->order                      = $order;
+$template->result                     = $result;
+$template->atime                      = $atime;
 
-if (!empty($calendar_sess_control_data["view_prv"])) {
-    echo "<form action=\"".URLHelper::getLink('?cmd='.$calendar_sess_control_data['view_prv'])."\" method=\"post\">";
-} else {
-    echo "<form action=\"".URLHelper::getLink('?cmd=showweek')."\" method=\"post\">";
-}
-echo CSRFProtection::tokenTag();
-echo "\n<tr>\n";
-echo "<th width=\"2%\" nowrap colspan=\"2\" align=\"center\">";
-echo "&nbsp;<a href=\"" . URLHelper::getLink('dispatch.php/meine_seminare/groups') . "\">";
-$tooltip = tooltip(_("Gruppe ändern"));
-echo "<img src=\"" . Assets::image_path('icons/16/blue/group.png') . "\" {$tooltip}>";
-echo "</a></th>\n";
-echo "<th width=\"64%\" align=\"left\">";
-echo "<a href=\"".URLHelper::getLink('?cmd=bind&sortby=Name&order='.$order)."\">" . _("Name") . "</a></th>\n";
-echo "<th width=\"7%\"><a href=\"".URLHelper::getLink('?cmd=bind&sortby=count&order='.$order)."\">";
-echo _("Termine") . "</a></th>\n";
-echo "<th width=\"13%\"><b>" . _("besucht") . "</b></th>\n";
-echo "<th width=\"13%\"><a href=\"".URLHelper::getLink('?cmd=bind&sortby=status&order='.$order)."\">";
-echo _("Status") . "</a></th>\n";
-echo "<th width=\"2%\">&nbsp;</th>\n</tr>\n";
+$template->infobox = array(
+    'picture' => 'infobox/dates.jpg',
+    'content' => array(array(
+        'kategorie' => _('Information:'),
+        'eintrag'   => array(array(
+            'icon' => 'icons/16/black/info.png',
+            'text' => _('Termine aus den ausgew&auml;hlten Veranstaltungen werden in Ihren Terminkalender &uuml;bernommen.')
+        ))
+    ))
+);
 
-$css_switcher = new cssClassSwitcher();
-echo $css_switcher->GetHoverJSFunction();
-$css_switcher->enableHover();
-$css_switcher->switchClass();
-
-foreach ($result as $row) {
-    $name = $row['Name'] . " (" . $row['startsem'] . ($row['startsem'] != $row['endsem'] ? " - " . $row['endsem'] : "") . ")";
-    $style = $css_switcher->getFullClass();
-    echo "<tr" . $css_switcher->getHover() . "><td width=\"1%\" class=\"gruppe" . $row['gruppe'] . "\">";
-    echo "<img src=\"" . $GLOBALS['ASSETS_URL'] . "images/blank.gif\" alt=\"Gruppe\" border=\"0\" width=\"7\" height=\"12\"></td>\n";
-    echo "<td$style>&nbsp; </td>";
-    echo "<td$style><font size=\"-1\">";
-    echo "<a href=\"" . $CANONICAL_RELATIVE_PATH_STUDIP;
-    echo "seminar_main.php?auswahl=" . $row['Seminar_id'] . "\">";
-    echo htmlReady(mila($name));
-    echo "</a></font></td>\n";
-    echo "<td$style align=\"center\"><font size=\"-1\">";
-    echo $row['count'];
-    echo "</font></td>\n";
-    if ($row['visitdate'] == 0) {
-        echo "<td$style align=\"center\"><font size=\"-1\">";
-        echo _("nicht besucht") . "</font></td>\n";
-    } else {
-        echo "<td$style align=\"center\"><font size=\"-1\">";
-        echo strftime("%x", $row['visitdate']);
-        echo "</font></td>";
-    }
-    echo "<td$style align=\"center\"><font size=\"-1\">";
-    echo $row['status'];
-    echo "</font></td>\n";
-    if ($row['bind_calendar']) {
-        $is_checked = ' checked';
-    } else {
-        $is_checked = '';
-    }
-    echo "<td$style>";
-    echo "<input type=\"checkbox\" name=\"sem[" . $row['Seminar_id']
-    . "]\" value=\"1\"$is_checked></td></tr>\n",
-    $css_switcher->switchClass();
-}
-
-echo "<tr><td class=\"blank\">&nbsp;</td></tr>\n";
-echo "<tr><td class=\"blank\" colspan=\"6\" align=\"center\">";
-echo Button::create(_('Auswählen'));
-
-// Dummy-Wert damit $sem auch ohne ausgewaehlte Seminare ausgewertet wird
-echo "\n<input type=\"hidden\" name=\"sem[1]\" value=\"FALSE\">\n";
-echo "<input type=\"hidden\" name=\"atime\" value=\"$atime\">";
-echo "\n</form>\n";
-echo "</table>";
-echo "\n</td>\n";
-echo "<td class=\"blank\" width=\"270\" valign=\"top\" align=\"right\">\n";
-$info_content = array(array("kategorie" => _("Information:"),
-        "eintrag" => array(
-            array("icon" => "icons/16/black/info.png",
-                "text" => _("Termine aus den ausgew&auml;hlten Veranstaltungen werden in Ihren Terminkalender &uuml;bernommen.")
-        ))));
-
-print_infobox($info_content, "infobox/dates.jpg");
-echo "</td></tr></table>\n";
-
-echo "</tr><tr><td class=\"blank\">&nbsp;";
+echo $template->render();
---
+++ templates/calendar/bind.php
@@ -0,0 +1,92 @@
+<? use Studip\Button, Studip\LinkButton; ?>
+
+<? if (!empty($calendar_sess_control_data['view_prv'])): ?>
+<form action="<?= URLHelper::getLink('?cmd=' . $calendar_sess_control_data['view_prv']) ?>" method="post">
+<? else: ?>
+<form action="<?= URLHelper::getLink('?cmd=showweek') ?>" method="post">
+<? endif; ?>
+
+<?= CSRFProtection::tokenTag() ?>
+<table class="default zebra-hover" id="main_content">
+    <colgroup>
+        <col width="8px">
+        <col width="8px">
+        <col>
+        <col width="7%">
+        <col width="13%">
+        <col width="13%">
+        <col width="2%">
+    </colgroup>
+    <thead>
+        <tr>
+            <th colspan="2" align="center">
+                <a href="<?= URLHelper::getLink('dispatch.php/meine_seminare/groups') ?>">
+                    <?= Assets::img('icons/16/blue/group', tooltip2(_('Gruppe ändern'))) ?>
+                </a>
+            </th>
+            <th align="left">
+                <a href="<?= URLHelper::getLink('?cmd=bind&sortby=Name&order=' . $order) ?>">
+                    <?= _('Name') ?>
+                </a>
+            </th>
+            <th>
+                <a href="<?= URLHelper::getLink('?cmd=bind&sortby=count&order=' . $order) ?>">
+                    <?= _('Termine') ?>
+                </a>
+            </th>
+            <th><?= _('besucht') ?></th>
+            <th>
+                <a href="<?= URLHelper::getLink('?cmd=bind&sortby=status&order=' . $order) ?>">
+                    <?= _('Status') ?>
+                </a>
+            </th>
+            <th>&nbsp;</th>
+        </tr>
+    </thead>
+    <tbody>
+    <? foreach ($result as $row): ?>
+        <?
+            $name  = $row['Name'];
+            $name .= ' (' . $row['startsem'];
+            $name .= ($row['startsem'] != $row['endsem'] ? ' - ' . $row['endsem'] : '');
+            $name .= ')';
+        ?>
+        <tr>
+            <td class="gruppe<?= $row['gruppe'] ?>">
+                <?= Assets::img('blank.gif', array('alt' => _('Gruppe'), 'width' => 8, 'height' => 12)) ?>
+            </td>
+            <td>&nbsp;</td>
+            <td>
+                <a href="<?= URLHelper::getLink('seminar_main.php?auswahl=' . $row['Seminar_id']) ?>">
+                    <?= htmlReady(mila($name)) ?>
+                </a>
+            </td>
+            <td align="center"><?= $row['count'] ?></td>
+            <td align="center">
+            <? if ($row['visitdate'] == 0): ?>
+                <?= _('nicht besucht') ?>
+            <? else: ?>
+                <?= strftime('%x', $row['visitdate']) ?>
+            <? endif; ?>
+            </td>
+            <td align="center"><?= $row['status'] ?></td>
+            <td>
+                <input type="checkbox" name="sem[<?= $row['Seminar_id'] ?>]" value="1"
+                       <? if ($row['bind_calendar']) echo 'checked'; ?>>
+            </td>
+        </tr>
+    <? endforeach; ?>
+    </tbody>
+    <tfoot>
+        <tr>
+            <td colspan="6" align="center">
+                <?= Button::create(_('Auswählen')) ?>
+
+                <? // Dummy-Wert damit $sem auch ohne ausgewaehlte Seminare ausgewertet wird ?>
+                <input type="hidden" name="sem[1]" value="FALSE">
+                <input type="hidden" name="atime" value="<?= $atime ?>">
+            </td>
+        </tr>
+    </tfoot>
+</table>
+</form>

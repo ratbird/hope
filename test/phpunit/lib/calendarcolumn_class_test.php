31cfa5991ec4d1c27594ad348c64e29d3a8c439d
mlunzena <mlunzena@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2013-01-22 06:11:43 +0000

    fix unit tests, refs #812
    git-svn-id: svn://develop.studip.de/studip/trunk@25441 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- test/phpunit/lib/calendarcolumn_class_test.php
+++ test/phpunit/lib/calendarcolumn_class_test.php
@@ -12,6 +12,10 @@
 
 require_once dirname(__FILE__) . '/../bootstrap.php';
 require_once 'lib/classes/DbView.class.php';
+require_once 'lib/classes/StudipCache.class.php';
+require_once 'lib/classes/StudipNullCache.class.php';
+require_once 'lib/classes/StudipFormat.php';
+require_once 'lib/classes/WikiFormat.php';
 require_once 'lib/calendar/CalendarColumn.class.php';
 require_once 'lib/functions.php';
 

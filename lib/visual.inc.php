02dde1106cd4dac70bacc4055db4f55ad653f40d
mlunzena <mlunzena@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2013-01-22 08:54:24 +0000

    autoload class WikiFormat, refs #812
    git-svn-id: svn://develop.studip.de/studip/trunk@25463 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/visual.inc.php
+++ lib/visual.inc.php
@@ -6,7 +6,6 @@
 
 
 require_once 'config.inc.php';
-require_once 'lib/classes/WikiFormat.php';
 require_once 'lib/classes/cssClassSwitcher.inc.php';
 include_once 'vendor/idna_convert/idna_convert.class.php';
 include_once 'lib/classes/searchtypes/SQLSearch.class.php';

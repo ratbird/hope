2156472f89601197a9d831793d1119e204890e76
mlunzena <mlunzena@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2013-01-22 10:11:02 +0000

    autoload class StudipForm, refs #812
    git-svn-id: svn://develop.studip.de/studip/trunk@25471 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/classes/StudipStmSearch.class.php
+++ lib/classes/StudipStmSearch.class.php
@@ -23,7 +23,6 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // +---------------------------------------------------------------------------+
 
-require_once("lib/classes/StudipForm.class.php");
 require_once("lib/visual.inc.php");
 require_once("lib/functions.php");
 
--- public/stm_abstract_assi.php
+++ public/stm_abstract_assi.php
@@ -11,7 +11,6 @@ page_open(array("sess" => "Seminar_Session", "auth" => "Seminar_Auth", "perm" =>
 
 include ("lib/seminar_open.php"); // initialise Stud.IP-Session
 
-require_once('lib/classes/StudipForm.class.php');
 
 require_once("lib/classes/StmAbstractAssiVisualization.class.php");
 require_once("lib/classes/AbstractStm.class.php");

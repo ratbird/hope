a313fce2bf8142f6b8da40fda05cc89e3a44f42b
BlackScorp <BlackScorp@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2013-03-07 12:44:30 +0000

    fix #3319
    git-svn-id: svn://develop.studip.de/studip/trunk@25932 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/teilnehmer.php
+++ public/teilnehmer.php
@@ -1187,9 +1187,9 @@ while (list ($key, $val) = each ($gruppe)) {
                 $statement->bindParam(':status', $key);
                 $statement->execute();
                 $all_user = $statement->fetchColumn();
-
-                echo '<a href="mailto:'.$all_user.'">';
-                echo Assets::img('icons/16/blue/move_right/mail.png', array('title' => sprintf(_('E-Mail an alle %s schicken'), $val), 'align' => 'absmiddle'));
+                $text = sprintf(_('E-Mail an alle %s schicken'), $val);
+                echo '<a href="mailto:'.$all_user.'" title="'.$text.'">';
+                echo Assets::img('icons/16/blue/move_right/mail.png', array('title' => $text,'alt'=>$text, 'align' => 'absmiddle'));
                 echo '</a>&nbsp;';
             }
 
@@ -1198,8 +1198,9 @@ while (list ($key, $val) = each ($gruppe)) {
             } else {
                 $msg_params = array('filter' => 'send_sms_to_all', 'who' => $key, 'sms_source_page' => 'teilnehmer.php?cid=' . $_SESSION['SessionSeminar'], 'course_id' => $SessSemName[1], 'subject' => $subject);
             }
-            echo '<a href="'.URLHelper::getLink('sms_send.php', $msg_params).'">';
-            echo Assets::img('icons/16/blue/mail.png', array('title' => sprintf(_('Nachricht an alle %s schicken'), $val), 'align' => 'absmiddle'));
+            $text = sprintf(_('Nachricht an alle %s schicken'), $val);
+            echo '<a href="'.URLHelper::getLink('sms_send.php', $msg_params).'" title="'.$text.'">';
+            echo Assets::img('icons/16/blue/mail.png', array('title' => $text,'alt'=>$text, 'align' => 'absmiddle'));
             echo '</a>';
             echo '</td>';
         } else {

b0e4bccaee5b24bcc07fe0c734d987c7753d6845
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2013-03-08 18:52:52 +0000

    ENGINE=MyISAM re #3239
    git-svn-id: svn://develop.studip.de/studip/trunk@25948 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- db/migrations/105_step_00247_forum.php
+++ db/migrations/105_step_00247_forum.php
@@ -12,15 +12,15 @@ class Step00247Forum extends Migration
         // check if the plugin has been installed previously and is in a know db-state
         $forumpp_version = DBManager::get()->query("SELECT version FROM schema_version
             WHERE domain = 'ForumPP'")->fetchColumn();
-        
+
         if ($forumpp_version !== false && $forumpp_version != 6) {  // version 6 is the DB-Version of the latest ForumPP-Plugin
             throw new Exception(_('Sie verwenden das ForumPP-Plugin in einer alten Version. '
                 . 'Bitte aktualisieren Sie es zuerst auf die neueste Version, sonst kann '
                 . 'Die Stud.IP-Migration nicht ausgeführt werden'));
-            
+
         } else if ($forumpp_version == 6) { // prepare the tables for the rest of the migration
             // rename the forum-tables
-            DBManager::get()->exec("RENAME TABLE 
+            DBManager::get()->exec("RENAME TABLE
                 forumpp_abo_users           TO forum_abo_users,
                 forumpp_categories          TO forum_categories,
                 forumpp_categories_entries  TO forum_categories_entries,
@@ -37,7 +37,7 @@ class Step00247Forum extends Migration
                     `entry_name` varchar(255) NOT NULL,
                     `pos` INT NOT NULL DEFAULT '0',
                     PRIMARY KEY ( `category_id` )
-                );
+                ) ENGINE=MyISAM
             ");
 
             DBManager::get()->exec("
@@ -46,7 +46,7 @@ class Step00247Forum extends Migration
                     `topic_id` varchar(32) NOT NULL,
                     `pos` INT NOT NULL DEFAULT '0',
                     PRIMARY KEY ( `category_id` , `topic_id` )
-                );
+                ) ENGINE=MyISAM
             ");
 
 
@@ -67,7 +67,7 @@ class Step00247Forum extends Migration
                     `depth` int(11) NOT NULL,
                     `anonymous` tinyint(4) NOT NULL DEFAULT '0',
                     PRIMARY KEY (`topic_id`)
-                );
+                ) ENGINE=MyISAM
             ");
 
             DBManager::get()->exec("
@@ -75,9 +75,9 @@ class Step00247Forum extends Migration
                   `topic_id` varchar(32) NOT NULL,
                   `user_id` varchar(32) NOT NULL,
                   PRIMARY KEY (`topic_id`,`user_id`)
-                );
+                ) ENGINE=MyISAM
             ");
-            
+
             DBManager::get()->exec("
                 CREATE TABLE IF NOT EXISTS `forum_visits` (
                     user_id varchar(32) NOT NULL,
@@ -85,7 +85,7 @@ class Step00247Forum extends Migration
                     visitdate int(11) NOT NULL,
                     last_visitdate int(11) NOT NULL,
                     PRIMARY KEY ( `user_id` , `seminar_id` )
-                );
+                ) ENGINE=MyISAM
             ");
 
             DBManager::get()->exec("
@@ -93,7 +93,7 @@ class Step00247Forum extends Migration
                     user_id varchar(32) NOT NULL,
                     topic_id varchar(32) NOT NULL,
                     PRIMARY KEY ( `user_id` , `topic_id` )
-                );
+                ) ENGINE=MyISAM
             ");
 
             DBManager::get()->exec("
@@ -101,22 +101,22 @@ class Step00247Forum extends Migration
                     `topic_id` varchar(32) NOT NULL,
                     `user_id` varchar(32) NOT NULL,
                     PRIMARY KEY (`topic_id`,`user_id`)
-                )
-            ");            
+                ) ENGINE=MyISAM
+            ");
         }
-        
+
         // add new table for the issue-connection
         DBManager::get()->exec("CREATE TABLE IF NOT EXISTS `forum_entries_issues` (
             `topic_id` varchar(32) NOT NULL,
             `issue_id` varchar(32) NOT NULL,
             PRIMARY KEY (`topic_id`,`issue_id`)
-        )");
-        
+        ) ENGINE=MyISAM");
+
         // add some highly needed indices
         DBManager::get()->exec("ALTER TABLE `forum_entries` ADD INDEX (  `seminar_id` ,  `lft` )");
         DBManager::get()->exec("ALTER TABLE `forum_entries` ADD INDEX (  `seminar_id` ,  `rgt` )");
         DBManager::get()->exec("ALTER TABLE `forum_entries` ADD INDEX (  `user_id` )");
-        DBManager::get()->exec("ALTER TABLE `forum_categories` ADD INDEX (  `seminar_id` )");        
+        DBManager::get()->exec("ALTER TABLE `forum_categories` ADD INDEX (  `seminar_id` )");
 
         // get highest position
         $navpos = DBManager::get()->query("SELECT navigationpos FROM plugins
@@ -127,36 +127,36 @@ class Step00247Forum extends Migration
             (pluginclassname, pluginpath, pluginname, plugintype, enabled, navigationpos)
             VALUES ('CoreForum', 'core/Forum', 'Forum', 'ForumModule,StandardPlugin,StudipModule', 'yes', ?)");
         $stmt->execute(array($navpos));
-        
+
         // get id of newly created plugin (we purposely do not use PDO::lastInserId())
         $plugin_id = DBManager::get()->query("SELECT pluginid FROM plugins
             WHERE pluginpath = 'core/Forum'")->fetchColumn();
-        
+
         // set all default roles for the plugin (including nobody)
         $stmt = DBManager::get()->prepare("INSERT INTO roles_plugins
             (roleid, pluginid) VALUES (?, ?)");
         foreach (range(1,7) as $role_id) {
             $stmt->execute(array($role_id, $plugin_id));
         }
-        
+
 
         // remove old ForumPP-plugin
-        $old_forum = DBManager::get()->query("SELECT * FROM plugins 
+        $old_forum = DBManager::get()->query("SELECT * FROM plugins
             WHERE pluginclassname = 'ForumPP'")->fetch(PDO::FETCH_ASSOC);
-    
+
         if ($old_forum) {
-            DBManager::get()->exec("DELETE FROM plugins 
+            DBManager::get()->exec("DELETE FROM plugins
                 WHERE pluginclassname = 'ForumPP'");
             DBManager::get()->exec("DELETE FROM plugins_activated
                 WHERE pluginid = " . $old_forum['pluginid']);
-            DBManager::get()->exec("DELETE FROM plugins_default_activations 
+            DBManager::get()->exec("DELETE FROM plugins_default_activations
                 WHERE pluginid = " . $old_forum['pluginid']);
             DBManager::get()->exec("DELETE FROM roles_plugins
                 WHERE pluginid = " . $old_forum['pluginid']);
             DBManager::get()->exec("DELETE FROM schema_version
                 WHERE domain = 'ForumPP'");
         }
-        
+
         // remove user-settings for the old forum
         DBManager::get()->exec("DELETE FROM user_config WHERE `field` = 'FORUM_SETTINGS'");
     }

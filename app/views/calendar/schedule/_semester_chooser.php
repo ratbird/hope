6ac676316821eae8e2b5c65dd2516ceafd50aea2
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2013-03-19 16:25:01 +0000

    calendar: prevent xss, fixes #3404
    git-svn-id: svn://develop.studip.de/studip/trunk@26061 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/calendar/schedule/_semester_chooser.php
+++ app/views/calendar/schedule/_semester_chooser.php
@@ -3,7 +3,7 @@
     <? foreach ($semesters as $semester) : ?>
         <? if ($semester['ende'] > time() - strtotime('1year 1day')) : ?>
         <option value="<?= $semester['semester_id'] ?>" <?= $current_semester['semester_id'] == $semester['semester_id'] ? 'selected="selected"' : '' ?>>
-            <?= $semester['name'] ?>
+            <?= htmlReady($semester['name']) ?>
             <?= $semester['beginn'] < time() && $semester['ende'] > time() ? '('. _('akt. Semester') .')' : '' ?>
         </option>
         <? endif ?>

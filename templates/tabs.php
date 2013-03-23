24e418f7cfa5465f58e5de8854bcac1dad319a16
ckater <ckater@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2013-03-23 12:54:11 +0000

    Ausbauen Assets::image_path re #3417
    git-svn-id: svn://develop.studip.de/studip/trunk@26111 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/tabs.php
+++ templates/tabs.php
@@ -22,14 +22,14 @@ foreach (Navigation::getItem("/")->getSubNavigation() as $path => $nav) {
 
                     <a href="<?= URLHelper::getLink($nav->getURL()) ?>"<?= $badge_attr ?>>
                         <? if ($image = $nav->getImage()) : ?>
-                            <img class="tab-icon" src="<?=$image['src']?>" title="<?= $nav->getTitle() ? htmlReady($nav->getTitle()) : htmlReady($nav->getDescription()) ?>" />
+                            <?= Assets::img($image['src'], array('class' => "tab-icon", 'alt' => htmlReady($nav->getTitle()), 'title' => $nav->getTitle() ? htmlReady($nav->getTitle()) : htmlReady($nav->getDescription()))) ?>
                         <? endif ?>
                         <span title="<?= $nav->getDescription() ? htmlReady($nav->getDescription()) :  htmlReady($nav->getTitle())?>" ><?= $nav->getTitle() ? htmlReady($nav->getTitle()) : '&nbsp;' ?></span>
                     </a>
                 <? else: ?>
                     <span class="quiet">
                         <? if ($image = $nav->getImage()) : ?>
-                            <img class="tab-icon" src="<?=$image['src']?>" title="<?= htmlReady($nav->getTitle()) ?>" />
+                            <?= Assets::img($image['src'], array('class' => "tab-icon", 'alt' => htmlReady($nav->getTitle()), 'title' => htmlReady($nav->getTitle()))) ?>
                         <? endif ?>
                         <?= htmlReady($nav->getTitle()) ?>
                     </span>

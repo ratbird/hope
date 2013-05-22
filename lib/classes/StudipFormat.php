f5c200d771f28fbe4b01f6ecc9cd3129dfb5aefe
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2013-05-22 09:00:54 +0000

    fixes #3553
    git-svn-id: svn://develop.studip.de/studip/trunk@26826 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/classes/StudipFormat.php
+++ lib/classes/StudipFormat.php
@@ -559,7 +559,9 @@ class StudipFormat extends TextFormat
 
         $linkmarkup = clone $markup;
         $linkmarkup->removeMarkup("links");
-        
+        $linkmarkup->removeMarkup("wiki-links");
+        $linkmarkup->removeMarkup("wiki-links-short");
+
         return sprintf('<a class="%s" href="%s"%s>%s</a>',
             $intern ? "link-intern" : "link-extern",
             $url,

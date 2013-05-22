17d3224d43089af0d32d419f615e9b92ff563dcd
tgloeggl <tgloeggl@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2013-05-22 10:59:13 +0000

    fixes #3555
    git-svn-id: svn://develop.studip.de/studip/trunk@26832 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/plugins_packages/core/Forum/views/index/_favorite.php
+++ public/plugins_packages/core/Forum/views/index/_favorite.php
@@ -3,10 +3,10 @@
 <!-- set/unset favorite -->
 <? if (!$favorite) : ?>
     <a href="<?= PluginEngine::getLink('coreforum/index/set_favorite/'. $topic_id) ?>" onClick="STUDIP.Forum.setFavorite('<?= $topic_id ?>');return false;">
-        <?= Assets::img('icons/16/blue/star.png', array('title' => _('Beitrag merken'))) ?>
+        <?= Assets::img('icons/16/blue/exclaim.png', array('title' => _('Beitrag merken'))) ?>
     </a>
 <? else : ?>
     <a href="<?= PluginEngine::getLink('coreforum/index/unset_favorite/'. $topic_id) ?>" onClick="STUDIP.Forum.unsetFavorite('<?= $topic_id ?>');return false;">
-        <?= Assets::img('icons/16/blue/accept/star.png', array('title' => _('Beitrag nicht mehr merken'))) ?>
+        <?= Assets::img('icons/16/red/exclaim.png', array('title' => _('Beitrag nicht mehr merken'))) ?>
     </a>
 <? endif ?>

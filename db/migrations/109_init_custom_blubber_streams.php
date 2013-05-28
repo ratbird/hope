2e6a33b02a1ffccb8065e3abe03c08446f47df43
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2013-05-28 07:47:13 +0000

    refs #3565: insert migration
    git-svn-id: svn://develop.studip.de/studip/trunk@26874 c5b85792-5220-0410-ad23-ed5b6b8ad999

---
+++ db/migrations/109_init_custom_blubber_streams.php
@@ -0,0 +1,74 @@
+<?php
+
+class InitCustomBlubberStreams extends Migration {
+
+    function description() {
+        return 'Introduces custom blubber streams and makes hashtags more reliable';
+    }
+
+    function up() {
+        DBManager::get()->exec("
+            CREATE TABLE IF NOT EXISTS `blubber_tags` (
+                `topic_id` varchar(32) NOT NULL,
+                `tag` varchar(128) NOT NULL,
+                UNIQUE KEY `unique_tags` (`topic_id`,`tag`),
+                KEY `tag` (`tag`),
+                KEY `topic_id` (`topic_id`)
+            ) ENGINE=MyISAM
+        ");
+        DBManager::get()->exec("
+            CREATE TABLE IF NOT EXISTS `blubber_streams` (
+                `stream_id` varchar(32) NOT NULL,
+                `user_id` varchar(32) NOT NULL,
+                `name` varchar(32) NOT NULL,
+                `sort` enum('activity','age') NOT NULL DEFAULT 'age',
+                `defaultstream` tinyint(2) NOT NULL DEFAULT '0',
+                `pool_courses` text,
+                `pool_groups` text,
+                `pool_hashtags` text,
+                `filter_type` text,
+                `filter_courses` text,
+                `filter_groups` text,
+                `filter_users` text,
+                `filter_hashtags` text,
+                `filter_nohashtags` text,
+                `chdate` bigint(20) NOT NULL,
+                `mkdate` bigint(20) NOT NULL,
+                PRIMARY KEY (`stream_id`),
+                KEY `user_id` (`user_id`)
+            ) ENGINE=MyISAM
+        ");
+        
+        //noch Hashtags/Tags in eigene Tabelle packen:
+        $statement = DBManager::get()->prepare(
+            "SELECT blubber.* " .
+            "FROM blubber " .
+            "WHERE LOCATE('#', blubber.description) > 0 " .
+        "");
+        $statement->execute();
+        $hashtag_regexp = "(?:^|\s)#([\w\d_\.\-\?!\+=%]*[\w\d])";
+        $insert_statement = DBManager::get()->prepare(
+            "INSERT IGNORE INTO blubber_tags " .
+            "SET topic_id = :topic_id, " .
+                "tag = :tag " .
+        "");
+        while($blubber = $statement->fetch(PDO::FETCH_ASSOC)) {
+            preg_match_all("/".$hashtag_regexp."/", $blubber['description'], $matches);
+            foreach ($matches as $match) {
+                $match = trim($match[0]);
+                $tag = $match[0] === "#" ? substr($match, 1) : $match;
+                if ($tag) {
+                    $insert_statement->execute(array(
+                        'topic_id' => $blubber['root_id'],
+                        'tag' => strtolower($tag)
+                    ));
+                }
+            }
+        }
+    }
+
+    function down() {
+
+    }
+
+}

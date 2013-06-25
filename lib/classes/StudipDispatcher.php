dc7b186993a49c12536c8b724b2a23a7d8531afe
mlunzena <mlunzena@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2013-06-25 08:24:12 +0000

    add StudipDispatcher, refs #3628
    git-svn-id: svn://develop.studip.de/studip/trunk@27120 c5b85792-5220-0410-ad23-ed5b6b8ad999

---
+++ lib/classes/StudipDispatcher.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * StudipDispatcher.php - create the default Trails dispatcher
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * @author      <mlunzena@uos.de>
+ * @copyright   2013 Stud.IP Core-Group
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GPL version 2
+ * @category    Stud.IP
+ */
+require_once 'vendor/trails/trails.php';
+
+/**
+ * Use this subclass to easily get an Stud.IP specific
+ * Trails_Dispatcher.
+ *
+ * Example of use:
+ * @code
+ * // deep in the Stud.IP jungle
+ * $dispatcher = new StudipDispatcher();
+ * $dispatcher->dispatch($requested_uri);
+ * @endcode
+ */
+class StudipDispatcher extends Trails_Dispatcher {
+
+  /**
+   * Create a new Trails_Dispatcher with Stud.IP specific parameters
+   * for: trails_root is "$STUDIP_BASE_PATH/app", trails_uri is
+   * "dispatch.php" and default_controller is "default" (which does
+   * not map to anything).
+   */
+    public function __construct()
+    {
+        global $STUDIP_BASE_PATH, $ABSOLUTE_URI_STUDIP;
+
+        $trails_root = $STUDIP_BASE_PATH . DIRECTORY_SEPARATOR . 'app';
+        $trails_uri = rtrim($ABSOLUTE_URI_STUDIP, '/') . '/dispatch.php';
+        $default_controller = 'default';
+
+        parent::__construct($trails_root, $trails_uri, $default_controller);
+    }
+}
--- public/dispatch.php
+++ public/dispatch.php
@@ -18,23 +18,12 @@
 
 require '../lib/bootstrap.php';
 require_once 'lib/functions.php';
-
-# define root
-$trails_root = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'app';
-
-$trails_uri = rtrim($ABSOLUTE_URI_STUDIP, '/') . '/dispatch.php';
-
-# load trails
 require_once 'vendor/trails/trails.php';
+require_once 'lib/classes/StudipDispatcher.php';
 
-# set base url for URLHelper class
 URLHelper::setBaseUrl($ABSOLUTE_URI_STUDIP);
 
-# dispatch
 $request_uri = isset($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : '/';
 
-$default_controller = 'default';
-
-$dispatcher = new Trails_Dispatcher($trails_root, $trails_uri,
-                                    $default_controller);
+$dispatcher = new StudipDispatcher();
 $dispatcher->dispatch($request_uri);

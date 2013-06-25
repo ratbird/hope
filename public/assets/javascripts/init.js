f9dddb6c208d624a0ad1df5a5ffbf582bdc36bab
mlunzena <mlunzena@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2013-06-25 12:57:58 +0000

    add modernizr, fixes #3629
    git-svn-id: svn://develop.studip.de/studip/trunk@27128 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/assets/javascripts/init.js
+++ public/assets/javascripts/init.js
@@ -2,17 +2,6 @@
 /*global window, $, jQuery, _ */
 
 /* ------------------------------------------------------------------------
- * Remove "no-js" class from <html> element, if it exists:
- * Add the new classes to the <html> element.
- *
- * Copied from https://github.com/Modernizr/Modernizr/blob/master/modernizr.js
- * ------------------------------------------------------------------------ */
-(function (elem) {
-    elem.className = elem.className.replace(/\bno-js\b/, '') + ' js';
-}(document.documentElement));
-
-
-/* ------------------------------------------------------------------------
  * jQuery plugin "metadata" configuration
  * ------------------------------------------------------------------------ */
 if ("metadata" in jQuery) {

f4af5d984a97040b7a4fa15ea58fa04176e48a32
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2013-07-01 13:37:05 +0000

    fix migration re #3574
    git-svn-id: svn://develop.studip.de/studip/trunk@27193 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- db/migrations/111_step_3574_domain.php
+++ db/migrations/111_step_3574_domain.php
@@ -9,6 +9,12 @@ class Step3574domain extends Migration {
     function up() {
         $query = "ALTER TABLE `auto_insert_sem` ADD `domain_id` VARCHAR( 45 ) NOT NULL DEFAULT ''";
         DBManager::get()->exec($query);
+        $query = "ALTER TABLE  `auto_insert_sem` DROP PRIMARY KEY ,
+                  ADD PRIMARY KEY (  `seminar_id` ,  `status` ,  `domain_id` )";
+        DBManager::get()->exec($query);
+        $query = "INSERT IGNORE INTO `auto_insert_sem`
+                  SELECT `seminar_id`, `status` , `userdomain_id` FROM  `auto_insert_sem` JOIN `userdomains`";
+        DBManager::get()->exec($query);
     }
 
     function down() {

fd376f680a20d31e758c734621d68e6b508aae07
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2013-07-24 13:19:47 +0000

    studygroups: always allow members to be kicked out, fixes #3607
    git-svn-id: svn://develop.studip.de/studip/trunk@27396 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/course/studygroup/_members_options.php
+++ app/views/course/studygroup/_members_options.php
@@ -11,6 +11,8 @@ use Studip\Button, Studip\LinkButton;
     <?= LinkButton::create(_("Runterstufen"), $controller->url_for('course/studygroup/edit_members/'.$sem_id.'/promote/autor?user='.$m['username'])) ?>
 <? elseif($GLOBALS['perm']->have_studip_perm('dozent', $sem_id)) : ?>
     <?= LinkButton::create(_("Hochstufen"), $controller->url_for('course/studygroup/edit_members/'.$sem_id.'/promote/tutor?user='.$m['username'])) ?>
-<? else : ?>
-    <?= LinkButton::create(_("Rauswerfen"), $controller->url_for('course/studygroup/edit_members/'.$sem_id.'/remove?user='.$m['username'])) ?>
 <? endif ?>
+<? if ($m['user_id'] !== $GLOBALS['user']->id): ?>
+    <?= LinkButton::create(_("Rauswerfen"), $controller->url_for('course/studygroup/edit_members/'.$sem_id.'/remove?user='.$m['username']))
+ ?>
+<? endif; ?>

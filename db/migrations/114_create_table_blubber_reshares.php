496783843314fa237b795891426f66aa1fa7e002
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2013-08-17 20:28:11 +0000

    re #3746 add functionality for resharing, but still some work is needed.
    git-svn-id: svn://develop.studip.de/studip/trunk@27683 c5b85792-5220-0410-ad23-ed5b6b8ad999

---
+++ db/migrations/114_create_table_blubber_reshares.php
@@ -0,0 +1,27 @@
+<?php
+class CreateTableBlubberReshares extends Migration
+{
+    function description()
+    {
+        return 'Creates the table that allows blubber reshares.';
+    }
+
+    function up()
+    {
+        //Add table
+        $query = "CREATE TABLE IF NOT EXISTS `blubber_reshares` (
+            `topic_id` varchar(32) NOT NULL,
+            `user_id` varchar(32) NOT NULL,
+            `external_contact` tinyint(4) NOT NULL DEFAULT '0',
+            `chdate` int(11) NOT NULL,
+            KEY `topic_id` (`topic_id`),
+            KEY `user_id` (`user_id`)
+        ) ENGINE=MyISAM";
+        $statement = DBManager::get()->prepare($query);
+        $statement->execute();
+    }
+
+    function down()
+    {
+    }
+}

8e90ecb2d9dae5cd58f756faffd583e4ad80325b
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2013-10-28 17:18:32 +0000

    fix encoding re #3929
    git-svn-id: svn://develop.studip.de/studip/trunk@28235 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/models/OpenGraphURL.class.php
+++ lib/models/OpenGraphURL.class.php
@@ -1,54 +1,44 @@
 <?php
 
 class OpenGraphURL extends SimpleORMap {
-    
-    static private $desiredEncoding = "WINDOWS-1252";
+
     static public $tempURLStorage = array();
-    
-    static public function setEncoding($encoding) {
-        self::$desiredEncoding = $encoding;
-    }
-    
+
     public function __construct($url = null) {
         $this->db_table = "opengraphdata";
         $this->registerCallback('before_store', 'serializeData');
         $this->registerCallback('after_store after_initialize', 'unserializeData');
         parent::__construct($url);
     }
-    
+
     protected function serializeData() {
         $this->data = json_encode(studip_utf8encode((array) $this->data));
     }
-    
+
     protected function unserializeData() {
         $this->data = (array) studip_utf8decode(json_decode($this->data));
     }
-    
-    public function fetch($desiredEncoding = null) {
+
+    public function fetch() {
         if (!get_config("OPENGRAPH_ENABLE")) {
             return;
         }
-        $desiredEncoding || $desiredEncoding = strtoupper(self::$desiredEncoding);
-        $content = file_get_contents($this['url']);
-        if ($content) {
+        $response = parse_link($this['url']);
+        if ($response['response_code'] == 200) {
+            $content = file_get_contents($this['url']);
             $currentEncoding = mb_detect_encoding($content);
             $currentEncoding || $currentEncoding = "UTF-8";
-            
+            $content = mb_encode_numericentity($content, array(0x80, 0xffff, 0, 0xffff), $currentEncoding);
             $old_libxml_error = libxml_use_internal_errors(true);
             $doc = new DOMDocument();
             $doc->loadHTML($content);
             libxml_use_internal_errors($old_libxml_error);
-            
+
             $metatags = $doc->getElementsByTagName('meta');
             $reservedTags = array('url', 'chdate', 'mkdate', 'last_update', 'is_opengraph', 'data');
             $isOpenGraph = false;
             $ogTags = array();
             $data = array();
-            foreach ($metatags as $tag) {
-                if ($tag->hasAttribute('charset')) {
-                    $currentEncoding = $tag->getAttribute('charset');
-                }
-            }
             foreach ($metatags as $tag) {
                 $key = false;
                 if ($tag->hasAttribute('property') &&
@@ -61,7 +51,7 @@ class OpenGraphURL extends SimpleORMap {
                     $key = strtolower(substr($tag->getAttribute('name'), 3));
                 }
                 if ($key) {
-                    $content = mb_convert_encoding($tag->getAttribute('content'), $desiredEncoding, $currentEncoding);
+                    $content = studip_utf8decode($tag->getAttribute('content'));
                     $data[] = array('og:'.$key => $content);
                     $ogTags[$key] = $content;
                     $isOpenGraph = true;
@@ -75,14 +65,14 @@ class OpenGraphURL extends SimpleORMap {
             if (!$this['title'] && $isOpenGraph) {
                 $titles = $doc->getElementsByTagName('title');
                 if ($titles->length > 0) {
-                    $this['title'] = mb_convert_encoding($titles->item(0)->textContent, $desiredEncoding, $currentEncoding);
+                    $this['title'] = studip_utf8decode($titles->item(0)->textContent);
                 }
             }
             if (!$this['description'] && $isOpenGraph) {
                 foreach ($metatags as $tag) {
-                    if (stripos($tag->getAttribute('name'), "description") !== false 
+                    if (stripos($tag->getAttribute('name'), "description") !== false
                             || stripos($tag->getAttribute('property'), "description") !== false) {
-                        $this['description'] = mb_convert_encoding($tag->getAttribute('content'), $desiredEncoding, $currentEncoding);
+                        $this['description'] = studip_utf8decode($tag->getAttribute('content'));
                     }
                 }
             }
@@ -90,16 +80,16 @@ class OpenGraphURL extends SimpleORMap {
         }
         $this['is_opengraph'] = (int) $isOpenGraph;
     }
-    
+
     public function render() {
-        if (!get_config("OPENGRAPH_ENABLE")) {
+        if (!get_config("OPENGRAPH_ENABLE") || !$this->getValue('is_opengraph')) {
             return "";
         }
         $template = $GLOBALS['template_factory']->open("shared/opengraphinfo_wide.php");
         $template->set_attribute('og', $this);
         return $template->render();
     }
-    
+
     public function getAudioFiles() {
         $files = array();
         if ($_SERVER['HTTPS'] === 'on') {
@@ -128,7 +118,7 @@ class OpenGraphURL extends SimpleORMap {
         }
         return $files;
     }
-    
+
     public function getVideoFiles() {
         $files = array();
         if ($_SERVER['HTTPS'] === 'on') {
@@ -157,6 +147,6 @@ class OpenGraphURL extends SimpleORMap {
         }
         return $files;
     }
-    
-    
+
+
 }

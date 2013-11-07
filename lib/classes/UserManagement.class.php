109bc965ab71a35ac6da23a7c8acc9eed437856e
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2013-11-07 18:21:20 +0000

    fixes #3956
    git-svn-id: svn://develop.studip.de/studip/trunk@28330 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/classes/UserManagement.class.php
+++ lib/classes/UserManagement.class.php
@@ -209,7 +209,8 @@ class UserManagement
                     $downgrade->execute(array(
                         'user_id' => $this->user_data['auth_user_md5.user_id'],
                         'old_status' => $old_status,
-                        'studygroups' => studygroup_sem_types()
+                        'studygroups' => studygroup_sem_types(),
+                        'new_max_status' => $this->user_data['auth_user_md5.perms']
                     ));
                 }
 
@@ -945,7 +946,7 @@ class UserManagement
         foreach ($queries as $query) {
             DBManager::get()->prepare($query)->execute(array($this->user_data['auth_user_md5.user_id']));
         }
-        
+
         object_kill_visits($this->user_data['auth_user_md5.user_id']);
         object_kill_views($this->user_data['auth_user_md5.user_id']);
 

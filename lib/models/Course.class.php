8a4f7ff59748566fe496de582ff8065e6aa0800f
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2013-11-21 12:38:42 +0000

    fixes #3983, re #3697
    git-svn-id: svn://develop.studip.de/studip/trunk@28460 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/models/Course.class.php
+++ lib/models/Course.class.php
@@ -161,17 +161,12 @@ class Course extends SimpleORMap
                         $course->duration_time = 0;
                     }
                 };
-        $notification_map['after_create'] = 'CourseDidCreateOrUpdate CourseDidCreate';
-        $notification_map['after_store'] = 'CourseDidCreateOrUpdate CourseDidUpdate';
-        $notificator = function($course, $cb_type) use ($notification_map) {
-            if (isset($notification_map[$cb_type])) {
-                foreach(words($notification_map[$cb_type]) as $notification) {
-                    NotificationCenter::postNotification($notification, $course);
-                }
-            }
-        };
-        $this->registerCallback(array_keys($notification_map), $notificator);
-        
+
+        $this->notification_map['after_create'] = 'CourseDidCreateOrUpdate CourseDidCreate';
+        $this->notification_map['after_store'] = 'CourseDidCreateOrUpdate CourseDidUpdate';
+        $this->notification_map['before_create'] = 'CourseWillCreate';
+        $this->notification_map['before_store'] = 'CourseWillUpdate';
+
         parent::__construct($id);
     }
 }

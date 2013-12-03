853e3f42f1f51af4aa26a6df440bde476e7046c2
fl00 <fl00@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2013-12-03 16:00:57 +0000

    re #4005 update function
    git-svn-id: svn://develop.studip.de/studip/trunk@28571 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/models/StudipNews.class.php
+++ lib/models/StudipNews.class.php
@@ -403,6 +403,34 @@ class StudipNews extends SimpleORMap {
             return false;
         }
     }
+    
+    /**
+     * Checks if the news are displayed as open
+     * 
+     * @return type
+     */
+    function isOpen() {
+        return Request::get('nopen') == $this->id;
+    }
+    
+    /**
+     * Performs all updates on news
+     * 
+     * @param type $range_id
+     * @return boolean
+     */
+    public static function update() {
+
+        // Check if user wrote a comment
+        if (Request::submitted('accept')) {
+            CSRFProtection::verifySecurityToken();
+            StudipComment::create(array(
+            'object_id' => Request::get('comsubmit'),
+            'user_id' => $GLOBALS['user']->id,
+            'content' => Request::get('comment_content')
+            ));
+        }
+    }
 
     function deleteRange($range_id) {
         if ($this->issetRange($range_id)) {

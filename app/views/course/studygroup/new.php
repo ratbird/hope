33364bec4f7c55bd2d0055ac267acd8a27b9ca4f
BlackScorp <BlackScorp@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2013-12-12 12:54:01 +0000

    Fixed: Typo
    git-svn-id: svn://develop.studip.de/studip/trunk@28682 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/course/studygroup/new.php
+++ app/views/course/studygroup/new.php
@@ -8,7 +8,7 @@ $infobox['content'] = array(
     array(
         'kategorie'=>_("Information"),
         'eintrag'=>array(
-            array("text"=>_("Studiengruppen sind eine einfache Möglichkeit, mit KommilitonInnen, KollegInnen und anderen zusammenzuarbeiten. JedeR kann Studiengruppen gründen."),"icon"=>"icons/16/black/info.png"),
+            array("text"=>_("Studiengruppen sind eine einfache Möglichkeit, mit KommilitonInnen, KollegInnen und anderen zusammenzuarbeiten. Jeder kann Studiengruppen gründen."),"icon"=>"icons/16/black/info.png"),
             array("text"=>_("Wählen Sie 'Offen für alle', wenn beliebige Nutzer der Gruppe ohne Nachfrage beitreten können sollen. 'Auf Anfrage' erfordert Ihr Eingreifen: Sie müssen jede einzelne Aufnahmeanfrage annehmen oder ablehnen."),"icon"=>"icons/16/black/info.png"),
             array("text"=>_("Alle Einstellungen können auch später noch unter dem Reiter 'Admin' geändert werden."),"icon"=>"icons/16/black/info.png")
             )

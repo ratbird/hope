6920710157f0f0abb541e315d73b48017900fe7e
BlackScorp <BlackScorp@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2013-12-16 10:27:22 +0000

    TIC #4046
    git-svn-id: svn://develop.studip.de/studip/trunk@28728 c5b85792-5220-0410-ad23-ed5b6b8ad999

---
+++ cli/fix_collate.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author Witali Mik <mik@data-quest.de>
+ * Script um Collation Konflikte automatisiert zu lösen
+ */
+
+require_once dirname(__FILE__) . '/studip_cli_env.inc.php';
+require_once 'lib/classes/DBManager.class.php';
+require_once 'config/config_local.inc.php';
+
+$charset = 'latin1';
+$collate = 'latin1_german1_ci';
+$sql = "SELECT CONCAT('ALTER TABLE `".$DB_STUDIP_DATABASE."`.`', TABLE_NAME, '` CONVERT TO CHARACTER SET ".$charset." COLLATE ".$collate.";') as query  FROM `information_schema`.TABLES WHERE TABLE_SCHEMA='".$DB_STUDIP_DATABASE."' AND TABLE_COLLATION!='".$collate."'";
+
+$db = DBManager::get();
+
+
+$result = $db->query($sql);
+foreach($result->fetchAll(PDO::FETCH_OBJ) as $row){
+   $db->exec($row->query);
+    fwrite(STDOUT, sprintf("Execute: %s \n",$row->query));
+}
+fwrite(STDOUT, "Finished");

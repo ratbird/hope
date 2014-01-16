c7996993c4345781fb7e2da67d576801a26a90c0
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-01-16 16:39:13 +0000

    rewrite html generation (include url and image as printf-parameter to avoid conflicts), fixes #4103
    git-svn-id: svn://develop.studip.de/studip/trunk@29078 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/classes/SemBrowse.class.php
+++ lib/classes/SemBrowse.class.php
@@ -344,10 +344,16 @@ class SemBrowse {
                     printf(_(" / %sVorlesungsverzeichnis%s"),"<a href=\"".URLHelper::getLink('?level=vv&cmd=qs&sset=0')."\">","</a>");
                 }
             } else {
-                printf ("<table align=\"center\" cellspacing=\"10\"><tr><td nowrap align=\"center\"><a href=\"".URLHelper::getLink('?level=ev&cmd=qs&sset=0')."\"><b>%s</b><br><br><img src=\"".$GLOBALS['ASSETS_URL']."images/institute.jpg\" %s border=\"0\"></a></td>", _("Suche in Einrichtungen"), tooltip(_("Suche im Einrichtungsverzeichnis")));
+                printf ("<table align=\"center\" cellspacing=\"10\"><tr><td nowrap align=\"center\"><a href=\"%s\"><b>%s</b><br><br>%s</a></td>",
+                        URLHelper::getLink('?level=ev&cmd=qs&sset=0'),
+                        _("Suche in Einrichtungen"),
+                        Assets::img('institute.jpg', tooltip2(_("Suche im Einrichtungsverzeichnis"))));
                 if ($this->show_class()){
                     SkipLinks::addLink(_("Suche im Vorlesungsverzeichnis"), URLHelper::getLink('sem_portal.php', array('level' => 'vv', 'cmd' => 'qs', 'sset' => '0')));
-                    printf ("<td nowrap align=\"center\"><a href=\"".URLHelper::getLink('?level=vv&cmd=qs&sset=0')."\"><b>%s</b><br><br><img src=\"".$GLOBALS['ASSETS_URL']."images/kommentar.jpg\" %s border=\"0\"></a></td>", _("Suche im Vorlesungsverzeichnis"), tooltip(_("Suche im Vorlesungsverzeichnis")));
+                    printf ("<td nowrap align=\"center\"><a href=\"%s\"><b>%s</b><br><br>%s</a></td>",
+                            URLHelper::getLink('?level=vv&cmd=qs&sset=0'),
+                            _("Suche im Vorlesungsverzeichnis"),
+                            Assets::img('kommentar.jpg', tooltip2(_("Suche im Vorlesungsverzeichnis"))));
                 }
                 SkipLinks::addLink(_("Suche im Einrichtungsverzeichnis"), URLHelper::getLink('sem_portal.php', array('level' => 'ev', 'cmd' => 'qs', 'sset' => '0')));
                 printf ("</tr></table>");

67380dd0a7d9b76b17c36150ec82e4c16040b320
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-01-20 14:01:05 +0000

    rewrite link generation to avoid printf()-glitches, fixes #4103
    git-svn-id: svn://develop.studip.de/studip/trunk@29101 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/resources/views/ShowGroupSchedules.class.php
+++ lib/resources/views/ShowGroupSchedules.class.php
@@ -287,12 +287,13 @@ class ShowGroupSchedules extends ShowSemSchedules {
             </tr>
             <tr>
                 <td class="<? echo $cssSw->getClass() ?>" width="4%" align="center" valign="bottom">&nbsp;
-                    <?
-                    if ((!$_SESSION['resources_data']["schedule_time_range"]) || ($_SESSION['resources_data']["schedule_time_range"] == -1))
-                        printf ("<a href=\"".URLHelper::getLink('?quick_view=%s&quick_view_mode=%s&time_range=%s')."\">"
-                        . Assets::img("icons/16/blue/arr_2down.png", array('class' => "middle", 'alt' => _("Frühere Belegungen anzeigen"), 'title' => _("Spätere Belegungen anzeigen")))
-                        . "</a>", $this->used_view, $view_mode, ($_SESSION['resources_data']["schedule_time_range"]) ? "FALSE" : 1);
-                    ?>
+                <? if ((!$_SESSION['resources_data']['schedule_time_range']) || ($_SESSION['resources_data']['schedule_time_range'] == -1)): ?>
+                    <a href="<?= URLHelper::getLink('', array('quick_view' => $this->used_view,
+                                                              'quick_view_mode' => $view_mode,
+                                                              'time_range' => $_SESSION['resources_data']['schedule_time_range'] ? 'FALSE' : 1)) ?>">
+                        <?= Assets::img('icons/16/blue/arr_2down.png', array('class' => 'middle') + tooltip2(_('Frühere Belegungen anzeigen'))) ?>
+                    </a>
+                <? endif; ?>
                 </td>
                 <td class="<? echo $cssSw->getClass() ?>" width="20%" nowrap colspan="3">
                 &nbsp;

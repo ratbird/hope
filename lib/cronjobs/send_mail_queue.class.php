57f144d9502532c7a969699f20021d73be1ebd67
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-01-24 16:37:14 +0000

    refs #3713
    Add docu for cronjob.
    
    In der Klasse MailQueueEntry fehlt noch echte Doku. Die baue ich die nächsten Tage noch ein. Stay tuned.
    
    git-svn-id: svn://develop.studip.de/studip/trunk@29164 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/cronjobs/send_mail_queue.class.php
+++ lib/cronjobs/send_mail_queue.class.php
@@ -8,19 +8,37 @@
 */
 require_once 'lib/classes/CronJob.class.php';
 
+/**
+ * Cronjob class to send the mailqueue each interval.
+ */
 class SendMailQueueJob extends CronJob
 {
 
+    /**
+     * Returns the name of the cronjob.
+     * @return string : name of the cronjob
+     */
     public static function getName()
     {
         return _('Mailqueue senden');
     }
 
+    /**
+     * Returns the description of the cronjob.
+     * @return string : description of the cronjob.
+     */
     public static function getDescription()
     {
         return _('Sendet alle Einträge in der Mailqueue bis zu 24 Stunden, nachdem sie hinzugefügt wurden.');
     }
 
+    /**
+     * Sends all mails in the queue.
+     * @param integer $last_result : not evaluated for execution, so any integer 
+     * will do. Usually it would be a unix-timestamp of last execution. But in 
+     * this case we don't care at all.
+     * @param array $parameters : not needed here
+     */
     public function execute($last_result, $parameters = array())
     {
         MailQueueEntry::sendAll();

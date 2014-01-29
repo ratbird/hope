0bc44504157984c5c5ef7352c6e9ee9e9c0726a1
BlackScorp <BlackScorp@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-01-29 10:38:50 +0000

    re #4006 alle parameter in url_for eingetragen
    git-svn-id: svn://develop.studip.de/studip/trunk@29212 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/admin/autoinsert/_status.php
+++ app/views/admin/autoinsert/_status.php
@@ -2,20 +2,19 @@
 # Lifter010: TODO
 ?>
 <td align="left">
-    
- <?php foreach($domains as $domain) :?>
-       <div>
-       
-<? if (isset( $auto_sem['status'][$domain['id']]) && in_array($status,$auto_sem['status'][$domain['id']])) : ?>
-    <a href="<?= $controller->url_for('admin/autoinsert/edit/'.$auto_sem['seminar_id']) ?>/<?= $domain['id'] ?>/<?= $status ?>/1">
-        <?= Assets::img('icons/16/blue/checkbox-checked.png') ?>
-    </a>
-<? else : ?>
-    <a href="<?= $controller->url_for('admin/autoinsert/edit/'.$auto_sem['seminar_id']) ?>/<?= $domain['id'] ?>/<?= $status ?>">
-        <?= Assets::img('icons/16/blue/checkbox-unchecked.png') ?>
-    </a>
-<? endif ?>
-       <?= htmlReady($domain['name'])?></div>
-    <?php endforeach;?>
-        
+
+    <?php foreach ($domains as $domain) : ?>
+        <div>
+
+            <? if (isset($auto_sem['status'][$domain['id']]) && in_array($status, $auto_sem['status'][$domain['id']])) : ?>
+                <a href="<?= $controller->url_for('admin/autoinsert/edit/' . $auto_sem['seminar_id'] . '/' . $domain['id'] . '/' . $status . '/1') ?>">
+                    <?= Assets::img('icons/16/blue/checkbox-checked.png') ?>
+                </a>
+            <? else : ?>
+                <a href="<?= $controller->url_for('admin/autoinsert/edit/' . $auto_sem['seminar_id'] . '/' . $domain['id'] . '/' . $status . '') ?>">        <?= Assets::img('icons/16/blue/checkbox-unchecked.png') ?>
+                </a>
+            <? endif ?>
+            <?= htmlReady($domain['name']) ?></div>
+    <?php endforeach; ?>
+
 </td>

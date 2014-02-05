1c1d3052abdfa86145db14252a7fdf4acb107234
mlunzena <mlunzena@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-02-05 09:31:00 +0000

    add dedicated recursive version of studip_utf8encode, fixes #4135
    git-svn-id: svn://develop.studip.de/studip/trunk@29267 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/functions.php
+++ lib/functions.php
@@ -1691,7 +1691,7 @@ function words($string)
  */
 function studip_utf8encode($data)
 {
-    if (is_array($data) || is_object($data) && $data instanceof Traversable) {
+    if (is_array($data) || $data instanceof Traversable) {
         $new_data = array();
         foreach ($data as $key => $value) {
             $key = studip_utf8encode($key);
@@ -1709,6 +1709,61 @@ function studip_utf8encode($data)
             'UTF-8'
         );
     }
+}
+
+/**
+ * This function tries to encode data of any type from Windows-1252 to
+ * UTF-8, and returns the encoded version.
+ *
+ * If the argument `$data` is an array or an object that implements
+ * `Traversable`, this function returns an associative array. Its keys
+ * are encoded to UTF-8 and its values are send to this function
+ * again.
+ *
+ * If the argument `$data` is a string or an object that responds to
+ * `__toString`, this function casts it to a string and encodes it to
+ * UTF-8.
+ *
+ * If the argument `$data` is of another scalar type (integer, float
+ * or boolean) or is null, this function just returns that value
+ * unchanged.
+ *
+ * If neither of these criteria match, this functions throws an
+ * InvalidArgumentException.
+ *
+ * @param $data mixed  some data of any type that shall be encoded to
+ *                     UTF-8 in the aforementioned manner
+ *
+ * @return mixed  that data encoded to UTF-8 as far as possible, see above
+ *
+ * @throws InvalidArgumentException This exception is thrown if there
+ * is no way to encode such an object to UTF-8, e.g. database
+ * connections, file handles etc.
+ */
+function studip_utf8encode_recursive($data)
+{
+    // array-artiges wird rekursiv durchlaufen
+    if (is_array($data) || $data instanceof Traversable) {
+        $new_data = array();
+        foreach ($data as $key => $value) {
+            $key = studip_utf8encode($key);
+            $new_data[$key] = studip_utf8encode_recursive($value);
+        }
+        return $new_data;
+    }
+
+    // string-artiges wird an die nicht-rekursive Variante übergeben
+    else if (is_string($data) || is_callable(array($data, '__toString'))) {
+        return studip_utf8encode((string)$data);
+    }
+
+    // skalare Werte und `null` wird so durchgeschleift
+    elseif (is_null($data) || is_scalar($data)) {
+        return $data;
+    }
+
+    // alles andere ist ungültig
+    throw new InvalidArgumentException();
 }
 
 /**
--- test/phpunit/lib/functions_test.php
+++ test/phpunit/lib/functions_test.php
@@ -12,6 +12,17 @@
 require_once dirname(__FILE__) . '/../bootstrap.php';
 require_once 'lib/functions.php';
 
+class StringWrapper {
+    function __construct($string)
+    {
+        $this->string = $string;
+    }
+    function __toString()
+    {
+        return (string) $this->string;
+    }
+}
+
 class FunctionsTest extends PHPUnit_Framework_TestCase {
 
   function testWords() {
@@ -31,5 +42,35 @@ class FunctionsTest extends PHPUnit_Framework_TestCase {
         );
 
         $this->assertEquals(range(1, 7), array_flatten($array));
+    }
+
+    function testUTF8Encoding()
+    {
+        $pairs = array(
+            array(null, null),
+            array(true, true),
+            array(false, false),
+            array(23, 23),
+            array(1/9998, 1/9998),
+            array('äbc', "\xc3\xa4bc"),
+        );
+
+        // an object responding to __toString
+        $pairs[] = array(new StringWrapper("äbc"), "\xc3\xa4bc");
+
+        // an array
+        $array_data = array(null, 23, 'äbc');
+        $array_expected = array(null, 23, "\xc3\xa4bc");
+        $pairs[] = array($array_data, $array_expected);
+
+        // something Traversable
+        $traversable = new ArrayIterator($array_data);
+        $pairs[] = array($traversable, $array_expected);
+
+
+        foreach ($pairs as $pair) {
+            list($data, $expected) = $pair;
+            $this->assertEquals($expected, studip_utf8encode_recursive($data));
+        }
     }
 }

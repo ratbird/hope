13ab919e33f23ae4b17c03a69f2be3f2d5362e5f
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-02-06 12:07:38 +0000

    refs #4022
    fix jslint
    
    git-svn-id: svn://develop.studip.de/studip/trunk@29286 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/assets/javascripts/register.js
+++ public/assets/javascripts/register.js
@@ -6,16 +6,16 @@ STUDIP.register = {
     re_email: null,
     re_name: null,
             
-    clearErrors: function(field) {
+    clearErrors: function (field) {
         jQuery('input[name=' + field + ']').parent().find('div').remove();
     },
 
-    addError: function(field, error) {
+    addError: function (field, error) {
         jQuery('input[name=' + field + ']').parent().append('<div class="error">' + error + '</div>');
         jQuery('div[class=error]').show();
     },
     
-    checkusername: function(){
+    checkusername: function () {
         STUDIP.register.clearErrors('username');
         
         var checked = true;
@@ -25,7 +25,7 @@ STUDIP.register = {
             checked = false;
         }
         
-        if (STUDIP.register.re_username.test(jQuery('input[name=username]').val()) ==false) {
+        if (STUDIP.register.re_username.test(jQuery('input[name=username]').val()) === false) {
             STUDIP.register.addError('username', "Der Benutzername enthält unzulässige Zeichen, er darf keine Sonderzeichen oder Leerzeichen enthalten.".toLocaleString());
             document.login.username.focus();
             checked = false;
@@ -33,7 +33,7 @@ STUDIP.register = {
         return checked;
     },
 
-    checkpassword: function(){
+    checkpassword: function () {
         STUDIP.register.clearErrors('password');
 
         var checked = true;
@@ -45,11 +45,11 @@ STUDIP.register = {
         return checked;
     },
 
-    checkpassword2: function(){
+    checkpassword2: function () {
         STUDIP.register.clearErrors('password2');
 
         var checked = true;
-        if (jQuery('input[name=password]').val() != jQuery('input[name=password2]').val()) {
+        if (jQuery('input[name=password]').val() !== jQuery('input[name=password2]').val()) {
             STUDIP.register.addError('password2', "Das Passwort stimmt nicht mit dem Bestätigungspasswort überein!".toLocaleString());
             document.login.password2.focus();
             checked = false;
@@ -57,11 +57,11 @@ STUDIP.register = {
         return checked;
     },
 
-    checkVorname: function(){
+    checkVorname: function () {
         STUDIP.register.clearErrors('Vorname');
 
         var checked = true;
-        if (STUDIP.register.re_name.test(jQuery('input[name=Vorname]').val()) == false) {
+        if (STUDIP.register.re_name.test(jQuery('input[name=Vorname]').val()) === false) {
             STUDIP.register.addError('Vorname', "Bitte geben Sie Ihren tatsächlichen Vornamen an.".toLocaleString());
             document.login.Vorname.focus();
             checked = false;
@@ -69,11 +69,11 @@ STUDIP.register = {
         return checked;
     },
 
-    checkNachname: function() {
+    checkNachname: function () {
         STUDIP.register.clearErrors('Nachname');
         
         var checked = true;
-        if (STUDIP.register.re_name.test(jQuery('input[name=Nachname]').val()) == false) {
+        if (STUDIP.register.re_name.test(jQuery('input[name=Nachname]').val()) === false) {
             STUDIP.register.addError('Nachname', "Bitte geben Sie Ihren tatsächlichen Nachnamen an.".toLocaleString());
             document.login.Nachname.focus();
             checked = false;
@@ -81,13 +81,13 @@ STUDIP.register = {
         return checked;
     },
 
-    checkEmail: function() {
+    checkEmail: function () {
         STUDIP.register.clearErrors('Email');
 
         var Email = jQuery('input[name=Email]').val();
 
         var checked = true;
-        if ((STUDIP.register.re_email.test(Email)) == false || Email.length==0) {
+        if ((STUDIP.register.re_email.test(Email)) === false || Email.length === 0) {
             STUDIP.register.addError('Email', "Die E-Mail-Adresse ist nicht korrekt!".toLocaleString());
             document.login.Email.focus();
             checked = false;
@@ -95,17 +95,12 @@ STUDIP.register = {
         return checked;
     },
 
-    checkdata: function(){
-        // kompletter Check aller Felder vor dem Abschicken
-        var checked = true;
-        
-        if (!this.checkusername()) checked = false;
-        if (!this.checkpassword()) checked = false;
-        if (!this.checkpassword2()) checked = false;
-        if (!this.checkVorname()) checked = false;
-        if (!this.checkNachname()) checked = false;
-        if (!this.checkEmail()) checked = false;
-
-        return checked;
+    checkdata: function () {
+        return this.checkusername() && 
+                this.checkpassword() && 
+                this.checkpassword2() && 
+                this.checkVorname() && 
+                this.checkNachname() && 
+                this.checkEmail();
     }
-}
+};

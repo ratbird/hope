fb9ef531364e8563aaa017c561f2349ecef56ac9
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-02-06 12:24:35 +0000

    refs #3989
    fix jslint
    Du hast noch einige Stellen, wo Variablen nicht definiert worden sind oder doppelt definiert worden sind. Ich poste hier einfach mal zu:
    'dialog_width' is not defined.
    'dialog_height' is not defined.
    'button' is not defined. (hier fehlt wohl ein var in Zeile 113?)
    'form_data' is not defined. (ebenfalls ein var in Zeile 115?)
    'pending_ajax_request' is not defined.
    'obj' is already defined. (Zeile 160)
    'url' is not defined. (fehlendes var? Zeile 164)
    
    
    git-svn-id: svn://develop.studip.de/studip/trunk@29287 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/assets/javascripts/news.js
+++ public/assets/javascripts/news.js
@@ -42,9 +42,9 @@ STUDIP.News = {
     },
 
     get_dialog: function (id, route, from_x, from_y) {
-	    // initialize dialog
-    	jQuery('body').append('<div id="'+id+'"></div>');
-    	jQuery('#'+id).dialog({ 
+    // initialize dialog
+        jQuery('body').append('<div id="' + id + '"></div>');
+        jQuery('#' + id).dialog({ 
             modal: true, 
             resizable: false,
             width: 100,
@@ -52,69 +52,69 @@ STUDIP.News = {
             title: 'Dialog wird geladen...'.toLocaleString(),
             hide: 'fadeOut',
             // define close animation
-            beforeClose: function( event, ui ) {
-    			jQuery('#'+id).dialog('widget').stop(true, true);
-    			jQuery('#'+id).dialog('widget').animate({
-    				width: 100,
-    				height: 40,
-    				left: from_x - 50,
-    				top: jQuery(document).scrollTop() + from_y - 20,
-    				opacity: 0
-    			}, {
-    				duration: 400,
-    				easing: 'swing'
-    			});
-    		}
+            beforeClose: function (event, ui) {
+                jQuery('#' + id).dialog('widget').stop(true, true);
+                jQuery('#' + id).dialog('widget').animate({
+                    width: 100,
+                    height: 40,
+                    left: from_x - 50,
+                    top: jQuery(document).scrollTop() + from_y - 20,
+                    opacity: 0
+                }, {
+                    duration: 400,
+                    easing: 'swing'
+                });
+            }
         });
-	    // show pre-loading dialog animation
-    	jQuery('#'+id).html('<div class="ajax_notification" style="text-align: center; padding-right: 24px; padding-top: 55px"><div class="notification"></div></div>');
-    	jQuery('#'+id).dialog('option', 'position', [from_x - 50, from_y - 20]);
-        jQuery('#'+id).dialog('widget').css('opacity', 0);
-        jQuery('#'+id).dialog('widget').animate({
+        // show pre-loading dialog animation
+        jQuery('#' + id).html('<div class="ajax_notification" style="text-align: center; padding-right: 24px; padding-top: 55px"><div class="notification"></div></div>');
+        jQuery('#' + id).dialog('option', 'position', [from_x - 50, from_y - 20]);
+        jQuery('#' + id).dialog('widget').css('opacity', 0);
+        jQuery('#' + id).dialog('widget').animate({
             width: dialog_width,
             height: dialog_height,
             left: (window.innerWidth / 2) - (dialog_width / 2),
             top: jQuery(document).scrollTop() + (window.innerHeight / 2) - (dialog_height / 2),
-			opacity: 1
+            opacity: 1
         }, {
             duration: 400,
             easing: 'swing'
         });
         // load actual dialog content
-    	jQuery.ajax({
+        jQuery.ajax({
             'url': route,
             'dataType': 'HTML',
             'success': function (html, status, xhr) {
-                jQuery('#'+id).dialog('option', 'title', xhr.getResponseHeader('X-Title'));
-                jQuery('#'+id).dialog('widget').stop(true, true);
+                jQuery('#' + id).dialog('option', 'title', xhr.getResponseHeader('X-Title'));
+                jQuery('#' + id).dialog('widget').stop(true, true);
                 // set to full size (even if dialog.close was triggered before)
-                jQuery('#'+id).dialog('widget').animate({
+                jQuery('#' + id).dialog('widget').animate({
                     left: (window.innerWidth / 2) - (dialog_width / 2),
                     top: jQuery(document).scrollTop() + (window.innerHeight / 2) - (dialog_height / 2),
-        			opacity: 1
+                    opacity: 1
                 }, 0);
-                jQuery('#'+id).dialog({
+                jQuery('#' + id).dialog({
                     height: dialog_height,
                     width: dialog_width
                 });
-                jQuery('#'+id).html(html);
-                jQuery('#'+id+'_content').css({
-                	'height' : dialog_height-110+"px", 
-                    'maxHeight': dialog_height-110+"px"
+                jQuery('#' + id).html(html);
+                jQuery('#' + id + '_content').css({
+                    'height' : dialog_height - 110 + "px", 
+                    'maxHeight': dialog_height - 110 + "px"
                 });
                 jQuery('.ui-dialog-content').css({'padding-right' : '1px'});
 
                 // prevent forms within dialog from reloading whole page, and reload dialog instead
-                jQuery('#'+id+' form').live('click', function(event) {
-                	jQuery(this).data('clicked', $(event.target));
+                jQuery('#' + id + ' form').live('click', function (event) {
+                    jQuery(this).data('clicked', $(event.target));
                 });
-                jQuery('#'+id+' form').live('submit', function (event) {
-                	event.preventDefault();
-            		button = jQuery(this).data('clicked').attr('name');
-            		route = jQuery(this).attr('action');
-            		form_data = jQuery(this).serialize()+'&'+button+'=1';
-            		jQuery(this).find('input[name='+button+']').showAjaxNotification('left');
-            		STUDIP.News.update_dialog(id, route, form_data);
+                jQuery('#' + id + ' form').live('submit', function (event) {
+                    event.preventDefault();
+                    button = jQuery(this).data('clicked').attr('name');
+                    route = jQuery(this).attr('action');
+                    form_data = jQuery(this).serialize() + '&' + button + '=1';
+                    jQuery(this).find('input[name=' + button + ']').showAjaxNotification('left');
+                    STUDIP.News.update_dialog(id, route, form_data);
                 });
 
                 // fix added elements (as in application.js)
@@ -136,61 +136,61 @@ STUDIP.News = {
 
     update_dialog: function (id, route, form_data) {
         if (!pending_ajax_request) {
-        	pending_ajax_request = true;
-        	jQuery.ajax({
-        		'url': route,
-        		'type': 'POST',
-        		'data': form_data,
-        		'dataType': 'HTML',
-        		'success': function (html) {
-        			pending_ajax_request = false;
-        			if (html.length > 0) {
-        				jQuery('#'+id).html(html);
-        				jQuery('#'+id+'_content').css({
-        					'height' : dialog_height-110+"px", 
-        					'maxHeight': dialog_height-110+"px"
-        				});
-        				// scroll to anker
-        				var obj = jQuery('a[name=anker]');
-        				if (obj.length > 0) {
-        					jQuery('#'+id+'_content').scrollTop(obj.position().top);
-        				}                
-        			} else {
-        				jQuery('#'+id).dialog('close');
-        				var obj = jQuery('#admin_news_form');
-        				if (obj.length > 0) {
-            				jQuery('#admin_news_form').submit();
-            			} else {
-            				url = location.href.split('?');
-            				location.replace(url[0]+'?nsave=1');
-            			}
-        			}
-        			// fix added elements (as in application.js)
+            pending_ajax_request = true;
+            jQuery.ajax({
+                'url': route,
+                'type': 'POST',
+                'data': form_data,
+                'dataType': 'HTML',
+                'success': function (html) {
+                    pending_ajax_request = false;
+                    if (html.length > 0) {
+                        jQuery('#' + id).html(html);
+                        jQuery('#' + id + '_content').css({
+                            'height' : dialog_height - 110 + "px", 
+                            'maxHeight': dialog_height - 110 + "px"
+                        });
+                        // scroll to anker
+                        var obj = jQuery('a[name=anker]');
+                        if (obj.length > 0) {
+                            jQuery('#' + id + '_content').scrollTop(obj.position().top);
+                        }                
+                    } else {
+                        jQuery('#' + id).dialog('close');
+                        var obj = jQuery('#admin_news_form');
+                        if (obj.length > 0) {
+                            jQuery('#admin_news_form').submit();
+                        } else {
+                            url = location.href.split('?');
+                            location.replace(url[0] + '?nsave=1');
+                        }
+                    }
+                    // fix added elements (as in application.js)
                     if (!("autofocus" in document.createElement("input"))) {
                         jQuery('[autofocus]').last().focus();
                     }
-        			jQuery('.add_toolbar').addToolbar();
-        			jQuery('textarea.resizable').resizable({
-        				handles: 's',
-        				minHeight: 50,
-        				zIndex: 1
-        			});
-            	},
-            	'fail': function () {
-            		pending_ajax_request = false;
-            		alert("Fehler beim Aufruf des News-Controllers");
-            	}
-        	});
+                    jQuery('.add_toolbar').addToolbar();
+                    jQuery('textarea.resizable').resizable({
+                        handles: 's',
+                        minHeight: 50,
+                        zIndex: 1
+                    });
+                },
+                'fail': function () {
+                    pending_ajax_request = false;
+                    alert("Fehler beim Aufruf des News-Controllers");
+                }
+            });
         }
     },
     
     toggle_category_view: function (id) {
-		if (jQuery("input[name=" + id + "_js]").val() == "toggle") {
-			jQuery("input[name=" + id + "_js]").val("")
-		} else {
-			jQuery("input[name=" + id + "_js]").val("toggle");
-		}
-    	if (jQuery("#" + id + "_content").is(':visible')) {
+        if (jQuery("input[name=" + id + "_js]").val() === "toggle") {
+            jQuery("input[name=" + id + "_js]").val("");
+        } else {
+            jQuery("input[name=" + id + "_js]").val("toggle");
+        }
+        if (jQuery("#" + id + "_content").is(':visible')) {
             jQuery("#" + id + "_content").slideUp(400);
             jQuery("#" + id + " input[type=image]:first")
                 .attr('src', STUDIP.ASSETS_URL + "images/icons/16/blue/arr_1right.png");
@@ -203,32 +203,34 @@ STUDIP.News = {
 };
 
 jQuery(function () {
-	dialog_height = window.innerHeight - 60;
-	dialog_width = window.innerWidth * 1 / 2;
-	if (dialog_width < 550)
-		dialog_width = 550;
-    if (dialog_height < 400)
+    dialog_height = window.innerHeight - 60;
+    dialog_width = window.innerWidth * 1 / 2;
+    if (dialog_width < 550) {
+        dialog_width = 550;
+    }
+    if (dialog_height < 400) {
         dialog_height = 400;
+    }
     pending_ajax_request = false;
 
     jQuery('a[rel~="get_dialog"]').live('click', function (event) {
-	    event.preventDefault();
+        event.preventDefault();
         var from_x = jQuery(this).position().left + (jQuery(this).outerWidth() / 2);
         var from_y = jQuery(this).position().top + (jQuery(this).outerHeight() / 2) - jQuery(document).scrollTop();
-		STUDIP.News.get_dialog('news_dialog', jQuery(this).attr('href'), from_x, from_y);
+        STUDIP.News.get_dialog('news_dialog', jQuery(this).attr('href'), from_x, from_y);
     });
 
     jQuery('a[rel~="close_dialog"]').live('click', function (event) {
-	    event.preventDefault();
-	    jQuery('#news_dialog').dialog('close');
+        event.preventDefault();
+        jQuery('#news_dialog').dialog('close');
     });
 
     // open/close categories without ajax-request
     jQuery('.news_category_header').live('click', function (event) {
-    	event.preventDefault();
-    	STUDIP.News.toggle_category_view(jQuery(this).parent('div').attr('id'));
+        event.preventDefault();
+        STUDIP.News.toggle_category_view(jQuery(this).parent('div').attr('id'));
     });
     jQuery('.news_category_header input[type=image]').live('click', function (event) {
-    	event.preventDefault();
+        event.preventDefault();
     });
 });

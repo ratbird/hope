fcd9ef4e61d7484981994f5f92ef90e780ee7455
mlunzena <mlunzena@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-02-06 07:45:42 +0000

    remove studip_utf8encode_recursive again, refs #3484, #4120, #4135, #4136
    git-svn-id: svn://develop.studip.de/studip/trunk@29277 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/classes/restapi/renderer/JSONRenderer.php
+++ lib/classes/restapi/renderer/JSONRenderer.php
@@ -28,7 +28,63 @@ class JSONRenderer extends DefaultRenderer
         }
 
         if (isset($response->body)) {
-            $response->body = json_encode(studip_utf8encode_recursive($response->body));
+            $response->body = json_encode(self::utf8encodeRecursive($response->body));
         }
+    }
+
+
+    /**
+     * This function tries to encode data of any type from Windows-1252 to
+     * UTF-8, and returns the encoded version.
+     *
+     * If the argument `$data` is an array or an object that implements
+     * `Traversable`, this function returns an associative array. Its keys
+     * are encoded to UTF-8 and its values are send to this function
+     * again.
+     *
+     * If the argument `$data` is a string or an object that responds to
+     * `__toString`, this function casts it to a string and encodes it to
+     * UTF-8.
+     *
+     * If the argument `$data` is of another scalar type (integer, float
+     * or boolean) or is null, this function just returns that value
+     * unchanged.
+     *
+     * If neither of these criteria match, this functions throws an
+     * InvalidArgumentException.
+     *
+     * @param $data mixed  some data of any type that shall be encoded to
+     *                     UTF-8 in the aforementioned manner
+     *
+     * @return mixed  that data encoded to UTF-8 as far as possible, see above
+     *
+     * @throws InvalidArgumentException This exception is thrown if there
+     * is no way to encode such an object to UTF-8, e.g. database
+     * connections, file handles etc.
+     */
+    private static function utf8encodeRecursive($data)
+    {
+        // array-artiges wird rekursiv durchlaufen
+        if (is_array($data) || $data instanceof \Traversable) {
+            $new_data = array();
+            foreach ($data as $key => $value) {
+                $key = studip_utf8encode((string) $key);
+                $new_data[$key] = self::utf8encodeRecursive($value);
+            }
+            return $new_data;
+        }
+
+        // string-artiges wird an die nicht-rekursive Variante übergeben
+        else if (is_string($data) || is_callable(array($data, '__toString'))) {
+            return studip_utf8encode((string) $data);
+        }
+
+        // skalare Werte und `null` wird so durchgeschleift
+        elseif (is_null($data) || is_scalar($data)) {
+            return $data;
+        }
+
+        // alles andere ist ungültig
+        throw new \InvalidArgumentException();
     }
 }

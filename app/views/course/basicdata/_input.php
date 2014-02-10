1d116d7369aaa319ee7288015fd7be16f5fa85b8
davka <davka@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-02-10 12:46:06 +0000

    prevent xss-atacks fixes #4184
    git-svn-id: svn://develop.studip.de/studip/trunk@29350 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/course/basicdata/_input.php
+++ app/views/course/basicdata/_input.php
@@ -2,16 +2,16 @@
 # Lifter010: TODO
 $is_locked = $input['locked'] ? 'disabled readonly' : '';
 if ($input['type'] === "text") : ?>
-    <input <?=$is_locked ?> type="text" name="<?= $input['name'] ?>" value="<?= $input['value'] ?>" style="width: 80%">
+    <input <?=$is_locked ?> type="text" name="<?= $input['name'] ?>" value="<?= htmlReady($input['value']) ?>" style="width: 80%">
 <? endif;
 
 if ($input['type'] === "number") : ?>
-    <input <?=$is_locked ?> type="number" name="<?= $input['name'] ?>" value="<?= $input['value'] ?>" style="width: 80%" min="<?= $input['min'] ?>">
+    <input <?=$is_locked ?> type="number" name="<?= $input['name'] ?>" value="<?= htmlReady($input['value']) ?>" style="width: 80%" min="<?= $input['min'] ?>">
 <? endif; 
 
 if ($input['type'] === "textarea") : ?>
     <textarea <?=$is_locked ?> name="<?= $input['name'] ?>" style="width: 80%; height: 100px;" class=""><?=
-        $input['value']
+        htmlReady($input['value'])
     ?></textarea>
 <? endif;
 
@@ -21,21 +21,21 @@ if ($input['type'] === "select") : ?>
     <? else : ?>
     <select <?=$is_locked ?> name="<?= $input['name'] ?>" style="width: 80%">
     <? if ($input['choices']) : foreach ($input['choices'] as $choice_value => $choice_name) : ?>
-        <option value="<?= $choice_value ?>"<?
+        <option value="<?= htmlReady($choice_value) ?>"<?
             if ($choice_value == $input['value']) print " selected"
-            ?>><?= $choice_name ?></option>
+            ?>><?= htmlReady($choice_name) ?></option>
     <? endforeach; endif; ?>
     </select>
     <? endif ?>
 <? endif;
 
 if ($input['type'] === "multiselect") : ?>
-    <select <?=$is_locked ?> name="<?= $input['name'] ?>" style="width: 80%" multiple size="8">
+    <select <?=$is_locked ?> name="<?= htmlReady($input['name']) ?>" style="width: 80%" multiple size="8">
     <? if ($input['choices']) : foreach ($input['choices'] as $choice_value => $choice_name) : ?>
-        <option value="<?= $choice_value ?>"<?=
+        <option value="<?= htmlReady($choice_value) ?>"<?=
             in_array($choice_value, is_array($input['value']) ? $input['value'] : array($input['value']))
             ? " selected"
-            : "" ?>><?= $choice_name ?></option>
+            : "" ?>><?= htmlReady($choice_name) ?></option>
     <? endforeach; endif; ?>
     </select>
 <? endif;

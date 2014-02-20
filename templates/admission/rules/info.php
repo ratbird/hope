b30daed71382448a89f5e7dd01b9cf35324de4f8
hackl <hackl@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-02-20 11:16:17 +0000

    re #4063 use newline only when necessary
    git-svn-id: svn://develop.studip.de/studip/trunk@29554 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/admission/rules/info.php
+++ templates/admission/rules/info.php
@@ -1,13 +1,12 @@
 <i><?= _('Nachricht bei nicht erfolgreicher Anmeldung:') ?></i>
 <br/>
 <?= $rule->getMessage() ?>
-<br/>
 <?php
 if ($rule->getStartTime() && $rule->getEndTime()) {
-    echo sprintf(_('Diese Regel gilt von %s bis %s'), strftime('%d.%m.%Y',
+    echo '<br/>'.sprintf(_('Diese Regel gilt von %s bis %s'), strftime('%d.%m.%Y',
         $rule->getStartTime()), strftime('%d.%m.%Y', $rule->getEndTime()));
 } else if ($rule->getStartTime() && !$rule->getEndTime()) {
-    echo sprintf(_('Diese Regel gilt ab %s'), strftime('%d.%m.%Y', $rule->getStartTime()));
+    echo '<br/>'.sprintf(_('Diese Regel gilt ab %s'), strftime('%d.%m.%Y', $rule->getStartTime()));
 } else if (!$rule->getStartTime() && $rule->getEndTime()) {
-    echo sprintf(_('Diese Regel gilt bis %s'), strftime('%d.%m.%Y', $rule->getEndTime()));
+    echo '<br/>'.sprintf(_('Diese Regel gilt bis %s'), strftime('%d.%m.%Y', $rule->getEndTime()));
 }

28229b644a6d350b1d5c65c6d311a22bcb675540
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-02-26 09:58:37 +0000

    enhance compatibility re #4070
    git-svn-id: svn://develop.studip.de/studip/trunk@29652 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- db/migrations/130_step_00267_preliminary_accounts.php
+++ db/migrations/130_step_00267_preliminary_accounts.php
@@ -10,5 +10,6 @@ Class Step00267PreliminaryAccounts extends Migration {
     function up()
     {
         DBManager::get()->exec("UPDATE auth_user_md5 SET auth_plugin='standard' WHERE auth_plugin IS NULL");
+        DBManager::get()->exec("ALTER TABLE `auth_user_md5` CHANGE `auth_plugin` `auth_plugin` VARCHAR(64) NULL DEFAULT 'standard'");
     }
 }

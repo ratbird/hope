72884bc0a5b0547574e77db054bc4cc3d6ab78db
mlunzena <mlunzena@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-03-18 10:47:26 +0000

    use correct method, refs #3484
    git-svn-id: svn://develop.studip.de/studip/trunk@29857 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/routes/Contacts.php
+++ app/routes/Contacts.php
@@ -194,7 +194,7 @@ class Contacts extends \RESTAPI\RouteMap
         // prevent duplicates
         $exists = $group->members->findBy('user_id', $user_id)->first();
         if ($exists) {
-            $this->error(204);
+            $this->halt(204);
         }
 
         AddNewContact($user_id);

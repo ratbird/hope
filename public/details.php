883644809bddb48cbd4dbc91fc243a7e1da38021
ckater <ckater@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-03-21 16:53:28 +0000

    Bugfix Tabellen Part 1 re #3767
    git-svn-id: svn://develop.studip.de/studip/trunk@29873 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/details.php
+++ public/details.php
@@ -129,7 +129,7 @@ else
 <table width="100%" border="0" cellpadding="0" cellspacing="0">
     <tr>
         <td class="blank">
-        <table class="zebra" id="main_content" align="center" width="100%" border="0" cellpadding="2" cellspacing="0">
+        <table class="default" id="main_content" align="center" width="100%" border="0" cellpadding="2" cellspacing="0">
         <tr>
             <td width="1%">&nbsp; <img src="<?= $GLOBALS['ASSETS_URL'] ?>images/blank.gif" width="25" height="10" border="0">
             </td>

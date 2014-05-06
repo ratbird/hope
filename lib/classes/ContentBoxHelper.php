a46fb742a9827827c109d497ed3c0c6954a6301a
fl00 <fl00@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-05-06 07:58:55 +0000

    re #4375 Fix double encoding
    git-svn-id: svn://develop.studip.de/studip/trunk@30367 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/classes/ContentBoxHelper.php
+++ lib/classes/ContentBoxHelper.php
@@ -1,34 +1,58 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of ContentBoxHelper
+ * ContentBoxHelper.php
+ * 
+ * The ContentBoxHelper controls ids of contentboxes
  *
- * @author intelec
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * @author      Florian Bieringer <florian.bieringer@uni-passau.de>
+ * @copyright   2013 Stud.IP Core-Group
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GPL version 2
+ * @category    Stud.IP
+ * @since       3.0
  */
 class ContentBoxHelper {
 
+    /**
+     * Returns the class open if the id was clicked
+     * 
+     * @param String $id id of the content box
+     * @return String open if the contentbox is open otherwise an empty String 
+     */
     public static function classes($id) {
         return Request::get('contentbox_open') == $id ? 'open' : '' . '"';
     }
 
+    /**
+     * Produces an html link to open a contentbox if javascript is not active
+     * 
+     * @param String $id Id of the content box
+     * @param Array $params other needed parameters
+     * @return String Url to open the contentbox
+     */
     public static function switchhref($id, $params = array()) {
         if (Request::get('contentbox_open') != $id || $params) {
             $params['contentbox_open'] = $id;
         } else {
             $params['contentbox_close'] = $id;
         }
-        return URLHelper::getLink("#$id", $params);
+        return URLHelper::getURL("#$id", $params);
     }
 
+    /**
+     * Link to the contentbox (Required when some action should take place)
+     * 
+     * @param String $id Id of the content box
+     * @param Array $params other needed parameters
+     * @return String Url to the contentbox
+     */
     public static function href($id, $params = array()) {
         $params['contentbox_open'] = $id;
-        return URLHelper::getLink("#$id", $params);
+        return URLHelper::getURL("#$id", $params);
     }
 
 }

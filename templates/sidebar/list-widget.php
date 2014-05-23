43ddf020ddc18c293c5a041d852e3c34b1d6a791
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-05-23 18:21:36 +0000

    sidebar: refine icon handling for list items by changing from list style images to background images, re #3768
    git-svn-id: svn://develop.studip.de/studip/trunk@30702 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/sidebar/list-widget.php
+++ templates/sidebar/list-widget.php
@@ -1,6 +1,6 @@
 <ul class="<?= implode(' ', $css_classes) ?>">
 <? foreach ($elements as $element): ?>
-    <li<?= $element->icon ? ' style="list-style-image: url(' . $element->icon .');"' : "" ?><?= $element->active ? ' class="active"' : '' ?>>
+    <li<?= $element->icon ? ' style="background-image: url(' . $element->icon .');"' : "" ?><?= $element->active ? ' class="active"' : '' ?>>
         <?= $element->render() ?>
     </li>
 <? endforeach; ?>

32491d37d2952262ac4ba753f5b8e4f6959375b4
arne <arne@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-05-23 19:39:48 +0000

    StEP00272: add tour template, re #4409
    git-svn-id: svn://develop.studip.de/studip/trunk@30705 c5b85792-5220-0410-ad23-ed5b6b8ad999

---
+++ templates/tour/tour.php
@@ -0,0 +1,41 @@
+<? use Studip\Button, Studip\LinkButton ?>
+<div id="tour_controls" style="display: none">
+    <div id="tour_title"></div>
+    <div id="tour_buttons">
+        <table cellpadding="0" cellspacing="0">
+            <tr>
+                <td style="text-align: center">
+                    <div id="tour_editor" style="display: none">
+                    <?= LinkButton::create(_('Neuer Schritt'), '', array('id' => 'tour_new_step', 'data-dialog' => 'size=auto')) ?>
+                    <?= LinkButton::create(_('Schritt bearbeiten'), '', array('id' => 'tour_edit', 'data-dialog' => 'size=auto')) ?>
+                    <?= Button::create(_('Position wählen'), 'tour_select_css', array('id' => 'tour_select_css')) ?>
+                    <?= Button::create(_('Keine Position'), 'tour_no_css', array('id' => 'tour_no_css')) ?>
+                    <?= Button::create(_('Schritt löschen'), 'tour_delete_step', array('id' => 'tour_delete_step')) ?>
+                    <?= LinkButton::create(_('Seitenwechsel'), '', array('id' => 'tour_new_page', 'rel' => 'lightbox')) ?>
+                    </div>
+                </td>
+                <td width="120" style="text-align: center">
+                    <?= Button::create(_('Zurück'), 'tour_prev', array('id' => 'tour_prev', 'style' => 'display:none')) ?>
+                    <?= Button::create(_('Tour neu beginnen'), 'tour_reset', array('id' => 'tour_reset', 'style' => 'display:none')) ?>
+                </td>
+                <td width="120" style="text-align: center">
+                    <?= Button::create(_('Weiter'), 'tour_next', array('id' => 'tour_next')) ?>
+                    <?= Button::create(_('Tour fortsetzen'), 'tour_proceed', array('id' => 'tour_proceed', 'style' => 'display:none')) ?>
+                </td>
+                <td width="120" style="text-align: center">
+                    <?= Button::createCancel(_('Abbrechen'), 'tour_cancel', array('id' => 'tour_cancel', 'style' => 'display:none')) ?>
+                    <?= Button::createCancel(_('Beenden'), 'tour_end', array('id' => 'tour_end')) ?></td>
+            </tr>
+        </table>
+    </div>
+    <div id="tour_interactive_text" style="display:none"><?=_('Zum Fortsetzen bitte die Anweisungen aus dem Text befolgen.')?>
+    </div>
+</div>
+<div id="tour_tip" class="tour_tip" style="display: none">
+    <div id="tour_tip_title"></div>
+    <div id="tour_tip_content"></div>
+</div>
+<div id="tour_tip_interactive" class="tour_tip" style="display: none">
+    <div id="tour_tip_title"></div>
+    <div id="tour_tip_content"></div>
+</div>

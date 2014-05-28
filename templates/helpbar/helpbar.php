5205588c67f754d5d3b3f7d38092209d4b2613ef
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-05-28 14:12:00 +0000

    helpbar: remove sticky toggle and delay hiding of helpbar (at gui commission's suggestion), re #3768
    git-svn-id: svn://develop.studip.de/studip/trunk@30820 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/helpbar/helpbar.php
+++ templates/helpbar/helpbar.php
@@ -3,9 +3,7 @@
         <div class="helpbar-header">
             <?= Assets::img('icons/24/white/question-circle.png') ?>
         </div>
-        <input type="checkbox" id="helpbar-sticky" class="helpbar-sticky">
         <div class="helpbar-content">
-            <label for="helpbar-sticky" class="helpbar-sticky-toggle" title="<?= _('Hilfe nicht einklappen') ?>"></label>
             <h2 class="helpbar-title"><?= _('Tipps & Hilfe') ?></h2>
             <ul class="helpbar-widgets">
             <? foreach ($widgets as $index => $widget): ?>

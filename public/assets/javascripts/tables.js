6f6dbcc5bf4ed008927f5dd35ca3321c8e701147
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-06-11 11:57:21 +0000

    re #3768
    add slidedown
    
    git-svn-id: svn://develop.studip.de/studip/trunk@31003 c5b85792-5220-0410-ad23-ed5b6b8ad999

---
+++ public/assets/javascripts/tables.js
@@ -0,0 +1,20 @@
+/*jslint browser: true, white: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, newcap: true, immed: true, indent: 4, onevar: false */
+/*global window, $, jQuery, _ */
+
+jQuery(function () {
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if (mutation.attributeName === "class") {
+                if (jQuery(mutation.target).attr("class").indexOf("open") !== -1) {
+                    jQuery(mutation.target).next().find("td").slideDown().find(".detailscontainer").hide().slideDown();
+                } else {
+                    jQuery(mutation.target).next().show().find("td").slideUp().find(".detailscontainer").slideUp();;
+                }
+            }
+
+        });
+    });
+    jQuery("table.withdetails > tbody > tr:not(.details)").each(function (index, element) {
+        observer.observe(element, { attributes: true });
+    });
+});

252837f44950491f9cfcf9645737bba61a48cc69
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-06-12 22:39:26 +0000

    helpbar: refine styles according to mockup, no effects for now/plain hover, re #3768
    git-svn-id: svn://develop.studip.de/studip/trunk@31050 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/helpbar/helpbar.php
+++ templates/helpbar/helpbar.php
@@ -1,20 +1,17 @@
 <div class="helpbar-container">
     <div class="helpbar">
-        <div class="helpbar-header">
-            <?= Assets::img('icons/24/white/question-circle.png') ?>
-        </div>
-        <div class="helpbar-content">
-            <h2 class="helpbar-title"><?= _('Tipps & Hilfe') ?></h2>
-            <ul class="helpbar-widgets">
-            <? foreach ($widgets as $index => $widget): ?>
-                <li>
-                <? if ($widget->icon): ?>
-                    <?= Assets::img($widget->icon, array('class' => 'helpbar-widget-icon')) ?>
-                <? endif; ?>
-                    <?= $widget->render(array('base_class' => 'helpbar')) ?>
-                </li>
-            <? endforeach; ?>
-            </ul>
-        </div>
+        <h2 class="helpbar-title">
+            <?= _('Tipps & Hilfe') ?>
+        </h2>
+        <ul class="helpbar-widgets">
+        <? foreach ($widgets as $index => $widget): ?>
+            <li>
+            <? if (false && $widget->icon): ?>
+                <?= Assets::img($widget->icon, array('class' => 'helpbar-widget-icon')) ?>
+            <? endif; ?>
+                <?= $widget->render(array('base_class' => 'helpbar')) ?>
+            </li>
+        <? endforeach; ?>
+        </ul>
     </div>
 </div>

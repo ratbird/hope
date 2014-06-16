2b64a59899aa5024636563069be0e4d872a8c500
davka <davka@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-06-16 09:57:55 +0000

    StEP: new semester selector, re #4484
    git-svn-id: svn://develop.studip.de/studip/trunk@31080 c5b85792-5220-0410-ad23-ed5b6b8ad999

---
+++ app/views/my_courses/_semester_filter.php
@@ -0,0 +1,28 @@
+<form action="<?=$controller->url_for('my_courses/set_semester')?>">
+<select name="sem_select">
+    <option <?= ($sem == 'future' ? 'selected' : '')?> value="current"><?= _('Aktuelles Semester') ?></option>
+    <option <?= ($sem == 'future' ? 'selected' : '')?> value="future"><?= _('Aktuelles und zukünftiges Semester') ?></option>
+    <option <?= ($sem == 'last' ? 'selected' : '')?> value="last"><?= _('Aktuelles und letztes Semester') ?></option>
+    <? if (Config::get()->MY_COURSES_ENABLE_ALL_SEMESTERS) : ?>
+        <option <?= ($sem == 'all' ? 'selected' : '')?> value="all"><?= _('Alle Semester') ?></option>
+    <? endif ?>
+
+    <? if (!empty($semesters)) : ?>
+        <optgroup label="<?=_('Semester auswählen')?>">
+        <? foreach ($semesters as $semester) :?>
+            <option value="<?=$semester->id?>" <?= ($sem == $semester->id ? 'selected' : '')?>>
+                <?= htmlReady($semester->name)?>
+            </option>
+        <? endforeach ?>
+        </optgroup>
+    <? endif ?>
+</select>
+    <noscript>
+        <?= \Studip\Button::createAccept(_('Auswählen'))?>
+    </noscript>
+    <script>
+        jQuery('select[name="sem_select"]').live('change', function () {
+            jQuery(this).closest('form').submit();
+        });
+    </script>
+</form>

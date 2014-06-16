ebf7f5ee9e4e453289483859f9f99b77c1fa46ef
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-06-16 14:16:22 +0000

    sidebar: remove warning, use blue icons and adjust general style, re #3768
    git-svn-id: svn://develop.studip.de/studip/trunk@31093 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/calendar/schedule/index.php
+++ app/views/calendar/schedule/index.php
@@ -12,29 +12,30 @@ $sidebar->addWidget($semester_widget, 'semester');
 
 $actions = new ActionsWidget();
 if (!$inst_mode) {
-    $actions->addLink(_("Neuer Eintrag"), $controller->url_for('calendar/schedule/entry'), 'icons/16/black/add/date.png');
+    $actions->addLink(_("Neuer Eintrag"), $controller->url_for('calendar/schedule/entry'), 'icons/16/blue/add/date.png');
 }
-$link = new LinkElement();
-$link->label = _("Druckansicht");
-$link->url = $controller->url_for('calendar/schedule/index/'. implode(',', $days)
-           .  '?printview=true&semester_id=' . $current_semester['semester_id']);
-$link->setTarget("_blank");
-$link->icon = Assets::image_path("icons/16/black/print.png");
-$actions->addElement($link, 'print');
-$actions->addLink(_("Darstellung ändern"), $controller->url_for('calendar/schedule/index?show_settings=true'), 'icons/16/black/admin.png');
+
+$actions->addLink(_("Darstellung ändern"), $controller->url_for('calendar/schedule/index?show_settings=true'), 'icons/16/blue/admin.png');
 if (!$show_hidden) {
-    $actions->addLink(_("Ausgeblendete Veranstaltungen anzeigen"), $controller->url_for('calendar/schedule/?show_hidden=1'), 'icons/16/black/visibility-visible.png');
+    $actions->addLink(_("Ausgeblendete Veranstaltungen anzeigen"), $controller->url_for('calendar/schedule/?show_hidden=1'), 'icons/16/blue/visibility-visible.png');
 } else {
-    $actions->addLink(_("Ausgeblendete Veranstaltungen verbergen"), $controller->url_for('calendar/schedule/?show_hidden=0'), 'icons/16/black/visibility-visible.png');
+    $actions->addLink(_("Ausgeblendete Veranstaltungen verbergen"), $controller->url_for('calendar/schedule/?show_hidden=0'), 'icons/16/blue/visibility-visible.png');
 }
 $sidebar->addWidget($actions);
 
-$views = new LinksWidget();
-$views->setTitle(_("Darstellungsgröße"));
-$views->addLink(_("klein"), URLHelper::getURL('', array('zoom' => 0)), 'icons/16/'. ($zoom == 0 ? 'red' : 'black') . '/schedule.png');
-$views->addLink(_("mittel"), URLHelper::getURL('', array('zoom' => 1)), 'icons/16/'. ($zoom == 1 ? 'red' : 'black') . '/schedule.png');
-$views->addLink(_("groß"), URLHelper::getURL('', array('zoom' => 2)), 'icons/16/'. ($zoom == 2 ? 'red' : 'black') . '/schedule.png');
-$sidebar->addWidget($views, 'view');
+$widget = new ExportWidget();
+$widget->addLink(_('Druckansicht'),
+                 $controller->url_for('calendar/schedule/index/'. implode(',', $days) .  '?printview=true&semester_id=' . $current_semester['semester_id']),
+                 'icons/16/blue/print.png',
+                 array('target' => '_blank'));
+$sidebar->addWidget($widget);
+
+$options = new OptionsWidget();
+$options->setTitle(_("Darstellungsgröße"));
+$options->addRadioButton(_("klein"), URLHelper::getURL('', array('zoom' => 0)), $zoom == 0);
+$options->addRadioButton(_("mittel"), URLHelper::getURL('', array('zoom' => 1)), $zoom == 1);
+$options->addRadioButton(_("groß"), URLHelper::getURL('', array('zoom' => 2)), $zoom == 2);
+$sidebar->addWidget($options, 'options');
 
 ?>
 <div style="text-align: center; font-weight: bold; font-size: 1.2em">

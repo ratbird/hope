d4f64e763d641f8b6f48ab592eea4e908a87c9ef
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-06-18 16:05:08 +0000

    admin area: prevent search form from messing with the layout and provide links to found courses, re #3768
    git-svn-id: svn://develop.studip.de/studip/trunk@31192 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/include/admin_search_form.inc.php
+++ lib/include/admin_search_form.inc.php
@@ -171,12 +171,11 @@ if ($perm->have_perm('tutor')) {    // Navigationsleiste ab status "Tutor"
                         </td>
                     </tr>
                     <tr>
-                        <td class="table_row_even">
-                            <?=_("Semester:")?><br>
-                            <?=SemesterData::GetSemesterSelector(array('name'=>'srch_sem'), $_SESSION['links_admin_data']['srch_sem'])?>
-                        </td>
-
-                        <td class="table_row_even">
+                        <td class="table_row_even" colspan="5">
+                            <label style="display: inline-block;">
+                                <?=_("Semester:")?><br>
+                                <?=SemesterData::GetSemesterSelector(array('name'=>'srch_sem'), $_SESSION['links_admin_data']['srch_sem'])?>
+                            </label>
                         <?
                         if ($perm->have_perm("root")) {
                             $dbquery = "SELECT Institut_id, Name FROM Institute WHERE Institut_id!=fakultaets_id ORDER BY Name";
@@ -187,37 +186,36 @@ if ($perm->have_perm('tutor')) {    // Navigationsleiste ab status "Tutor"
                             $dbparams = array($user->id);
                         }
                         ?>
-                        <?=_("Einrichtung:")?><br>
-                        <select name="srch_inst">
-                            <option value="0"><?=_("alle")?></option>
-                            <?
-                            $dbstatement = DBManager::get()->prepare($dbquery);
-                            $dbstatement->execute($dbparams);
+                        <label style="display: inline-block;">
+                            <?=_("Einrichtung:")?><br>
+                            <select name="srch_inst">
+                                <option value="0"><?=_("alle")?></option>
+                                <?
+                                $dbstatement = DBManager::get()->prepare($dbquery);
+                                $dbstatement->execute($dbparams);
 
-                            while ($dbrow = $dbstatement->fetch(PDO::FETCH_ASSOC)) {
-                                $my_inst[]=$dbrow['Institut_id'];
-                                if ($_SESSION['links_admin_data']['srch_inst'] == $dbrow['Institut_id'])
-                                    echo"<option selected value=\"".$dbrow['Institut_id']."\">".htmlReady(substr($dbrow['Name'], 0, 30))."</option>";
-                                else
-                                    echo"<option value=\"".$dbrow['Institut_id']."\">".htmlReady(substr($dbrow['Name'], 0, 30))."</option>";
-                                if ($dbrow['is_fak']) {
-                                    $db2query = "SELECT Institut_id, Name FROM Institute WHERE fakultaets_id='" .$dbrow['Institut_id'] . "' AND institut_id!='" .$dbrow['Institut_id'] . "' ORDER BY Name";
-                                    foreach (DBManager::get()->query($db2query) as $dbrow2) {
-                                        if ($_SESSION['links_admin_data']['srch_inst'] == $dbrow2['Institut_id'])
-                                            echo"<option selected value=\"".$dbrow2['Institut_id']."\">&nbsp;&nbsp;&nbsp;".htmlReady(substr($dbrow2['Name'], 0, 30))."</option>";
-                                        else
-                                            echo"<option value=\"".$dbrow2['Institut_id']."\">&nbsp;&nbsp;&nbsp;".htmlReady(substr($dbrow2['Name'], 0, 30))."</option>";
-                                        $my_inst[]=$dbrow2['Institut_id'];
+                                while ($dbrow = $dbstatement->fetch(PDO::FETCH_ASSOC)) {
+                                    $my_inst[]=$dbrow['Institut_id'];
+                                    if ($_SESSION['links_admin_data']['srch_inst'] == $dbrow['Institut_id'])
+                                        echo"<option selected value=\"".$dbrow['Institut_id']."\">".htmlReady(substr($dbrow['Name'], 0, 30))."</option>";
+                                    else
+                                        echo"<option value=\"".$dbrow['Institut_id']."\">".htmlReady(substr($dbrow['Name'], 0, 30))."</option>";
+                                    if ($dbrow['is_fak']) {
+                                        $db2query = "SELECT Institut_id, Name FROM Institute WHERE fakultaets_id='" .$dbrow['Institut_id'] . "' AND institut_id!='" .$dbrow['Institut_id'] . "' ORDER BY Name";
+                                        foreach (DBManager::get()->query($db2query) as $dbrow2) {
+                                            if ($_SESSION['links_admin_data']['srch_inst'] == $dbrow2['Institut_id'])
+                                                echo"<option selected value=\"".$dbrow2['Institut_id']."\">&nbsp;&nbsp;&nbsp;".htmlReady(substr($dbrow2['Name'], 0, 30))."</option>";
+                                            else
+                                                echo"<option value=\"".$dbrow2['Institut_id']."\">&nbsp;&nbsp;&nbsp;".htmlReady(substr($dbrow2['Name'], 0, 30))."</option>";
+                                            $my_inst[]=$dbrow2['Institut_id'];
+                                        }
                                     }
                                 }
-                            }
-                            ?>
-                        </select>
-                        </td>
-                        <td class="table_row_even">
-                        <?
-                        if (($perm->have_perm("admin")) && (!$perm->have_perm("root"))) {
-                            ?>
+                                ?>
+                            </select>
+                        </label>
+                    <? if (($perm->have_perm("admin")) && (!$perm->have_perm("root"))): ?>
+                        <label style="display: inline-block;">
                             <?=_("DozentIn:")?><br>
                             <select name="srch_doz">
                             <option value="0"><?=_("alle")?></option>
@@ -236,12 +234,14 @@ if ($perm->have_perm('tutor')) {    // Navigationsleiste ab status "Tutor"
                             }
                             ?>
                             </select>
-                            <?
-                        }
+                        </label>
+                    <? endif; ?>
 
+                    <?
                         if ($perm->have_perm("root")) {
                             $dbquery = "SELECT Institut_id,Name FROM Institute WHERE Institut_id=fakultaets_id ORDER BY Name";
                             ?>
+                        <label style="display: inline-block;">
                             <?=_("Fakultät:")?><br>
                             <select name="srch_fak">
                                 <option value="0"><?=_("alle")?></option>
@@ -257,32 +257,32 @@ if ($perm->have_perm('tutor')) {    // Navigationsleiste ab status "Tutor"
                                 }
                                 ?>
                             </select>
+                        </label>
                             <?
                         }
                         ?>&nbsp;
-                        </td>
-                        <td class="table_row_even">
-                            <?=_("freie Suche:")?><br>
-                            <input type="text" name="srch_exp" maxlength=255 size=20 value="<? echo htmlReady($_SESSION['links_admin_data']['srch_exp']) ?>">
-                            <input type="hidden" name="srch_send" value="TRUE">
-                        </td>
-                        <td class="table_row_even" valign="bottom" width="20%" nowrap="nowrap">
-                            <?= Button::create(_("Anzeigen"), 'anzeigen'); ?>
-                            <?
-                            if ($_SESSION['links_admin_data']['srch_on']){
-                                echo Button::create(_("Zurücksetzen"), 'links_admin_reset_search');
-                            }
-                            ?>
-                            <input type="hidden" name="view" value="<? echo htmlReady($_SESSION['links_admin_data']['view'])?>">
-                        </td>
-                    </tr>
+                    </td>
+                </tr>
                 <tr>
                     <td class="table_row_even" colspan="5">
-                        <br>
+                        <label style="display: inline-block;">
+                            <?=_("freie Suche:")?><br>
+                            <input type="text" name="srch_exp" maxlength=255 style="width: 250px; vertical-align: middle;" value="<?= htmlReady($_SESSION['links_admin_data']['srch_exp']) ?>">
+                            <input type="hidden" name="srch_send" value="TRUE">
+                        </label>
+
+                        <?= Button::create(_("Anzeigen"), 'anzeigen'); ?>
+                    <? if ($_SESSION['links_admin_data']['srch_on']): ?>
+                        <?= Button::create(_("Zurücksetzen"), 'links_admin_reset_search') ?>
+                    <? endif; ?>
+                        <input type="hidden" name="view" value="<?= htmlReady($_SESSION['links_admin_data']['view'])?>">
+                    </td>
+                </tr>
+                <tr>
+                    <td class="table_row_even" colspan="5">
                         <label>
                             <input type="checkbox" name="show_rooms_check" value="on" <? if ($show_rooms_check == 'on') echo 'checked'; ?> >&nbsp; <?=_("Raumdaten einblenden")?>
                         </label>
-                        <br>
                     </td>
                 </tr>
                     <?
@@ -304,16 +304,6 @@ if ($perm->have_perm('tutor')) {    // Navigationsleiste ab status "Tutor"
                         <?
                     }
                     ?>
-                    <tr>
-                        <td class="table_row_even" colspan=5>&nbsp;
-
-                        </td>
-                    </tr>
-                    <tr>
-                        <td class="blank" colspan=5>&nbsp;
-
-                        </td>
-                    </tr>
                     <? if (! empty($message)) : ?>
                     <tr>
                         <td class="blank" colspan=5>
@@ -499,8 +489,10 @@ if ($perm->have_perm('tutor')) {    // Navigationsleiste ab status "Tutor"
             echo "<td align=\"center\" class=\"".$cssSw->getClass()."\">".htmlReady($result['startsem']);
             if ($result['startsem'] != $result['endsem']) echo '<br>( - '.htmlReady($result['endsem']).')';
             echo "</td>";
-            echo "<td class=\"".$cssSw->getClass()."\">".htmlReady($result["VeranstaltungsNummer"])."</td>";
-            echo "<td class=\"".$cssSw->getClass()."\">".htmlReady(substr($result["Name"],0,100));
+            echo "<td class=\"".$cssSw->getClass()."\">" . htmlReady($result["VeranstaltungsNummer"]) . "</td>";
+            echo "<td class=\"".$cssSw->getClass()."\">";
+            echo '<a href="' . URLHelper::getLink('dispatch.php/course/overview?cid=' . $seminar_id) . '">';
+            echo htmlReady(substr($result["Name"],0,100));
             if (strlen($result["Name"]) > 100) {
                 echo "(...)";
             }
@@ -508,6 +500,7 @@ if ($perm->have_perm('tutor')) {    // Navigationsleiste ab status "Tutor"
                 echo "&nbsp;". _("(versteckt)");
             }
 
+            echo "</a>";
             echo "</td>";
 
             // if "show room-data" is enabled, show cell

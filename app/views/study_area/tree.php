121a5195025bc9cc2a2b461ab851305c6db928cd
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-06-18 15:43:41 +0000

    course details: provide link to study areas, re #4520
    git-svn-id: svn://develop.studip.de/studip/trunk@31191 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/study_area/tree.php
+++ app/views/study_area/tree.php
@@ -1,13 +1,22 @@
 <li>
-<? if($node->required_children): ?>
+<? if ($node->id !== 'root' && $node->required_children): ?>
     <input id='<?= htmlReady($node->id) ?>' type='checkbox' <?= $open && !in_array($layer, $dont_open) ? 'checked' : ''?>>
 <? endif; ?>
-    <label for='<?= htmlReady($node->id) ?>'><?= htmlReady($node->name) ?></label>
-    <? if($node->required_children): ?>
+    <label for='<?= htmlReady($node->id) ?>'></label>
+
+<? if ($node->id !== 'root'): ?>
+    <a href="<?= URLHelper::getLink('show_bereich.php?level=sbb&id=' . $node->id) ?>">
+        <?= htmlReady($node->name) ?>
+    </a>
+<? else: ?>
+    <?= htmlReady($node->name) ?>
+<? endif; ?>
+
+<? if ($node->required_children): ?>
     <ul>
-        <? foreach($node->required_children as $child): ?>
+    <? foreach ($node->required_children as $child): ?>
         <?= $this->render_partial('study_area/tree.php', array('node' => $child, 'open' => $open, 'layer' => $layer + 1)) ?>
-        <? endforeach; ?>
+    <? endforeach; ?>
     </ul>
-    <? endif; ?>
+<? endif; ?>
 </li>

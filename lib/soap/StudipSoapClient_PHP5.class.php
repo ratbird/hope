5fd6e2d73d02e6160b66e7c87b33e113f7cae643
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-06-23 14:10:02 +0000

    remove obsolete wsdl correction re #4538
    git-svn-id: svn://develop.studip.de/studip/trunk@31259 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/soap/StudipSoapClient_PHP5.class.php
+++ lib/soap/StudipSoapClient_PHP5.class.php
@@ -18,41 +18,43 @@ class StudipSoapClient
     {
         try {
             $this->soap_client = new SoapClient($path, array('trace' => 0));
-        } catch (SoapFault $fault){
+        } catch (SoapFault $fault) {
             $this->error = "<b>Soap Constructor Error</b><br>" . $fault->faultcode . ": ".$fault->faultstring."<br><br>";
         }
     }
 
     function call($method, $params)
     {
-        $this->faultstring = "";
-        $this->soap_client->_cookies = array();
-        try{
-            $result = $this->soap_client->__soapCall($method, $params);
-        } catch  (SoapFault $fault){
-            $this->faultstring = $fault->faultstring;
-            if (!in_array(strtolower($this->faultstring), array("session not valid","session invalid", "session idled"))) {
-                $this->error .= "<hr><font size=\"-1\"><b>" . sprintf(_("SOAP-Fehler, Funktion \"%s\":"), $method) . "</b> " . $fault->faultstring . " (" .  $fault->faultcode . ")<br>".print_r($params,1).'</font><hr>';
-                error_log($this->error);
-            }
-            $this->soap_client->fault = true;
-            return false;
-        }
-        if (is_object($result)) $result = (array)$result;
-        if (is_array($result)){
-            foreach($result as $index => $one){
-                if (is_object($one)) $result[$index] = (array)$one;
-                if (is_array($result[$index])){
-                    //hmmm
-                } else {
-                    $result[$index] = studip_utf8decode($result[$index]);
+        $result = false;
+        if ($this->soap_client instanceOf SoapClient) {
+            $this->faultstring = "";
+            $this->soap_client->_cookies = array();
+            try {
+                $result = $this->soap_client->__soapCall($method, $params);
+            } catch  (SoapFault $fault) {
+                $this->faultstring = $fault->faultstring;
+                if (!in_array(strtolower($this->faultstring), array("session not valid","session invalid", "session idled"))) {
+                    $this->error .= "<hr><font size=\"-1\"><b>" . sprintf(_("SOAP-Fehler, Funktion \"%s\":"), $method) . "</b> " . $fault->faultstring . " (" .  $fault->faultcode . ")<br>".print_r($params,1).'</font><hr>';
+                    error_log($this->error);
                 }
+                $this->soap_client->fault = true;
+                return false;
             }
-        } else {
-            $result = studip_utf8decode($result);
+            if (is_object($result)) $result = (array)$result;
+            if (is_array($result)){
+                foreach($result as $index => $one){
+                    if (is_object($one)) $result[$index] = (array)$one;
+                    if (is_array($result[$index])){
+                        //hmmm
+                    } else {
+                        $result[$index] = studip_utf8decode($result[$index]);
+                    }
+                }
+            } else {
+                $result = studip_utf8decode($result);
+            }
+            $this->soap_client->fault = false;
         }
-
-        $this->soap_client->fault = false;
         return $result;
     }
 

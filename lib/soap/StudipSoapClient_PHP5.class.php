59e49ca49b3727a1bc1e3af80723b126623a6818
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-06-23 14:03:55 +0000

    remove obsolete wsdl correction re #4538
    git-svn-id: svn://develop.studip.de/studip/trunk@31258 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/soap/StudipSoapClient_PHP5.class.php
+++ lib/soap/StudipSoapClient_PHP5.class.php
@@ -16,9 +16,8 @@ class StudipSoapClient
 
     function StudipSoapClient($path)
     {
-        $this->getCorrectedWsdl($path);
-        try{
-            $this->soap_client = new SoapClient($GLOBALS['TMP_PATH'] . '/' . md5($path) . '.wsdl', array('trace' => 0));
+        try {
+            $this->soap_client = new SoapClient($path, array('trace' => 0));
         } catch (SoapFault $fault){
             $this->error = "<b>Soap Constructor Error</b><br>" . $fault->faultcode . ": ".$fault->faultstring."<br><br>";
         }
@@ -65,57 +64,6 @@ class StudipSoapClient
              return $error;
         else
             return false;
-    }
-
-    function getCorrectedWsdl($path){
-        $correct_elements = array (
-        0 => 'usr_id',
-        1 => 'institution',
-        2 => 'street',
-        3 => 'city',
-        4 => 'zipcode',
-        5 => 'country',
-        6 => 'phone_office',
-        7 => 'last_login',
-        8 => 'last_update',
-        9 => 'create_date',
-        10 => 'hobby',
-        11 => 'department',
-        12 => 'phone_home',
-        13 => 'phone_mobile',
-        14 => 'fax',
-        15 => 'time_limit_owner',
-        16 => 'time_limit_unlimited',
-        17 => 'time_limit_from',
-        18 => 'time_limit_until',
-        19 => 'time_limit_message',
-        20 => 'referral_comment',
-        21 => 'matriculation',
-        22 => 'active',
-        23 => 'accepted_agreement',
-        24 => 'approve_date',
-        25 => 'user_skin',
-        26 => 'user_style',
-        27 => 'user_language',
-        28 => 'import_id'
-        );
-
-        //grmpf. !$this->cms_type == aufruf von admin_elearning_interface.php
-        if(!$this->cms_type) @unlink($GLOBALS['TMP_PATH'] . '/' . md5($path) . '.wsdl');
-
-        if($path && !file_exists($GLOBALS['TMP_PATH'] . '/' . md5($path) . '.wsdl')){
-            $wsdl = simplexml_load_file($path);
-            $xsd = $wsdl->types->children('http://www.w3.org/2001/XMLSchema');
-            foreach($xsd->schema->complexType as $ct){
-                if ($ct->attributes()->name == 'ilUserData') break;
-            }
-            foreach($ct->all->element as $element){
-                if( in_array($element->attributes()->name, $correct_elements) && !isset($element->attributes()->minOccurs) ){
-                    $element->addAttribute('minOccurs','0');
-                }
-            }
-            $wsdl->asXml($GLOBALS['TMP_PATH'] . '/' . md5($path) . '.wsdl');
-        }
     }
 }
 ?>

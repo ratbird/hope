f62f69601d12aaa0e6d9d9bae0a4aa586646b63b
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-06-26 11:50:39 +0000

    re #3768
    prevent fatal errors in IE10 and below
    
    git-svn-id: svn://develop.studip.de/studip/trunk@31305 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/assets/javascripts/tables.js
+++ public/assets/javascripts/tables.js
@@ -2,19 +2,21 @@
 /*global window, $, jQuery, _ */
 
 jQuery(function () {
-    var observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-            if (mutation.attributeName === "class") {
-                if (jQuery(mutation.target).attr("class").indexOf("open") !== -1) {
-                    jQuery(mutation.target).next().find("td").slideDown().find(".detailscontainer").hide().slideDown();
-                } else {
-                    jQuery(mutation.target).next().show().find("td").slideUp().find(".detailscontainer").slideUp();;
+    if (typeof MutationObserver !== "undefined") {
+        var observer = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutation) {
+                if (mutation.attributeName === "class") {
+                    if (jQuery(mutation.target).attr("class").indexOf("open") !== -1) {
+                        jQuery(mutation.target).next().find("td").slideDown().find(".detailscontainer").hide().slideDown();
+                    } else {
+                        jQuery(mutation.target).next().show().find("td").slideUp().find(".detailscontainer").slideUp();;
+                    }
                 }
-            }
 
+            });
         });
-    });
-    jQuery("table.withdetails > tbody > tr:not(.details)").each(function (index, element) {
-        observer.observe(element, { attributes: true });
-    });
+        jQuery("table.withdetails > tbody > tr:not(.details)").each(function (index, element) {
+            observer.observe(element, { attributes: true });
+        });
+    }
 });

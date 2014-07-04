eebd320e707bc0d23cd5a8410992ea663090ae7f
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-07-04 12:11:59 +0000

    tooltips: some more refinements for the forum, re #4587
    git-svn-id: svn://develop.studip.de/studip/trunk@31369 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/plugins_packages/core/Forum/views/index/_like.php
+++ public/plugins_packages/core/Forum/views/index/_like.php
@@ -11,24 +11,24 @@ shuffle($likes);
     $text = '';
     if (array_search($GLOBALS['user']->id, $likes) !== false) {
         if (sizeof($likes) > 1) {
-            $text = '<a class="tooltip">' . sprintf(_('Dir und %s weiteren gefällt das.'), (sizeof($likes) - 1));
+            $text = '<span class="tooltip">' . sprintf(_('Dir und %s weiteren gefällt das.'), (sizeof($likes) - 1));
             $text .= '<span class="tooltip-content">';
             foreach ($likes as $user_id) {
                 if ($user_id != $GLOBALS['user']->id) {
                     $text .= get_fullname($user_id) .'<br>';
                 }
             }
-            $text .= '</span></a>';
+            $text .= '</span></span>';
         } else {
             $text = _('Dir gefällt das.');
         }
     } else {
-        $text = '<a class="tooltip">' . sprintf(_('%s gefällt das.'), sizeof($likes));
+        $text = '<span class="tooltip">' . sprintf(_('%s gefällt das.'), sizeof($likes));
         $text .= '<span class="tooltip-content">';
         foreach ($likes as $user_id) {
             $text .= get_fullname($user_id) .'<br>';
         }
-        $text .= '</span></a>';
+        $text .= '</span></span>';
     }
     
     $text .= ' <br>';

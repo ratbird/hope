9888f9f072c7774fb493bc881b452f558f4e6789
davka <davka@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-07-17 08:05:34 +0000

    StEP00249: fix of various size of dialogs, re #4484
    git-svn-id: svn://develop.studip.de/studip/trunk@31464 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/my_courses/_course.php
+++ app/views/my_courses/_course.php
@@ -38,7 +38,7 @@
         </td>
         <td>
             <? if (!$sem_class['studygroup_mode']) : ?>
-                <a data-dialog="size=auto" href="<?= $controller->url_for(sprintf('course/details/index/%s', $course['seminar_id'])) ?>">
+                <a data-dialog="size=50%" href="<?= $controller->url_for(sprintf('course/details/index/%s', $course['seminar_id'])) ?>">
                     <? $params = tooltip2(_("Veranstaltungsdetails anzeigen")); ?>
                     <? $params['style'] = 'cursor: pointer'; ?>
                     <?= Assets::img('icons/20/grey/info-circle.png', $params) ?>

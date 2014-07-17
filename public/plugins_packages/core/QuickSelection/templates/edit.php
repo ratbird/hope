b88867b62ae781e0ac978a2bb3bcab3ee4b6a01c
aklassen <aklassen@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-07-17 13:16:25 +0000

    refine dialog of quickselection widget according to gui-feedback fixes #4626
    git-svn-id: svn://develop.studip.de/studip/trunk@31469 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/plugins_packages/core/QuickSelection/templates/edit.php
+++ public/plugins_packages/core/QuickSelection/templates/edit.php
@@ -1,16 +1,15 @@
 <div id="quickSelectionEdit">
-    <form id="configure_quickselection" action="#" data-url="<?=PluginEngine::getURL($plugin, array(), 'save') ?>" method="POST">
-        <div style="float:left; width:65%;" id="link_choices">
+    <form id="configure_quickselection" action="#" data-url="<?=PluginEngine::getURL($plugin, array(), 'save') ?>" method="POST" class="studip_form">
+        <fieldset>
+            <legend><?= _("Inhalte des Schnellzugriff-Widget:") ?></legend>
+            <fieldset>
             <? foreach ($links as $key=>$nav) : ?>
                 <label>
                     <input type="checkbox" name="add_removes[]" value="<?= htmlReady($key) ?>" <?= empty($config) || in_array($key, $config) ? 'checked' : ''?>>
                     <?= htmlReady($nav->getTitle()) ?>
                 </label>
-                <br>
             <? endforeach ?>
-        </div>
-        <div id="link_description">
-            <?= _("Wählen Sie links einen Link aus, um ihn im Schnellzugriff-Widget anzuzeigen.") ?>
-        </div>
+            </fieldset>
+        </fieldset>
     </form>
 </div>

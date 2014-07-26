ec7b58a92017394c28761ac50eb8d9e70f7a3b42
hackl <hackl@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-07-26 21:42:37 +0000

    re #4525 adjust copyright notice
    git-svn-id: svn://develop.studip.de/studip/trunk@31608 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/plugins_packages/core/WelcomeWidget/WelcomeWidget.php
+++ public/plugins_packages/core/WelcomeWidget/WelcomeWidget.php
@@ -2,11 +2,15 @@
 /*
  * WelcomeWidget.php - widget plugin for start page
  *
- * Copyright (C) 2014 - Nadine Werner <nadwerner@uos.de>
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; either version 2 of
  * the License, or (at your option) any later version.
+ *
+ * @author      Thomas Hackl <thomas.hackl@uni-passau.de>
+ * @copyright   2015 Stud.IP Core Group
+ * @license     http://www.gnu.org/licenses/gpl-2.0.html GPL version 2
+ * @category    Stud.IP
  */
 
 class WelcomeWidget extends StudIPPlugin implements PortalPlugin

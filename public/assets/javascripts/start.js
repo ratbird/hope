e79071173e00ecf0433725ea6483257b1d8ba020
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-08-11 11:44:57 +0000

    start page: fix sorting and ordering (and satisfy jslint), fixes #4826
    git-svn-id: svn://develop.studip.de/studip/trunk@31809 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/assets/javascripts/start.js
+++ public/assets/javascripts/start.js
@@ -1,85 +1,80 @@
-STUDIP.startpage = {
-    /*
-     THIS CODE ADDS DYNAMIC COLUMNING. WILL BE USED IN 3.2
+/*jslint browser: true, sloppy: true, unparam: true */
+(function ($, STUDIP) {
+    STUDIP.startpage = {
+        /*
+         THIS CODE ADDS DYNAMIC COLUMNING. WILL BE USED IN 3.2
 
-     init: function() {
-     $(".start-widgetcontainer ul").sortable({
-     connectWith: "ul",
-     start: function(event, ui) {
-     $(this).parent().find('ul').addClass('ui-sortable').addClass('move');
-     },
-     stop: function(event, ui) {
-     $.get(STUDIP.ABSOLUTE_URI_STUDIP + 'dispatch.php/start/storeNewOrder',
-     {
-     widget: $(ui.item).attr('id'),
-     position: $(ui.item).index(),
-     column: $(ui.item).parent().index()
-     }
-     );
-     $(this).parent().find('ul').removeClass('move empty');
-     $(this).parent().find('ul:empty').remove();
-     $(this).parent().append($('<ul>').addClass('empty'));
-     STUDIP.startpage.init();
-     }
+         init: function() {
+         $(".start-widgetcontainer ul").sortable({
+         connectWith: "ul",
+         start: function(event, ui) {
+         $(this).parent().find('ul').addClass('ui-sortable').addClass('move');
+         },
+         stop: function(event, ui) {
+         $.get(STUDIP.ABSOLUTE_URI_STUDIP + 'dispatch.php/start/storeNewOrder',
+         {
+         widget: $(ui.item).attr('id'),
+         position: $(ui.item).index(),
+         column: $(ui.item).parent().index()
+         }
+         );
+         $(this).parent().find('ul').removeClass('move empty');
+         $(this).parent().find('ul:empty').remove();
+         $(this).parent().append($('<ul>').addClass('empty'));
+         STUDIP.startpage.init();
+         }
 
-     });
-     }*/
+         });
+         }*/
 
 
-    init: function() {
-        $(".start-widgetcontainer ul").sortable({
-            connectWith: "ul.portal-widget-list",
-            start: function(event, ui) {
-                $(this).parent().find('ul').addClass('ui-sortable').addClass('move');
-            },
-            stop: function(event, ui) {
-
-                $.get(STUDIP.ABSOLUTE_URI_STUDIP + 'dispatch.php/start/storeNewOrder',
-                        {
-                            widget: $(ui.item).attr('id'),
-                            position: $(ui.item).index(),
-                            column: $(ui.item).parent().index()
-                        }
-                );
-                $(this).parent().find('ul').removeClass('move');
-            }
-
-        });
-    },
-
-    init_edit: function(perm) {
-        $(".edit-widgetcontainer ul").sortable({
-            connectWith: ".edit-widgetcontainer ul.portal-widget-list",
-            start: function(event, ui) {
-                $(this).parent().parent().find('ul').addClass('ui-sortable').addClass('move');
-            },
-            stop: function(event, ui) {
-                // store the whole widget constellation
-                widgets = {
-                    left: {},
-                    right: {}
+        init: function () {
+            $('.start-widgetcontainer .portal-widget-list').sortable({
+                connectWith: 'ul.portal-widget-list',
+                start: function () {
+                    $(this).closest('.start-widgetcontainer').find('.portal-widget-list').addClass('ui-sortable move');
+                },
+                stop: function (event, ui) {
+                    $.get(STUDIP.ABSOLUTE_URI_STUDIP + 'dispatch.php/start/storeNewOrder', {
+                        widget: $(ui.item).attr('id'),
+                        position: $(ui.item).index(),
+                        column: $(ui.item).parent().index()
+                    });
+                    $(this).closest('.start-widgetcontainer').find('.portal-widget-list').removeClass('move');
                 }
 
-                $('.edit-widgetcontainer .start-widgetcontainer ul.portal-widget-list:first-child').find('li').each(function () {
-                    widgets.left[$(this).attr('id')] = $(this).index();
-                });
+            });
+        },
 
-                $('.edit-widgetcontainer .start-widgetcontainer ul.portal-widget-list:last-child').find('li').each(function () {
-                    widgets.right[$(this).attr('id')] = $(this).index();
-                });
+        init_edit: function (perm) {
+            $('.edit-widgetcontainer .portal-widget-list').sortable({
+                connectWith: '.edit-widgetcontainer .portal-widget-list',
+                start: function () {
+                    $(this).closest('.edit-widgetcontainer').find('.portal-widget-list').addClass('ui-sortable move');
+                },
+                stop: function () {
+                    // store the whole widget constellation
+                    var widgets = {
+                        left: {},
+                        right: {}
+                    };
 
-                $.ajax({
-                    type: 'POST',
-                    url: STUDIP.ABSOLUTE_URI_STUDIP + 'dispatch.php/start/update_defaults/' +  perm,
-                    data: widgets
-                });
+                    $('.edit-widgetcontainer .start-widgetcontainer .portal-widget-list:first-child > li').each(function () {
+                        widgets.left[$(this).attr('id')] = $(this).index();
+                    });
 
-                $(this).parent().parent().find('ul').removeClass('move');
-            }
+                    $('.edit-widgetcontainer .start-widgetcontainer .portal-widget-list:last-child > li').each(function () {
+                        widgets.right[$(this).attr('id')] = $(this).index();
+                    });
 
-        });
-    }
-};
-$(document).ready(function() {
-    STUDIP.startpage.init();
-});
+                    $.post(STUDIP.ABSOLUTE_URI_STUDIP + 'dispatch.php/start/update_defaults/' +  perm, widgets);
+
+                    $(this).closest('.edit-widgetcontainer').find('.portal-widget-list').removeClass('move');
+                }
+
+            });
+        }
+    };
+
+    $(document).ready(STUDIP.startpage.init);
+}(jQuery, STUDIP));

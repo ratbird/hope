b142e7ecb2d4dddf579ba420896945f53efd859d
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-08-14 15:56:02 +0000

    fixes #4712
    git-svn-id: svn://develop.studip.de/studip/trunk@31896 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/course/enrolment/apply.php
+++ app/views/course/enrolment/apply.php
@@ -22,24 +22,32 @@
     <form name="claim_admission" action="<?= $controller->link_for('/claim/' . $course_id) ?>" method="post">
     <? if (is_array($priocourses)): ?>
         <?= $this->render_partial('course/enrolment/_priocourses.php') ?>
+        <div data-dialog-button>
+        <?= Studip\Button::createAccept(_("Speichern"), 'claim', array('data-dialog' => '')) ?>
+        </div>
     <? else : ?>
-        <input type="checkbox" name="courseset_claimed" id="courseset_claimed" value="1" <?= ($already_claimed ? 'checked' : '') ?>>
-        <label for="courseset_claimed" style="font-weight:bold"><?=_("Zur Platzverteilung anmelden")?></label>
-        &nbsp;(<?= sprintf(_("max. Teilnehmeranzahl: %s / Anzahl der Anmeldungen: %s"), $priocourses->admission_turnout, $num_claiming) ?>)
+        <? if (!$already_claimed) :?>
+            <?= \Studip\Button::createAccept(_("Zur Platzverteilung anmelden"), 'claim', array('data-dialog' => '')); ?>
+        <? else : ?>
+            <?= \Studip\Button::createCancel(_("Von der Platzverteilung abmelden"), 'claim', array('data-dialog' => '')); ?>
+        <? endif ?>
+        <input type="hidden" name="courseset_claimed" value="<?= ($already_claimed ? '0' : '1') ?>" >
+        <div>
+        (<?= sprintf(_("max. Teilnehmeranzahl: %s / Anzahl der Anmeldungen: %s"), $priocourses->admission_turnout, $num_claiming) ?>)
+        </div>
     <? endif ?>
     <div data-dialog-button>
-    <?= Studip\Button::createAccept(_("Speichern"), 'claim', array('data-dialog' => '')) ?>
     <?= Studip\Button::createCancel(_("Schließen"), 'cancel') ?>
     </div>
     <?= CSRFProtection::tokenTag() ?>
     </form>
 <? endif ?>
 <? if (!$priocourses && !$admission_form) :?>
-    <div>
+    <div data-dialog-button>
     <? if ($enrol_user) : ?>
-        <?=Studip\LinkButton::createAccept(_('Zur Veranstaltung'), URLHelper::getURL('seminar_main.php', array('auswahl' => $course_id))) ?>
+        <?=Studip\LinkButton::createAccept(_('Zur Veranstaltung'), URLHelper::getScriptURL('seminar_main.php', array('auswahl' => $course_id))) ?>
     <? else : ?>
-        <?=Studip\LinkButton::createCancel(_('Schließen'), URLHelper::getURL('dispatch.php/course/details/', array('sem_id' => $course_id))) ?>
+        <?=Studip\LinkButton::createAccept(_('OK'), URLHelper::getScriptURL('dispatch.php/course/details/', array('sem_id' => $course_id))) ?>
     <? endif ?>
     </div>
 <? endif ?>

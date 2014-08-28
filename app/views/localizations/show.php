835477f686cdfade54d9651d27f03e1c3260491c
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-08-28 13:38:53 +0000

    js translation: provide extracted strings, fixes #4919
    git-svn-id: svn://develop.studip.de/studip/trunk@32037 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/localizations/show.php
+++ app/views/localizations/show.php
@@ -1,48 +1,107 @@
-<?
-# Lifter010: TODO
+<?php
 
 $translations = array(
-    "suchen" => _("suchen")
-  , "Sonntag" => _("Sonntag")
-  , "Montag" => _("Montag")
-  , "Dienstag" => _("Dienstag")
-  , "Mittwoch" => _("Mittwoch")
-  , "Donnerstag" => _("Donnerstag")
-  , "Freitag" => _("Freitag")
-  , "Samstag" => _("Samstag")
-  , "Bitte ändern Sie ihre Eingabe" => _("Bitte ändern Sie ihre Eingabe")
-  , "Bitte wählen Sie einen Wert aus" => _("Bitte wählen Sie einen Wert aus")
-  , "Bitte geben Sie eine gültige E-Mail-Adresse ein" => _("Bitte geben Sie eine gültige E-Mail-Adresse ein")
-  , "Bitte geben Sie eine Zahl ein" => _("Bitte geben Sie eine Zahl ein")
-  , "Bitte geben Sie eine gültige Web-Adresse ein" => _("Bitte geben Sie eine gültige Web-Adresse ein")
-  , "Der eingegebene Wert darf nicht größer als $1 sein" => _("Der eingegebene Wert darf nicht größer als $1 sein")
-  , "Der eingegebene Wert darf nicht kleiner als $1 sein" => _("Der eingegebene Wert darf nicht kleiner als $1 sein")
-  , "Dies ist ein erforderliches Feld" => _("Dies ist ein erforderliches Feld")
-  , "Nicht buchbare Räume:" => _("Nicht buchbare Räume:")
-  , 'Die beiden Werte "$1" und "$2" stimmen nicht überein. Bitte überprüfen Sie Ihre Eingabe.' => _('Die beiden Werte "$1" und "$2" stimmen nicht überein. Bitte überprüfen Sie Ihre Eingabe.')
-  , "Bitte geben Sie Ihren tatsächlichen Vornamen an." => _("Bitte geben Sie Ihren tatsächlichen Vornamen an.")
-  , "Bitte geben Sie Ihren tatsächlichen Nachnamen an." => _("Bitte geben Sie Ihren tatsächlichen Nachnamen an.")
-  , "Blenden Sie die restlichen Termine ein" => _("Blenden Sie die restlichen Termine ein")
-  , "Blenden Sie die restlichen Termine aus" => _("Blenden Sie die restlichen Termine aus")
-  , 'Alle Räume anzeigen' => _('Alle Räume anzeigen')
-  , 'Nur buchbare Räume anzeigen' => _('Nur buchbare Räume anzeigen')
-  , 'Jeder Termin muss mindestens eine Person haben, die ihn durchführt!' => _('Jeder Termin muss mindestens eine Person haben, die ihn durchführt!')
-  , 'Übernehmen' => _('Übernehmen')
-  , 'Abbrechen' => _('Abbrechen')
+    // app/views/admin/autoinsert/manual.php
+    'Fehler' => _('Fehler'),
+    'Fehler bei der Übertragung' => _('Fehler bei der Übertragung'),
+    'Gefundene Nutzer' => _('Gefundene Nutzer'),
+    'Wollen Sie diesen Filter wirklich entfernen?' => _('Wollen Sie diesen Filter wirklich entfernen?'),
 
-  // public/assets/javascripts/register.js
-  , 'Der Benutzername ist zu kurz, er sollte mindestens 4 Zeichen lang sein.' => _('Der Benutzername ist zu kurz, er sollte mindestens 4 Zeichen lang sein.')
-  , 'Der Benutzername enthält unzulässige Zeichen, er darf keine Sonderzeichen oder Leerzeichen enthalten.' => _('Der Benutzername enthält unzulässige Zeichen, er darf keine Sonderzeichen oder Leerzeichen enthalten.')
-  , 'Das Passwort ist zu kurz, es sollte mindestens 4 Zeichen lang sein.' => _('Das Passwort ist zu kurz, es sollte mindestens 4 Zeichen lang sein.')
-  , 'Das Passwort stimmt nicht mit dem Bestätigungspasswort überein!' => _('Das Passwort stimmt nicht mit dem Bestätigungspasswort überein!')
-  , 'Bitte geben Sie Ihren tatsächlichen Vornamen an.' => _('Bitte geben Sie Ihren tatsächlichen Vornamen an.')
-  , 'Bitte geben Sie Ihren tatsächlichen Nachnamen an.' => _('Bitte geben Sie Ihren tatsächlichen Nachnamen an.')
-  , 'Die E-Mail-Adresse ist nicht korrekt!' => _('Die E-Mail-Adresse ist nicht korrekt!')
+    // public/assets/javascripts/admission.js
+    'Wird geladen' => _('Wird geladen'),
+    'Anmelderegel konfigurieren' => _('Anmelderegel konfigurieren'),
+    'Sie haben noch keine Anmelderegeln festgelegt.' => _('Sie haben noch keine Anmelderegeln festgelegt.'),
+    'Sie haben noch niemanden hinzugefügt.' => _('Sie haben noch niemanden hinzugefügt.'),
 
-  // public/assets/javascripts/messages.js
-  , 'Sie haben nicht angegeben, wer die Nachricht empfangen soll!' => _('Sie haben nicht angegeben, wer die Nachricht empfangen soll!')
+    // public/assets/javascripts/application.js
+    'Blenden Sie die restlichen Termine aus' => _('Blenden Sie die restlichen Termine aus'),
+    'Blenden Sie die restlichen Termine ein' => _('Blenden Sie die restlichen Termine ein'),
+    'Ihre Eingaben wurden bislang noch nicht gespeichert.' => _('Ihre Eingaben wurden bislang noch nicht gespeichert.'),
 
-  // add your translations here
+    // public/assets/javascripts/forms.js
+    'Bitte ändern Sie ihre Eingabe' => _('Bitte ändern Sie ihre Eingabe'),
+    'Bitte wählen Sie einen Wert aus.' => _('Bitte wählen Sie einen Wert aus.'),
+    'Bitte geben Sie gültige E-Mail-Adresse ein' => _('Bitte geben Sie gültige E-Mail-Adresse ein'),
+    'Bitte geben Sie eine Zahl ein' => _('Bitte geben Sie eine Zahl ein'),
+    'Bitte geben Sie eine gültige Web-Adresse ein' => _('Bitte geben Sie eine gültige Web-Adresse ein'),
+    'Der eingegebene Wert darf nicht größer als $1 sein' => _('Der eingegebene Wert darf nicht größer als $1 sein'),
+    'Der eingegebene Wert darf nicht kleiner als $1 sein' => _('Der eingegebene Wert darf nicht kleiner als $1 sein'),
+    'Dies ist ein erforderliches Feld' => _('Dies ist ein erforderliches Feld'),
+
+    // public/assets/javascripts/messages.js
+    'Sie haben nicht angegeben, wer die Nachricht empfangen soll!' => _('Sie haben nicht angegeben, wer die Nachricht empfangen soll!'),
+
+    // public/assets/javascripts/multi_person_search.js
+    'Suchergebnisse' => _('Suchergebnisse'),
+    'Sie haben <%= count %> Personen ausgewählt' => _('Sie haben <%= count %> Personen ausgewählt'),
+    'Alle hinzufügen' => _('Alle hinzufügen'),
+    'Alle entfernen' => _('Alle entfernen'),
+    ' Dieser Filter enthält keine Personen.' => _(' Dieser Filter enthält keine Personen.'),
+    'Es wurden keine neuen Ergebnisse für "<%= needle %>" gefunden.' => _('Es wurden keine neuen Ergebnisse für "<%= needle %>" gefunden.'),
+
+    // public/assets/javascripts/multi_select.js
+    'ausgewählt' => _('ausgewählt'),
+
+    // public/assets/javascripts/news.js
+    'Dialog wird geladen...' => _('Dialog wird geladen...'),
+
+    // public/assets/javascripts/raumzeit.js
+    'Nicht buchbare Räume:' => _('Nicht buchbare Räume:'),
+    'Alle Räume anzeigen' => _('Alle Räume anzeigen'),
+    'Jeder Termin muss mindestens eine Person haben, die ihn durchführt!' => _('Jeder Termin muss mindestens eine Person haben, die ihn durchführt!'),
+    'Nur buchbare Räume anzeigen' => _('Nur buchbare Räume anzeigen'),
+
+    // public/assets/javascripts/register.js
+    'Der Benutzername ist zu kurz, er sollte mindestens 4 Zeichen lang sein.' => _('Der Benutzername ist zu kurz, er sollte mindestens 4 Zeichen lang sein.'),
+    'Der Benutzername enthält unzulässige Zeichen, er darf keine Sonderzeichen oder Leerzeichen enthalten.' => _('Der Benutzername enthält unzulässige Zeichen, er darf keine Sonderzeichen oder Leerzeichen enthalten.'),
+    'Das Passwort ist zu kurz, es sollte mindestens 4 Zeichen lang sein.' => _('Das Passwort ist zu kurz, es sollte mindestens 4 Zeichen lang sein.'),
+    'Das Passwort stimmt nicht mit dem Bestätigungspasswort überein!' => _('Das Passwort stimmt nicht mit dem Bestätigungspasswort überein!'),
+    'Bitte geben Sie Ihren tatsächlichen Vornamen an.' => _('Bitte geben Sie Ihren tatsächlichen Vornamen an.'),
+    'Bitte geben Sie Ihren tatsächlichen Nachnamen an.' => _('Bitte geben Sie Ihren tatsächlichen Nachnamen an.'),
+    'Die E-Mail-Adresse ist nicht korrekt!' => _('Die E-Mail-Adresse ist nicht korrekt!'),
+
+    // public/assets/javascripts/schedule.js
+    'Neuen Termin eintragen' => _('Neuen Termin eintragen'),
+    'Veranstaltungsdetails' => _('Veranstaltungsdetails'),
+    'Termindetails bearbeiten' => _('Termindetails bearbeiten'),
+    'Detaillierte Veranstaltungsliste' => _('Detaillierte Veranstaltungsliste'),
+
+    // public/assets/javascripts/settings.js
+    'Die beiden Werte "$1" und "$2" stimmen nicht überein. ' => _('Die beiden Werte "$1" und "$2" stimmen nicht überein. '),
+
+    // public/assets/javascripts/smiley-picker.js
+    'Smileys' => _('Smileys'),
+    'Zur Gesamtübersicht' => _('Zur Gesamtübersicht'),
+    'Schliessen' => _('Schliessen'),
+
+    // public/assets/javascripts/studip-dialog-1.0.js
+    'Schließen' => _('Schließen'),
+
+    // public/assets/javascripts/studip-helper-attributes.js
+    'Zeichen verbleibend: ' => _('Zeichen verbleibend: '),
+    'Eingegebene Zeichen: ' => _('Eingegebene Zeichen: '),
+
+    // public/assets/javascripts/updater.js
+    'Bitte laden Sie die Seite neu, um fortzufahren' => _('Bitte laden Sie die Seite neu, um fortzufahren'),
+    'Neu laden' => _('Neu laden'),
+    'Sie sind nicht mehr im System angemeldet.' => _('Sie sind nicht mehr im System angemeldet.'),
+
+    // public/assets/javascripts/userfilter.js
+    'Bedingung konfigurieren' => _('Bedingung konfigurieren'),
+    'oder' => _('oder'),
+
+    // public/plugins_packages/core/Forum/javascript/forum.js
+    'Wenn Sie die Seite verlassen, gehen ihre Änderungen verloren!' => _('Wenn Sie die Seite verlassen, gehen ihre Änderungen verloren!'),
+    'Alle Bereiche werden dann nach "Allgemein" verschoben!' => _('Alle Bereiche werden dann nach "Allgemein" verschoben!'),
+    'Es werden auch alle Beiträge in diesem Bereich gelöscht!' => _('Es werden auch alle Beiträge in diesem Bereich gelöscht!'),
+    'Anonym' => _('Anonym'),
+    'Die Senderin/der Sender dieser Nachricht möchte Sie auf den folgenden Beitrag aufmerksam machen. ' => _('Die Senderin/der Sender dieser Nachricht möchte Sie auf den folgenden Beitrag aufmerksam machen. '),
+    'Link zum Beitrag: ' => _('Link zum Beitrag: '),
+    'Thema schließen' => _('Thema schließen'),
+    'Thema öffnen' => _('Thema öffnen'),
+    'Hervorhebung aufheben' => _('Hervorhebung aufheben'),
+    'Thema hervorheben' => _('Thema hervorheben'),
+
 );
 
 // translations have to be UTF8 for #json_encode
@@ -51,5 +110,4 @@ $translations = $plugin->utf8EncodeArray($translations);
 ?>
 String.toLocaleString({
   "<?= strtr($language, "_", "-") ?>": <?= json_encode($translations) ?>
-
 });

35b6b14cd5bc6d43b7377e156db8ea3a63737dc7
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-08-28 14:53:04 +0000

    js translation: add documentation for cli tool, re #4919
    git-svn-id: svn://develop.studip.de/studip/trunk@32040 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- cli/extract-js-localizations.php
+++ cli/extract-js-localizations.php
@@ -14,7 +14,16 @@
 
 require 'studip_cli_env.inc.php';
 
-
+/**
+ * Determines whether the file should be skipped depending on an exclude list
+ * with an additional include list. This allows inclusion inside of previously
+ * excluded entries. We need this for the assets directory.
+ * Furthermore, the file is checked against a list of mime types to include.
+ *
+ * @param String $filename Adjusted filename (stripped to path inside trunk)
+ * @param String $realfile Actual file name (needed for mime type detection)
+ * @return bool indicating whether the file should be skipped or not
+ */
 function should_skip_file($filename, $realfile) {
     $exclude = array(
         'locale/*',
@@ -36,6 +45,7 @@ function should_skip_file($filename, $realfile) {
         'application/javascript',
     );
     
+    // Check if the file should be excluded, depending on it's path.
     $matching_pattern = null;
     $skip             = false;
     foreach ($exclude as $pattern) {
@@ -46,6 +56,11 @@ function should_skip_file($filename, $realfile) {
         }
     }
     
+    // If it should be skipped in step 1, check if it matches the include
+    // patterns and no longer skip it, if it matches.
+    // Matches are only from patterns that are longer than the pattern that
+    // set the entry to be skipped. Thus it is detected if the file is in a
+    // subdirectory.
     if ($skip) {
         foreach ($include as $pattern) {
             if (fnmatch($pattern, $filename) && strlen($pattern) > strlen($matching_pattern)) {
@@ -55,6 +70,8 @@ function should_skip_file($filename, $realfile) {
         }
     }
     
+    // If the file should not be skipped, check it's mime type and skip it
+    // if the mime type is not allowed.
     if (!$skip && is_file($realfile)) {
         $mime_type = mime_content_type($realfile);
         
@@ -70,6 +87,15 @@ function should_skip_file($filename, $realfile) {
     return $skip;
 }
 
+/**
+ * Extract the actual text strings from a file. This will only detect single
+ * line text strings. Multi line strings are just a hassle to handle in js
+ * anyways.
+ *
+ * @param String $file Filename to extract text strings from
+ * @return mixed Array with found text strings or false if no text strings
+ *               were found
+ */
 function extract_strings($file) {
     $contents = file_get_contents($file);
     $regexp   = '/(?:\'([^\']+)\'|"([^"]+)")\\.toLocaleString\\(\\s*\\)/';
@@ -85,6 +111,16 @@ function extract_strings($file) {
     return false;
 }
 
+/**
+ * Recursively find text strings in files in the given directory.
+ * This skips invalid files.
+ *
+ * @param String $directory Directory to search files in
+ * @param mixed  $base      Optional base directory to strip from file names,
+ *                          will default to the initial passed directory.
+ * @return Array Associative array with filenames as index and an array of
+ *               the text strings the file contains.
+ */
 function find_strings_in_dir($directory, $base = null) {
     $result = array();
 
@@ -109,8 +145,10 @@ function find_strings_in_dir($directory, $base = null) {
     return $result;
 }
 
+// Find text strings in all stud.ip files
 $occurences = find_strings_in_dir(realpath(__DIR__ . '/..'));
 
+// Remove duplicates
 $hashes = array();
 foreach ($occurences as $file => $strings) {
     foreach ($strings as $index => $string) {
@@ -128,6 +166,7 @@ foreach ($occurences as $file => $strings) {
     }
 }
 
+// Create trails view as output
 ob_start();
 ?>
 <?= '<?php' . PHP_EOL ?>
@@ -153,6 +192,8 @@ String.toLocaleString({
 <?
 $view = ob_get_clean();
 
+// Write output to the corresponding file
 file_put_contents(__DIR__ . '/../app/views/localizations/show.php', $view);
 
+// Show some statistics
 printf('%u strings written to file' . PHP_EOL, array_sum(array_map('count', $occurences)));

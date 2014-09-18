c20d0d35a79cf931b00f63c807827f7f43031146
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-09-18 10:31:03 +0000

    js/scrolling: use animation frames to synch scroll events to browser display updates, leads to a smoother/slicker ui, fixes #5001
    git-svn-id: svn://develop.studip.de/studip/trunk@32162 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/assets/javascripts/studip-scroll.js
+++ public/assets/javascripts/studip-scroll.js
@@ -1,32 +1,68 @@
+// (request/cancel)AnimationFrame polyfill,
+// see https://gist.github.com/paulirish/1579671
+(function () {
+    var lastTime = 0,
+        vendors = ['ms', 'moz', 'webkit', 'o'],
+        x;
+    for (x = 0; x < vendors.length && !window.requestAnimationFrame; x += 1) {
+        window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame']
+                                   || window[vendors[x] + 'CancelRequestAnimationFrame'];
+    }
+    if (!window.requestAnimationFrame) {
+        window.requestAnimationFrame = function (callback) {
+            var currTime = new Date().getTime(),
+                timeToCall = Math.max(0, 16 - (currTime - lastTime)),
+                id;
+            id = window.setTimeout(function () {
+                callback(currTime + timeToCall);
+            }, timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+    }
+    if (!window.cancelAnimationFrame) {
+        window.cancelAnimationFrame = function (id) {
+            clearTimeout(id);
+        };
+    }
+}());
+
+/**
+ * Provides means to hook into the scroll event. Registered callbacks are
+ * called with the current scroll top and scroll left position so both
+ * vertical and horizontal scroll events can be handled.
+ *
+ * Updates/calls to the callback are synchronized to screen refresh by using
+ * the animation frame method (which will fallback to a timer based solution).
+ */
 (function ($, STUDIP) {
     STUDIP = STUDIP || {};
     
     var handlers  = {},
-        activated = false,
-        throttle  = 30;
+        animId    = false;
         
     function scrollHandler (event) {
-        var scrollTop = $(document).scrollTop();
+        var scrollTop  = $(document).scrollTop(),
+            scrollLeft = $(document).scrollLeft();
         $.each(handlers, function (index, handler) {
-            handler(scrollTop);
+            handler(scrollTop, scrollLeft);
         });
+        animId = window.requestAnimationFrame(scrollHandler);
     };
 
     function refresh() {
         var hasHandlers = !$.isEmptyObject(handlers);
-        if (!hasHandlers && activated) {
-            $(document).off('scroll.studip'); 
-            activated = false;
-        } else if (hasHandlers) {
-            if (!activated) {
-                $(document).on('scroll.studip', _.throttle(scrollHandler, throttle));
-                activated = true;
-            }
-            $(document).trigger('scroll.studip');
+        if (!hasHandlers && animId !== false) {
+            window.cancelAnimationFrame(animId);
+            animId = false;
+        } else if (hasHandlers && animId === false) {
+            animId = window.requestAnimationFrame(scrollHandler);
         }
     }
 
     STUDIP.Scroll = {};
+    
     STUDIP.Scroll.addHandler = function (index, handler) {
         handlers[index] = handler;
         refresh();

de12949c2b7e2b20e2e942f6c6293e30475d0844
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-09-26 15:58:29 +0000

    fixes #5032
    git-svn-id: svn://develop.studip.de/studip/trunk@32256 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/cronjobs/check_admission.class.php
+++ lib/cronjobs/check_admission.class.php
@@ -59,14 +59,45 @@ class CheckAdmissionJob extends CronJob
                   echo date('r') . ' - Starting seat distribution ' . chr(10);
                   $old_logger = Log::get()->getHandler();
                   $old_log_level = Log::get()->getLogLevel();
-                  Log::get()->setHandler(function($l) {echo $l['formatted'] ."\n";});
-                  Log::get()->setLogLevel(Log::DEBUG);
+                  @mkdir($GLOBALS['TMP_PATH'] . '/seat_distribution_logs');
+                  $logfile = $GLOBALS['TMP_PATH'] . '/seat_distribution_logs/' .  date('Y-m-d-H-i') . '_seat_distribution.log';
+                  if (is_dir($GLOBALS['TMP_PATH'] . '/seat_distribution_logs')) {
+                      Log::get()->setHandler($logfile);
+                      Log::get()->setLogLevel(Log::DEBUG);
+                      echo 'logging to ' . $logfile . chr(10);
+                  } else {
+                      echo 'could not create directory ' . $GLOBALS['TMP_PATH'] . '/seat_distribution_logs' . chr(10);
+                  }
               }
               foreach($sets as $set_id) {
                   $courseset = new CourseSet($set_id);
                   if ($courseset->isSeatDistributionEnabled() && !$courseset->hasAlgorithmRun() && $courseset->getSeatDistributionTime() < time()) {
                       if ($verbose) {
                           echo ++$i . ' ' . $courseset->getId() . ' : ' . $courseset->getName() . chr(10);
+                          $applicants = AdmissionPriority::getPriorities($set_id);
+                          $courses = SimpleCollection::createFromArray(Course::findMany($courseset->getCourses()))->toGroupedArray('seminar_id', words('name veranstaltungsnummer'));
+                          $captions = array(_("Nachname"), _("Vorname"), _("Nutzername"),_('Nutzer-ID'), _('Veranstaltung-ID'), _("Veranstaltung"), _("Nummer"), _("Priorität"));
+                          $data = array();
+                          $users = User::findEachMany(function($user) use ($courses,$applicants,&$data) {
+                                  $app_courses = $applicants[$user->id];
+                                  asort($app_courses);
+                                  foreach ($app_courses as $course_id => $prio) {
+                                      $row = array();
+                                      $row[] = $user->nachname;
+                                      $row[] = $user->vorname;
+                                      $row[] = $user->username;
+                                      $row[] = $user->id;
+                                      $row[] = $course_id;
+                                      $row[] = $courses[$course_id]['name'];
+                                      $row[] = $courses[$course_id]['veranstaltungsnummer'];
+                                      $row[] = $prio;
+                                      $data[] = $row;
+                                  }
+                          }, array_keys($applicants), 'ORDER BY Nachname');
+                          $applicants_file = $GLOBALS['TMP_PATH'] . '/seat_distribution_logs/applicants_' . $set_id . '.csv';
+                          if (array_to_csv($data, $applicants_file, $captions)) {
+                              echo 'applicants written to ' . $applicants_file . chr(10);
+                          }
                       }
                       $courseset->distributeSeats();
                   }

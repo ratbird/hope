17f38603946fc4914705420fc5a9938b9b045f3e
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-10-06 12:51:43 +0000

    re #5054
    add migration
    
    git-svn-id: svn://develop.studip.de/studip/trunk@32316 c5b85792-5220-0410-ad23-ed5b6b8ad999

---
+++ db/migrations/151_add_automatic_updates_to_plugins.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Adds two columns into plugins to enable automatic updates
+ */
+class AddAutomaticUpdatesToPlugins extends Migration
+{
+    function description()
+    {
+        return 'Plugins can now be updated automatically via github or other repositories';
+    }
+
+    function up()
+    {
+        DBManager::get()->exec("
+            ALTER TABLE `plugins` ADD `automatic_update_url` VARCHAR( 256 ) NOT NULL AFTER `dependentonid` ,
+            ADD `automatic_update_secret` VARCHAR( 32 ) NULL AFTER `automatic_update_url`
+        ");
+    }
+
+    function down()
+    {
+        DBManager::get()->exec("ALTER TABLE `plugins` DROP COLUMN `automatic_update_secret` ");
+        DBManager::get()->exec("ALTER TABLE `plugins` DROP COLUMN `automatic_update_url` ");
+    }
+}

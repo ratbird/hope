faf5c2a22d3cc252ea1dc176db6d832b4dbceadb
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-10-06 09:50:18 +0000

    profile modules: allow all strings to be translated, remove infobox and use sidebar instead (with the ability to (de)activate all modules) and some more touchups, fixes #5052
    git-svn-id: svn://develop.studip.de/studip/trunk@32306 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/controllers/profilemodules.php
+++ app/controllers/profilemodules.php
@@ -34,9 +34,8 @@ class ProfileModulesController extends AuthenticatedController {
      * @param $action Name of the action to perform
      * @param $args   Arguments for the given action
      */
-    function before_filter(&$action, &$args) {
-        global $user;
-
+    public function before_filter(&$action, &$args)
+    {
         parent::before_filter($action, $args);
 
         $this->modules = array();
@@ -47,8 +46,8 @@ class ProfileModulesController extends AuthenticatedController {
         Navigation::activateItem('/profile/modules');
 
         // Get current user.
-        $this->user_id = get_userid(Request::username('username', $user->username));
-        $this->username = Request::username('username', $user->username);
+        $this->username = Request::username('username', $GLOBALS['user']->username);
+        $this->user_id  = get_userid($this->username);
 
         $this->plugins = array();
         $blubber = PluginEngine::getPlugin('Blubber');
@@ -56,73 +55,120 @@ class ProfileModulesController extends AuthenticatedController {
         if ($blubber) {
             $this->plugins[] = $blubber;
         }
+
         // Get homepage plugins from database.
         $this->plugins = array_merge($this->plugins, PluginEngine::getPlugins('HomepagePlugin'));
+
         // Show info message if user is not on his own profile
         if ($this->user_id != $GLOBALS['user']->id) {
             $current_user = User::find($this->user_id);
             $message = sprintf(_('Daten von: %s %s (%s), Status: %s'),
-                    htmlReady($current_user->Vorname),
-                    htmlReady($current_user->Nachname),
-                    $current_user->username,
-                    $current_user->perms);
+                               htmlReady($current_user->Vorname),
+                               htmlReady($current_user->Nachname),
+                               htmlReady($current_user->username),
+                               htmlReady($current_user->perms));
             PageLayout::postMessage(MessageBox::info($message));
         }
+
+        $this->setupSidebar();
+    }
+
+    /**
+     * Creates the sidebar.
+     */
+    private function setupSidebar()
+    {
+        $sidebar = Sidebar::get();
+        $sidebar->setImage('sidebar/plugin-sidebar.png');
+        $sidebar->setTitle(PageLayout::getTitle());
+
+        $widget = new ActionsWidget();
+        $widget->addLink(_('Alle Inhaltselemente aktivieren'),
+                         $this->url_for('profilemodules/reset/true'),
+                         'icons/16/blue/accept.png');
+        $widget->addLink(_('Alle Inhaltselemente deaktivieren'),
+                         $this->url_for('profilemodules/reset'),
+                         'icons/16/blue/decline.png');
+        $sidebar->addWidget($widget);
     }
 
     /**
      * Generates an overview of installed plugins and provides the possibility
      * to (de-)activate each of them.
      */
-    function index_action() {
+    public function index_action()
+    {
+        $manager = PluginManager::getInstance();
+        
         // Now loop through all found plugins.
         foreach ($this->plugins as $plugin) {
             // Check local activation status.
             $id = $plugin->getPluginId();
-            $activated = PluginManager::getInstance()->isPluginActivatedForUser($id, $this->user_id);
+            $activated = $manager->isPluginActivatedForUser($id, $this->user_id);
             // Load plugin data (e.g. name and description)
             $metadata = $plugin->getMetadata();
             $this->modules[$id] = array(
-                    'id' => $id,
-                    'name' => $plugin->getPluginName(),
-                    'description' => $metadata['description'],
-                    'homepage' => $metadata['homepage'],
-                    'activated' => $activated
-                );
+                'id' => $id,
+                'name' => $plugin->getPluginName(),
+                'description' => $metadata['description'],
+                'homepage' => $metadata['homepage'],
+                'activated' => $activated
+            );
         }
     }
 
     /**
      * Updates the activation status of user's homepage plugins.
      */
-    function update_action() {
-
+    public function update_action()
+    {
         CSRFProtection::verifyUnsafeRequest();
-        PageLayout::clearMessages();
 
-        $success = '';
+        $manager = PluginManager::getInstance();
+        $modules = Request::optionArray('modules');
+
+        $success = null;
         // Plugins
         foreach ($this->plugins as $plugin) {
             // Check local activation status.
             $id = $plugin->getPluginId();
-            $activated = PluginManager::getInstance()->isPluginActivatedForUser($id, $this->user_id);
-            if ((!$activated && Request::get('module_'.$id)) || ($activated && !Request::get('module_'.$id))) {
-                $updated = PluginManager::getInstance()->setPluginActivated($id, $this->user_id, Request::get('module_'.$id), 'user');
-                $success = ($success === '' ? true : $success) && $updated;
-                if ($updated) {
-                    $this->modules[$id]['activated'] = Request::get('module_'.$id);
-                }
+
+            $state_before = $manager->isPluginActivatedForUser($id, $this->user_id);
+            $state_after  = in_array($id, $modules);
+
+            if ($state_before !== $state_after) {
+                $updated = $manager->setPluginActivated($id, $this->user_id, $state_after, 'user');
+
+                $success = $success || $updated;
             }
         }
 
-        if ($success !== '') {
-            if ($success) {
-                $this->flash['message'] = _("Ihre Änderungen wurden gespeichert.");
-            } else {
-                $this->flash['error'] = _("Ihre Änderungen konnten nicht gespeichert werden.");
-            }
+        if ($success === true) {
+            $message = MessageBox::success(_('Ihre Änderungen wurden gespeichert.'));
+        } elseif ($success === false) {
+            $message = MessageBox::error(_('Ihre Änderungen konnten nicht gespeichert werden.'));
         }
-        $this->redirect(URLHelper::getURL('dispatch.php/profilemodules/index', array('username' => $this->username)));
+        if ($message) {
+            PageLayout::postMessage($message);
+        }
+
+        $this->redirect($this->url_for('profilemodules/index', array('username' => $this->username)));
     }
 
+    /**
+     * Resets/deactivates all profile modules.
+     */
+    public function reset_action($state = false)
+    {
+        $manager = PluginManager::getInstance();
+        foreach ($this->plugins as $plugin) {
+            // Check local activation status.
+            $id = $plugin->getPluginId();
+
+            $manager->setPluginActivated($plugin->getPluginId(), $this->user_id, $state, 'user');
+        }
+
+        PageLayout::postMessage(MessageBox::success(_('Ihre Änderungen wurden gespeichert.')));
+        $this->redirect($this->url_for('profilemodules/index', array('username' => $this->username)));
+    }
 }
--- app/views/profile_modules/index.php
+++ app/views/profile_modules/index.php
@@ -1,97 +1,45 @@
-<?php
-/*
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation; either version 2
-  of the License, or (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- */
-
-use Studip\Button, Studip\LinkButton;
-
-//Standard herstellen
-if ($flash->flash['message']) {
-    echo MessageBox::success($flash->flash['message']);
-} else if ($flash->flash['error']) {
-    echo MessageBox::error($flash->flash['error']);
-}
-
-$infobox = array(
-    array(
-        'kategorie' => _("Information"),
-        'eintrag'   => array(
-            array(
-                'icon' => "icons/16/black/info",
-                'text' => _("Sie können hier einzelne Inhaltselemente ".
-                            "nachträglich aktivieren oder deaktivieren.")
-            ),
-            array(
-                'icon' => "icons/16/black/plugin",
-                'text' => _("Aktivierte Inhaltselemente fügen neue Funktionen ".
-                            "zu Ihrem Profil oder Ihren Einstellungen hinzu.
-                            Diese werden meist als neuer Reiter im Menü ".
-                            "erscheinen.")
-            ),
-            array(
-                'icon' => "icons/16/black/info",
-                'text' => _("Wenn Sie bestimmte dieser Funktionalitäten nicht ".
-                            "benötigen, können Sie sie einfach hier ".
-                            "deaktivieren, die entsprechenden Menüpunkte ".
-                            "werden dann ausgeblendet.")
-            )
-        )
-    )
-
-);
-$infobox = array(
-    'picture' => "sidebar/plugin-sidebar.png",
-    'content' => $infobox
-);
-?>
-
-<form action="<?= URLHelper::getURL('dispatch.php/profilemodules/update', array('username' => $username)) ?>" method="post">
+<form action="<?= $controller->url_for('profilemodules/update', compact('username')) ?>" method="post">
     <?= CSRFProtection::tokenTag() ?>
-    <table class="default">
-        <caption>Inhaltselemente</caption>
+    <table class="default" id="profile_modules">
+        <caption><?= _('Inhaltselemente') ?></caption>
         <thead>
             <tr>
-                <th></th>
-                <th>Name</th>
-                <th>Beschreibung</th>
+                <th>
+                    <input type="checkbox" name="modules[]" value="all"
+                           data-proxyfor="#profile_modules tbody td :checkbox[name^='module']">
+                </th>
+                <th><?= _('Name') ?></th>
+                <th><?= _('Beschreibung') ?></th>
             </tr>
         </thead>
         <tbody>
         <? foreach ($this->controller->modules as $module): ?>
             <tr>
                 <td>
-                    <input type="checkbox" name="module_<?= $module['id'] ?>" <?= $module['activated'] ? ' checked="checked"' : '' ?>>
+                    <input type="checkbox" name="modules[]" value="<?= $module['id'] ?>"
+                           id="module_<?= $module['id'] ?>"
+                           <? if ($module['activated']) echo 'checked'; ?>>
                 </td>
                 <td>
                     <label for="module_<?= $module['id'] ?>">
-                        <b><?= _($module['name']) ?><b>
+                        <strong><?= htmlReady($module['name']) ?><strong>
                     </label>
                 </td>
                 <td>
-                    <? if (isset($module['description'])) : ?>
-                        <?= formatReady($module['description']) ?>
-                    <? else: ?>
-                        <?= _("Für dieses Element ist keine Beschreibung vorhanden.") ?>
-                    <? endif ?>
-            
-                    <? if (isset($module['homepage'])) : ?>
-                        <p>
-                            <strong><?= _('Weitere Informationen:') ?></strong>
-                            <a href="<?= htmlReady($module['homepage']) ?>"><?= htmlReady($module['homepage']) ?></a>
-                        </p>
-                    <? endif ?>
+                <? if (isset($module['description'])) : ?>
+                    <?= formatReady($module['description']) ?>
+                <? else: ?>
+                    <?= _('Für dieses Element ist keine Beschreibung vorhanden.') ?>
+                <? endif ?>
+        
+                <? if (isset($module['homepage'])) : ?>
+                    <p>
+                        <strong><?= _('Weitere Informationen:') ?></strong>
+                        <a href="<?= htmlReady($module['homepage']) ?>">
+                            <?= htmlReady($module['homepage']) ?>
+                        </a>
+                    </p>
+                <? endif ?>
                 </td>
             </tr>
         <? endforeach; ?>
@@ -99,7 +47,7 @@ $infobox = array(
         <tfoot>
             <tr>
                 <td colspan="3">
-                    <?= Button::createAccept(_('Übernehmen'), 'submit') ?>
+                    <?= Studip\Button::createAccept(_('Übernehmen'), 'submit') ?>
                 </td>
             </tr>
         </tfoot>

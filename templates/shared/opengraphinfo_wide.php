45a831f2e1d2f6bd54cc80c936e6f9631b83d589
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-10-06 10:13:38 +0000

    fixes #5053
    display embedding link instead of iframe at first
    
    git-svn-id: svn://develop.studip.de/studip/trunk@32308 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/shared/opengraphinfo_wide.php
+++ templates/shared/opengraphinfo_wide.php
@@ -1,9 +1,9 @@
 <? $videofiles = $og->getVideoFiles() ?>
 <? $audiofiles = $og->getAudioFiles() ?>
 <div class="opengraph<?= count($videofiles) > 0 ? " video" : "" ?>">
-    <a href="<?= URLHelper::getLink($og['url'], array(), false) ?>" class="opengraph_info" target="_blank">
+    <a href="<?= URLHelper::getLink($og['url'], array(), false) ?>" class="info" target="_blank">
         <? if ($og['image'] && !count($videofiles)) : ?>
-        <div class="opengraph_info_image" style="background-image: url('<?= htmlReady($og['image']) ?>');">
+        <div class="info_image" style="background-image: url('<?= htmlReady($og['image']) ?>');">
         </div>
         <? endif ?>
         <strong><?= htmlReady($og['title']) ?></strong>
@@ -16,7 +16,17 @@
     <? if (count($videofiles)) : ?>
     <div class="video">
         <? if (count($videofiles) === 1 && $videofiles[0][1] === "application/x-shockwave-flash") : ?>
-        <iframe width="100%" height="200px" frameborder="0" src="<?= htmlReady($videofiles[0][0]) ?>"></iframe>
+            <?
+            $embedder = '<iframe width="100%" height="200px" frameborder="0" src="'. htmlReady($videofiles[0][0]).'"></iframe>';
+            ?>
+            <a href="#"
+               class="flash-embedder"
+               style="background-image: url('<?= htmlReady($og['image']) ?>');"
+               onClick="jQuery(this).replaceWith(jQuery(this).attr('data-embedder')); return false;"
+               data-embedder="<?= htmlReady($embedder) ?>"
+               title="<?= _("Video abspielen") ?>">
+                <?= Assets::img("icons/80/blue/play.svg", array('class' => "play"))?>
+            </a>
         <? else : ?>
         <video width="100%" height="200px" controls>
             <? foreach ($videofiles as $file) : ?>
@@ -29,7 +39,17 @@
     <? if (count($audiofiles)) : ?>
     <div class="audio">
         <? if (count($audiofiles) === 1 && $audiofiles[0][1] === "application/x-shockwave-flash") : ?>
-        <iframe width="100%" height="100px" frameborder="0" src="<?= htmlReady($og['audio']) ?>"></iframe>
+            <?
+            $embedder = '<iframe width="100%" height="200px" frameborder="0" src="'. htmlReady($audiofiles[0][0]).'"></iframe>';
+            ?>
+            <a href="#"
+               class="flash-embedder"
+               style="background-image: url('<?= htmlReady($og['image']) ?>');"
+               onClick="jQuery(this).replaceWith(jQuery(this).attr('data-embedder')); return false;"
+               data-embedder="<?= htmlReady($embedder) ?>"
+               title="<?= _("Audio abspielen") ?>">
+                <?= Assets::img("icons/100/blue/play.svg")?>
+            </a>
         <? else : ?>
         <audio width="100%" height="50px" controls>
             <? foreach ($audiofiles as $file) : ?>

65a512b9119bb207dac27dbfabd86217c4734dcd
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-11-10 08:24:51 +0000

    cronjobs: add cli script, re #5144
    git-svn-id: svn://develop.studip.de/studip/trunk@32689 c5b85792-5220-0410-ad23-ed5b6b8ad999

---
+++ cli/cronjobs.php
@@ -0,0 +1,54 @@
+#!/usr/bin/php -q
+<?php
+/**
+* cronjobs - Helper script for the cronjobs
+*
+* @author Jan-Hendrik Willms <tleilax+studip@gmail.com>
+* @license http://www.gnu.org/licenses/gpl-2.0.html GPL version 2
+* @category Stud.IP
+* @since 3.1
+* @todo Parameter handling!
+*/
+ 
+require_once 'studip_cli_env.inc.php';
+ 
+$argc = $_SERVER['argc'];
+$argv = $_SERVER['argv'];
+
+$opts = getopt('hl', array('help', 'list'));
+ 
+if (isset($opts['l']) || isset($opts['list'])) {
+    $tasks = CronjobTask::findBySql('1');
+    foreach ($tasks as $task) {
+        $description = call_user_func(array($task->class, 'getDescription'));
+        fwrite(STDOUT, sprintf('%s %s' . PHP_EOL, $task->id, studip_utf8encode($description)));
+    }
+    exit(0);
+}
+ 
+if ($argc < 2 || isset($opts['h']) || isset($opts['help'])) {
+    fwrite(STDOUT,'Usage: ' . basename(__FILE__) . ' [--help] [--list] <task_id> [last_result]' . PHP_EOL);
+    exit(0);
+}
+
+
+$id = $_SERVER['argv'][1];
+$last_result = $argc > 2 ? $_SERVER['argv'][2] : null;
+$task = CronjobTask::find($id);
+if (!$task) {
+    fwrite(STDOUT, 'Unknown task id' . PHP_EOL);
+    exit(0);
+}
+ 
+if (!file_exists('../' . $task->filename)) {
+    fwrite(STDOUT, 'Invalid task, unknown filename "' . $task->filename . '"' . PHP_EOL);
+    exit(0);
+}
+ 
+require_once '../' . $task->filename;
+if (!class_exists($task->class)) {
+    fwrite(STDOUT, 'Invalid task, unknown class "' . $task->class . '"' . PHP_EOL);
+    exit(0);
+}
+ 
+$task->engage($last_result);

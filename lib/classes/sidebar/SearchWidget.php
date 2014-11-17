65a80668a08f80223c6616c900c1d4bc7d76a86f
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-11-17 13:53:39 +0000

    sidebar: extend search widget with a quick search option, fixes #5047
    git-svn-id: svn://develop.studip.de/studip/trunk@32747 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/classes/sidebar/SearchWidget.php
+++ lib/classes/sidebar/SearchWidget.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This class provides a generic search widget for the sidebar.
+ *
+ * @author    Jan-Hendrik Willms <tleilax+studip@gmail.com>
+ * @since     3.1
+ * @license   GPL2 or any later version
+ * @copyright Stud.IP Core Group
+ */
 class SearchWidget extends SidebarWidget
 {
     const INDEX = 'search';
@@ -9,11 +17,16 @@ class SearchWidget extends SidebarWidget
     protected $method = 'get';
     protected $id = null;
 
+    /**
+     * Constructor for the widget.
+     *
+     * @param String $url URL to send the search to
+     */
     public function __construct($url = '')
     {
         parent::__construct();
-        
-        $this->url      = $url;
+
+        $this->url      = $url ?: $_SERVER['REQUEST_URI'];
         $this->title    = _('Suche');
         $this->template = 'sidebar/search-widget';
 
@@ -21,42 +34,92 @@ class SearchWidget extends SidebarWidget
         $this->filters = $filters;
     }
 
+    /**
+     * Sets the id for the form element.
+     */
     public function setId($id)
     {
         $this->id = $id;
     }
-    
+
+    /**
+     * Returns the id for the form element.
+     *
+     * @return mixed String containing the id or null if no id has been set.
+     */
     public function getId()
     {
         return $this->id;
     }
 
+    /**
+     * Sets the request method used for the form.
+     */
     public function setMethod($method)
     {
         $this->method = $method;
     }
-    
+
+    /**
+     * Returns the request method used for the form.
+     *
+     * @return string containing the chosen request method.
+     */
     public function getMethod()
     {
         return $this->method;
     }
 
-    public function addNeedle($label, $name, $placeholder = false)
+    /**
+     * Add a needle to search (optionally as quick search)
+     *
+     * @param String $label        Label for the input element
+     * @param String $name         Name of the input (which will be the
+     *                             transmitted name attribute)
+     * @param bool   $placeholder  Use label as placeholder (this will hide
+     *                             the associated label)
+     * @param mixed  $quick_search An optional SearchType object if quick
+     *                             search should be used
+     * @param mixed  $js_func      Optional name of a js function or a js
+     *                             function itself that's executed when an
+     *                             entry of the found elements is selected
+     */
+    public function addNeedle($label, $name, $placeholder = false, SearchType $quick_search = null, $js_func = null)
     {
         $value = Request::get($name);
-        $this->needles[] = compact(words('label name placeholder value'));
+        $this->needles[] = compact(words('label name placeholder value quick_search js_func'));
     }
 
+    /**
+     * Add a filter option. This will create a checkbox with the given key
+     * as the name attribute.
+     *
+     * @param String $label Label of the filter
+     * @param String $key   Key/name of the filter (this will be the name
+     *                      attribute)
+     */
     public function addFilter($label, $key)
     {
         $this->filters[$key] = $label;
     }
 
+    /**
+     * Returns whether the widgets has any elements. Since this widget uses
+     * a special template, not all elements are "real" SidebarElements.
+     *
+     * @return bool If widget has any element.
+     */
     public function hasElements()
     {
         return count($this->elements) + count($this->needles) + count($this->filters) > 0;
     }
 
+    /**
+     * Renders the widget.
+     * 
+     * @param Array $variables Unused variables parameter
+     * @return String containing the html output of the widget
+     */
     public function render($variables = array())
     {
         $query = parse_url($this->url, PHP_URL_QUERY);
@@ -67,20 +130,40 @@ class SearchWidget extends SidebarWidget
             $query_params = array();
         }
 
-        $this->template_variables['url']     = URLHelper::getLink($this->url);
+        $this->template_variables['url']        = URLHelper::getLink($this->url);
         $this->template_variables['url_params'] = $query_params;
 
-        $this->template_variables['method']     = $this->method;
-        $this->template_variables['id']      = $this->id;
+        $this->template_variables['method'] = $this->method;
+        $this->template_variables['id']     = $this->id;
+
+        foreach ($this->needles as $index => $needle) {
+            if ($needle['quick_search']) {
+                $quick_search = QuickSearch::get($needle['name'], $needle['quick_search']);
+                if ($needle['js_func'] !== null) {
+                    $quick_search->fireJSFunctionOnSelect($needle['js_func']);
+                }
+
+                $needle['quick_search'] = $quick_search;
+                $this->needles[$index] = $needle;
+            }
+        }
 
         $this->template_variables['needles'] = $this->needles;
         $this->template_variables['filters'] = $this->filters;
-        
+
         $this->template_variables['has_data'] = $this->hasData();
 
         return parent::render($variables);
     }
-    
+
+    /**
+     * Returns whether the widget has any data.
+     * The widget has data if it was submitted and any of the needles
+     * or needles has been filled out.
+     *
+     * @return bool indicating whether the request method matches and any
+     *              element has data.
+     */
     protected function hasData()
     {
         if (!Request::method() === strtoupper($this->method)) {
--- templates/sidebar/search-widget.php
+++ templates/sidebar/search-widget.php
@@ -8,10 +8,14 @@
             <label for="needle-<?= $hash = md5($url . '|' . $needle['name']) ?>" <? if ($needle['placeholder']) echo 'style="display:none;"'; ?>>
                 <?= htmlReady($needle['label']) ?>
             </label>
+        <? if ($needle['quick_search']): ?>
+            <?= $needle['quick_search']->render() ?>
+        <? else: ?>
             <input type="text" id="needle-<?= $hash ?>"
                    name="<?= htmlReady($needle['name']) ?>"
                    value="<?= htmlReady($needle['value']) ?>"
                    <? if ($needle['placeholder']) printf('placeholder="%s"', htmlReady($needle['label'])); ?>>
+        <? endif; ?>
             <input type="submit" value="<?= _('Suchen') ?>">
         </li>
     <? endforeach; ?>
@@ -25,5 +29,14 @@
         </label>
     <? endforeach; ?>
     </ul>
+<? endif; ?>
+<? if (!empty($quick_search)): ?>
+<script>
+(function ($) {
+<? foreach ($quick_search as $needle): ?>
+    STUDIP.QuickSearch.autocomplete('needle-<?= md5($url . '|' . $needle['name']) ?>', '<?= $url ?>');
+<? endforeach; ?>
+}(jQuery));
+</script>
 <? endif; ?>
 </form>

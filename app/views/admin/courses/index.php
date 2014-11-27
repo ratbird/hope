053f6b1a769c9afa87a89483ebfe3e13ef237af8
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-11-27 12:55:28 +0000

    fix gettext mess, re #5181
    git-svn-id: svn://develop.studip.de/studip/trunk@32818 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/admin/courses/index.php
+++ app/views/admin/courses/index.php
@@ -3,8 +3,8 @@
     <? if (!empty($courses)) : ?>
         <?= $this->render_partial('admin/courses/courses.php', compact('courses')) ?>
     <? else : ?>
-        <?= MessageBox::info(_(sprintf('Im %s sind bisher keine Veranstaltungen vorhanden.', $semester->name))); ?>
+        <?= MessageBox::info(sprintf(_('Im %s sind bisher keine Veranstaltungen vorhanden.'), $semester->name)); ?>
     <? endif ?>
 <? else : ?>
-    <?= MessageBox::info(_(sprintf("Sie wurden noch keinen Einrichtungen zugeordnet. Bitte wenden Sie sich an einen der zuständigen %sAdministratoren%s.", "<a href=\"dispatch.php/siteinfo/show\">", "</a>"))) ?>
+    <?= MessageBox::info(sprintf(_('Sie wurden noch keinen Einrichtungen zugeordnet. Bitte wenden Sie sich an einen der zuständigen %sAdministratoren%s.'), '<a href="' . URLHelper::getLink('dispatch.php/siteinfo/show') . '">', '</a>')) ?>
 <? endif ?>

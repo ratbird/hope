e417e491ff0e3251bcba26ae16c5f3ac2f4769b3
tgloeggl <tgloeggl@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2014-12-09 17:36:03 +0000

    refs #5203, fix expected exception for StudipFileLoader
    git-svn-id: svn://develop.studip.de/studip/trunk@32927 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- tests/unit/lib/classes/StudipFileloaderTest.php
+++ tests/unit/lib/classes/StudipFileloaderTest.php
@@ -60,7 +60,7 @@ class StudipFileloaderTestCase extends PHPUnit_Framework_TestCase {
   }
 
   /**
-   * @expectedException PHPUnit_Framework_Exception
+   * @expectedException PHPUnit_Framework_Error_Warning
    */
   function test_should_balk_upon_file_not_found()
   {

5891d09ac956e27daf21214b4fc31f6ddf1d20dc
hackl <hackl@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2015-01-29 14:17:05 +0000

    fixes #5325 show correctly decoded value (works with PHP 5.4 and later)
    git-svn-id: svn://develop.studip.de/studip/trunk@33253 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/admin/configuration/type-edit.php
+++ app/views/admin/configuration/type-edit.php
@@ -8,7 +8,8 @@
            <? if ($value) echo 'checked'; ?>>
     <label for="item-value"><?= _('aktiviert') ?></label>
 <? elseif ($type === 'array') : ?>
-    <textarea cols="80" rows="5" name="value" id="item-value"><?= htmlReady(json_encode(studip_utf8encode($value)), true, true)?></textarea>
+    <?php $v = version_compare(PHP_VERSION, '5.4.0', '>=') ? studip_utf8decode(json_encode(studip_utf8encode($value),JSON_UNESCAPED_UNICODE)) : json_encode(studip_utf8encode($value)) ?>
+    <textarea cols="80" rows="5" name="value" id="item-value"><?= htmlReady($v, true, true)?></textarea>
 <? else: ?>
     <textarea cols="80" rows="3" name="value" id="item-value"><?= htmlReady($value)?></textarea>
 <? endif; ?>

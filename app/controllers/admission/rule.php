ee2b79e4e5e55d30c57f38876d24fee284a0dbe0
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2015-02-12 13:19:20 +0000

    fixes #5358
    git-svn-id: svn://develop.studip.de/studip/trunk@33362 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/controllers/admission/rule.php
+++ app/controllers/admission/rule.php
@@ -16,6 +16,7 @@
 
 require_once('app/controllers/authenticated_controller.php');
 require_once('lib/classes/admission/AdmissionRule.class.php');
+require_once 'lib/classes/admission/CourseSet.class.php';
 
 class Admission_RuleController extends AuthenticatedController {
 
@@ -42,7 +43,7 @@ class Admission_RuleController extends AuthenticatedController {
 
     /**
      * Gets the template for the rule configuration form.
-     * 
+     *
      * @param String $ruleType Class name of the rule to configure.
      * @param String $ruleId   Optional ID of an existing rule.
      */
@@ -55,7 +56,7 @@ class Admission_RuleController extends AuthenticatedController {
 
     /**
      * Shows a form for selecting which rule type to use.
-     * 
+     *
      * @param String $cs_id ID of a courseset the rule shall belong to.
      */
     public function select_type_action($cs_id = '') {
@@ -67,12 +68,12 @@ class Admission_RuleController extends AuthenticatedController {
             if ($rule instanceof AdmissionRule) {
                 $this->courseset->addAdmissionRule($rule);
             }
-        } 
+        }
     }
 
     /**
      * Saves the given rule.
-     * 
+     *
      * @param String $ruleType The class name of the configured rule.
      * @param String $ruleId   ID of the rule to save, or empty if this is a new rule.
      */
@@ -100,7 +101,7 @@ class Admission_RuleController extends AuthenticatedController {
 
     /**
      * Deletes the given rule with all dependencies.
-     * 
+     *
      * @param String $ruleType Class name of the given rule.
      * @param String $ruleId   ID of the given rule.
      */
@@ -113,7 +114,7 @@ class Admission_RuleController extends AuthenticatedController {
     /**
      * Validates if the values given in the current request are sufficient to
      * configure a rule of the given type.
-     * 
+     *
      * @param String $ruleType Class name of the rule to check.
      */
     public function validate_action($ruleType) {

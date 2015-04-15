33d8dbcafecec9f4a2ccfdf1e25e3ccb2cbcf7e3
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2015-04-15 11:40:28 +0000

    pluginmarketplace: change plugins repository url to a neutral xml one, re #5523
    git-svn-id: svn://develop.studip.de/studip/trunk@33788 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- config/config_local_develop.inc.php
+++ config/config_local_develop.inc.php
@@ -157,7 +157,7 @@ $WEBSERVICES_ENABLE = TRUE;
 $PLUGINS_UPLOAD_ENABLE = TRUE;                  //Upload of Plugins is enabled
 
 $PLUGIN_REPOSITORIES = array(
-    'http://develop.studip.de/studip/plugins.php/pluginmarket/extern/xml',
+    'http://plugins.studip.de/plugins.xml',
 );
 
 /*system functions

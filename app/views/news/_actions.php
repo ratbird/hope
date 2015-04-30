4b504cce44d34d8382db74be434bab67d16c5ce8
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2015-04-30 16:50:41 +0000

    news widget: consider news comments as well when marking all news as read, re #5594
    git-svn-id: svn://develop.studip.de/studip/trunk@33947 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/news/_actions.php
+++ app/views/news/_actions.php
@@ -10,35 +10,32 @@
     <?= object_return_views($new['news_id']) ?>
 </span>
 
-<?
-if ($new['allow_comments']):
+<? if ($new['allow_comments']):
     $num = StudipComment::NumCommentsForObject($new['news_id']);
     $visited = object_get_visit($new['news_id'], 'news', false, false);
     $isnew = StudipComment::NumCommentsForObjectSinceLastVisit($new['news_id'], $visited, $GLOBALS['user']->id);
-    ?>
-
+?>
     <? if ($isnew): ?>
-        <span style="color: red;" title="<?= sprintf(_('%s neue(r) Kommentar(e)'), $isnew) ?>">
+        <span class="news-comments-count news-comments-unread" title="<?= sprintf(_('%s neue(r) Kommentar(e)'), $isnew) ?>">
     <? else: ?>
-        <span style="color: #aa6;">
+        <span class="news-comments-count">
     <? endif; ?>
             <?= $num ?>
         </span>
-    <? endif; ?>
+<? endif; ?>
 
-    <? if ($new->havePermission('edit')): ?>
-        <a href=" <?= URLHelper::getLink('dispatch.php/news/edit_news/' . $new->id) ?>" rel='get_dialog' >
-            <?= Assets::img('icons/16/blue/admin.png'); ?>
+<? if ($new->havePermission('edit')): ?>
+    <a href=" <?= URLHelper::getLink('dispatch.php/news/edit_news/' . $new->id) ?>" rel='get_dialog' >
+        <?= Assets::img('icons/16/blue/admin.png'); ?>
+    </a>
+    <? if ($new->havePermission('unassign', $range)): ?>
+        <a href=" <?= URLHelper::getLink('', array('remove_news' => $new->id, 'news_range' => $range)) ?>" >
+            <?= Assets::img('icons/16/blue/remove.png'); ?>
         </a>
-        <? if ($new->havePermission('unassign', $range)): ?>
-            <a href=" <?= URLHelper::getLink('', array('remove_news' => $new->id, 'news_range' => $range)) ?>" >
-                <?= Assets::img('icons/16/blue/remove.png'); ?>
-            </a>
-        <? endif; ?>
-        <? if ($new->havePermission('delete')): ?>
-            <a href=" <?= URLHelper::getLink('', array('delete_news' => $new->id)) ?>" >
-                <?= Assets::img('icons/16/blue/trash.png'); ?>
-            </a>
+    <? endif; ?>
+    <? if ($new->havePermission('delete')): ?>
+        <a href=" <?= URLHelper::getLink('', array('delete_news' => $new->id)) ?>" >
+            <?= Assets::img('icons/16/blue/trash.png'); ?>
         </a>
     <? endif; ?>
 <? endif; ?>
--- public/assets/javascripts/start.js
+++ public/assets/javascripts/start.js
@@ -91,6 +91,7 @@
         $.getJSON(url).then(function (response) {
             if (response) {
                 $('article.new', widget).removeClass('new');
+                $('.news-comments-unread', widget).removeClass('news-comments-unread').removeAttr('title');
                 $('#nav_start [data-badge]').attr('data-badge', 0).trigger('badgechange');
                 icon.remove();
             }

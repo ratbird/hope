207413a942d15fc0d58fb807d15b4fdb50784116
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2015-05-06 15:12:47 +0000

    re #5608
    refactor votecontent
    
    git-svn-id: svn://develop.studip.de/studip/trunk@33980 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/vote/_votecontent.php
+++ app/views/vote/_votecontent.php
@@ -2,57 +2,59 @@
 <? $show_result = $controller->showResult($vote) ?>
 <? $maxvotes = $vote->maxvotes ?>
 <section>
-    <?= formatReady($vote->question) ?>
-</section>
-<form action="<?= ContentBoxHelper::href($vote->id) ?>" method="post">
-    <section class="answers">
-        <? foreach (Request::get('sort') ? $vote->answers->orderBy("count desc", SORT_NUMERIC) : $vote->answers->orderBy("position", SORT_NUMERIC) as $answer): ?>
-            <div class="answer">
-                <? if ($show_result): ?>
-                    <div class="bar">
-                        <div class="percent">
-                            <?= htmlReady($vote->count ? (round($answer->count * 100 / $vote->count)) : 0) ?>%
+    <div>
+        <?= formatReady($vote->question) ?>
+    </div>
+    <form action="<?= ContentBoxHelper::href($vote->id) ?>" method="post">
+        <section class="answers">
+            <? foreach (Request::get('sort') ? $vote->answers->orderBy("count desc", SORT_NUMERIC) : $vote->answers->orderBy("position", SORT_NUMERIC) as $answer): ?>
+                <div class="answer">
+                    <? if ($show_result): ?>
+                        <div class="bar">
+                            <div class="percent">
+                                <?= htmlReady($vote->count ? (round($answer->count * 100 / $vote->count)) : 0) ?>%
+                            </div>
+                            <? $width = $maxvotes ? $answer->count / $maxvotes : 0; ?>
+                            <div style="display: inline-block;
+                                border:1px solid black;
+                                width: <?= 100 * ($width) ?>px;
+                                height: 8px;
+                                background-color: rgb(<?= 255 - round(215 * ($width)) ?>, <?= 255 - round(182 * ($width)) ?>, <?= 255 - round(131 * ($width)) ?>); ">
+                            </div>
                         </div>
-                        <? $width = $maxvotes ? $answer->count / $maxvotes : 0; ?>
-                        <div style="display: inline-block;
-                            border:1px solid black;
-                            width: <?= 100 * ($width) ?>px;
-                            height: 8px;
-                            background-color: rgb(<?= 255 - round(215 * ($width)) ?>, <?= 255 - round(182 * ($width)) ?>, <?= 255 - round(131 * ($width)) ?>); ">
+                        <div class="text">
+                            <?= formatReady($answer->answer) ?>
                         </div>
-                    </div>
-                    <div class="text">
-                        <?= formatReady($answer->answer) ?>
-                    </div>
-                    <div class="infotext">
-                        (<?= $answer->count ?> <?= $answer->count == 1 ? _("Stimme") : _("Stimmen") ?>)
-                        <? if (Request::get('revealNames') === $vote->id && !$vote->anonymous && ($admin || $vote->namesvisibility)): ?>
-                            ( <?= join(', ', $answer->getUsernames()) ?> )
-                        <? endif; ?>
-                    </div>
-                <? else: ?>
-                    <label>
-                        <? if ($vote->multiplechoice): ?>
-                            <input type="checkbox" name="vote_answers[]" value="<?= $answer->position ?>" <?= !$vote->isRunning() ? 'disabled="disabled"' : ''?>>
-                        <? else: ?>
-                            <input type="radio" name="vote_answers[]" value="<?= $answer->position ?>" <?= !$vote->isRunning() ? 'disabled="disabled"' : ''?>>
-                        <? endif ?>
-                        <?= formatReady($answer->answer) ?>
-                    </label>
-                <? endif; ?>
-            </div>
-        <? endforeach; ?>
-    </section>
+                        <div class="infotext">
+                            (<?= $answer->count ?> <?= $answer->count == 1 ? _("Stimme") : _("Stimmen") ?>)
+                            <? if (Request::get('revealNames') === $vote->id && !$vote->anonymous && ($admin || $vote->namesvisibility)): ?>
+                                ( <?= join(', ', $answer->getUsernames()) ?> )
+                            <? endif; ?>
+                        </div>
+                    <? else: ?>
+                        <label>
+                            <? if ($vote->multiplechoice): ?>
+                                <input type="checkbox" name="vote_answers[]" value="<?= $answer->position ?>" <?= !$vote->isRunning() ? 'disabled="disabled"' : ''?>>
+                            <? else: ?>
+                                <input type="radio" name="vote_answers[]" value="<?= $answer->position ?>" <?= !$vote->isRunning() ? 'disabled="disabled"' : ''?>>
+                            <? endif ?>
+                            <?= formatReady($answer->answer) ?>
+                        </label>
+                    <? endif; ?>
+                </div>
+            <? endforeach; ?>
+        </section>
 
-    <footer>
-        <? if ($vote->multiplechoice): ?>
-            <?= _('Sie konnten mehrere Antworten auswählen.') ?>
-        <? endif; ?>
-        <?= $vote->countInfo ?>
-        <?= $vote->anonymousInfo ?>
-        <?= $vote->endInfo ?>
-        <div class="buttons">
-            <?= $this->render_partial('vote/_buttons.php', array('vote' => $vote)); ?>
-        </div>
-    </footer>
-</form>
+        <footer>
+            <? if ($vote->multiplechoice): ?>
+                <?= _('Sie konnten mehrere Antworten auswählen.') ?>
+            <? endif; ?>
+            <?= $vote->countInfo ?>
+            <?= $vote->anonymousInfo ?>
+            <?= $vote->endInfo ?>
+            <div class="buttons">
+                <?= $this->render_partial('vote/_buttons.php', array('vote' => $vote)); ?>
+            </div>
+        </footer>
+    </form>
+</section>

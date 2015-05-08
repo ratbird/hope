9cb80df53723750c7e7509d9e12d438815f79728
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2015-05-08 08:47:29 +0000

    refine re #5603
    git-svn-id: svn://develop.studip.de/studip/trunk@33991 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- lib/classes/StudipFormat.php
+++ lib/classes/StudipFormat.php
@@ -438,6 +438,7 @@ class StudipFormat extends TextFormat
     protected static function markupIndent($markup, $matches)
     {
         $text = preg_replace('/^ +/', '', $matches[0]);
+        $text = preg_replace('/^  /m', '', $text);
 
         return sprintf('<div class="indent">%s</div>', $markup->format($text));
     }

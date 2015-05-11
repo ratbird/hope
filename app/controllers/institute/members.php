0280dbf09b1aee0344ecd0e2b780c1aeb369f5ba
davka <davka@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2015-05-11 12:58:21 +0000

    remove bugfix, re #5500
    git-svn-id: svn://develop.studip.de/studip/trunk@34003 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/controllers/institute/members.php
+++ app/controllers/institute/members.php
@@ -30,6 +30,7 @@ class Institute_MembersController extends AuthenticatedController
         parent::before_filter($action, $args);
 
         $this->admin_view = $GLOBALS['perm']->have_perm('admin') && Request::option('admin_view') !== null;
+        
         PageLayout::addStylesheet('multi-select.css');
         PageLayout::addScript('jquery/jquery.multi-select.js');
         PageLayout::addScript('multi_person_search.js');
@@ -48,7 +49,7 @@ class Institute_MembersController extends AuthenticatedController
 
         // this page is used for administration (if the user has the proper rights)
         // or for just displaying the workers and their roles
-        if ($this->admin_view || $GLOBALS['perm']->have_perm('admin')) {
+        if ($this->admin_view) {
             PageLayout::setTitle(_("Verwaltung der MitarbeiterInnen"));
             Navigation::activateItem('/admin/institute/faculty');
             $GLOBALS['perm']->check("admin");

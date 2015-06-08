6f7f5776f2dd4da34eed856613465d6541761a53
zabbarob <zabbarob@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2015-06-08 13:29:46 +0000

    move ckeditor's studip skin to assets/stylesheets; fixes #5670
    git-svn-id: svn://develop.studip.de/studip/trunk@34231 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/assets/javascripts/wysiwyg.js
+++ public/assets/javascripts/wysiwyg.js
@@ -160,7 +160,13 @@ jQuery(function ($) {
                 tr: {}
             },
             width: textareaWidth,
-            skin: 'studip',
+            skin: 'studip,' + 
+                (function () {
+                    var skinPath = 'assets/stylesheets/ckeditor-skin/';
+                    var a = document.createElement('a');
+                    a.href = STUDIP.URLHelper.getURL(skinPath);
+                    return a.pathname;
+                })(),
             // NOTE codemirror crashes when not explicitely loaded in CKEditor 4.4.7
             extraPlugins: 'codemirror,studip-settings,studip-wiki'
                 // only enable uploads in courses with a file section

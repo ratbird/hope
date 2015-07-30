24603be665b54e5e541d6f58197ef5f01c075858
anoack <anoack@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2015-07-30 13:26:30 +0000

    apply fix for #5673
    git-svn-id: svn://develop.studip.de/studip/trunk@34799 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/WysiwygHtmlHead.php
+++ app/views/WysiwygHtmlHead.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * WysiwygHtmlHead.php - 
+ * WysiwygHtmlHead.php -
  * Include this file in HTML-files after Stud.IP's JS library is loaded.
  */
 use \Studip\Markup;
@@ -11,33 +11,35 @@ if (Settings::getInstance()->isGloballyDisabled()) {
 }
 ?>
 <script type="text/javascript">
-    STUDIP.wysiwyg = {
-        disabled: <?= Settings::getInstance()->isDisabled() ? 'true' : 'false' ?>,
-        settings: <?= Settings::getInstance()->asJson() ?>,
-        seminarId: '<?= $_SESSION['SessionSeminar'] ?>',
-        htmlMarker: '<?= addslashes(Markup::HTML_MARKER) ?>',
-        htmlMarkerRegExp: '<?= addslashes(Markup::HTML_MARKER_REGEXP) ?>',
-        isHtml: function isHtml(text) {
-            // NOTE keep this function in sync with
-            // Markup::isHtml in Markup.class.php
-            if (this.hasHtmlMarker(text)) {
-                return true;
+    if (<?= Settings::getInstance()->isDisabled() ? 'true' : 'false' ?> || CKEDITOR.env.isCompatible) {
+        STUDIP.wysiwyg = {
+            disabled: <?= Settings::getInstance()->isDisabled() ? 'true' : 'false' ?>,
+            settings: <?= Settings::getInstance()->asJson() ?>,
+            seminarId: '<?= $_SESSION['SessionSeminar'] ?>',
+            htmlMarker: '<?= addslashes(Markup::HTML_MARKER) ?>',
+            htmlMarkerRegExp: '<?= addslashes(Markup::HTML_MARKER_REGEXP) ?>',
+            isHtml: function isHtml(text) {
+                // NOTE keep this function in sync with
+                // Markup::isHtml in Markup.class.php
+                if (this.hasHtmlMarker(text)) {
+                    return true;
+                }
+                text = text.trim();
+                return text[0] === '<' && text[text.length - 1] === '>';
+            },
+            hasHtmlMarker: function hasHtmlMarker(text) {
+                // NOTE keep this function in sync with
+                // Markup::hasHtmlMarker in Markup.class.php
+                return (new RegExp(this.htmlMarkerRegExp)).test(text);
+            },
+            markAsHtml: function markAsHtml(text) {
+                // NOTE keep this function in sync with
+                // Markup::markAsHtml in Markup.class.php
+                if (this.hasHtmlMarker(text)) {
+                    return text; // marker already set, don't set twice
+                }
+                return this.htmlMarker + '\n' + text;
             }
-            text = text.trim();
-            return text[0] === '<' && text[text.length - 1] === '>';
-        },
-        hasHtmlMarker: function hasHtmlMarker(text) {
-            // NOTE keep this function in sync with
-            // Markup::hasHtmlMarker in Markup.class.php
-            return (new RegExp(this.htmlMarkerRegExp)).test(text);
-        },
-        markAsHtml: function markAsHtml(text) {
-            // NOTE keep this function in sync with
-            // Markup::markAsHtml in Markup.class.php
-            if (this.hasHtmlMarker(text)) {
-                return text; // marker already set, don't set twice
-            }
-            return this.htmlMarker + '\n' + text;
-        }
-    };
+        };
+    }
 </script>

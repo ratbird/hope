0929d3bda8e628077c71bd018d8fc17f01a11424
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2015-09-01 11:21:32 +0000

    re #5955
    bugfixing
    
    
    git-svn-id: svn://develop.studip.de/studip/trunk@35209 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/controllers/messages.php
+++ app/controllers/messages.php
@@ -71,7 +71,7 @@ class MessagesController extends AuthenticatedController {
             foreach (Request::getArray("bulk") as $message_id) {
                 $this->delete_message($message_id);
             }
-            PageLayout::postMessage(MessageBox::success(sprintf(_("%s Nachrichten wurden gelöscht"), count(Request::getArray("bulk")))));
+            PageLayout::postMessage(MessageBox::success(sprintf(_("%u Nachrichten wurden gelöscht"), count(Request::getArray("bulk")))));
         }
 
         $this->messages = $this->get_messages(

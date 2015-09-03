69af1506836b731ec3a60fe5e1b8c38c6915087a
davka <davka@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2015-09-03 09:15:45 +0000

    typo, re #5970
    git-svn-id: svn://develop.studip.de/studip/trunk@35243 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/my_courses/index.php
+++ app/views/my_courses/index.php
@@ -66,7 +66,7 @@
     <?= PageLayout::postMessage(MessageBox::info(_('Es wurden keine Veranstaltungen gefunden. Mögliche Ursachen:'), array(
         sprintf(_('Sie haben zur Zeit keine Veranstaltungen abonniert, an denen Sie teilnehmen können.
     Bitte nutzen Sie %s<b>Veranstaltung suchen / hinzufügen</b>%s um neue Veranstaltungen aufzunehmen'),'<a href="' . URLHelper::getLink('dispatch.php/search/courses') . '">', '</a>'),
-        _('In dem ausgewählten <b>Semester</b> wurden keine Veranstaltungen abonniert.<br/> Wählen Sie links im <b>Semesterfilter</b> ein anderes Semester aus')
+        _('In dem ausgewählten <b>Semester</b> wurden keine Veranstaltungen abonniert.').'<br>'._('Wählen Sie links im <b>Semesterfilter</b> ein anderes Semester aus')
     )))?>
 <? endif ?>
 <? if (count($my_bosses)) : ?>

68c699030afa9ebdf1cf01e0c53e6b1121a6ba4b
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2015-09-25 12:56:33 +0000

    calendar: remove obsolete and obviously unused file, re #6016
    git-svn-id: svn://develop.studip.de/studip/trunk@35436 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/raumzeit/calendar_jump.php
+++
@@ -1,8 +0,0 @@
-<?
-# Lifter010: TODO
-?>
-<a href="<?= URLHelper::getLink("calendar.php?caluser=self&cmd=showweek&atime=" . $start) ?>">
-    <?= Assets::img('icons/16/blue/schedule.png',
-                    tooltip2(sprintf(_('Zum %s in den persönlichen Terminkalender springen'),
-                                     date('d.m.', $start)))) ?>
-</a>

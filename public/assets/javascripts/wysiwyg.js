db1c4665534a27869702d9de00df233059cdb2a3
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2015-10-06 11:57:36 +0000

    wysiwyg/dialogs: apply hotfix for focusses in non-jquery ui dialogs, fixes #5987
    git-svn-id: svn://develop.studip.de/studip/trunk@35508 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/assets/javascripts/wysiwyg.js
+++ public/assets/javascripts/wysiwyg.js
@@ -480,3 +480,21 @@ jQuery(function ($) {
         return prefix + i;
     }
 }); // jQuery(function($){
+
+// Hotfix for Dialogs
+$.widget( "ui.dialog", $.ui.dialog, {
+ /**
+  * jQuery UI v1.11+ fix to accommodate CKEditor (and other iframed content) inside a dialog
+  * @see http://bugs.jqueryui.com/ticket/9087
+  * @see http://dev.ckeditor.com/ticket/10269
+  */
+  _allowInteraction: function( event ) {
+    return this._super( event ) ||
+
+      // addresses general interaction issues with iframes inside a dialog
+      event.target.ownerDocument !== this.document[ 0 ] ||
+
+      // addresses interaction issues with CKEditor's dialog windows and iframe-based dropdowns in IE
+      !!$( event.target ).closest( ".cke_dialog, .cke_dialog_background_cover, .cke" ).length;
+  }
+});

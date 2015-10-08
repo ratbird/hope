7552a12ce1048418dc654f79d7c0d0469731a637
zabbarob <zabbarob@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2015-10-08 22:57:57 +0000

    add english translations to studip-settings ckeditor plugin (language files); re #6041
    git-svn-id: svn://develop.studip.de/studip/trunk@35559 c5b85792-5220-0410-ad23-ed5b6b8ad999

---
+++ public/assets/javascripts/ckeditor/plugins/studip-settings/lang/de.js
@@ -0,0 +1,18 @@
+CKEDITOR.plugins.setLang('studip-settings', 'de', {
+    buttonLabel: 'WYSIWYG Einstellungen\n(Editor deaktivieren, usw.)',
+    savingChanges: '...speichere Änderungen.',
+    abort: 'Abbrechen',
+    savedChanges: 'Änderungen wurden gespeichert.',
+    information: 'Info',
+    status: 'Status:',
+    response: 'Response:',
+    savingFailed: 'Speichern fehlgeschlagen.',
+    dialogTitle: 'Einstellungen',
+    disableEditorLabel: 'WYSIWYG Editor ausschalten',
+    disableEditorInfo: 'Mit dieser Einstellung können Sie den'
+        + ' WYSIWYG Editor ausschalten. Dadurch müssen'
+        + ' Sie gegebenenfalls Texte in HTML schreiben.'
+        + ' Der Editor wird erst vollständig entfernt'
+        + ' wenn man die Seite neu lädt.',
+});
+
---
+++ public/assets/javascripts/ckeditor/plugins/studip-settings/lang/en.js
@@ -0,0 +1,17 @@
+CKEDITOR.plugins.setLang('studip-settings', 'en', {
+    buttonLabel: 'WYSIWYG Settings\n(Deactivate the editor, etc.)',
+    savingChanges: '...saving changes.',
+    abort: 'Abort',
+    savedChanges: 'All changes saved.',
+    information: 'Info',
+    status: 'Status:',
+    response: 'Response:',
+    savingFailed: 'Saving failed.',
+    dialogTitle: 'Settings',
+    disableEditorLabel: 'Switch WYSIWYG editor off',
+    disableEditorInfo: 'This setting will disable the WYSIWYG'
+        + ' Editor. You might have to write HTML code if you'
+        + ' do so. The editor will only be fully removed after'
+        + ' you reload the page.',
+});
+

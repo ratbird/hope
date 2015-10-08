c0b8002b30ef4e4355fcfd88f563e7354873e315
zabbarob <zabbarob@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2015-10-08 16:33:15 +0000

    sort english translations of studip-upload ckeditor plugin alphabetically by property name; re #6041
    git-svn-id: svn://develop.studip.de/studip/trunk@35557 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- public/assets/javascripts/ckeditor/plugins/studip-upload/lang/en.js
+++ public/assets/javascripts/ckeditor/plugins/studip-upload/lang/en.js
@@ -1,7 +1,7 @@
 CKEDITOR.plugins.setLang('studip-upload', 'en', {
+    buttonLabel: 'Upload Files',
+    error: 'Error: ',
     uploadError: 'Upload failed for some of the files.',
     uploadFailed: 'Uploading files failed.',
-    error: 'Error: ',
-    buttonLabel: 'Upload Files',
 });
 

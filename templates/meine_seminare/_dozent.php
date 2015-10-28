436d56cca2d3db42ec5268e6293f1d2dfe1db26e
tleilax <tleilax@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2015-10-28 10:58:10 +0000

    cleanup: remove obsolete templates/meine_seminare/, re #6006
    git-svn-id: svn://develop.studip.de/studip/trunk@35820 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- templates/meine_seminare/_dozent.php
+++
@@ -1,4 +0,0 @@
-<?
-# Lifter010: TODO
-?>
-<?= $colon ? ', ' : '' ?><a href="<?= URLHelper::getLink('dispatch.php/profile', array('username' => $_dozent['username'])) ?>"><?= htmlReady($_dozent["Nachname"]) ?></a><? $this->colon = true; ?>

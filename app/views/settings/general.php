69ccb9ed3f2adf12bfb952338086f39ee8525002
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2015-10-30 08:18:47 +0000

    re #4292
    update settings pages
    
    git-svn-id: svn://develop.studip.de/studip/trunk@35895 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/settings/general.php
+++ app/views/settings/general.php
@@ -1,5 +1,3 @@
-<? use Studip\Button; ?>
-
 <?
 $start_pages = array(
     '' => _('keine'),
@@ -11,185 +9,120 @@ $start_pages = array(
 );
 ?>
 
-<form method="post" action="<?= $controller->url_for('settings/general/store') ?>">
+<form method="post" action="<?= $controller->url_for('settings/general/store') ?>" class="default">
     <?= CSRFProtection::tokenTag() ?>
     <input type="hidden" name="studipticket" value="<?= get_ticket() ?>">
 
-    <table id="main_content" class="default">
-        <colgroup>
-            <col width="50%">
-            <col width="50%">
-        </colgroup>
-        <caption>
-            <?= _('Allgemeine Einstellungen anpassen') ?>
-        </caption>
-        <tbody>
-            <tr>
-                <th colspan="2"><?= _('Allgemein') ?></th>
-            </tr>
-            <tr>
-                <td>
-                    <label for="forced_language"><?= _('Sprache') ?></label>
-                </td>
-                <td>
-                    <select name="forced_language" id="forced_language">
-                    <? foreach ($GLOBALS['INSTALLED_LANGUAGES'] as $key => $language): ?>
-                        <option value="<?= $key ?>"
-                                <? if ($user_language == $key) echo 'selected'; ?>>
-                            <?= $language['name'] ?>
-                        </option>
-                    <? endforeach; ?>
-                    </select>
-                </td>
-            </tr>
+    <fieldset>
+        <legend>
+            <?= _('Allgemeine Einstellungen') ?>
+        </legend>
+
+        <label>
+            <?= _('Sprache') ?>
+            <select name="forced_language" class="size-s">
+                <? foreach ($GLOBALS['INSTALLED_LANGUAGES'] as $key => $language): ?>
+                    <option value="<?= $key ?>"
+                        <? if ($user_language == $key) echo 'selected'; ?>>
+                        <?= $language['name'] ?>
+                    </option>
+                <? endforeach; ?>
+            </select>
+        </label>
+
         <? if (!$GLOBALS['perm']->have_perm('root')): ?>
-            <tr>
-                <td>
-                    <label for="personal_startpage">
-                        <?= _('Persönliche Startseite') ?><br>
-                        <dfn id="personal_startpage_description">
-                            <?= _('Sie können hier einstellen, welche Seite standardmäßig nach dem Einloggen '
-                                 .'angezeigt wird. Wenn Sie zum Beispiel regelmäßig die Seite &raquo;Meine '
-                                 .'Veranstaltungen&laquo; nach dem Login aufrufen, so können Sie dies hier '
-                                 .'direkt einstellen.') ?>
-                        </dfn>
-                    </label>
-                </td>
-                <td>
-                    <select name="personal_startpage" id="personal_startpage" aria-describedby="personal_startpage_description">
-                    <? foreach ($start_pages as $index => $label): ?>
-                        <option value="<?= $index ?>" <? if ($config->PERSONAL_STARTPAGE == $index) echo 'selected'; ?>>
-                            <?= htmlReady($label) ?>
-                        </option>
-                    <? endforeach; ?>
-                    </select>
-                </td>
-            </tr>
-        <? endif; ?>
-            <tr>
-                <td>
-                    <label for="skiplinks_enable">
-                        <?= _('Skiplinks einblenden') ?><br>
-                        <dfn id="skiplinks_enable_description">
-                            <?= _('Mit dieser Einstellung wird nach dem ersten Drücken der Tab-Taste eine '
-                                 .'Liste mit Skiplinks eingeblendet, mit deren Hilfe Sie mit der Tastatur '
-                                 .'schneller zu den Hauptinhaltsbereichen der Seite navigieren können. '
-                                 .'Zusätzlich wird der aktive Bereich einer Seite hervorgehoben.') ?>
-                        </dfn>
-                    </label>
-                </td>
-                <td>
-                    <input type="checkbox" name="skiplinks_enable" id="skiplinks_enable"
-                           aria-describedby="skiplinks_enable_description" value="1"
-                           <? if ($config->SKIPLINKS_ENABLE) echo 'checked'; ?>>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label for="accesskey_enable">
-                        <?= _('Tastenkombinationen für Hauptfunktionen') ?><br>
-                        <dfn id="accesskey_enable_description">
-                            <?= _('Mit dieser Einstellung können Sie für die meisten in der Kopfzeile '
-                                 .'erreichbaren Hauptfunktionen eine Bedienung über Tastenkombinationen '
-                                 .'aktivieren. <br>Die Tastenkombination wird im Tooltip des jeweiligen '
-                                 .'Icons angezeigt.') ?>
-                            <?= _('Diese kann für jeden Browser und jedes Betriebssystem unterschiedlich '
-                                 .'sein (siehe <a href="http://en.wikipedia.org/wiki/Accesskey" '
-                                 .'target="_blank"">Wikipedia</a>).') ?>
-                        </dfn>
-                    </label>
-                </td>
-                <td>
-                    <input type="checkbox" name="accesskey_enable" id="accesskey_enable"
-                           aria-describedby="accesskey_enable_description" value="1"
-                           <? if ($config->ACCESSKEY_ENABLE) echo 'checked'; ?>>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label for="showsem_enable">
-                        <?= _('Semesteranzeige auf &raquo;Meine Veranstaltungen&laquo;');?><br>
-                        <dfn id="showsem_enable_description">
-                            <?= _('Mit dieser Einstellung können Sie auf der Seite &raquo;Meine '
-                                .'Veranstaltungen&laquo; die Einblendung des Start- und Endsemesters '
-                                .'hinter jeder Veranstaltung aktivieren.') ?>
-                        </dfn>
-                    </label>
-                </td>
-                <td>
-                    <input type="checkbox" name="showsem_enable" id="showsem_enable"
-                           aria-describedby="showsem_enable_description" value="1"
-                           <? if ($config->SHOWSEM_ENABLE) echo 'checked'; ?>>
-                </td>
-            </tr>
-            <? if (get_config('TOURS_ENABLE')) : ?>
-            <tr>
-                <td>
-                    <label for="tour_autostart_disable">
-                        <?= _('Autostart von Touren verhindern');?><br>
-                        <dfn id="tour_autostart_disable_description">
-                            <?= _('Mit dieser Einstellung können Sie verhindern, dass Touren zu einzelnen '
-                                .'Stud.IP-Seiten automatisch starten, wenn Sie die Seite aufrufen. Die Touren '
-                                .'können weiterhin über die Hilfe gestartet werden.') ?>
-                        </dfn>
-                    </label>
-                </td>
-                <td>
-                    <input type="checkbox" name="tour_autostart_disable" id="tour_autostart_disable"
-                           aria-describedby="tour_autostart_disable_description" value="1"
-                           <? if ($config->TOUR_AUTOSTART_DISABLE) echo 'checked'; ?>>
-                </td>
-            </tr>
-            <? endif ?>
-        </tbody>
-        <? if (PersonalNotifications::isGloballyActivated()): ?>
-        <tbody>
-            <tr>
-                <th colspan="2"><?= _('Benachrichtigungen') ?></th>
-            </tr>
-            <tr>
-                <td>
-                    <label for="personal_notifications_activated">
-                        <?= _('Benachrichtigungen über Javascript') ?><br>
-                        <dfn id="personal_notifications_activated_description">
-                            <?= _('Hiermit wird in der Kopfzeile dargestellt, wenn es Benachrichtigungen für '
-                                 .'Sie gibt. Die Benachrichtigungen werden auch angezeigt, wenn Sie nicht die '
-                                 .'Seite neuladen.') ?>
-                        </dfn>
-                    </label>
-                </td>
-                <td>
-                    <input type="checkbox" name="personal_notifications_activated" id="personal_notifications_activated"
-                           aria-describedby="personal_notifications_activated_description" value="1"
-                           <? if (PersonalNotifications::isActivated($user->user_id)) echo 'checked'; ?>>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label for="personal_notifications_audio_activated">
-                        <?= _('Audio-Feedback zu Benachrichtigungen') ?><br>
-                        <dfn id="personal_notifications_audio_activated_description">
-                            <?= _('Wenn eine neue Benachrichtigung für Sie reinkommt, ' .
-                                  'werden Sie mittels eines kleinen Plopps darüber in Kenntnis gesetzt ' .
-                                  '- auch wenn Sie gerade einen anderen Browsertab anschauen. Der Plopp ist ' .
-                                  'nur zu hören, wenn Sie die Benachrichtigungen über Javascript aktiviert haben.') ?>
-                        </dfn>
-                    </label>
-                </td>
-                <td>
-                    <input type="checkbox" name="personal_notifications_audio_activated" id="personal_notifications_audio_activated"
-                           aria-describedby="personal_notifications_audio_activated_description" value="1"
-                           <? if (PersonalNotifications::isAudioActivated($user->user_id)) echo 'checked'; ?>>
-                </td>
-            </tr>
-        </tbody>
-        <? endif; ?>
-        <tfoot>
-            <tr>
-                <td class="table_row_odd" colspan="2" align="center">
-                    <?= Button::create(_('Übernehmen'), 'submit', array('title' => _('Änderungen übernehmen'))) ?>
-                </td>
-            </tr>
-        </tfoot>
-    </table>
+        <label>
+            <?= _('Persönliche Startseite') ?>
+            <?= tooltipIcon(_('Sie können hier einstellen, welche Seite standardmäßig nach dem Einloggen '
+                .'angezeigt wird. Wenn Sie zum Beispiel regelmäßig die Seite &raquo;Meine '
+                .'Veranstaltungen&laquo; nach dem Login aufrufen, so können Sie dies hier '
+                .'direkt einstellen.')) ?>
+            <select name="personal_startpage"
+                <? foreach ($start_pages as $index => $label): ?>
+                    <option value="<?= $index ?>" <? if ($config->PERSONAL_STARTPAGE == $index) echo 'selected'; ?>>
+                        <?= htmlReady($label) ?>
+                    </option>
+                <? endforeach; ?>
+            </select>
+        </label>
+        <? endif ?>
+
+        <label>
+            <input type="checkbox" name="skiplinks_enable"
+                   value="1"
+                <? if ($config->SKIPLINKS_ENABLE) echo 'checked'; ?>>
+            <?= _('Skiplinks einblenden') ?>
+            <?= tooltipIcon(_('Mit dieser Einstellung wird nach dem ersten Drücken der Tab-Taste eine '
+                .'Liste mit Skiplinks eingeblendet, mit deren Hilfe Sie mit der Tastatur '
+                .'schneller zu den Hauptinhaltsbereichen der Seite navigieren können. '
+                .'Zusätzlich wird der aktive Bereich einer Seite hervorgehoben.')) ?>
+        </label>
+
+        <label>
+            <input type="checkbox" name="accesskey_enable"
+                   aria-describedby="accesskey_enable_description" value="1"
+                <? if ($config->ACCESSKEY_ENABLE) echo 'checked'; ?>>
+            <?= _('Tastenkombinationen für Hauptfunktionen') ?>
+            <?= tooltipIcon(_('Mit dieser Einstellung können Sie für die meisten in der Kopfzeile '
+                .'erreichbaren Hauptfunktionen eine Bedienung über Tastenkombinationen '
+                .'aktivieren. <br>Die Tastenkombination wird im Tooltip des jeweiligen '
+                .'Icons angezeigt.')." "._('Diese kann für jeden Browser und jedes Betriebssystem unterschiedlich '
+                    .'sein (siehe <a href="http://en.wikipedia.org/wiki/Accesskey" '
+                    .'target="_blank"">Wikipedia</a>).')) ?>
+        </label>
+
+        <label>
+            <input type="checkbox"
+                   name="showsem_enable"
+                   value="1"
+                <? if ($config->SHOWSEM_ENABLE) echo 'checked'; ?>>
+            <?= _('Semesteranzeige auf &raquo;Meine Veranstaltungen&laquo;');?>
+            <?= tooltipIcon(_('Mit dieser Einstellung können Sie auf der Seite &raquo;Meine '
+                .'Veranstaltungen&laquo; die Einblendung des Start- und Endsemesters '
+                .'hinter jeder Veranstaltung aktivieren.')) ?>
+        </label>
+
+        <? if (get_config('TOURS_ENABLE')) : ?>
+        <label>
+            <input type="checkbox" name="tour_autostart_disable"
+                   aria-describedby="tour_autostart_disable_description" value="1"
+                <? if ($config->TOUR_AUTOSTART_DISABLE) echo 'checked'; ?>>
+            <?= _('Autostart von Touren verhindern');?>
+            <?= tooltipIcon(_('Mit dieser Einstellung können Sie verhindern, dass Touren zu einzelnen '
+                .'Stud.IP-Seiten automatisch starten, wenn Sie die Seite aufrufen. Die Touren '
+                .'können weiterhin über die Hilfe gestartet werden.')) ?>
+        </label>
+        <? endif ?>
+    </fieldset>
+
+    <fieldset>
+        <legend>
+            <?= _('Benachrichtigungen') ?>
+        </legend>
+
+        <label>
+            <input type="checkbox" name="personal_notifications_activated"
+                   aria-describedby="personal_notifications_activated_description" value="1"
+                <? if (PersonalNotifications::isActivated($user->user_id)) echo 'checked'; ?>>
+            <?= _('Benachrichtigungen über Javascript') ?>
+            <?= tooltipIcon(_('Hiermit wird in der Kopfzeile dargestellt, wenn es Benachrichtigungen für '
+                .'Sie gibt. Die Benachrichtigungen werden auch angezeigt, wenn Sie nicht die '
+                .'Seite neuladen.')) ?>
+        </label>
+
+        <label>
+            <input type="checkbox" name="personal_notifications_audio_activated"
+                   aria-describedby="personal_notifications_audio_activated_description" value="1"
+                <? if (PersonalNotifications::isAudioActivated($user->user_id)) echo 'checked'; ?>>
+            <?= _('Audio-Feedback zu Benachrichtigungen') ?>
+            <?= tooltipIcon(_('Wenn eine neue Benachrichtigung für Sie reinkommt, ' .
+                'werden Sie mittels eines kleinen Plopps darüber in Kenntnis gesetzt ' .
+                '- auch wenn Sie gerade einen anderen Browsertab anschauen. Der Plopp ist ' .
+                'nur zu hören, wenn Sie die Benachrichtigungen über Javascript aktiviert haben.')) ?>
+        </label>
+    </fieldset>
+
+    <footer>
+        <?= \Studip\Button::create(_("Speichern")) ?>
+    </footer>
 </form>
--- app/views/settings/messaging.php
+++ app/views/settings/messaging.php
@@ -28,153 +28,103 @@
     ) ?>
 <? endif; ?>
 
-<form action="<?= $controller->url_for('settings/messaging') ?>" method="post">
+<form action="<?= $controller->url_for('settings/messaging') ?>" method="post" class="default">
     <?= CSRFProtection::tokenTag() ?>
     <input type="hidden" name="studipticket" value="<?= get_ticket() ?>">
 
-    <table class="default" id="main_content">
-        <colgroup>
-            <col width="50%">
-            <col width="50%">
-        </colgroup>
-        <caption>
-            <?= _('Einstellungen des Nachrichtensystems anpassen') ?>
-        </caption>
-        <tbody>
-            <tr>
-                <th colspan="2"><?= _('Nachrichten') ?></th>
-            </tr>
-            <tr>
-                <td>
-                    <label for="save_snd"><?= _('Gesendete Nachrichten im Postausgang speichern') ?></label>
-                </td>
-                <td>
-                    <input type="checkbox" value="1" name="save_snd" id="save_snd"
-                           <? if ($settings['save_snd'] == 1) echo 'checked'; ?>>
-                </td>
-            </tr>
-        <? if ($GLOBALS['MESSAGING_FORWARD_AS_EMAIL']): ?>
-            <tr>
-                <td>
-                    <label for="request_mail_forward"><?= _('Gesendete Nachrichten auch als E-Mail verschicken') ?></label>
-                </td>
-                <td>
-                    <input type="checkbox" value="1" name="request_mail_forward" id="request_mail_forward"
-                           <? if ($settings['request_mail_forward'] == 1) echo 'checked'; ?>>
-                </td>
-            </tr>
-        <? endif ?>
-            <tr>
-                <td>
-                    <label for="logout_markreaded"><?= _('Beim Logout alle Nachrichten als gelesen speichern') ?></label>
-                </td>
-                <td>
-                    <input type="checkbox" value="1" name="logout_markreaded" id="logout_markreaded"
-                           <? if ($settings['logout_markreaded'] == 1) echo 'checked'; ?>>
-                </td>
-            </tr>
-        <? if ($GLOBALS['MESSAGING_FORWARD_AS_EMAIL']): ?>
-            <tr>
-                <td>
-                    <label><?= _('Kopie empfangener Nachrichten an eigene E-Mail-Adresse schicken') ?></label>
-                </td>
-                <td>
-                <? foreach ($send_as_email as $key => $label): ?>
-                    <label>
-                        <input type="radio" name="send_as_email" value="<?= $key ?>"
-                               <? if (($user->email_forward ?: $GLOBALS['MESSAGING_FORWARD_DEFAULT']) == $key) echo 'checked'; ?>>
-                        <?= htmlReady($label) ?>
-                    </label>
-                    <br>
-                <? endforeach; ?>
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label><?= _('E-Mail in folgendem Format versenden') ?></label>
-                </td>
-                <td>
-                <? foreach ($mail_formats as $key => $label): ?>
-                    <label>
-                        <input type="radio" name="mail_format" value="<?= $key ?>"
-                               <? if ($config->getValue('MAIL_AS_HTML') == $key) echo 'checked'; ?>>
-                        <?= htmlReady($label) ?>
-                    </label>
-                    <br>
-                <? endforeach; ?>
-               </td>
-             </tr>
-        <? endif; ?>
-            <tr>
-                <td>
-                    <label for="search_exp"><?= _('Weiterleitung empfangener Nachrichten') ?></label>
-                </td>
-                <td>
-                <? if ($user->smsforward_rec):  // empfaenger ausgewaehlt ?>
-                    <?= _('Empfänger') ?>
-                    <a href="<?= URLHelper::getLink('dispatch.php/profile?username=' . get_username($user->smsforward_rec)) ?>">
-                        <?= Avatar::getAvatar($user->smsforward_rec)->getImageTag(Avatar::SMALL) ?>
-                        <?= get_fullname($user->smsforward_rec, 'full', true) ?>
-                    </a>
-                    <a href="<?= $controller->url_for('settings/messaging/verify/forward_receiver') ?>">
-                        <?= Assets::img('icons/16/blue/trash.png') ?>
-                    </a>
-                    <br>
-                    <label>
-                        <input type="checkbox" value="1" name="smsforward_copy"
-                               <? if ($user->smsforward_copy) echo 'checked'; ?>>
-                        <?= _('Kopie im persönlichen Posteingang speichern.') ?>
-                    </label>
-                <? else: ?>
-                    <?= QuickSearch::get('new_smsforward_rec', new StandardSearch('username'))->withButton()->render() ?>
-                <? endif; ?>
-                </td>
-            </tr>
-        </tbody>
-        <tbody>
-            <tr>
-                <th colspan="2">
-                    <?= _('Signatur') ?>
-                </th>
-            </tr>
-            <tr>
-                <td>
-                    <label for="signature"><?= _('Signatur') ?></label>
-                </td>
-                <td>
-                    <textarea class="add_toolbar" name="sms_sig" id="signature" aria-label="<?= _('Signatur') ?>" style="width: 100%;" rows="3"><?= htmlready($settings['sms_sig']) ?></textarea>
-                </td>
-            </tr>
-        </tbody>
-        <tbody>
-            <tr>
-                <th colspan="2">
-                    <?= _('Buddies/ Wer ist online?') ?>
-                </th>
-            </tr>
-            <tr>
-                <td>
-                    <label for="online_format"><?= _('Formatierung der Namen auf &raquo;Wer ist Online?&laquo;');?></label>
-                </td>
-                <td>
-                    <select name="online_format" id="online_format">
-                    <? foreach ($GLOBALS['NAME_FORMAT_DESC'] as $key => $value): ?>
-                        <option value="<?= $key ?>" <? if ($config->getValue('ONLINE_NAME_FORMAT') == $key) echo 'selected '; ?>>
-                            <?= htmlReady($value) ?>
-                        </option>
-                    <? endforeach; ?>
-                    </select>
+    <fieldset>
+        <legend>
+            <?= _('Einstellungen des Nachrichtensystems') ?>
+        </legend>
 
-                </td>
-            </tr>
-        </tbody>
-        <tfoot>
-            <tr>
-                <td colspan="2">
-                    <?= Button::createAccept(_('Übernehmen'), 'store', array('title' => _('Änderungen übernehmen'))) ?>
-                    <?= LinkButton::create(_('Zurücksetzen'), $controller->url_for('settings/messaging/verify/reset'), array('title' => _('Einstellungen zurücksetzen'))) ?>
-                </td>
-            </tr>
-        </tfoot>
-    </table>
+        <label>
+            <input type="checkbox" value="1" name="save_snd" id="save_snd"
+                <? if ($settings['save_snd'] == 1) echo 'checked'; ?>>
+            <?= _('Gesendete Nachrichten im Postausgang speichern') ?>
+        </label>
+
+        <? if ($GLOBALS['MESSAGING_FORWARD_AS_EMAIL']): ?>
+            <label>
+                <input type="checkbox" value="1" name="request_mail_forward" id="request_mail_forward"
+                    <? if ($settings['request_mail_forward'] == 1) echo 'checked'; ?>>
+                <?= _('Gesendete Nachrichten auch als E-Mail verschicken') ?>
+            </label>
+        <? endif ?>
+
+        <label>
+            <input type="checkbox" value="1" name="logout_markreaded" id="logout_markreaded"
+                <? if ($settings['logout_markreaded'] == 1) echo 'checked'; ?>>
+            <?= _('Beim Logout alle Nachrichten als gelesen speichern') ?>
+        </label>
+
+        <? if ($GLOBALS['MESSAGING_FORWARD_AS_EMAIL']): ?>
+            <label>
+                <?= _('Kopie empfangener Nachrichten an eigene E-Mail-Adresse schicken') ?>
+                <select name="send_as_email">
+                <? foreach ($send_as_email as $key => $label): ?>
+                    <option value="<?= htmlReady($key) ?>" <? if (($user->email_forward ?: $GLOBALS['MESSAGING_FORWARD_DEFAULT']) == $key) echo 'selected' ?>>
+                        <?= htmlReady($label) ?>
+                    </option>
+                <? endforeach ?>
+                </select>
+            </label>
+
+            <label>
+                <?= _('E-Mail in folgendem Format versenden') ?>
+                <select name="">
+                    <? foreach ($mail_formats as $key => $label): ?>
+                        <option value="<?= htmlReady($key) ?>" <? if ($config->getValue('MAIL_AS_HTML') == $key) echo 'selected' ?>>
+                            <?= htmlReady($label) ?>
+                        </option>
+                    <? endforeach ?>
+                </select>
+            </label>
+        <? endif ?>
+
+        <div>
+            <? if ($user->smsforward_rec):  // empfaenger ausgewaehlt ?>
+                <?= _('Empfänger') ?>
+                <a href="<?= URLHelper::getLink('dispatch.php/profile?username=' . get_username($user->smsforward_rec)) ?>">
+                    <?= Avatar::getAvatar($user->smsforward_rec)->getImageTag(Avatar::SMALL) ?>
+                    <?= get_fullname($user->smsforward_rec, 'full', true) ?>
+                </a>
+                <a href="<?= $controller->url_for('settings/messaging/verify/forward_receiver') ?>">
+                    <?= Assets::img('icons/16/blue/trash.png') ?>
+                </a>
+                <br>
+                <label>
+                    <input type="checkbox" value="1" name="smsforward_copy"
+                        <? if ($user->smsforward_copy) echo 'checked'; ?>>
+                    <?= _('Kopie im persönlichen Posteingang speichern.') ?>
+                </label>
+            <? else: ?>
+                <label>
+                    <?= _('Weiterleitung empfangener Nachrichten') ?>
+                    <?= QuickSearch::get('new_smsforward_rec', new StandardSearch('username'))->withButton()->render() ?>
+                </label>
+            <? endif; ?>
+        </div>
+
+        <label style="clear:both;">
+            <?= _('Signatur') ?>
+            <textarea name="sms_sig" aria-label="<?= _('Signatur') ?>"><?= htmlready($settings['sms_sig']) ?></textarea>
+        </label>
+
+        <label>
+            <?= _('Buddies/ Wer ist online?') ?>
+            <select name="online_format">
+                <? foreach ($GLOBALS['NAME_FORMAT_DESC'] as $key => $value): ?>
+                    <option value="<?= $key ?>" <? if ($config->getValue('ONLINE_NAME_FORMAT') == $key) echo 'selected '; ?>>
+                        <?= htmlReady($value) ?>
+                    </option>
+                <? endforeach; ?>
+            </select>
+        </label>
+    </fieldset>
+
+
+    <footer>
+        <?= Button::createAccept(_('Übernehmen'), 'store', array('title' => _('Änderungen übernehmen'))) ?>
+        <?= LinkButton::create(_('Zurücksetzen'), $controller->url_for('settings/messaging/verify/reset'), array('title' => _('Einstellungen zurücksetzen'))) ?>
+    </footer>
 </form>

6b0d8e8c8eb31de76b9465eda37a68eb03eee6fe
krassmus <krassmus@c5b85792-5220-0410-ad23-ed5b6b8ad999>
2015-10-30 08:54:38 +0000

    re #4292: update privacy settings page
    git-svn-id: svn://develop.studip.de/studip/trunk@35896 c5b85792-5220-0410-ad23-ed5b6b8ad999

--- app/views/settings/privacy.php
+++ app/views/settings/privacy.php
@@ -3,49 +3,29 @@
 use Studip\Button, Studip\LinkButton;
 
 ?>
-<form method="post" action="<?= $controller->url_for('settings/privacy/global') ?>">
+<form method="post" action="<?= $controller->url_for('settings/privacy/global') ?>" class="default">
     <?= CSRFProtection::tokenTag() ?>
     <input type="hidden" name="studipticket" value="<?= get_ticket() ?>">
 
-    <table id="main_content" class="default">
-        <colgroup>
-            <col width="50%">
-            <col width="50%">
-        </colgroup>
-        <caption>
+    <fieldset>
+        <legend>
             <?= _('Privatsphäre') ?>:
             <?= _('Globale Einstellungen') ?>
-        </caption>
-        <thead>
-            <tr>
-                <th>
-                    <?= _('Element') ?>
-                </th>
-                                <th>
-                    <?= _('Einstellung') ?>
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>
-                    <label for="global_vis">
-                        <?= _('Globale Sichtbarkeit') ?>
-                        <dfn>
-                        <?= _('Sie können wählen, ob Sie für andere NutzerInnen sichtbar sein '
-                              .'und alle Kommunikationsfunktionen von Stud.IP nutzen können '
-                              .'wollen, oder ob Sie unsichtbar sein möchten und dann nur '
-                              .'eingeschränkte Kommunikationsfunktionen nutzen können.') ?>
-                        </dfn>
-                    </label>
-                </td>
-                <td>
+        </legend>
+
+        <label>
+            <?= _('Globale Sichtbarkeit') ?>
+            <?= tooltipIcon(_('Sie können wählen, ob Sie für andere NutzerInnen sichtbar sein '
+                .'und alle Kommunikationsfunktionen von Stud.IP nutzen können '
+                .'wollen, oder ob Sie unsichtbar sein möchten und dann nur '
+                .'eingeschränkte Kommunikationsfunktionen nutzen können.')) ?>
+            <div>
                 <? if ($global_visibility != 'always' && $global_visibility != 'never' &&
-                        ($user_perm != 'dozent' || !get_config('DOZENT_ALWAYS_VISIBLE'))):
-                        // only show selection if visibility can be changed
-                ?>
+                    ($user_perm != 'dozent' || !get_config('DOZENT_ALWAYS_VISIBLE'))):
+                    // only show selection if visibility can be changed
+                    ?>
                     <select name="global_visibility" aria-describedby="global_vis_description" id="global_vis">
-                    <?php
+                        <?php
                         if (count($user_domains)) {
                             printf ("<option %s value=\"global\">"._("sichtbar für alle Nutzer")."</option>", $global_visibility=='global' ? 'selected="selected"' : '');
                             $visible_text = _('sichtbar für eigene Nutzerdomäne');
@@ -54,7 +34,7 @@ use Studip\Button, Studip\LinkButton;
                         }
                         printf ("<option %s value=\"yes\">".$visible_text."</option>", ($global_visibility=='yes' || ($global_visibility=='unknown' && get_config('USER_VISIBILITY_UNKNOWN'))) ? 'selected' : '');
                         printf ("<option %s value=\"no\">"._("unsichtbar")."</option>", ($global_visibility=='no' || ($global_visibility=='unknown' && !get_config('USER_VISIBILITY_UNKNOWN'))) ? 'selected' : '');
-                    ?>
+                        ?>
                     </select>
                 <? else: ?>
                     <? if ($global_visibility == 'never'): ?>
@@ -66,151 +46,137 @@ use Studip\Button, Studip\LinkButton;
                     <? endif; ?>
                     <input type="hidden" name="global_visibility" value="<?= $global_visibility ?>">
                 <? endif; ?>
-                </td>
-            </tr>
-    <? if (($global_visibility == 'yes' || $global_visibility == 'global' ||
+            </div>
+        </label>
+
+        <? if (($global_visibility == 'yes' || $global_visibility == 'global' ||
             ($global_visibility == 'unknown' && get_config('USER_VISIBILITY_UNKNOWN')) ||
-           ($user_perm == 'dozent' && get_config('DOZENT_ALWAYS_VISIBLE'))) &&
-           (!$NOT_HIDEABLE_FIELDS[$user_perm]['online'] ||
+            ($user_perm == 'dozent' && get_config('DOZENT_ALWAYS_VISIBLE'))) &&
+        (!$NOT_HIDEABLE_FIELDS[$user_perm]['online'] ||
             !$NOT_HIDEABLE_FIELDS[$user_perm]['search'] ||
-            !$NOT_HIDEABLE_FIELDS[$user_perm]['email'])):
-    ?>
-            <tr>
-                <td>
-                    <label>
-                        <?= _('Erweiterte Einstellungen') ?>
-                        <dfn>
-                            <?= _('Stellen Sie hier ein, in welchen Bereichen des Systems Sie erscheinen wollen.') ?>
-                        <? if (!$NOT_HIDEABLE_FIELDS[$user_perm]['email']): ?>
-                            <br>
-                            <?=  _('Wenn Sie hier Ihre E-Mail-Adresse verstecken, wird stattdessen die E-Mail-Adresse Ihrer (Standard-)Einrichtung angezeigt.') ?>
-                        <? endif; ?>
-                            </dfn>
-                    </label>
-                </td>
-                <td>
+            !$NOT_HIDEABLE_FIELDS[$user_perm]['email'])) : ?>
+            <div>
+                <?= _('Erweiterte Einstellungen') ?>
+                <?= tooltipIcon(
+                    _('Stellen Sie hier ein, in welchen Bereichen des Systems Sie erscheinen wollen.')
+                    .(!$NOT_HIDEABLE_FIELDS[$user_perm]['email']
+                        ?  _('Wenn Sie hier Ihre E-Mail-Adresse verstecken, wird stattdessen die E-Mail-Adresse Ihrer (Standard-)Einrichtung angezeigt.')
+                        : "")
+                ) ?>
+
                 <? if (!$NOT_HIDEABLE_FIELDS[$user_perm]['online']): ?>
                     <label>
                         <input type="checkbox" name="online" value="1"
-                               <? if ($online_visibility) echo 'checked'; ?>>
+                            <? if ($online_visibility) echo 'checked'; ?>>
                         <?= _('sichtbar in "Wer ist online"') ?>
                     </label>
-                    <br>
                 <? endif; ?>
                 <? if (!$NOT_HIDEABLE_FIELDS[$user_perm]['search']): ?>
                     <label>
                         <input type="checkbox" name="search" value="1"
-                               <? if ($search_visibility) echo 'checked'; ?>>
+                            <? if ($search_visibility) echo 'checked'; ?>>
                         <?= _('auffindbar über die Personensuche') ?>
                     </label>
-                    <br>
                 <? endif; ?>
                 <? if (!$NOT_HIDEABLE_FIELDS[$user_perm]['email']): ?>
                     <label>
                         <input type="checkbox" name="email" value="1"
-                               <? if ($email_visibility) echo 'checked'; ?>>
+                            <? if ($email_visibility) echo 'checked'; ?>>
                         <?= _('eigene E-Mail Adresse sichtbar') ?>
                     </label>
-                    <br>
                 <? endif; ?>
-                </td>
-            </tr>
-    <? endif; ?>
-        </tbody>
-        <tfoot>
-            <tr>
-                 <td colspan="<?= $user_domains ? 6 : 5; ?>">
-                    <?= Button::create(_('Übernehmen'), 'store', array('title' =>  _('Änderungen speichern')))?>
-                </td>
-            </tr>
-        </tfoot>
-    </table>
+            </div>
+        <? endif; ?>
+    </fieldset>
+    <footer>
+        <?= Button::create(_('Übernehmen'), 'store', array('title' =>  _('Änderungen speichern')))?>
+    </footer>
 </form>
 
-<form method="post" action="<?= $controller->url_for('settings/privacy/homepage') ?>">
+<div style="margin-top: 50px;"></div>
+
+<form method="post" action="<?= $controller->url_for('settings/privacy/homepage') ?>" class="default">
     <?= CSRFProtection::tokenTag() ?>
     <input type="hidden" name="studipticket" value="<?= get_ticket() ?>">
 
-    <table class="default">
-        <colgroup>
-            <col width="34%">
-        <? for($i = 1; $i <= $colCount; $i++): ?>
-            <col width="<?= $colWidth ?>%">
-        <? endfor; ?>
-        </colgroup>
-        <caption><?= _('Privatsphäre') ?>:
-        <?= _('Eigenes Profil') ?></caption>
-        <thead>
-            <tr>
-                <th ><?= _('Profil-Element'); ?></th>
-                <th style='text-align: center;' colspan="<?= $colCount++ ?>"><?= _('sichtbar für'); ?></th>
-            </tr>
-        </thead>
-        <tbody class="privacy">
-            <tr>
-                <td>&nbsp;</td>
-                <? foreach ($visibilities as $visibility): ?>
-                    <td><?= htmlReady($visibility) ?></td>
+    <fieldset>
+        <legend>
+            <?= _('Privatsphäre') ?>:
+            <?= _('Eigenes Profil') ?>
+        </legend>
+
+        <table class="default">
+            <colgroup>
+                <col width="34%">
+            <? for($i = 1; $i <= $colCount; $i++): ?>
+                <col width="<?= $colWidth ?>%">
+            <? endfor; ?>
+            </colgroup>
+            <thead>
+                <tr>
+                    <th ><?= _('Profil-Element'); ?></th>
+                    <th style='text-align: center;' colspan="<?= $colCount++ ?>"><?= _('sichtbar für'); ?></th>
+                </tr>
+            </thead>
+            <tbody class="privacy">
+                <tr>
+                    <td>&nbsp;</td>
+                    <? foreach ($visibilities as $visibility): ?>
+                        <td><?= htmlReady($visibility) ?></td>
+                    <? endforeach; ?>
+                </tr>
+                <? foreach ($homepage_elements['entry'] as $element): ?>
+                    <? if ($element['is_header']): ?>
+                        <tr>
+                            <th colspan="<?= 1 + $colCount ?>">
+                                <?= htmlReady($element['name']) ?>
+                            </th>
+                        </tr>
+                    <? else: ?>
+                        <tr>
+                            <td style="padding-left: <?= $element['padding'] ?>"><?= htmlReady($element['name']) ?></td>
+                            <? if ($element['is_category']): ?>
+                                <td colspan="<?= $colCount ?>"></td>
+                            <? else: ?>
+                                <? foreach ($homepage_elements['states'] as $state): ?>
+                                    <td>
+                                        <input type="radio" name="visibility_update[<?= $element['id'] ?>]" value="<?= $state ?>"
+                                               <? if ($element['state'] == $state) echo 'checked'; ?>>
+                                    </td>
+                                <? endforeach; ?>
+                            <? endif; ?>
+                        </tr>
+                    <? endif; ?>
                 <? endforeach; ?>
-            </tr>
-            <? foreach ($homepage_elements['entry'] as $element): ?>
-                <? if ($element['is_header']): ?>
-                    <tr>
-                        <th colspan="<?= 1 + $colCount ?>">
-                            <?= htmlReady($element['name']) ?>
-                        </th>
-                    </tr>
-                <? else: ?>
-                    <tr>
-                        <td style="padding-left: <?= $element['padding'] ?>"><?= htmlReady($element['name']) ?></td>
-                        <? if ($element['is_category']): ?>
-                            <td colspan="<?= $colCount ?>"></td>
-                        <? else: ?>
-                            <? foreach ($homepage_elements['states'] as $state): ?>
-                                <td>
-                                    <input type="radio" name="visibility_update[<?= $element['id'] ?>]" value="<?= $state ?>"
-                                           <? if ($element['state'] == $state) echo 'checked'; ?>>
-                                </td>
-                            <? endforeach; ?>
-                        <? endif; ?>
-                    </tr>
-                <? endif; ?>
-            <? endforeach; ?>
-        </tbody>
-        <tfoot>
-            <tr>
-                <td colspan="<?= 1 + $colCount ?>">
-                    <?= _('Neue Elemente auf') ?>
-                    <select name="default">
-                        <option value="">-- <?= _('bitte wählen'); ?> --</option>
-                        <? foreach ($visibilities as $visibility => $label): ?>
-                            <option value="<?= $visibility ?>" <? if ($default_homepage_visibility == $visibility) echo 'selected'; ?>>
-                                <?= htmlReady($label) ?>
-                            </option>
-                        <? endforeach; ?>
-                    </select>
-                    <?= _('setzen') ?>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="<?= 1 + $colCount ?>">
-                    <?= _('Jetzt alle Sichtbarkeiten auf') ?>
-                    <select name="all">
-                        <option value="">-- <?= _("bitte wählen"); ?> --</option>
-                        <? foreach ($visibilities as $visibility => $label): ?>
-                            <option value="<?= $visibility ?>">
-                                <?= htmlReady($label) ?>
-                            </option>
-                        <? endforeach; ?>
-                    </select>
-                    <?= _('setzen') ?>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="<?= 1 + $colCount ?>">
-                    <?= Button::create(_('Übernehmen'), 'store', array('title' =>  _('Änderungen speichern')))?>
-                </td>
-            </tr>
-        </tfoot>
-    </table>
+            </tbody>
+        </table>
+
+        <label>
+            <?= _('Neue Elemente') ?>
+            <select name="default">
+                <option value="">-- <?= _('bitte wählen'); ?> --</option>
+                <? foreach ($visibilities as $visibility => $label): ?>
+                    <option value="<?= $visibility ?>" <? if ($default_homepage_visibility == $visibility) echo 'selected'; ?>>
+                        <?= htmlReady($label) ?>
+                    </option>
+                <? endforeach; ?>
+            </select>
+        </label>
+
+        <label>
+            <?= _('Jetzt alle Sichtbarkeiten auf') ?>
+            <select name="default">
+                <option value="">-- <?= _('bitte wählen'); ?> --</option>
+                <? foreach ($visibilities as $visibility => $label): ?>
+                    <option value="<?= $visibility ?>" <? if ($default_homepage_visibility == $visibility) echo 'selected'; ?>>
+                        <?= htmlReady($label) ?>
+                    </option>
+                <? endforeach; ?>
+            </select>
+        </label>
+    </fieldset>
+
+    <footer>
+        <?= Button::create(_('Übernehmen'), 'store', array('title' =>  _('Änderungen speichern')))?>
+    </footer>
 </form>
